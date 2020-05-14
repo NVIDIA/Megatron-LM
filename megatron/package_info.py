14fac298ab357f97fbafb8803e5be8aa73ff6dd5
Evelina Bakhturina <ebakhturina@nvidia.com>
2020-05-14 13:39:05 -0700

    update
    Signed-off-by: Evelina Bakhturina <ebakhturina@nvidia.com>

--- megatron/package_info.py
+++ megatron/package_info.py
@@ -1,4 +1,3 @@
-# ! /usr/bin/python
 # coding=utf-8
 # Copyright (c) 2020, NVIDIA CORPORATION.  All rights reserved.
 #
@@ -26,6 +25,6 @@ __contact_names__ = 'NVIDIA INC'
 __url__ = 'https://github.com/NVIDIA/Megatron-LM'
 __download_url__ = 'https://github.com/NVIDIA/Megatron-LM/releases'
 __description__ = 'Megatron-LM: Training Multi-Billion Parameter Language Models Using Model Parallelism.'
-__license__ = 'https://github.com/NVIDIA/Megatron-LM/blob/master/LICENSE'
+__license__ = 'See https://github.com/NVIDIA/Megatron-LM/blob/master/LICENSE'
 __keywords__ = 'deep learning, Megatron, gpu, NLP, nvidia, pytorch, torch, language'
 
