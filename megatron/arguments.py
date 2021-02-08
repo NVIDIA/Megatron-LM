08a848c7e8ecb98694cfa765fa2b1350c89c2aaa
Jared Casper <jcasper@nvidia.com>
2021-02-08 14:36:53 -0800

    Improve handling of rng states in checkpoints.

--- megatron/arguments.py
+++ megatron/arguments.py
@@ -494,9 +494,9 @@ def _add_checkpointing_args(parser):
                        help='Output directory to save checkpoints to.')
     group.add_argument('--save-interval', type=int, default=None,
                        help='Number of iterations between checkpoint saves.')
-    group.add_argument('--no-save-optim', action='store_true',
+    group.add_argument('--no-save-optim', action='store_true', default=None,
                        help='Do not save current optimizer.')
-    group.add_argument('--no-save-rng', action='store_true',
+    group.add_argument('--no-save-rng', action='store_true', default=None,
                        help='Do not save current rng state.')
     group.add_argument('--load', type=str, default=None,
                        help='Directory containing a model checkpoint.')
