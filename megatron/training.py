816fb89025e8ed67035b04c66c7f74da19c9bb74
Mostofa Patwary <mostofa.patwary@gmail.com>
2021-02-17 22:04:42 -0800

    fixed a minor bug

--- megatron/training.py
+++ megatron/training.py
@@ -311,7 +311,7 @@ def setup_model_and_optimizer(model_provider_func):
     # We only support local DDP with multiple micro-batches.
     if get_num_microbatches() > 1:
         assert args.DDP_impl == 'local'
-    if len(model) == 1:
+    if len(model) > 1:
         assert args.DDP_impl == 'local'
     if mpu.get_pipeline_model_parallel_world_size() > 1:
         assert args.DDP_impl == 'local'
