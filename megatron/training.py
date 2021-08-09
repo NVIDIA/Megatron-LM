f0131ce0287f008d85101fdc62a49d5ec30c2e47
Shaden Smith <Shaden.Smith@microsoft.com>
2021-08-09 16:42:45 -0700

    query deepspeed global grad norm (#8)

--- megatron/training.py
+++ megatron/training.py
@@ -380,7 +380,7 @@ def train_step(forward_step_func, data_iterator,
         assert isinstance(model[0], deepspeed.PipelineEngine), model
         loss = model[0].train_batch(data_iter=data_iterator)
         skipped_iter = 0
-        grad_norm = 0.
+        grad_norm = model[0].get_global_grad_norm()
         num_zeros_in_grad = 0
         return {'lm loss' : loss}, skipped_iter, grad_norm, num_zeros_in_grad
 
