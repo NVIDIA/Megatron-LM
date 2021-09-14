Fixes a bug in broadcasting that was causing hanging

megatron/text_generation_server.py
@@ -30,9 +30,7 @@ class MegatronGenerate(Resource):
     @staticmethod
     def send_do_generate():
         choice = torch.cuda.LongTensor([GENERATE_NUM])
-        torch.distributed.broadcast(choice,
-                                    mpu.get_tensor_model_parallel_src_rank(),
-                                    group=mpu.get_tensor_model_parallel_group())
+        torch.distributed.broadcast(choice, 0)
      
     def put(self):
         args = get_args()

tools/run_text_generation_server.py
@@ -78,8 +78,6 @@ if __name__ == "__main__":
 
     while True:
         choice = torch.cuda.LongTensor(1)
-        torch.distributed.broadcast(choice,
-                                    mpu.get_tensor_model_parallel_src_rank(),
-                                    group=mpu.get_tensor_model_parallel_group())
+        torch.distributed.broadcast(choice, 0)
         if choice[0].item() == 0:
             generate(model)