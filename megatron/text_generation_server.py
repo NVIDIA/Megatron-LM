5ab28fd107686e5cb5490a803bdc988bcbbd698f
rprenger <rprenger@nvidia.com>
2021-10-20 10:09:44 -0700

    getting rid of unnecessary just_score

--- megatron/text_generation_server.py
+++ megatron/text_generation_server.py
@@ -54,15 +54,12 @@ class MegatronGenerate(Resource):
             return "Maximum number of prompts is 128", 400
 
         tokens_to_generate = 64  # Choosing hopefully sane default.  Full sequence is slow
-        just_score=False
         if "tokens_to_generate" in request.get_json():
             tokens_to_generate = request.get_json()["tokens_to_generate"]
             if not isinstance(tokens_to_generate, int):
                 return "tokens_to_generate must be an integer greater than 0"
             if tokens_to_generate < 0:
                 return "tokens_to_generate must be an integer greater than or equal to 0"
-            if tokens_to_generate == 0:
-                just_score = True
 
         logprobs = False
         if "logprobs" in request.get_json():
@@ -70,8 +67,8 @@ class MegatronGenerate(Resource):
             if not isinstance(logprobs, bool):
                 return "logprobs must be a boolean value"
         
-        if just_score and not logprobs:
-            return "tokens_to_generate=0 implies logprobs=True"
+        if tokens_to_generate == 0 and not logprobs:
+            return "tokens_to_generate=0 implies logprobs should be True"
         
         temperature = 1.0
         if "temperature" in request.get_json():
@@ -117,8 +114,7 @@ class MegatronGenerate(Resource):
                     top_p_sampling=top_p,
                     temperature=temperature,
                     add_BOS=add_BOS,
-                    use_eod_token_for_early_termination=True,
-                    just_score=just_score)
+                    use_eod_token_for_early_termination=True)
         
         return jsonify({"text": response,
             "segments": response_seg,
