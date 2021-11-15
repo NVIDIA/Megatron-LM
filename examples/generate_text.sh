b5b37bcd34fdb97c85a280781d420a9c76ba0704
Ammar Ahmad Awan <ammar.awan@microsoft.com>
2021-11-16 00:24:00 +0500

    Make a runnable inference pipeline. No DS changes.

--- examples/generate_text.sh
+++ examples/generate_text.sh
@@ -3,8 +3,9 @@
 CHECKPOINT_PATH=checkpoints/gpt2_345m
 VOCAB_FILE=gpt2-vocab.json
 MERGE_FILE=gpt2-merges.txt
+b=8
 
-python tools/generate_samples_gpt2.py \
+python tools/generate_samples_gpt.py \
        --tensor-model-parallel-size 1 \
        --num-layers 24 \
        --hidden-size 1024 \
@@ -13,13 +14,14 @@ python tools/generate_samples_gpt2.py \
        --max-position-embeddings 1024 \
        --tokenizer-type GPT2BPETokenizer \
        --fp16 \
-       --batch-size 2 \
-       --seq-length 1024 \
-       --out-seq-length 1024 \
+       --micro-batch-size $b \
+       --seq-length 101 \
+       --out-seq-length 101 \
        --temperature 1.0 \
        --vocab-file $VOCAB_FILE \
        --merge-file $MERGE_FILE \
        --genfile unconditional_samples.json \
-       --num-samples 2 \
        --top_p 0.9 \
-       --recompute
+       --log-interval 1 \
+       --num-samples $((10*$b))
+#       --recompute
