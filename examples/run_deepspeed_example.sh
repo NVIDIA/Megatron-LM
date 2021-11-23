52db1fa2aa11005502d19ebcea098a90459da477
Jeff Rasley <jerasley@microsoft.com>
2021-11-22 16:34:57 -0800

    update ds example

--- examples/run_deepspeed_example.sh
+++ examples/run_deepspeed_example.sh
@@ -13,7 +13,7 @@ HIDDEN=512
 GLOBAL_BATCH=64
 MICRO_BATCH=4
 
-ZERO_STAGE=0
+ZERO_STAGE=2
 
 OUTPUT_DIR=ds_pp_z${ZERO_STAGE}_nl${NLAYERS}_hs${HIDDEN}_gb${GLOBAL_BATCH}_mb${MICRO_BATCH}
 #OUTPUT_DIR=baseline_nl${NLAYERS}_hs${HIDDEN}_gb${GLOBAL_BATCH}_mb${MICRO_BATCH}
@@ -42,42 +42,43 @@ export NCCL_DEBUG=warn
 
 ds_args=""
 ds_args=" --deepspeed ${ds_args}"
-#ds_args=" --no-pipeline-parallel ${ds_args}" 
+ds_args=" --no-pipeline-parallel ${ds_args}" 
 ds_args=" --deepspeed_config=$DS_CONFIG ${ds_args}"
 ds_args=" --zero-stage=$ZERO_STAGE ${ds_args}"
 ds_args=" --deepspeed-activation-checkpointing ${ds_args}"
 
 
 deepspeed pretrain_gpt.py \
-	--tensor-model-parallel-size $TP \
-	--pipeline-model-parallel-size $PP \
-	--num-layers $NLAYERS \
-	--hidden-size $HIDDEN \
-	--num-attention-heads 16 \
-	--seq-length 256 \
-	--loss-scale 12 \
-	--max-position-embeddings 1024 \
-	--micro-batch-size 4 \
-	--global-batch-size 1024 \
-	--train-iters 1000 \
-	--lr 6.0e-5 \
-	--min-lr 6.0e-6 \
-	--lr-decay-style cosine \
-	--log-interval 1 \
-	--eval-iters 40 \
-	--eval-interval 1000 \
-	--data-path $DATA_PATH \
-	--vocab-file $BASE_PATH/gpt2-vocab.json \
-	--merge-file $BASE_PATH/gpt2-merges.txt \
-	--save-interval 1000 \
-	--split 98,2,0 \
-	--clip-grad 1.0 \
-	--weight-decay 0.1 \
-	--adam-beta1 0.9 \
-	--adam-beta2 0.95 \
-	--init-method-std 0.006 \
-	--fp16 \
-	--checkpoint-activations \
-	--tensorboard-dir $OUTPUT_DIR \
-	$ds_args \
-	--exit-interval 5000 | tee ${OUTPUT_DIR}/output.log
+    --tensor-model-parallel-size $TP \
+    --pipeline-model-parallel-size $PP \
+    --num-layers $NLAYERS \
+    --hidden-size $HIDDEN \
+    --num-attention-heads 16 \
+    --seq-length 256 \
+    --loss-scale 12 \
+    --max-position-embeddings 1024 \
+    --micro-batch-size 4 \
+    --global-batch-size 1024 \
+    --train-iters 1000 \
+    --lr 6.0e-5 \
+    --min-lr 6.0e-6 \
+    --lr-decay-style cosine \
+    --log-interval 1 \
+    --eval-iters 40 \
+    --eval-interval 1000 \
+    --data-path $DATA_PATH \
+    --vocab-file $BASE_PATH/gpt2-vocab.json \
+    --merge-file $BASE_PATH/gpt2-merges.txt \
+    --save-interval 1000 \
+    --split 98,2,0 \
+    --clip-grad 1.0 \
+    --weight-decay 0.1 \
+    --adam-beta1 0.9 \
+    --adam-beta2 0.95 \
+    --init-method-std 0.006 \
+    --fp16 \
+    --checkpoint-activations \
+    --tensorboard-dir $OUTPUT_DIR \
+    $ds_args \
+    --exit-interval 5000 | tee ${OUTPUT_DIR}/output.log
+
