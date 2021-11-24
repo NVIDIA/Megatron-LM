9ce9c12e5b806d13870075a35a857c4c61f0a30b
Ammar Ahmad Awan <ammar.awan@microsoft.com>
2021-11-24 05:47:44 +0500

    add some flags to run easily.

--- examples/generate_text.sh
+++ examples/generate_text.sh
@@ -4,16 +4,22 @@ CHECKPOINT_PATH=checkpoints/gpt2_345m
 VOCAB_FILE=gpt2-vocab.json
 MERGE_FILE=gpt2-merges.txt
 b=8
-mp=2
-gpus=2
-experts=8
+mp=1
+gpus=1
+experts=1
+
+use_tutel=""
+#use_tutel="--use-tutel"
+
+#ds_inference=""
+ds_inference="--ds-inference"
 
 deepspeed --num_gpus=$gpus --num_nodes=1 tools/generate_samples_gpt.py \
        --tensor-model-parallel-size $mp \
-       --num-layers 2 \
-       --hidden-size 1024 \
+       --num-layers 12 \
+       --hidden-size 8192 \
        --load $CHECKPOINT_PATH \
-       --num-attention-heads 16 \
+       --num-attention-heads 32 \
        --max-position-embeddings 1024 \
        --tokenizer-type GPT2BPETokenizer \
        --fp16 \
@@ -28,7 +34,7 @@ deepspeed --num_gpus=$gpus --num_nodes=1 tools/generate_samples_gpt.py \
        --top_p 0.9 \
        --log-interval 1 \
        --num-samples $((20*$b)) \
-       --ds-inference \
+       $use_tutel $ds_inference
 
 #$((10*$b))
 
