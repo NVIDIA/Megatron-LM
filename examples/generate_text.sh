b74d1b48a7ca6c1e44b60fb01da6ab874b612bfe
Ammar Ahmad Awan <ammar.awan@microsoft.com>
2021-12-09 02:45:11 +0500

    reduce seq length to run 128 bs.

--- examples/generate_text.sh
+++ examples/generate_text.sh
@@ -3,7 +3,7 @@
 CHECKPOINT_PATH=checkpoints/gpt2_345m
 VOCAB_FILE=gpt2-vocab.json
 MERGE_FILE=gpt2-merges.txt
-b=8
+b=128
 mp=1
 experts=128
 nodes=4
@@ -16,23 +16,30 @@ use_tutel=""
 ds_inference=""
 ds_inference="--ds-inference"
 
-launch_cmd="mpirun --tag-output --allow-run-as-root -np $procs -npernode 8 -hostfile /job/hostfile  -mca pml ob1 --mca btl ^openib -mca btl_tcp_if_exclude lo,docker0 -mca  coll_hcoll_enable 0  -x PATH -x LD_LIBRARY_PATH=/opt/hpcx/nccl_rdma_sharp_plugin/lib/:/mellanox/sharp/lib:/opt/msft/nccl-rdma-sharp-plugins-2.8.3/lib:/home/amawa/saemal/msccl/build/lib:$LD_LIBRARY_PATH -x UCX_IB_ENABLE_CUDA_AFFINITY=n -x NCCL_IB_PCI_RELAXED_ORDERING=1 -x UCX_IB_PCI_RELAXED_ORDERING=on -x UCX_NET_DEVICES=mlx5_0:1 -x UCX_TLS=rc -x NCCL_SOCKET_IFNAME=eth0 -x NCCL_DEBUG=WARN -x NCCL_NET_GDR_LEVEL=5 -x NCCL_TOPO_FILE=/opt/msft/topo.xml -x SNCCL_DEBUG_SUBSYS=INIT,ENV  -x SCCL_XML_FILES=/home/amawa/a2a-32.xml -x NCCL_NET_SHARED_BUFFERS=0 -x NCCL_ALGO=SCCL,RING,TREE  -x CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python"
+#numa_bind=""
+numa_bind="--bind-to numa"
+
+#launch_cmd="LD_LIBRARY_PATH=/home/amawa/saemal/msccl/build/lib:/opt/hpcx/nccl_rdma_sharp_plugin/lib/:/mellanox/sharp/lib:/opt/msft/nccl-rdma-sharp-plugins-2.8.3/lib:/home/amawa/saemal/msccl/build/lib: 
+launch_cmd="mpirun $numa_bind --tag-output --allow-run-as-root -np $procs -npernode 8 -hostfile /job/hostfile  -mca pml ob1 --mca btl ^openib -mca btl_tcp_if_exclude lo,docker0 -mca  coll_hcoll_enable 0 -x LD_PRELOAD=/home/amawa/saemal/msccl/build/lib/libnccl.so -x PATH -x LD_LIBRARY_PATH=/opt/hpcx/nccl_rdma_sharp_plugin/lib/:/mellanox/sharp/lib:/opt/msft/nccl-rdma-sharp-plugins-2.8.3/lib:/home/amawa/saemal/msccl/build/lib:$LD_LIBRARY_PATH -x UCX_IB_ENABLE_CUDA_AFFINITY=n -x NCCL_IB_PCI_RELAXED_ORDERING=1 -x UCX_IB_PCI_RELAXED_ORDERING=on -x UCX_NET_DEVICES=mlx5_0:1 -x UCX_TLS=rc -x NCCL_SOCKET_IFNAME=eth0 -x NCCL_DEBUG=WARN -x NCCL_NET_GDR_LEVEL=5 -x NCCL_TOPO_FILE=/opt/msft/topo.xml -x NCCL_DEBUG_SUBSYS=ALL"
+
+sccl_cmd="-x NCCL_ALGO=SCCL,RING,TREE -x SCCL_XML_FILES=/home/amawa/a2a-32.xml -x NCCL_NET_SHARED_BUFFERS=0 -x CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python" 
+nccl_cmd="-x NCCL_ALGO=RING,TREE -x CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python"
 
 #launch_cmd="deepspeed --num_gpus=$gpus --num_nodes=$nodes"
 
 program_cmd="tools/generate_samples_gpt.py \
        --tensor-model-parallel-size $mp \
-       --num-layers 24 \
-       --hidden-size 2048 \
+       --num-layers 40 \
+       --hidden-size 4096 \
        --load $CHECKPOINT_PATH \
-       --num-attention-heads 16 \
+       --num-attention-heads 32 \
        --max-position-embeddings 1024 \
        --tokenizer-type GPT2BPETokenizer \
        --fp16 \
        --num-experts $experts \
        --micro-batch-size $b \
-       --seq-length 101 \
-       --out-seq-length 101 \
+       --seq-length 30 \
+       --out-seq-length 30 \
        --temperature 1.0 \
        --vocab-file $VOCAB_FILE \
        --merge-file $MERGE_FILE \
@@ -43,6 +50,7 @@ program_cmd="tools/generate_samples_gpt.py \
        --deepspeed \
        $use_tutel $ds_inference"
 
-echo $launch_cmd $program_cmd
-$launch_cmd $program_cmd
+echo $launch_cmd $nccl_cmd $program_cmd
+
+$launch_cmd $nccl_cmd $program_cmd
 #       --recompute
