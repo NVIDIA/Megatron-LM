48dbd9caf911ad5057f5a2d299ec0360bbf32127
zihanl <zihanl@nvidia.com>
2021-12-09 02:51:40 -0800

    update commands.sh

--- tasks/knwl_dialo/scripts/prompt_resp_gen.sh
+++ tasks/knwl_dialo/scripts/prompt_resp_gen.sh
@@ -42,4 +42,5 @@ python -m torch.distributed.launch $DISTRIBUTED_ARGS ./tasks/main.py \
         --num-prompt-examples 20 \
         --task KNWL-DIALO-PROMPT 
 
-# NOTE: If you use api for the model generation, please use the "--api-prompt" flag (setting this value as True). 
+# NOTE: If you use api for the model generation, please use 
+# the "--api-prompt" flag (setting this value as True). 
