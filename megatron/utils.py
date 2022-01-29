cd499559e3d5e848c81423808c49b8dfb1695203
Jared Casper <jcasper@nvidia.com>
2022-01-28 16:13:22 -0800

    Revert incorrect fix.

--- megatron/utils.py
+++ megatron/utils.py
@@ -84,7 +84,7 @@ def average_losses_across_data_parallel_group(losses):
         [loss.clone().detach().view(1) for loss in losses])
     torch.distributed.all_reduce(averaged_losses,
                                  group=mpu.get_data_parallel_group())
-    averaged_losses = averaged_losses /
+    averaged_losses = averaged_losses / \
         torch.distributed.get_world_size(group=mpu.get_data_parallel_group())
 
     return averaged_losses
