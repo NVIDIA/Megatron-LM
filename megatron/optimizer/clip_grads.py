Fixed various bugs for expert model parallelism. Don't use distributed optimizer (yet). Copy expert model parallel properties to main_params copies. Reduce gradient norm across expert model parallel ranks. Reduce inf flag across expert model parallel ranks

megatron/optimizer/clip_grads.py
@@ -8,6 +8,7 @@ from torch._six import inf
 from apex.multi_tensor_apply import multi_tensor_applier
 import amp_C
 
+from megatron import mpu
 from megatron.model.module import param_is_not_shared
 from megatron.core.tensor_parallel import param_is_not_tensor_parallel_duplicate
 
@@ -42,6 +43,15 @@ def clip_grad_norm_fp32(parameters, grads_for_norm,
     if isinstance(grads_for_norm, torch.Tensor):
         grads_for_norm = [grads_for_norm]
 
+    # Separate the gradients into data and expert parallel groups.
+    dp_grads_for_norm = []
+    ep_grads_for_norm = []
+    for (param, grad) in zip(parameters, grads_for_norm):
+        if mpu.param_is_expert_model_parallel(param):
+            ep_grads_for_norm.append(grad)
+        else:
+            dp_grads_for_norm.append(grad)
+
     # Grads.
     grads = []
     for param in parameters:
@@ -56,6 +66,7 @@ def clip_grad_norm_fp32(parameters, grads_for_norm,
 
     # Calculate norm.
     if norm_type == inf:
+        assert not ep_grads_for_norm
         total_norm = max(grad.abs().max() for grad in grads_for_norm)
         total_norm_cuda = torch.cuda.FloatTensor([float(total_norm)])
         # Take max across all model-parallel GPUs.
@@ -66,27 +77,49 @@ def clip_grad_norm_fp32(parameters, grads_for_norm,
 
     else:
         if norm_type == 2.0:
-            dummy_overflow_buf = torch.cuda.IntTensor([0])
             # Use apex's multi-tensor applier for efficiency reasons.
             # Multi-tensor applier takes a function and a list of list
             # and performs the operation on that list all in one kernel.
-            if grads_for_norm:
-                grad_norm, _ = multi_tensor_applier(
+            if dp_grads_for_norm:
+                dummy_overflow_buf = torch.cuda.IntTensor([0])
+                dp_grad_norm, _ = multi_tensor_applier(
                     amp_C.multi_tensor_l2norm,
                     dummy_overflow_buf,
-                    [grads_for_norm],
+                    [dp_grads_for_norm],
                     False # no per-parameter norm
                 )
             else:
-                grad_norm = torch.cuda.FloatTensor([0])
+                dp_grad_norm = torch.cuda.FloatTensor([0])
+
+            if ep_grads_for_norm:
+                dummy_overflow_buf = torch.cuda.IntTensor([0])
+                ep_grad_norm, _ = multi_tensor_applier(
+                    amp_C.multi_tensor_l2norm,
+                    dummy_overflow_buf,
+                    [ep_grads_for_norm],
+                    False # no per-parameter norm
+                )
+            else:
+                ep_grad_norm  = torch.cuda.FloatTensor([0])
+
             # Since we will be summing across data parallel groups,
             # we need the pow(norm-type).
-            total_norm = grad_norm ** norm_type
+            ep_total_norm = ep_grad_norm ** norm_type
+            total_norm = dp_grad_norm ** norm_type
 
         else:
+            assert not ep_grads_for_norm
             for grad in grads_for_norm:
                 grad_norm = torch.norm(grad, norm_type)
                 total_norm += grad_norm ** norm_type
+
+        # Sum across expert-parallel GPUs for expert parallel parameters
+        # and combine the expert-parallel norm with the rest of the norm.
+        if ep_grads_for_norm:
+            torch.distributed.all_reduce(ep_total_norm,
+                                     op=torch.distributed.ReduceOp.SUM,
+                                     group=mpu.get_data_parallel_group())
+            total_norm += ep_total_norm
 
         # Sum across all model-parallel GPUs.
         torch.distributed.all_reduce(total_norm,

megatron/optimizer/optimizer.py
@@ -10,6 +10,7 @@ import torch
 from torch.nn.parallel.distributed import DistributedDataParallel as torchDDP
 from torch._utils import _flatten_dense_tensors, _unflatten_dense_tensors
 
+from megatron import get_args
 from megatron import get_timers
 from megatron import print_rank_0
 from megatron.core import mpu, tensor_parallel
@@ -262,7 +263,7 @@ class MegatronOptimizer(ABC):
                 args.sequence_parallel:
             grads = []
             for model_module in self.models:
-                unwrapped_model = unwrap_model( 
+                unwrapped_model = unwrap_model(
                     model_module, (torchDDP, LocalDDP, Float16Module))
                 for param in unwrapped_model.parameters():
                     if getattr(param, 'sequence_parallel', False):
@@ -397,6 +398,13 @@ class MixedPrecisionOptimizer(MegatronOptimizer):
                                      op=torch.distributed.ReduceOp.MAX,
                                      group=self.get_model_parallel_group())
 
+        # Update across all data parallel instances if we're using expert
+        # model parallelism.
+        if get_args().expert_model_parallelism:
+            torch.distributed.all_reduce(self.found_inf,
+                                         op=torch.distributed.ReduceOp.MAX,
+                                         group=mpu.get_data_parallel_group())
+
         # Check for nan.
         found_inf_flag = (self.found_inf.item() > 0)
 
@@ -405,7 +413,6 @@ class MixedPrecisionOptimizer(MegatronOptimizer):
 
     @torch.no_grad()
     def step(self, args, timers):
-
         # Copy gradients from model params to main params.
         timers('optimizer-copy-to-main-grad', log_level=1).start(
             barrier=args.barrier_with_L1_time)
@@ -530,6 +537,9 @@ class Float16OptimizerWithFloat16Params(MixedPrecisionOptimizer):
                         # Copy tensor model parallel attributes.
                         tensor_parallel.copy_tensor_model_parallel_attributes(main_param,
                                                                               param)
+                        tensor_parallel.copy_expert_model_parallel_attributes(main_param,
+                                                                              param)
+
                         if hasattr(param, 'shared'):
                             main_param.shared = param.shared
                         # Replace the optimizer params with the new fp32 copy.
@@ -573,7 +583,6 @@ class Float16OptimizerWithFloat16Params(MixedPrecisionOptimizer):
 
 
     def _collect_main_grad_data_for_unscaling(self):
-
         main_grads = []
 
         # fp32 params from float16 ones.
@@ -587,7 +596,6 @@ class Float16OptimizerWithFloat16Params(MixedPrecisionOptimizer):
             for main_param in main_group:
                 if main_param.grad is not None:
                     main_grads.append(main_param.grad.data)
-        
         return main_grads
 
 