3aaabf941ef342b83ac7638600e2bb2efc3d3dcb
Sourab Mangrulkar <13534540+pacman100@users.noreply.github.com>
2022-10-17 17:36:58 +0530

    Create setup.py

--- setup.py
+++ setup.py
@@ -1,111 +1,45 @@
-from setuptools import setup, find_packages
-
 """Setup for pip package."""
 
-import importlib.util
 import os
+import sys
 import setuptools
 
-spec = importlib.util.spec_from_file_location('package_info', 'megatron/core/package_info.py')
-package_info = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(package_info)
+if sys.version_info < (3,):
+    raise Exception("Python 2 is not supported by Megatron.")
 
-
-__contact_emails__ = package_info.__contact_emails__
-__contact_names__ = package_info.__contact_names__
-__description__ = package_info.__description__
-__download_url__ = package_info.__download_url__
-__homepage__ = package_info.__homepage__
-__keywords__ = package_info.__keywords__
-__license__ = package_info.__license__
-__package_name__ = package_info.__package_name__
-__repository_url__ = package_info.__repository_url__
-__version__ = package_info.__version__
-
-
-if os.path.exists('megatron/core/README.md'):
-    with open("megatron/core/README.md", "r", encoding='utf-8') as fh:
-        long_description = fh.read()
-    long_description_content_type = "text/markdown"
-
-else:
-    long_description = 'See ' + __homepage__
-    long_description_content_type = "text/plain"
-
-
-###############################################################################
-#                             Dependency Loading                              #
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
-
-def req_file(filename, folder="megatron/core"):
-    with open(os.path.join(folder, filename), encoding='utf-8') as f:
-        content = f.readlines()
-    # you may also want to remove whitespace characters
-    # Example: `\n` at the end of each line
-    return [x.strip() for x in content]
-
-install_requires = req_file("requirements.txt")
-
-###############################################################################
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setuptools.setup(
-    name=__package_name__,
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
-    version=__version__,
-    description=__description__,
+    name="megatron-lm",
+    version="3.0.0",
+    description="Megatron-LM: Training Multi-Billion Parameter Language Models Using Model Parallelism.",
     long_description=long_description,
-    long_description_content_type=long_description_content_type,
+    long_description_content_type="text/markdown",
     # The project's main homepage.
-    url=__repository_url__,
-    download_url=__download_url__,
-    # Author details
-    author=__contact_names__,
-    author_email=__contact_emails__,
-    # maintainer Details
-    maintainer=__contact_names__,
-    maintainer_email=__contact_emails__,
+    url="https://github.com/NVIDIA/Megatron-LM",
+    author="NVIDIA INC",
+    maintainer="NVIDIA INC",
     # The licence under which the project is released
-    license=__license__,
+    license="See https://github.com/NVIDIA/Megatron-LM/blob/master/LICENSE",
     classifiers=[
-        # How mature is this project? Common values are
-        #  1 - Planning
-        #  2 - Pre-Alpha
-        #  3 - Alpha
-        #  4 - Beta
-        #  5 - Production/Stable
-        #  6 - Mature
-        #  7 - Inactive
-        'Development Status :: 5 - Production/Stable',
-        # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'Intended Audience :: Information Technology',
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Information Technology",
         # Indicate what your project relates to
-        'Topic :: Scientific/Engineering',
-        'Topic :: Scientific/Engineering :: Mathematics',
-        'Topic :: Scientific/Engineering :: Image Recognition',
-        'Topic :: Scientific/Engineering :: Artificial Intelligence',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Utilities',
-        # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: BSD License',
-        # Supported python versions
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development :: Libraries :: Python Modules",
         # Additional Setting
-        'Environment :: Console',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
+        "Environment :: Console",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
     ],
-    packages=['megatron.core', 'megatron.core.pipeline_parallel', 'megatron.core.tensor_parallel'], 
-    install_requires=install_requires,
-
+    python_requires=">=3.6",
+    packages=setuptools.find_packages(),
+    install_requires=["nltk", "six", "regex", "torch>=1.12.0", "pybind11"],
     # Add in any packaged data.
     include_package_data=True,
+    zip_safe=False,
     # PyPI package information.
-    keywords=__keywords__,
+    keywords="deep learning, Megatron, gpu, NLP, nvidia, pytorch, torch, language",
 )
