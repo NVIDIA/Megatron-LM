6982c4ee30787df990a3ce51c465c1aac4a66d3f
Raymond Li <raymond@dc1-wks-02.elementai.net>
2022-11-22 21:11:30 +0000

    fix: initialize tokenizer with TokenizerFromFile

--- megatron/checkpointing.py
+++ megatron/checkpointing.py
@@ -72,7 +72,7 @@ def check_checkpoint_args(checkpoint_args):
     # with alibi we can change `max_position_embeddings`
     if args.position_embedding_type != PositionEmbeddingType.alibi:
         _compare('max_position_embeddings')
-    if args.vocab_file:
+    if args.vocab_file or args.tokenizer_file:
         _compare('make_vocab_size_divisible_by')
         _compare('padded_vocab_size')
         _compare('tokenizer_type')
--- megatron/global_vars.py
+++ megatron/global_vars.py
@@ -104,7 +104,7 @@ def set_global_variables(args):
     set_args(args)
 
     _build_num_microbatches_calculator(args)
-    if args.vocab_file:
+    if args.vocab_file or args.tokenizer_file:
         _ = _build_tokenizer(args)
     _set_tensorboard_writer(args)
     _set_adlr_autoresume(args)
