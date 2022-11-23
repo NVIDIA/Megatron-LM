332e8dba8834ac2bf2f3e5b6919b07c8fcec970b
Raymond Li <raymond@dc1-wks-02.elementai.net>
2022-11-23 22:23:23 +0000

    load attention-head-type from checkpoint

--- megatron/checkpointing.py
+++ megatron/checkpointing.py
@@ -506,6 +506,7 @@ def load_args_from_checkpoint(args, load_arg='load'):
     _set_arg('max_position_embeddings')
     _set_arg('tokenizer_type')
     _set_arg('padded_vocab_size')
+    _set_arg('attention_head_type')
     if checkpoint_version < 3.0:
         _set_arg('tensor_model_parallel_size',
                  'model_parallel_size')
