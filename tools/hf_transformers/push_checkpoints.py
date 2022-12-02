5cb878fb7e16434eff03533327c0f376fd62ed2c
Raymond Li <raymond.li@servicenow.com>
2022-12-02 11:18:28 -0500

    add iter_interval argument

--- tools/hf_transformers/push_checkpoints.py
+++ tools/hf_transformers/push_checkpoints.py
@@ -29,6 +29,7 @@ def main():
     parser.add_argument("--exp_dir", type=str, help="Path to experiment folder.")
     parser.add_argument("--repo_name", type=str, help="Name of repository on the Hub in 'ORG/NAME' format.")
     parser.add_argument("--branch_name", type=str, help="Name of branch in repository to save experiments.")
+    parser.add_argument("--iter_interval", type=int, default=1, help="Iteration number must be divisble by iter_interval in order to be pushed")
     args = parser.parse_args()
 
     all_ckpt_dir = Path(args.exp_dir)
@@ -40,22 +41,24 @@ def main():
     head_hash = hf_repo.git_head_hash()
     commit_msg = subprocess.check_output(["git", "show", "-s", "--format=%B", head_hash], cwd=save_dir).decode()
     try:
-        last_uploaded_iter = get_iter_number(commit_msg.strip())
+        last_commit_iter = get_iter_number(commit_msg.strip())
+        print(f"Last commit iteration: {last_commit_iter}")
     except ValueError:
-        last_uploaded_iter = -1
+        last_commit_iter = -1
     
     # The checkpoint dirs should be in ascending iteration order, so that the last commit corresponds to the latest checkpoint
     ckpt_dirs = sorted([x for x in all_ckpt_dir.iterdir() if x.name.startswith("iter_") and x.is_dir()])
 
     for ckpt_dir in ckpt_dirs:
         iter_number = get_iter_number(ckpt_dir.name)
-        if iter_number <= last_uploaded_iter:
-            print(f"Will skip iter: {iter_number}")
+        if iter_number <= last_commit_iter:
             continue
-        # TODO: this only works for 1-way tensor/pipeline parallelism
-        file_path = next((ckpt_dir / "mp_rank_00").glob('*.pt'))
-        tools.hf_transformers.convert_checkpoint.main(path_to_checkpoint=file_path, output_dir=save_dir, print_checkpoint_structure=False)
-        hf_repo.push_to_hub(commit_message=f"{ckpt_dir.name}")
+        if iter_number % args.iter_interval == 0:
+            print(f"Will convert and push iteration {iter_number}")
+            # TODO: this only works for 1-way tensor/pipeline parallelism
+            file_path = next((ckpt_dir / "mp_rank_00").glob('*.pt'))
+            tools.hf_transformers.convert_checkpoint.main(path_to_checkpoint=file_path, output_dir=save_dir, print_checkpoint_structure=False)
+            hf_repo.push_to_hub(commit_message=f"{ckpt_dir.name}")
 
 if __name__ == "__main__":
     main()
