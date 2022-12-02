66beabe5439fa9af503b19231dd66c3d44d0468f
leandro <leandro.vonwerra@spoud.io>
2022-12-02 13:56:25 +0100

    add push to hub logic

--- tools/hf_transformers/push_checkpoints.py
+++ tools/hf_transformers/push_checkpoints.py
@@ -1,10 +1,29 @@
-"""
-Arguments:
-- `experiment_directory` (containing a list of iter_{} subdirectories)
-- `repo_name`: name of the repo to push to.
-Convert all intermediate checkpoints from a `experiment_directory` and push to a single repo `repo_name` on the hub.
-Each intermediate checkpoint is associated a different branch (for example) corresponding to its iteration.
-This script could use `{experiment_directory}/hf_checkpoints/iter_{}` as local path to save the converted checkpoints, and push those for example.
-"""
+from huggingface_hub import Repository
+from pathlib import Path
+import subprocess
+import argparse
 
 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--save_dir", type=str, help="Path where repository is cloned to locally.")
+    parser.add_argument("--exp_dir", type=str, help="Path to experiment folder.")
+    parser.add_argument("--repo_name", type=str, help="Name of repository on the Hub in 'ORG/NAME' format.")
+    parser.add_argument("--branch_name", type=str, help="Name of branch in repository to save experiments.")
+    args = parser.parse_args()
+
+    hf_repo = Repository(args.save_dir, clone_from=args.repo_name)
+    hf_repo.git_checkout(args.branch_name, create_branch_ok=True)
+    
+    all_ckpt_dir = Path(args.exp_dir) / "hf_checkpoints/"
+    ckpt_dirs = [x for x in all_ckpt_dir.iterdir() if x.name.startswith("iter_") and x.is_dir()]
+    # TODO: some sorting necessary? they `ckpt_dirs` should be in ascending order
+
+    for ckpt_dir in ckpt_dirs:
+        file_path = next(ckpt_dir.glob('*.pt'))
+        # TODO: if we format convert_checkpoint.py such that the main logic is in a function with args instead of argparse we can avoid using the `suprocess` and import the function instead
+        subprocess.Popen(["python", "convert_checkpoint.py", "--path_to_checkpoint", file_path, "--output-dir", args.save_dir])
+        hf_repo.push_to_hub(commit_message=f"{ckpt_dir.name}")
+
+if __name__ == "__main__":
+    main()
