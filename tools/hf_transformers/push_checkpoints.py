2ceaf709548eeb1ba3ff9dae7569e9c424ac7e93
Raymond Li <raymond.li@servicenow.com>
2022-12-01 21:54:23 -0500

    add some documentation

--- tools/hf_transformers/push_checkpoints.py
+++ tools/hf_transformers/push_checkpoints.py
@@ -1,6 +1,10 @@
 """
-Convert all intermediate checkpoints from a directory and push to a single repo on the hub.
-Each intermediate checkpoint is associated a different branch corresponding to its iteration
+Arguments:
+- `experiment_directory` (containing a list of iter_{} subdirectories)
+- `repo_name`: name of the repo to push to.
+Convert all intermediate checkpoints from a `experiment_directory` and push to a single repo `repo_name` on the hub.
+Each intermediate checkpoint is associated a different branch (for example) corresponding to its iteration.
+This script could use `{experiment_directory}/hf_checkpoints/iter_{}` as local path to save the converted checkpoints, and push those for example.
 """
 
 
