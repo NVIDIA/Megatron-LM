789f2a9047d565e47c53a813d296775387eb961f
Michael Wyatt <mrwyattii@gmail.com>
2022-12-20 15:42:47 -0800

    fix deprecated numpy types (#103)

--- megatron/data/indexed_dataset.py
+++ megatron/data/indexed_dataset.py
@@ -98,8 +98,8 @@ dtypes = {
     3: np.int16,
     4: np.int32,
     5: np.int64,
-    6: np.float,
-    7: np.double,
+    6: np.float32,
+    7: np.float64,
     8: np.uint16
 }
 
@@ -271,8 +271,8 @@ class IndexedDatasetBuilder(object):
         np.int16: 2,
         np.int32: 4,
         np.int64: 8,
-        np.float: 4,
-        np.double: 8
+        np.float32: 4,
+        np.float64: 8
     }
 
     def __init__(self, out_file, dtype=np.int32):
