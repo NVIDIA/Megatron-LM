f33446f3c642dc81ab3261d02f004cc6a15537ee
eharper <eharper@nvidia.com>
2023-02-08 17:45:18 -0700

    use config attribute
    Signed-off-by: eharper <eharper@nvidia.com>

--- megatron/core/transformer/parallel_mlp.py
+++ megatron/core/transformer/parallel_mlp.py
@@ -24,33 +24,22 @@ class ParallelMLP(MegatronModule):
     def __init__(self, config: TransformerConfig):
         super(ParallelMLP, self).__init__(config=config)
 
-        self.config = config
-        self.hidden_size = config.hidden_size
-        self.ffn_hidden_size = config.ffn_hidden_size
-        self.init_method = config.init_method
-        self.output_layer_init_method = config.output_layer_init_method
-        self.use_cpu_initialization = config.use_cpu_initialization
-        self.perform_initialization = config.perform_initialization
-        self.bias_gelu_fusion = config.bias_gelu_fusion
-        self.gradient_accumulation_fusion = config.gradient_accumulation_fusion
-        self.sequence_parallel_enabled = config.sequence_parallel_enabled
-        self.params_dtype = config.params_dtype
-        self.async_tensor_model_parallel_allreduce = config.async_tensor_model_parallel_allreduce
+        self.config: TransformerConfig = config
 
         # Project to 4h.
         # @jcasper should we change the name dense_h_to_4h here?
         self.dense_h_to_4h = tensor_parallel.ColumnParallelLinear(
-            self.hidden_size,
-            self.ffn_hidden_size,
+            self.config.hidden_size,
+            self.config.ffn_hidden_size,
             gather_output=False,
-            init_method=self.init_method,
+            init_method=self.config.init_method,
             skip_bias_add=True,
-            async_tensor_model_parallel_allreduce=self.async_tensor_model_parallel_allreduce,
-            params_dtype=self.params_dtype,
-            use_cpu_initialization=self.use_cpu_initialization,
-            perform_initialization=self.perform_initialization,
-            gradient_accumulation_fusion=self.gradient_accumulation_fusion,
-            sequence_parallel_enabled=self.sequence_parallel_enabled,
+            async_tensor_model_parallel_allreduce=self.config.async_tensor_model_parallel_allreduce,
+            params_dtype=self.config.params_dtype,
+            use_cpu_initialization=self.config.use_cpu_initialization,
+            perform_initialization=self.config.perform_initialization,
+            gradient_accumulation_fusion=self.config.gradient_accumulation_fusion,
+            sequence_parallel_enabled=self.config.sequence_parallel_enabled,
         )
 
         self.activation_func = F.gelu
@@ -65,16 +54,16 @@ class ParallelMLP(MegatronModule):
         # Project back to h.
         # @jcasper should we change the name here?
         self.dense_4h_to_h = tensor_parallel.RowParallelLinear(
-            self.ffn_hidden_size,
-            self.hidden_size,
+            self.config.ffn_hidden_size,
+            self.config.hidden_size,
             input_is_parallel=True,
-            init_method=self.output_layer_init_method,
+            init_method=self.config.output_layer_init_method,
             skip_bias_add=True,
-            params_dtype=self.params_dtype,
-            use_cpu_initialization=self.use_cpu_initialization,
-            perform_initialization=self.perform_initialization,
-            gradient_accumulation_fusion=self.gradient_accumulation_fusion,
-            sequence_parallel_enabled=self.sequence_parallel_enabled,
+            params_dtype=self.config.params_dtype,
+            use_cpu_initialization=self.config.use_cpu_initialization,
+            perform_initialization=self.config.perform_initialization,
+            gradient_accumulation_fusion=self.config.gradient_accumulation_fusion,
+            sequence_parallel_enabled=self.config.sequence_parallel_enabled,
         )
 
     def forward(self, hidden_states):
@@ -82,7 +71,7 @@ class ParallelMLP(MegatronModule):
         # [s, b, 4 * h/p]
         intermediate_parallel, bias_parallel = self.dense_h_to_4h(hidden_states)
 
-        if self.bias_gelu_fusion:
+        if self.config.bias_gelu_fusion:
             intermediate_parallel = bias_gelu_impl(intermediate_parallel, bias_parallel)
         else:
             intermediate_parallel = self.activation_func(intermediate_parallel + bias_parallel)
