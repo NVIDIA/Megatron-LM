5a76d30514bda1bbc86f459c21c18606c680c4e0
janEbert <janpublicebert@posteo.net>
2023-02-17 15:31:25 +0100

    Fix not supplying `--pack-samples` argument

--- megatron/data/dataset_utils.py
+++ megatron/data/dataset_utils.py
@@ -733,6 +733,7 @@ def build_dataset(name, data_prefix, data_impl, splits_string, max_num_samples,
             max_seq_length_dec=max_seq_length_dec,
             short_seq_prob=args.short_seq_prob,
             add_mask_tokens=args.add_mask_tokens,
+            pack_samples=args.pack_samples,
             data_cache_path=args.data_cache_path,
             **kwargs
         )
@@ -747,6 +748,7 @@ def build_dataset(name, data_prefix, data_impl, splits_string, max_num_samples,
             mean_span_lengths=args.ul2_mean_span_lengths,
             mask_ratios=args.ul2_mask_ratios,
             add_mask_tokens=args.add_mask_tokens,
+            pack_samples=args.pack_samples,
             denoiser_tokens={
                 'R': args.ul2_r_denoiser_token,
                 'S': args.ul2_s_denoiser_token,
