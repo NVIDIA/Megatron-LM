716da5d8e61cd4f6d8af8e55747a6e9e22831328
MaximumEntropy <sandeep.subramanian.1@umontreal.ca>
2023-04-05 11:56:26 -0700

    Fix mpu init
    Signed-off-by: MaximumEntropy <sandeep.subramanian.1@umontreal.ca>

--- megatron/initialize.py
+++ megatron/initialize.py
@@ -185,8 +185,7 @@ def _initialize_distributed():
             mpu.initialize_model_parallel(args.tensor_model_parallel_size,
                                            args.pipeline_model_parallel_size,
                                            args.virtual_pipeline_model_parallel_size,
-                                           args.pipeline_model_parallel_split_rank,
-                                           args.untie_embeddings_and_output_weights)
+                                           args.pipeline_model_parallel_split_rank)
             if args.rank == 0:
                 print(f'> initialized tensor model parallel with size '
                       f'{mpu.get_tensor_model_parallel_world_size()}')
