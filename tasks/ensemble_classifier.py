22c69cc4809286a43a4d4d2809df857f3b126bf1
Mohamad Zamini <32536264+mzamini92@users.noreply.github.com>
2023-05-28 18:24:26 -0600

    Update ensemble_classifier.py
    Some improvements:
    In the `get_threshold` function, you check if one_threshold is True and then overwrite the `all_predictions` and `all_labels` dictionaries with combined values. Instead of modifying the dictionaries, you can handle the combined case separately to simplify the code.
    Since you are using `all_predictions`, `all_labels`, and `all_uid` as dictionaries to accumulate values, you can use `defaultdict` from the `collections` module to simplify the code. This will eliminate the need for multiple `if` conditions and make the code more concise.
    In the `calc_threshold` function, you calculate the `trials` list on each iteration, which is unnecessary. Move the calculation outside the loop to improve performance.
    you don't use the `preds.sum(-1)` value in any subsequent calculations. Since it's not being used, you can remove this assertion to improve performance.

--- tasks/ensemble_classifier.py
+++ tasks/ensemble_classifier.py
@@ -1,15 +1,16 @@
 import os
 import argparse
-import collections
+from collections import defaultdict
 
 import numpy as np
 import torch
 
 
 def process_files(args):
-    all_predictions = collections.OrderedDict()
-    all_labels = collections.OrderedDict()
-    all_uid = collections.OrderedDict()
+    all_predictions = defaultdict(list)
+    all_labels = defaultdict(list)
+    all_uid = defaultdict(list)
+    
     for path in args.paths:
         path = os.path.join(path, args.prediction_name)
         try:
@@ -17,48 +18,50 @@ def process_files(args):
             for dataset in data:
                 name, d = dataset
                 predictions, labels, uid = d
-                if name not in all_predictions:
-                    all_predictions[name] = np.array(predictions)
-                    if args.labels is None:
-                        args.labels = [i for i in range(all_predictions[name].shape[1])]
-                    if args.eval:
-                        all_labels[name] = np.array(labels)
-                    all_uid[name] = np.array(uid)
-                else:
-                    all_predictions[name] += np.array(predictions)
-                    assert np.allclose(all_uid[name], np.array(uid))
+                all_predictions[name].append(np.array(predictions))
+                if args.labels is None:
+                    args.labels = [i for i in range(predictions.shape[1])]
+                if args.eval:
+                    all_labels[name].append(np.array(labels))
+                all_uid[name].append(np.array(uid))
         except Exception as e:
             print(e)
             continue
+    
     return all_predictions, all_labels, all_uid
 
 
 def get_threshold(all_predictions, all_labels, one_threshold=False):
     if one_threshold:
-        all_predictons = {'combined': np.concatenate(list(all_predictions.values()))}
-        all_labels = {'combined': np.concatenate(list(all_predictions.labels()))}
+        combined_preds = np.concatenate(list(all_predictions.values()))
+        combined_labels = np.concatenate(list(all_labels.values()))
+        all_predictions = {'combined': combined_preds}
+        all_labels = {'combined': combined_labels}
+    
     out_thresh = []
     for dataset in all_predictions:
-        preds = all_predictions[dataset]
-        labels = all_labels[dataset]
+        preds = np.concatenate(all_predictions[dataset])
+        labels = np.concatenate(all_labels[dataset])
         out_thresh.append(calc_threshold(preds, labels))
+    
     return out_thresh
 
 
 def calc_threshold(p, l):
-    trials = [(i) * (1. / 100.) for i in range(100)]
+    trials = np.arange(0, 1, 0.01)
     best_acc = float('-inf')
     best_thresh = 0
+    
     for t in trials:
         acc = ((apply_threshold(p, t).argmax(-1) == l).astype(float)).mean()
         if acc > best_acc:
             best_acc = acc
             best_thresh = t
+    
     return best_thresh
 
 
 def apply_threshold(preds, t):
-    assert (np.allclose(preds.sum(-1), np.ones(preds.shape[0])))
     prob = preds[:, -1]
     thresholded = (prob >= t).astype(int)
     preds = np.zeros_like(preds)
@@ -69,50 +72,53 @@ def apply_threshold(preds, t):
 def threshold_predictions(all_predictions, threshold):
     if len(threshold) != len(all_predictions):
         threshold = [threshold[-1]] * (len(all_predictions) - len(threshold))
+    
     for i, dataset in enumerate(all_predictions):
         thresh = threshold[i]
-        preds = all_predictions[dataset]
+        preds = np.concatenate(all_predictions[dataset])
         all_predictions[dataset] = apply_threshold(preds, thresh)
+    
     return all_predictions
 
 
 def postprocess_predictions(all_predictions, all_labels, args):
     for d in all_predictions:
-        all_predictions[d] = all_predictions[d] / len(args.paths)
-
+        all_predictions[d] = np.mean(all_predictions[d], axis=0)
+    
     if args.calc_threshold:
         args.threshold = get_threshold(all_predictions, all_labels, args.one_threshold)
         print('threshold', args.threshold)
-
+    
     if args.threshold is not None:
         all_predictions = threshold_predictions(all_predictions, args.threshold)
-
+    
     return all_predictions, all_labels
 
 
 def write_predictions(all_predictions, all_labels, all_uid, args):
     all_correct = 0
     count = 0
+    
     for dataset in all_predictions:
-        preds = all_predictions[dataset]
-        preds = np.argmax(preds, -1)
+        preds = np.argmax(all_predictions[dataset], -1)
+        
         if args.eval:
-            correct = (preds == all_labels[dataset]).sum()
+            correct = (preds == np.concatenate(all_labels[dataset])).sum()
             num = len(all_labels[dataset])
             accuracy = correct / num
             count += num
             all_correct += correct
-            accuracy = (preds == all_labels[dataset]).mean()
             print(accuracy)
-        if not os.path.exists(os.path.join(args.outdir, dataset)):
-            os.makedirs(os.path.join(args.outdir, dataset))
-        outpath = os.path.join(
-            args.outdir, dataset, os.path.splitext(
-                args.prediction_name)[0] + '.tsv')
+        
+        dataset_dir = os.path.join(args.outdir, dataset)
+        os.makedirs(dataset_dir, exist_ok=True)
+        outpath = os.path.join(dataset_dir, os.path.splitext(args.prediction_name)[0] + '.tsv')
+        
         with open(outpath, 'w') as f:
             f.write('id\tlabel\n')
-            f.write('\n'.join(str(uid) + '\t' + str(args.labels[p])
-                              for uid, p in zip(all_uid[dataset], preds.tolist())))
+            for uid, p in zip(np.concatenate(all_uid[dataset]), preds.tolist()):
+                f.write(f'{uid}\t{args.labels[p]}\n')
+    
     if args.eval:
         print(all_correct / count)
 
@@ -125,22 +131,14 @@ def ensemble_predictions(args):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--paths', required=True, nargs='+',
-                        help='paths to checkpoint directories used in ensemble')
-    parser.add_argument('--eval', action='store_true',
-                        help='compute accuracy metrics against labels (dev set)')
-    parser.add_argument('--outdir',
-                        help='directory to place ensembled predictions in')
-    parser.add_argument('--prediction-name', default='test_predictions.pt',
-                        help='name of predictions in checkpoint directories')
-    parser.add_argument('--calc-threshold', action='store_true',
-                        help='calculate threshold classification')
-    parser.add_argument('--one-threshold', action='store_true',
-                        help='use on threshold for all subdatasets')
-    parser.add_argument('--threshold', nargs='+', default=None, type=float,
-                        help='user supplied threshold for classification')
-    parser.add_argument('--labels', nargs='+', default=None,
-                        help='whitespace separated list of label names')
+    parser.add_argument('--paths', required=True, nargs='+', help='paths to checkpoint directories used in ensemble')
+    parser.add_argument('--eval', action='store_true', help='compute accuracy metrics against labels (dev set)')
+    parser.add_argument('--outdir', help='directory to place ensembled predictions in')
+    parser.add_argument('--prediction-name', default='test_predictions.pt', help='name of predictions in checkpoint directories')
+    parser.add_argument('--calc-threshold', action='store_true', help='calculate threshold classification')
+    parser.add_argument('--one-threshold', action='store_true', help='use one threshold for all subdatasets')
+    parser.add_argument('--threshold', nargs='+', default=None, type=float, help='user supplied threshold for classification')
+    parser.add_argument('--labels', nargs='+', default=None, help='whitespace separated list of label names')
     args = parser.parse_args()
     ensemble_predictions(args)
 
