19458bb77248c8f9cdab8245393bdf78ed55c908
Mohamad Zamini <32536264+mzamini92@users.noreply.github.com>
2023-05-28 18:08:46 -0600

    Update run_text_generation_server.py
    The `try-except` blocks in the while True loop have almost identical code. 
    Use argparse for argument parsing: Instead of defining a custom `add_text_generate_args` function, you can use the `argparse` module for argument parsing. It provides a more standard and flexible way to handle command-line arguments.

--- tools/run_text_generation_server.py
+++ tools/run_text_generation_server.py
@@ -2,11 +2,9 @@
 
 """Sample Generate GPT"""
 import os
-import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                             os.path.pardir)))
 import socket
-from megatron import get_args
+import torch
+from argparse import ArgumentParser
 from megatron import print_rank_0
 from megatron.core import mpu
 from megatron.checkpointing import load_checkpoint
@@ -14,31 +12,20 @@ from megatron.initialize import initialize_megatron
 from megatron.model import GPTModel
 from megatron.training import get_model
 from megatron.text_generation_server import MegatronServer
-from megatron.text_generation import generate_and_post_process
-from megatron.text_generation import beam_search_and_post_process
-import torch
+from megatron.text_generation import generate_and_post_process, beam_search_and_post_process
 
 def model_provider(pre_process=True, post_process=True):
     """Build the model."""
-
     print_rank_0('building GPT model ...')
     model = GPTModel(num_tokentypes=0, parallel_output=False, pre_process=pre_process, post_process=post_process)
-
     return model
 
 def add_text_generate_args(parser):
-    group = parser.add_argument_group(title='text generation')
-
-    group.add_argument("--temperature", type=float, default=1.0,
-                       help='Sampling temperature.')
-    group.add_argument("--top_p", type=float, default=0.0,
-                       help='Top p sampling.')
-    group.add_argument("--top_k", type=int, default=0,
-                       help='Top k sampling.')
-    group.add_argument("--out-seq-length", type=int, default=1024,
-                       help='Size of the output generated text.')
+    parser.add_argument("--temperature", type=float, default=1.0, help='Sampling temperature.')
+    parser.add_argument("--top_p", type=float, default=0.0, help='Top p sampling.')
+    parser.add_argument("--top_k", type=int, default=0, help='Top k sampling.')
+    parser.add_argument("--out-seq-length", type=int, default=1024, help='Size of the output generated text.')
     return parser
-
 
 if __name__ == "__main__":
     initialize_megatron(extra_args_provider=add_text_generate_args,
@@ -50,6 +37,7 @@ if __name__ == "__main__":
     if args.num_layers_per_virtual_pipeline_stage is not None:
         print("Interleaved pipeline schedule is not yet supported for text generation.")
         exit()
+    
     # Set up model and load checkpoint
     model = get_model(model_provider, wrap_with_ddp=False)
 
@@ -65,13 +53,10 @@ if __name__ == "__main__":
     while True:
         choice = torch.cuda.LongTensor(1)
         torch.distributed.broadcast(choice, 0)
-        if choice[0].item() == 0:
-            try:
+        try:
+            if choice[0].item() == 0:
                 generate_and_post_process(model)
-            except ValueError as ve:
-                pass
-        elif choice[0].item() == 1:
-            try:
+            elif choice[0].item() == 1:
                 beam_search_and_post_process(model)
-            except ValueError as ve:
-                pass
+        except ValueError:
+            pass
