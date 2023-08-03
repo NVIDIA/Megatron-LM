9b15c2e5a0a143bfa67df2ab56d8a6b48f75b18b
jasonwan <jasonwan@nvidia.com>
2023-08-02 20:29:51 -0700

    move InferenceParams into core. make variable names consistent
    Signed-off-by: jasonwan <jasonwan@nvidia.com>

--- megatron/core/inference_params.py
+++ megatron/core/inference_params.py
@@ -8,3 +8,16 @@ class InferenceParams:
         self.sequence_len_offset = 0
         self.batch_size_offset = 0
         self.key_value_memory_dict = {}
+
+    def swap_key_value_dict(self, batch_idx):
+        "swap between batches"
+        if len(self.key_value_memory_dict) == 0:
+            raise ValueError("should not swap when dict in empty")
+        
+        for layer_number in self.key_value_memory_dict.keys():
+            inference_key_memory, inference_value_memory = self.key_value_memory_dict[layer_number]
+            assert len(batch_idx) == inference_key_memory.shape[1] ## make sure batch size is the same
+            new_inference_key_memory = inference_key_memory[:, batch_idx]
+            new_inference_value_memory = inference_value_memory[:, batch_idx]
+            self.key_value_memory_dict[layer_number] = (
+                    new_inference_key_memory, new_inference_value_memory)
