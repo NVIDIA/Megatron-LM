4d9c11b80882cc58f9e76815b3f9dd5ecf666dcc
shanmugamr <shanmugamr@nvidia.com>
2023-08-17 10:23:01 -0700

    Changing image for functional tests

--- tests/functional_tests/test_scripts/bert/sbatch_bert_distributed_test.sh
+++ tests/functional_tests/test_scripts/bert/sbatch_bert_distributed_test.sh
@@ -10,6 +10,8 @@ DATA_PATH=/workspace/data/bert_data/my-bert_00_text_sentence
 CHECKPOINT_PATH=/workspace/checkpoints
 TENSORBOARD_DIR=/workspace/logs
 
+echo 'Running tests using $PYTORCH_IMAGE image'
+
 srun --output $BASE_DIR/results/slurm-%j.out --error $BASE_DIR/results/slurm-%j.out --container-image $PYTORCH_IMAGE --container-mounts $BASE_DIR/logs:/workspace/logs,$BASE_DIR/checkpoints:/workspace/checkpoints,$BUILD_DIR:/workspace/megatron-lm,$DATA_DIR:/workspace/data --no-container-mount-home bash -c "
   ls 
   cd /workspace/megatron-lm
