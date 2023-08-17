24888f1c79f3fcb4d66e559bb4c4ac4621580c82
Adam Moody <moody20@llnl.gov>
2023-08-17 17:02:01 -0500

    attempt to enable nvfuser but handle error for ROCm users (#220)

--- megatron/initialize.py
+++ megatron/initialize.py
@@ -300,7 +300,11 @@ def set_jit_fusion_options():
         torch._C._jit_override_can_fuse_on_cpu(False)
         torch._C._jit_override_can_fuse_on_gpu(False)
         torch._C._jit_set_texpr_fuser_enabled(False)
-        torch._C._jit_set_nvfuser_enabled(True)
+        try:
+            torch._C._jit_set_nvfuser_enabled(True)
+        except:
+            # catch error thrown on ROCm systems
+            pass
         torch._C._debug_set_autodiff_subgraph_inlining(False)
     else:
         # legacy pytorch fuser
