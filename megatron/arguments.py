2d76dd393dd794377a4520d47d4c8c67b7577bd6
Marko Hostnik <mare5x.dev@gmail.com>
2023-08-17 12:44:54 +0200

    Escape `%` in help message for `--rotary-percent`.

--- megatron/arguments.py
+++ megatron/arguments.py
@@ -575,7 +575,7 @@ def _add_network_size_args(parser):
                        help='Use rotary positional embeddings or not. '
                        'Deprecated: use --position-embedding-type')
     group.add_argument('--rotary-percent', type=float, default=1.0,
-                       help='Percent of rotary dimension to use, default 100%')
+                       help='Percent of rotary dimension to use, default 100%%')
     group.add_argument('--rotary-seq-len-interpolation-factor', type=int, default=None,
                        help='Sequence length interpolation factor for rotary embeddings.')
     group.add_argument('--no-position-embedding',
