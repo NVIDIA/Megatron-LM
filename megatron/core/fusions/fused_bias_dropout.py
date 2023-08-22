ac422cb9bd38e205985649282dc69a80776cb3a9
Sudhakar Singh <sudhakars@nvidia.com>
2023-08-21 21:07:40 -0700

    refactor bias dropout add a bit and include a mistakenly deleted line
    Signed-off-by: Sudhakar Singh <sudhakars@nvidia.com>

--- megatron/core/fusions/fused_bias_dropout.py
+++ megatron/core/fusions/fused_bias_dropout.py
@@ -1,33 +1,35 @@
 # Copyright (c) 2023, NVIDIA CORPORATION. All rights reserved.
-
 from typing import Optional, Tuple
 
 import torch
 
 
-def _bias_dropout_add_func(x, bias, residual, prob, training):
-    # type: (Tensor, Optional[Tensor], Tensor, float, bool) -> Tensor
+def _bias_dropout_add_func(x_with_bias, residual, prob, training):
+    # type: (Tuple[Tensor, Optional[Tensor]], Tensor, float, bool) -> Tensor
     # NOTE: Previously, the argument `bias` used to be passed as
     # `bias.expand_as(residual)` when the `bias_dropout_func` is called from the
     # transformer layer but broadcasting should automatically take care of that.
     # Also, looking at broadcasting semantics, `expand_as` and broadcasting
     # seem to be identical performance-wise (both just change the view).
+
+    x, bias = x_with_bias  # unpack
+
+    # If we want to train mixed precision, then the output of this function
+    # should be half precision. However, in AMP O1, the input (residual) is
+    # in fp32, and it will up-cast the result to fp32, causing pipeline parallel
+    # GPU communication to hang. Therefore, we need to cast residual to the same
+    # dtype as x.
+    residual = residual if residual.dtype == x.dtype else residual.to(x.dtype)
     if bias is not None:
         x = x + bias
     out = torch.nn.functional.dropout(x, p=prob, training=training)
     out = residual + out
     return out
 
-
-def bias_dropout_add_unfused_train(x_with_bias, residual, prob):
-    x, bias = x_with_bias  # unpack
-    return _bias_dropout_add_func(x, bias, residual, prob, True)
-
-
-def bias_dropout_add_unfused_inference(x_with_bias, residual, prob):
-    x, bias = x_with_bias  # unpack
-    return _bias_dropout_add_func(x, bias, residual, prob, False)
-
+def bias_dropout_add_unfused(training):
+    def _bias_dropout_add(x_with_bias, residual, prob):
+        return _bias_dropout_add_func(x_with_bias, residual, prob, training)
+    return _bias_dropout_add
 
 @torch.jit.script
 def bias_dropout_add_fused_train(
@@ -35,9 +37,7 @@ def bias_dropout_add_fused_train(
     residual: torch.Tensor,
     prob: float,
 ) -> torch.Tensor:
-    x, bias = x_with_bias  # unpack
-    return _bias_dropout_add_func(x, bias, residual, prob, True)
-
+    return _bias_dropout_add_func(x_with_bias, residual, prob, True)
 
 @torch.jit.script
 def bias_dropout_add_fused_inference(
@@ -45,9 +45,7 @@ def bias_dropout_add_fused_inference(
     residual: torch.Tensor,
     prob: float,
 ) -> torch.Tensor:
-    x, bias = x_with_bias  # unpack
-    return _bias_dropout_add_func(x, bias, residual, prob, False)
-
+    return _bias_dropout_add_func(x_with_bias, residual, prob, False)
 
 def get_bias_dropout_add(training, fused):
     if fused:
@@ -60,7 +58,4 @@ def get_bias_dropout_add(training, fused):
         else:
             return bias_dropout_add_fused_inference
     else:
-        if training:
-            return bias_dropout_add_unfused_train
-        else:
-            return bias_dropout_add_unfused_inference
+        return bias_dropout_add_unfused(training)
