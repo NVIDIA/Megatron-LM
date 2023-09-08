6bd821531e4478d1dbdb65b40a8bb3a686a95808
Sudhakar Singh <sudhakars@nvidia.com>
2023-09-07 20:20:51 -0700

    move residual before the layernorms

--- megatron/core/transformer/transformer_layer.py
+++ megatron/core/transformer/transformer_layer.py
@@ -164,11 +164,11 @@ class TransformerLayer(MegatronModule):
     ):
         # hidden_states: [s, b, h]
 
+        # Residual connection.
+        residual = hidden_states
+
         # Optional Input Layer norm
         input_layernorm_output = self.input_layernorm(hidden_states)
-
-        # Residual connection.
-        residual = input_layernorm_output
 
         # Self attention.
         attention_output_with_bias = self.self_attention(
@@ -185,11 +185,11 @@ class TransformerLayer(MegatronModule):
                 attention_output_with_bias, residual, self.config.hidden_dropout
             )
 
+        # Residual connection.
+        residual = hidden_states
+
         # Optional Layer norm after self-attention
         pre_cross_attn_layernorm_output = self.pre_cross_attn_layernorm(hidden_states)
-
-        # Residual connection.
-        residual = pre_cross_attn_layernorm_output
 
         # Cross attention.
         attention_output_with_bias = self.cross_attention(
@@ -206,11 +206,11 @@ class TransformerLayer(MegatronModule):
                 attention_output_with_bias, residual, self.config.hidden_dropout
             )
 
+        # Residual connection.
+        residual = hidden_states
+
         # Optional Layer norm post the cross-attention.
         pre_mlp_layernorm_output = self.pre_mlp_layernorm(hidden_states)
-
-        # Residual connection.
-        residual = pre_mlp_layernorm_output
 
         # MLP.
         mlp_output_with_bias = self.mlp(pre_mlp_layernorm_output)
