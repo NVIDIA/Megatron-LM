507ed824345f3acde66b6247ad5bc6b199359149
Lawrence McAfee <lmcafee@nvidia.com>
2023-10-02 12:22:29 -0700

    removed superfluous retro args.

--- megatron/arguments.py
+++ megatron/arguments.py
@@ -424,14 +424,9 @@ def core_transformer_config_from_args(args):
     else:
         kw_args['num_query_groups'] = None
 
+    # Retro preprocessing args.
     retro_args = get_retro_args()
     if retro_args:
-        kw_args['retro_workdir'] = args.retro_workdir
-        kw_args['retro_encoder_num_layers'] = args.retro_encoder_layers
-        kw_args['retro_encoder_hidden_dropout'] = args.retro_encoder_hidden_dropout
-        kw_args['retro_encoder_attention_dropout'] = args.retro_encoder_attention_dropout
-        kw_args['retro_num_neighbors'] = args.retro_num_neighbors
-        kw_args['retro_num_retrieved_chunks'] = args.retro_num_retrieved_chunks
         kw_args['retro_preprocess'] = retro_args
 
     return TransformerConfig(**kw_args)
