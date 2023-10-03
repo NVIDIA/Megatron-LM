1d2c86bd9ef5c1210f05fe87b762f7d6a0d3057a
Mikołaj Błaż <mblaz@nvidia.com>
2023-10-03 18:32:45 +0200

    Simplify conftest

--- tests/unit_tests/dist_checkpointing/conftest.py
+++ tests/unit_tests/dist_checkpointing/conftest.py
@@ -1,25 +1,9 @@
-import os
-import re
-import shutil
-import tempfile
 from pathlib import Path
 
 import pytest
-import torch.distributed
-from _pytest.fixtures import FixtureRequest, fixture
-from _pytest.tmpdir import TempPathFactory
 
-from tests.unit_tests.dist_checkpointing import empty_dir, TempNamedDir
+from tests.unit_tests.dist_checkpointing import TempNamedDir
 from tests.unit_tests.test_utilities import Utils
-
-
-def _mk_tmp_nonnumbered(request: FixtureRequest, factory: TempPathFactory) -> Path:
-    name = request.node.name
-    print('name', name, flush=True)
-    name = re.sub(r"[\W]", "_", name)
-    MAXVAL = 30
-    name = name[:MAXVAL]
-    return factory.mktemp(name)
 
 
 @pytest.fixture(scope="session")
