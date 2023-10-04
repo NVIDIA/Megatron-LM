59402c117ae2a2c12ab60a627266eec54b114cad
Jared Casper <jcasper@nvidia.com>
2023-10-03 20:38:16 -0700

    Merge branch 'layernorm1p_fix' into '23.08'
    Layernorm1p fix
    
    See merge request ADLR/megatron-lm!818
    
    (cherry picked from commit 1f4827148870a9a722f2477dc17148aefdea7310)
    
    d079a63a add arg

--- megatron/core/transformer/custom_layers/transformer_engine.py
+++ megatron/core/transformer/custom_layers/transformer_engine.py
@@ -45,11 +45,13 @@ class TENorm:
         normalization: str = "LayerNorm",
         **kwargs
     ):
+        zero_centered_gamma = kwargs.get('zero_centered_gamma', 'False')
         if normalization == "LayerNorm":
             instance = te.pytorch.LayerNorm(
                 hidden_size=hidden_size,
                 eps=eps,
                 sequence_parallel=sequence_parallel,
+                zero_centered_gamma=zero_centered_gamma,
                 **_get_extra_te_kwargs(config),
             )
         elif normalization == "RMSNorm":
@@ -60,6 +62,7 @@ class TENorm:
                 hidden_size=hidden_size,
                 eps=eps,
                 sequence_parallel=sequence_parallel,
+                zero_centered_gamma=zero_centered_gamma,
                 **_get_extra_te_kwargs(config),
             )
         else:
@@ -167,6 +170,7 @@ class TELayerNormColumnParallelLinear(te.pytorch.LayerNormLinear):
             params_dtype=self.config.params_dtype,
             parallel_mode="column",
             return_bias=self.te_return_bias,
+            zero_centered_gamma=self.config.layernorm_zero_centered_gamma,
             **_get_extra_te_kwargs(config),
         )
 
