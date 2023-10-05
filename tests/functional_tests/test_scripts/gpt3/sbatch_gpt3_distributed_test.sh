01d548e33fae4f756338d2eaf7671ede63493f86
Shanmugam Ramasamy <shanmugamr@shanmugamr-mlt.client.nvidia.com>
2023-10-05 12:04:35 -0700

    Testing way to locally store sbatch and pretrain scripts

--- tests/functional_tests/test_scripts/gpt3/sbatch_gpt3_distributed_test.sh
+++ tests/functional_tests/test_scripts/gpt3/sbatch_gpt3_distributed_test.sh
@@ -17,6 +17,10 @@ if [[ -n $VP_SIZE ]]; then VP_SIZE="" ; fi
 
 echo 'Running tests using $PYTORCH_IMAGE image'
 
+export DATA_PATH CHECKPOINT_PATH TENSORBOARD_DIR USE_TE TP_SIZE PP_SIZE NUM_NODES MAX_STEPS USE_CORE VP_SIZE MBS GBS ADDITIONAL_PARAMS
+
+envsubst <BUILD_DIR/tests/functional_tests/test_scripts/gpt3/sbatch_gpt3_distributed_test.sh > $BASE_DIR/scripts/sbatch_gpt3_distributed_test.sh
+
 srun --output $BASE_DIR/results/slurm-%j.out --error $BASE_DIR/results/slurm-%j.out --container-image $PYTORCH_IMAGE --container-mounts $BASE_DIR/logs:/workspace/logs,$BASE_DIR/checkpoints:/workspace/checkpoints,$BUILD_DIR:/workspace/megatron-lm,$DATA_DIR:/workspace/data --no-container-mount-home bash -c "
   ls 
   cd /workspace/megatron-lm
