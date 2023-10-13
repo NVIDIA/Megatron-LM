93377d7b829ff78b03177501db4ea5adffcc4ef4
xren <xren@nvidia.com>
2023-10-12 17:20:08 -0700

    small change to gpt dataset for cp
    Signed-off-by: xren <xren@nvidia.com>

--- megatron/data/gpt_dataset.py
+++ megatron/data/gpt_dataset.py
@@ -451,6 +451,7 @@ def _build_index_mappings(name, data_prefix, documents, sizes,
 
     counts = torch.cuda.LongTensor([data_cache_success])
     torch.distributed.all_reduce(counts, group=mpu.get_data_parallel_group())
+    torch.distributed.all_reduce(counts, group=mpu.get_context_parallel_group())
     torch.distributed.all_reduce(counts, group=mpu.get_pipeline_model_parallel_group())
     if counts[0].item() != (
         torch.distributed.get_world_size() //
