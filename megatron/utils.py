e3c2627a779f0332fb24682ba0e785085ce473dc
eric-weiss-zyphra <144280438+eric-weiss-zyphra@users.noreply.github.com>
2023-11-02 16:24:28 -0700

    Update utils.py
    Added throughput_calculator for estimating Tflops

--- megatron/utils.py
+++ megatron/utils.py
@@ -25,6 +25,7 @@ from megatron.core import mpu
 from megatron.core.tensor_parallel import param_is_not_tensor_parallel_duplicate
 from megatron.model import Float16Module
 from megatron.model.module import param_is_not_shared
+from megatron import get_num_microbatches
 
 
 ALL_MODULE_WRAPPER_CLASSNAMES = (DDP, Float16Module)
@@ -269,3 +270,41 @@ def print_rank_last(message):
             print(message, flush=True)
     else:
         print(message, flush=True)
+
+def get_parameters_in_billions(model):
+    gpus_per_model = torch.distributed.get_world_size(group=mpu.get_model_parallel_group())
+
+    approx_parameters_in_billions = sum([sum([p.ds_numel if hasattr(p,'ds_id') else  p.nelement() for p in model_module.parameters()])
+                                        for model_module in model])
+
+    return approx_parameters_in_billions*gpus_per_model/(1e9)
+
+def throughput_calculator(model, args, iteration_time, total_iterations):
+    gpus_per_model = torch.distributed.get_world_size(group = mpu.get_model_parallel_group())
+    batch_size = args.micro_batch_size * get_num_microbatches() * args.data_parallel_size
+    samples_per_model = batch_size * args.seq_length
+    model_replica_count = torch.distributed.get_world_size() / gpus_per_model
+    approx_parameters_in_billions = None if (model is None) else get_parameters_in_billions(model)
+    elapsed_time_per_iter = iteration_time/total_iterations
+    samples_per_second = batch_size / elapsed_time_per_iter
+
+    #flops calculator
+    hidden_size = args.hidden_size
+    num_layers = args.num_layers
+    vocab_size = args.padded_vocab_size
+
+    # General TFLOPs formula (borrowed from Equation 3 in Section 5.1 of
+    # https://arxiv.org/pdf/2104.04473.pdf).
+    # The factor of 4 is when used with activation check-pointing,
+    # otherwise it will be 3.
+    checkpoint_activations_factor = 3
+    if hasattr(args, 'checkpoint_activations') and args.checkpoint_activations:
+        checkpoint_activations_factor = 4
+    if hasattr(args, 'recompute_granularity') and (args.recompute_granularity == 'selective' or args.recompute_granularity == 'full'):
+        checkpoint_activations_factor = 4
+    seq_len = args.seq_length
+    if hasattr(args, 'actual_seq_length'):
+        seq_len = args.actual_seq_length
+    flops_per_iteration = (24 * checkpoint_activations_factor * batch_size * seq_len * num_layers * (hidden_size**2)) * (1. + (seq_len / (6. * hidden_size)) + (vocab_size / (16. * num_layers * hidden_size)))
+    tflops = flops_per_iteration / (elapsed_time_per_iter * args.world_size * (10**12))
+    return samples_per_second, tflops, approx_parameters_in_billions
