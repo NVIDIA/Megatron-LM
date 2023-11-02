8f35f8f85008006b391d993fb86631f28d28b108
jackal1586; sbmaruf <zarziskhan@gmail.com; sbmaruf@gmail.com>
2023-11-02 20:24:36 +0000

    add llama2tokenizer to argument

--- scripts/preprocess_data.sh
+++ scripts/preprocess_data.sh
@@ -1,17 +1,15 @@
 OUTPUT_PATH="/mnt/deva100/sbmaruf/OpenAlphaCodeAssets/indexed_data"
 mkdir -p $OUTPUT_PATH
 
-DATA_PATH="/mnt/deva100/sbmaruf/OpenAlphaCodeAssets/prompted_for_training_bigdata_v2/program_synthesis_codecontest.jsonl"
+DATA_PATH="/mnt/deva100/sbmaruf/OpenAlphaCodeAssets/prompted_for_training_bigdata_v2/chat_ml_*.jsonl"
 for _inp in $DATA_PATH
 do
-python tools/preprocess_data_sft.py \
+python tools/preprocess_data.py \
  --input $_inp \
- --json-keys 'inputs' 'targets' \
+ --json-keys 'prompted_sample' \
  --partitions 1 \
  --tokenizer-type 'Llama2Tokenizer' \
  --tokenizer-model '/mnt/deva100/sbmaruf/OpenAlphaCodeAssets/CodeLlama-7b-hf/tokenizer.model' \
- --worker 24 \
- --output-prefix $OUTPUT_PATH'/openalphacode_' \
- --chatml-template '{bos_token}[INST]{inputs}[/INST]\n{targets}\n{eos_token}' \
- --output-json-key 'prompted_sample'
+ --workers 24 \
+ --output-prefix $OUTPUT_PATH'/openalphacode_' 
 done
