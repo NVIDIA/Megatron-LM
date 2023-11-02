1dbc1862c66dba986e52a1073636a8e27f694457
jackal1586; sbmaruf <zarziskhan@gmail.com; sbmaruf@gmail.com>
2023-11-02 20:25:15 +0000

    rename file

--- scripts/preprocess_chatml.sh
+++ scripts/preprocess_chatml.sh
@@ -3,7 +3,7 @@ mkdir -p $OUTPUT_PATH
 
 DATA_PATH="/mnt/deva100/sbmaruf/OpenAlphaCodeAssets/prompted_for_training_bigdata_v2/*.jsonl"
 for _inp in $DATA_PATH; do
-    python tools/project_chatml.py \
+    python tools/preprocess_chatml.py \
     --input "$_inp" \
     --json-keys 'inputs' 'targets' \
     --tokenizer-type 'Llama2Tokenizer' \
