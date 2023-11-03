15d53f10c2344a079f32035dfd500b4c7a087c90
eric-weiss-zyphra <144280438+eric-weiss-zyphra@users.noreply.github.com>
2023-11-02 22:15:05 -0700

    Update pretrain_gpt_distributed.sh
    latest non-crashing global batch size

--- examples/pretrain_gpt_distributed.sh
+++ examples/pretrain_gpt_distributed.sh
@@ -12,31 +12,29 @@ NNODES=1
 NODE_RANK=0
 WORLD_SIZE=$(($GPUS_PER_NODE*$NNODES))
 
-CHECKPOINT_PATH=<Specify path>
-VOCAB_FILE=<Specify path to file>/gpt2-vocab.json
-MERGE_FILE=<Specify path to file>/gpt2-merges.txt
-DATA_PATH=<Specify path and file prefix>_text_document
+CHECKPOINT_PATH=/workspace/
+VOCAB_FILE=/workspace/Megatron-LM/data/gpt2-vocab.json
+MERGE_FILE=/workspace/Megatron-LM/data/gpt2-merges.txt
+DATA_PATH=/workspace/slimpajama/chunk1_text_sentence
 
 DISTRIBUTED_ARGS="
     --nproc_per_node $GPUS_PER_NODE \
     --nnodes $NNODES \
     --node_rank $NODE_RANK \
     --master_addr $MASTER_ADDR \
-    --master_port $MASTER_PORT
+    --master_port $MASTER_PORT \
 "
-
-# To train WITHOUT MoE, remove the argument line specifying --num-experts in GPT_ARGS
 
 GPT_ARGS="
     --num-layers 24 \
-    --hidden-size 1024 \
+    --hidden-size 2048 \
     --num-attention-heads 16 \
-    --seq-length 1024 \
-    --max-position-embeddings 1024 \
+    --seq-length 2048 \
+    --max-position-embeddings 2048 \
     --micro-batch-size 8 \
-    --global-batch-size 64 \
+    --global-batch-size 512 \
     --lr 0.00015 \
-    --train-iters 500000 \
+    --train-iters 500 \
     --lr-decay-iters 320000 \
     --lr-decay-style cosine \
     --min-lr 1.0e-5 \
@@ -44,15 +42,20 @@ GPT_ARGS="
     --lr-warmup-fraction .01 \
     --clip-grad 1.0 \
     --fp16 \
-    --num-experts 4
+    --num-experts 16 \
+    --expert-model-parallel-size 8 \
+    --use-distributed-optimizer \
+    --recompute-granularity selective \
+    --use-flash-attn
 "
-
 
 DATA_ARGS="
     --data-path $DATA_PATH \
     --vocab-file $VOCAB_FILE \
     --merge-file $MERGE_FILE \
-    --split 949,50,1
+    --split 949,50,1 \
+    --num-workers 0 \
+    --distributed-timeout-minutes 120
 "
 
 OUTPUT_ARGS="
@@ -62,7 +65,7 @@ OUTPUT_ARGS="
     --eval-iters 10
 "
 
-torchrun $DISTRIBUTED_ARGS pretrain_gpt.py \
+torchrun $DISTRIBUTED_ARGS /workspace/Megatron-LM/pretrain_gpt.py \
     $GPT_ARGS \
     $DATA_ARGS \
     $OUTPUT_ARGS \
