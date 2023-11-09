185e60b5ae377ff974903978b64513ad01a9736c
root <root@bb86837e38a4451696673716eac8596000000W.q4m1liplxpiujkf3nqwk5501jd.xx.internal.cloudapp.net>
2023-11-09 09:37:03 +0000

    update envs

---
+++ scripts/envs/env_vars.sh
@@ -0,0 +1,8 @@
+export DUMPED_FOLDER="../DUMPED"
+export RAW_DATA_FOLDER="../RAW_DATA_FOLDER"
+export CACHE_DATA_FOLDER="../cache"
+export TRANSFORMERS_CACHE="/cache/"
+
+mkdir -p $DUMPED_FOLDER
+mkdir -p $RAW_DATA_FOLDER
+mkdir -p $CACHE_DATA_FOLDER
