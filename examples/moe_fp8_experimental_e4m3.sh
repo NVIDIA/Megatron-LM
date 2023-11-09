efd0694762364b2ec05b5a0a992a7bdc9adedeb7
BerenMillidge <Bmillidgework@gmail.com>
2023-11-09 18:08:27 +0000

    fp8 e4m3 config

---
+++ examples/moe_fp8_experimental_e4m3.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+# Runs the "345M" parameter model
+
+export CUDA_DEVICE_MAX_CONNECTIONS=1
+
+GPUS_PER_NODE=8
+# Change for multinode config
+MASTER_ADDR=localhost
+MASTER_PORT=6000
+NNODES=1
+NODE_RANK=0
+WORLD_SIZE=$(($GPUS_PER_NODE*$NNODES))
+
+CHECKPOINT_PATH=/workspace/ckpts
+VOCAB_FILE=/datasets/SlimPajama-627B_megatron/gpt-neox-20b-tokenizer/vocab.json
+MERGE_FILE=/datasets/SlimPajama-627B_megatron/gpt-neox-20b-tokenizer/merges.txt
+DATA_PATH=/datasets/SlimPajama-627B_megatron/gpt-neox-20b-tokenizer/train_text_document
+
+WANDB_PROJECT=moe
+WANDB_EXP_NAME=moe_1p3b_16e_fp8_e4mc
+WANDB_SAVE_DIR=/workspace/wandb
+
+DISTRIBUTED_ARGS="
+    --nproc_per_node $GPUS_PER_NODE \
+    --nnodes $NNODES \
+    --node_rank $NODE_RANK \
+    --master_addr $MASTER_ADDR \
+    --master_port $MASTER_PORT \
+"
+
+GPT_ARGS="
+    --num-layers 24 \
+    --hidden-size 2048 \
+    --num-attention-heads 16 \
+    --seq-length 2048 \
+    --max-position-embeddings 2048 \
+    --micro-batch-size 8 \
+    --global-batch-size 960 \
+    --lr 0.00015 \
+    --train-iters 570000 \
+    --lr-decay-iters 450000 \
+    --lr-decay-style cosine \
+    --min-lr 1.0e-5 \
+    --weight-decay 0.0 \
+    --lr-warmup-fraction .01 \
+    --clip-grad 1.0 \
+    --fp16 \
+    --num-experts 16 \
+    --expert-model-parallel-size 8 \
+    --recompute-granularity selective \
+    --use-flash-attn \
+    --accumulate-allreduce-grads-in-fp32 \
+    --fp8-format e4m3 \
+    --transformer-impl transformer_engine 
+    --attention-dropout 0.0 \
+    --hidden-dropout 0.0
+    
+    "
+
+DATA_ARGS="
+    --data-path $DATA_PATH \
+    --vocab-file $VOCAB_FILE \
+    --merge-file $MERGE_FILE \
+    --split 949,50,1 \
+    --num-workers 0 \
+    --distributed-timeout-minutes 120
+"
+
+OUTPUT_ARGS="
+    --log-interval 10 \
+    --save-interval 500 \
+    --eval-interval 500 \
+    --eval-iters 10 \
+    --wandb-project $WANDB_PROJECT \
+    --wandb-exp-name $WANDB_EXP_NAME \
+    --wandb-save-dir $WANDB_SAVE_DIR
+"
+
+torchrun $DISTRIBUTED_ARGS /opt/Megatron-LM/pretrain_gpt.py \
+    $GPT_ARGS \
+    $DATA_ARGS \
+    $OUTPUT_ARGS \
+    --distributed-backend nccl \
+    --save $CHECKPOINT_PATH \
+    --load $CHECKPOINT_PATH
