72601a366afd51d71c58300ed9fae32455b0b197
root <root@bb86837e38a4451696673716eac8596000000W.q4m1liplxpiujkf3nqwk5501jd.xx.internal.cloudapp.net>
2023-11-09 09:40:17 +0000

    update envs

--- scripts/envs/env_vars.sh
+++ scripts/envs/env_vars.sh
@@ -6,3 +6,6 @@ export TRANSFORMERS_CACHE="/cache/"
 mkdir -p $DUMPED_FOLDER
 mkdir -p $RAW_DATA_FOLDER
 mkdir -p $CACHE_DATA_FOLDER
+
+git config --global credential.helper store
+huggingface-cli login --token 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
