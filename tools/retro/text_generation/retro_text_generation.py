577e77e44a1a964762b948e20449c80f76aba5ba
Boxin Wang <boxin.wbx@gmail.com>
2023-11-09 17:09:28 -0800

    Clean up code and comments (except example template bash files)

--- tools/retro/text_generation/retro_text_generation.py
+++ tools/retro/text_generation/retro_text_generation.py
@@ -1,24 +1,11 @@
-# coding=utf-8
-# Copyright (c) 2020, NVIDIA CORPORATION.  All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Copyright (c) 2023, NVIDIA CORPORATION. All rights reserved.
 
 """Sample Generate GPT"""
-import json
 import torch
 import os
 import sys
 from typing import Union
+
 sys.path.append(os.path.abspath(os.path.join(
     os.path.join(os.path.dirname(__file__), "../../../"))))
 from megatron import get_args, get_retro_args
@@ -28,7 +15,7 @@ from megatron.checkpointing import load_checkpoint
 from megatron.initialize import initialize_megatron
 from megatron.core.models.gpt import GPTModel
 from megatron.training import get_model
-from tools.retro.text_generation.retro_api import retro_generate_and_post_process, retro_beam_search_and_post_process
+from tools.retro.text_generation.retro_api import retro_generate_and_post_process
 from tools.retro.sft.sft_retro import get_tasks_args
 from tools.retro.sft.dataset_conv import reformat_prompt, preprocess, reformat_prompt_short
 import numpy as np
@@ -107,7 +94,6 @@ def pad_neighbours_for_query_only(args, nb_tokens, pad_id, ft_neighbours):
 
     for nb_token in valid_nb_tokens:
         if len(nb_token) >= r:
-            # print("max len is {}, and the current one is {}".format(args.r, len(nb_token)))
             nb_token = nb_token[:r]
         else:
             nb_token = nb_token + [pad_id] * (r - len(nb_token))
@@ -117,7 +103,7 @@ def pad_neighbours_for_query_only(args, nb_tokens, pad_id, ft_neighbours):
     print("args.retro_num_neighbors", args.retro_num_neighbors)
 
     if len(neighbours_tokens) < args.retro_num_neighbors:
-        assert ValueError("neighbours are not enough, to do: add empty ones and create mask for those empty ones")
+        assert ValueError("neighbours are not enough, add empty ones and create mask for those empty ones")
     neighbours_tokens = np.array(neighbours_tokens)
     return neighbours_tokens
 
@@ -155,12 +141,6 @@ def add_text_generate_args(parser):
                        help="Minimum factor by which each probability is multiplied")
     group.add_argument("--debug-gen", action='store_true',
                        help="If set, additional debugging output is printed to stdout")
-
-    # group.add_argument('--adaptor', action='store_true', default=False)
-    # group.add_argument('--project-size', type=int, default=256)
-    group.add_argument('--beam-search', action='store_true', help='activate beam search')
-    group.add_argument('--beam-size', type=int, default=5,
-                       help='beam size for beam search,')
     group.add_argument('--length-penalty', type=float, default=1.0,
                        help='length penalty')
     group.add_argument('--gen-start-idx', type=int, default=0,
@@ -186,18 +166,14 @@ def generate_samples_conditional(model):
     model.eval()
     if torch.distributed.get_rank() == 0:
 
-        # data = preprocess(args.sample_input_file, inference_only=True)
         data = preprocess(args.sample_input_file, inference_only=True,
                           retrieved_neighbours=args.use_retrieved_neighbours)
         print("total rows {}".format(len(data)))
-        all_data = data[args.gen_start_idx:]  ## start fron gen_start_idx
+        all_data = data[args.gen_start_idx:]  # start from gen_start_idx
         if args.num_gen > 0:
             all_data = all_data[:args.num_gen]
         input_count = len(all_data)
         input_pos = 0
-
-    if args.beam_search:
-        assert args.micro_batch_size == 1
 
     terminate_runs = 0
     while True:
@@ -215,86 +191,46 @@ def generate_samples_conditional(model):
                     sample = all_data[input_pos]
                 input_pos += 1
 
-                # valid_tasks = ['nq', 'tqa', 'benz', 'landrover', 'ford', 'att', 'iternal', 'carmanual', 'nvit', 'tcs', 'doc2dial', 'benefits']
-                # if args.task.lower() in valid_tasks or any([x in args.task.lower() for x in valid_tasks]):
                 if True:
                     max_target_len = args.out_seq_length
                     query, _, neighbours = sample
 
-                    # disable it for GPT for now
                     neighbours_array = pad_neighbours_for_query_only(args,
                                                                      [tokenizer.tokenize(neighbour) for neighbour in
                                                                       neighbours], tokenizer.eod, args.ft_neighbours)
-                    # print("neighbors", neighbours)
-                    # print("neighbours_array", neighbours_array)
                     print("neighbours_array.shape", neighbours_array.shape)
                     tokenizer = get_tokenizer()
 
                     if args.short_format:
-                        input_tokens = reformat_prompt_short(query, neighbours, args.task, args.ft_neighbours, max_target_len,
-                                                      tokenizer, args.seq_length)
+                        input_tokens = reformat_prompt_short(query, neighbours, args.task, args.ft_neighbours,
+                                                             max_target_len,
+                                                             tokenizer, args.seq_length)
                     else:
                         input_tokens = reformat_prompt(query, neighbours, args.task, args.ft_neighbours, max_target_len,
                                                        tokenizer, args.seq_length, template_id=args.template_id)
-                    # input_tokens = reformat_prompt_v1(query, neighbours, args.task, args.ft_neighbours, max_target_len, tokenizer, args.seq_length)
                     raw_text = tokenizer.detokenize(input_tokens)
                     print(raw_text)
-                    # if args.ft_neighbours > 0:
-                    # if args.shuffle_topn:
-                    #     import random
-                    #     random.seed(1234)
-                    #     random_neighbours = neighbours[0:args.ft_neighbours]
-                    #     random.shuffle(random_neighbours)
-                    #     neighbours = random_neighbours + neighbours[args.ft_neighbours:]
-                    # if args.add_retriever: ## should be reverse order or not
-                    #     raw_text = "\n".join(neighbours[0:args.ft_neighbours][::-1]) + "\n" + raw_text
-                    #     raw_text = tokenizer.detokenize(tokenizer.tokenize(raw_text)[-(args.seq_length - max_target_len):])
-                    # else:
-                    #     q_len = len(tokenizer.tokenize(raw_text))
-                    #     trun_neighbours = tokenizer.detokenize(tokenizer.tokenize("\n".join(neighbours[0:args.ft_neighbours]))[:(args.seq_length - max_target_len - q_len - 1)])
-                    #     raw_text = trun_neighbours + "\n" + raw_text
-                    ## to do: cut neighbours to max_len
                 else:
                     raise ValueError("invalid arg for task")
                 sentences.append(raw_text)
-                # n_arrays.append(neighbours_array)
-            # neighbours_array = np.array(n_arrays)
-            max_len = args.out_seq_length
             retro_args = get_retro_args()
-            if args.beam_search:
-                neighbours_array = neighbours_array.repeat(args.beam_size, axis=0)
-                resp_sentences, resp_sentences_seg, scores = \
-                    retro_beam_search_and_post_process(model, prompts=sentences,
-                                                       neighbours_array=neighbours_array,
-                                                       length_penalty=args.length_penalty,
-                                                       tokens_to_generate=args.seq_length - retro_args.retro_gpt_chunk_length,
-                                                       beam_size=args.beam_size,
-                                                       add_BOS=False)
-            else:
-                resp_sentences, resp_sentences_seg, scores, \
-                    tokens = retro_generate_and_post_process(model, prompts=sentences,
-                                                             neighbours_array=neighbours_array,
-                                                             tokens_to_generate=args.seq_length - retro_args.retro_gpt_chunk_length,
-                                                             return_output_log_probs=False,
-                                                             top_k_sampling=args.top_k,
-                                                             top_p_sampling=args.top_p,
-                                                             add_BOS=False,
-                                                             temperature=1.0)
-                # neighbours_array=neighbours_array, if retro
-            # print("len of tokens[0]", len(tokens[0]))
-            # print(resp_sentences_seg[0])
+
+            resp_sentences, resp_sentences_seg, scores, \
+                tokens = retro_generate_and_post_process(model, prompts=sentences,
+                                                         neighbours_array=neighbours_array,
+                                                         tokens_to_generate=args.seq_length - retro_args.retro_gpt_chunk_length,
+                                                         return_output_log_probs=False,
+                                                         top_k_sampling=args.top_k,
+                                                         top_p_sampling=args.top_p,
+                                                         add_BOS=False,
+                                                         temperature=1.0)
             print("len of resp_sentences", len(resp_sentences))
-            # print("len of scores", len(scores))
-            # print("scores", scores)
-            # exit(0)
             for prompt, generation in zip(sentences, resp_sentences):
-                # datum = generation[len(prompt):].replace("<|endoftext|>", "").strip()
                 datum = generation[len(prompt):]
                 print("prompt:", generation[:len(prompt)])
                 if "<|endoftext|>" in datum:
                     datum = datum[:datum.find("<|endoftext|>")].strip()
                 datum = datum.replace("\n", " ")
-                # print("len of tokens", len(token))
                 print("cont:", datum)
                 yield datum
             avg_time.append((time.time() - start) / args.global_batch_size)
@@ -304,10 +240,7 @@ def generate_samples_conditional(model):
                 print("finish all lines")
                 terminate_runs = 1
         else:
-            if args.beam_search:
-                retro_beam_search_and_post_process(model)
-            else:
-                retro_generate_and_post_process(model)
+            retro_generate_and_post_process(model)
 
         terminate_runs_tensor = torch.cuda.LongTensor([terminate_runs])
         torch.distributed.broadcast(terminate_runs_tensor, 0)
@@ -348,11 +281,9 @@ def main():
     model = model[0]
 
     # Generate samples.
-    if args.sample_input_file != None:
+    if args.sample_input_file is not None:
         print(f"{args.sample_input_file}")
         generate_and_write_samples_conditional(model)
-    else:
-        generate_and_write_samples_unconditional(model)
 
 
 if __name__ == "__main__":
