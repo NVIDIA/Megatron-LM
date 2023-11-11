4c469c3d111cf3e9013c12048a4b4d966b27c6ab
M Saiful Bari <sbmaruf@gmail.com>
2023-11-11 05:23:17 -0900

    update code

--- examples/pretrain-llama/data-processing/process_data_vocab_expanded_model.sh
+++ examples/pretrain-llama/data-processing/process_data_vocab_expanded_model.sh
@@ -3,12 +3,13 @@ source scripts/envs/env_vars.sh || true
 
 AZURE_TOK_SAS_TOKEN="https://provisioningte0256624006.blob.core.windows.net/sbmaruf/vocab_expansion_assets/expanded_models/Llama-2-7b-hf-ExpTok-32K_10M/tokenizer.model?sp=rl&st=2023-11-09T17:01:29Z&se=2024-12-31T01:01:29Z&spr=https&sv=2022-11-02&sr=c&sig=Wz6qUq94zBpmYXI66L809LaGnAjBoBvoF8dLIrxn6wY%3D"
 AZURE_DATA_SAS_TOKEN="https://provisioningte0256624006.blob.core.windows.net/noufs/allam_data_2-1_splits/*?sp=rl&st=2023-11-09T06:57:45Z&se=2024-12-31T14:57:45Z&spr=https&sv=2022-11-02&sr=c&sig=%2FWH5uOcWYMMX1JbcB%2FMcLC5NqC1Qy6rRxCzbieOG8FI%3D"
-DATA_PATH=$RAW_DATA_FOLDER'/allam_data_2-1_splits'
-# DATA_PATH=$RAW_DATA_FOLDER'/temp'
+DATA_PATH=$RAW_DATA_FOLDER'/allam_data_2-1_splits/'
 
 OUTPUT_PATH="$DUMPED_FOLDER/allam_data_2-1_splits-indexed_data/"
+BIN_IDX_FOLDER=$OUTPUT_PATH"/bin_idx"
 TOK_PATH="$OUTPUT_PATH/tokenizer/tokenizer.model"
 mkdir -p $OUTPUT_PATH
+mkdir -p $BIN_IDX_FOLDER
 
 if [ ! -d $DATA_PATH ]; then
     azcopy copy  $AZURE_DATA_SAS_TOKEN $DATA_PATH --overwrite=false --recursive
@@ -18,35 +19,10 @@ if [ ! -f $TOK_PATH ]; then
     azcopy copy  $AZURE_TOK_SAS_TOKEN $TOK_PATH --overwrite=false
 fi
 
-for _inp in $DATA_PATH/*.jsonl;
-do
-    echo 'Indexing '$_inp'...'
-    FILENAME=${_inp##*/}
-    FILENAME_WITHOUT_EXT=${FILENAME%.*}
-    TEMP_FOLDER=$DUMPED_FOLDER'/temp_bin_idx/temp_'$FILENAME_WITHOUT_EXT
-    INDEX_FOLDER=$OUTPUT_PATH'/bin_idx/'$FILENAME_WITHOUT_EXT
-    if [ -d $INDEX_FOLDER ]; then
-        echo '[SKIPPING] Already Folder Exists '$INDEX_FOLDER'!'
-        continue
-    fi
-    mkdir -p $TEMP_FOLDER
-    
-    echo 'Copying file from '$_inp' to '$TEMP_FOLDER
-    cp $_inp $TEMP_FOLDER
-
-    python tools/preprocess_data.py \
-    --input $TEMP_FOLDER/$FILENAME \
-    --json-keys 'text' \
-    --partitions 16 \
-    --tokenizer-type 'Llama2Tokenizer' \
-    --tokenizer-model $TOK_PATH \
-    --workers 128 \
-    --output-prefix $TEMP_FOLDER/$FILENAME_WITHOUT_EXT \
-    --keep-sequential-samples
-
-    mkdir -p $INDEX_FOLDER
-    mv $TEMP_FOLDER/$FILENAME_WITHOUT_EXT'_text_document.bin' $INDEX_FOLDER
-    mv $TEMP_FOLDER/$FILENAME_WITHOUT_EXT'_text_document.idx' $INDEX_FOLDER
-    rm -rf $TEMP_FOLDER/
-
-done
+python examples/pretrain-llama/data-processing/multiprocess_runner.py \
+ --glob-input-path "$DATA_PATH/*.jsonl" \
+ --output-folder $BIN_IDX_FOLDER \
+ --tokenizer-model $TOK_PATH \
+ --tokenizer-type 'Llama2Tokenizer' \
+ --per-file-workers 1 \
+ --num-file-workers 168
