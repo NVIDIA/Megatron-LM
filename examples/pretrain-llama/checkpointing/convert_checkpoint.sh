121019b3cb224e6376fbad537b33150fdb731410
M Saiful Bari <sbmaruf@gmail.com>
2023-11-14 21:40:27 +0000

    update script

---
+++ examples/pretrain-llama/checkpointing/convert_checkpoint.sh
@@ -0,0 +1,11 @@
+python tools/checkpoint/util.py \
+--load-dir "../DUMPED/Llama-2-7b-hf-ExpTok-32K_10M_tp2_pp2_seq4096_gb48/" \
+--save-dir "../DUMPED/tp1_pp1/" \
+--model-type "GPT" \
+--loader "megatron" \
+--saver "megatron" \
+--target-tensor-parallel-size 1 \
+--target-pipeline-parallel-size 1 \
+--no-checking \
+--max-queue-size 1 \
+--megatron-path "."
