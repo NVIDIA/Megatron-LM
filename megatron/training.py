15355af246ed3e745cf94627e14938506a661396
yuanwu2017 <yuan.wu@intel.com>
2023-11-14 22:43:43 +0800

    Enable the args.deepspeed_config to use dict type (#290)
    Signed-off-by: yuanwu <yuan.wu@intel.com>

--- megatron/training.py
+++ megatron/training.py
@@ -69,9 +69,12 @@ So, we use ds_config dict which is the more flexible option.
 '''
 def _create_ds_config_dict():
     args = get_args()
-    with open(args.deepspeed_config, 'r', encoding='utf-8') as config_file:
-        ds_config_dict = json.load(config_file)
-    
+    if isinstance(args.deepspeed_config, dict) :
+        ds_config_dict = args.deepspeed_config
+    else:
+        with open(args.deepspeed_config, 'r', encoding='utf-8') as config_file:
+            ds_config_dict = json.load(config_file)
+
     if args.universal_checkpoint:
         ds_config_dict["checkpoint"] = {"load_universal": True}
 
