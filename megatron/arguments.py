9b0f86e0abed3f5b90ae5d875e4912a9057466e1
Vijay Korthikanti <vkorthikanti@nvidia.com>
2023-11-14 19:25:52 -0800

    disallowing fp16 training with expert-parallelism

--- megatron/arguments.py
+++ megatron/arguments.py
@@ -390,7 +390,9 @@ def validate_args(args, defaults={}):
         assert args.num_experts % args.expert_model_parallel_size == 0, \
             "Number of experts should be a multiple of expert model parallel_size."
         assert not args.use_distributed_optimizer, \
-            "Expert parallelism is not suppored with distributed optimizer"
+            "Expert parallelism is not suppored with distributed optimizer."
+        assert not args.fp16, \
+            "Expert parallelism is not supported with fp16 training."
         if args.tensor_model_parallel_size > 1:
             assert args.sequence_parallel, \
                 "When using expert parallelism and tensor parallelism, sequence parallelism must be used."
