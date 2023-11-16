92c17603f5a04a0bf5586d5d5dc7d34b8b5273b7
SulRash <sultan.m.rashed@gmail.com>
2023-11-16 10:05:54 +0000

    Cleaned up detokenizer shell

--- examples/pretrain-llama/data-processing/detokenize/detokenize_check.sh
+++ examples/pretrain-llama/data-processing/detokenize/detokenize_check.sh
@@ -1,47 +1,46 @@
 #!/bin/bash
 set -e
 
-AR_FILE="allam_data_2-1_splits-llama2-indexed_data/llama2_bin_idx/ar_encyclopedias_split_00_text_document"
-AR_BIN="${AR_FILE}.bin"
-AR_IDX="${AR_FILE}.idx"
+ar_file="allam_data_2-1_splits-llama2-indexed_data/llama2_bin_idx/ar_encyclopedias_split_00_text_document"
+ar_bin="${ar_file}.bin"
+ar_idx="${ar_file}.idx"
 
-EN_FILE="allam_data_2-1_splits-llama2-indexed_data/llama2_bin_idx/en_books_books_split_02_text_document"
-EN_BIN="${EN_FILE}.bin"
-EN_IDX="${EN_FILE}.idx"
+en_file="allam_data_2-1_splits-llama2-indexed_data/llama2_bin_idx/en_books_books_split_02_text_document"
+en_bin="${en_file}.bin"
+en_idx="${en_file}.idx"
 
-OUTPUT_DATA="outputs/data"
-TOK_PATH="outputs/tokenizer"
-TOK_FILE="${TOK_PATH}/tokenizer.model"
-FINAL_OUTPUT="outputs/results"
+oputput_path="outputs/data"
+tok_path="outputs/tokenizer"
+tok_file="${tok_path}/tokenizer.model"
+final_output="outputs/results"
 
-if [ ! -d $OUTPUT_DATA ]; then
-    mkdir -p ${OUTPUT_DATA}
+if [ ! -d $oputput_path ]; then
+    mkdir -p ${oputput_path}
 fi
 
-if [ ! -d $TOK_PATH ]; then
-    mkdir -p ${TOK_PATH}
+if [ ! -d $tok_path ]; then
+    mkdir -p ${tok_path}
 fi
 
-if [ ! -d $FINAL_OUTPUT ]; then
-    mkdir -p ${FINAL_OUTPUT}
+if [ ! -d $final_output ]; then
+    mkdir -p ${final_output}
 fi
 
-if [ ! -f $TOK_FILE ]; then
-    AZURE_TOK="allam_data_2-1_splits-llama2-indexed_data/tokenizer/tokenizer.model"
-    AZURE_PATH="https://allamllmuksstandard.blob.core.windows.net/vocab-expanded-training-data/${AZURE_TOK}?sp=racwdl&st=2023-11-13T10:57:16Z&se=2025-04-17T18:57:16Z&spr=https&sv=2022-11-02&sr=c&sig=Gg2nHVydrJozaXGrcst0iF3mVgKkhRWO05Mt%2Bfp%2F69Y%3D"
-    azcopy copy "${AZURE_PATH}" "${TOK_FILE}"
+if [ ! -f $tok_file ]; then
+    azure_tok="allam_data_2-1_splits-llama2-indexed_data/tokenizer/tokenizer.model"
+    azure_path="https://allamllmuksstandard.blob.core.windows.net/vocab-expanded-training-data/${azure_tok}?sp=racwdl&st=2023-11-13T10:57:16Z&se=2025-04-17T18:57:16Z&spr=https&sv=2022-11-02&sr=c&sig=Gg2nHVydrJozaXGrcst0iF3mVgKkhRWO05Mt%2Bfp%2F69Y%3D"
+    azcopy copy "${azure_path}" "${tok_file}"
 fi
 
-FILE_PATHS=($AR_BIN $AR_IDX $EN_BIN $EN_IDX)
+file_paths=($ar_bin $ar_idx $en_bin $en_idx)
 
-for FILE_PATH in "${FILE_PATHS[@]}"
+for file_path in "${file_paths[@]}"
 do
-    OUTPUT_PATH="${OUTPUT_DATA}/${FILE_PATH}"
-    if [ ! -f $OUTPUT_PATH ]; then
-        AZURE_PATH="https://allamllmuksstandard.blob.core.windows.net/vocab-expanded-training-data/${FILE_PATH}?sp=racwdl&st=2023-11-13T10:57:16Z&se=2025-04-17T18:57:16Z&spr=https&sv=2022-11-02&sr=c&sig=Gg2nHVydrJozaXGrcst0iF3mVgKkhRWO05Mt%2Bfp%2F69Y%3D"
-        azcopy copy "${AZURE_PATH}" "${OUTPUT_PATH}"
+    output_path="${oputput_path}/${file_path}"
+    if [ ! -f $output_path ]; then
+        azure_path="https://allamllmuksstandard.blob.core.windows.net/vocab-expanded-training-data/${file_path}?sp=racwdl&st=2023-11-13T10:57:16Z&se=2025-04-17T18:57:16Z&spr=https&sv=2022-11-02&sr=c&sig=Gg2nHVydrJozaXGrcst0iF3mVgKkhRWO05Mt%2Bfp%2F69Y%3D"
+        azcopy copy "${azure_path}" "${output_path}"
     fi
 done
 
-python detokenize.py --tokenizer_model "${TOK_FILE}" --file_path "${OUTPUT_DATA}/${AR_FILE}" --output "${FINAL_OUTPUT}/ar_output.jsonl"
-python detokenize.py --tokenizer_model "${TOK_FILE}" --file_path "${OUTPUT_DATA}/${EN_FILE}" --output "${FINAL_OUTPUT}/en_output.jsonl"
+python yz_detokenize.py --tokenizer_path "${tok_file}" --source_prefix_path "${oputput_path}/${en_file}"
