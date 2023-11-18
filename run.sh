525902672cd0ff70d3be3c7f3f9721d88ee1d25a
M Saiful Bari <sbmaruf@gmail.com>
2023-11-18 10:05:44 +0000

    temporary run

---
+++ run.sh
@@ -0,0 +1,51 @@
+# DISTRIBUTED_ARGS=(
+#     --nproc_per_node 8
+#     --nnodes 1
+#     --master_addr "localhost"
+#     --master_port "12345"
+# )
+# torchrun ${DISTRIBUTED_ARGS[@]} pretrain_gpt.py \
+
+python pretrain_gpt.py \
+--tensor-model-parallel-size 2 \
+--pipeline-model-parallel-size 2 \
+--seq-length 4096 \
+--max-position-embeddings 4096 \
+--tokenizer-type "Llama2Tokenizer" \
+--tokenizer-model "../../cap/data-capability/wd/INPUT_tokenizer/tokenizer.model" \
+--load "../../cap/data-capability/wd/INPUT_load_initial_model/" \
+--exit-on-missing-checkpoint \
+--use-checkpoint-args \
+--no-initialization \
+--no-load-optim \
+--no-load-rng \
+--bf16 \
+--untie-embeddings-and-output-weights \
+--use-rotary-position-embeddings \
+--normalization "RMSNorm" \
+--no-position-embedding \
+--no-masked-softmax-fusion \
+--no-query-key-layer-scaling \
+--micro-batch-size 2 \
+--global-batch-size 32 \
+--train-iters 3000 \
+--lr 0.0003 \
+--lr-decay-style "cosine" \
+--min-lr 0.00003  \
+--lr-warmup-iters 20 \
+--weight-decay 0.1 \
+--clip-grad 1.0 \
+--save "../../cap/data-capability/wd/model_dir" \
+--data-path 1.0 "../../cap/data-capability/wd/INPUT_data/ar_books_split_00_text_document" \
+--data-cache-path "../../cap/data-capability/wd/cache/" \
+--split "9998,1,1" \
+--log-interval 10 \
+--log-validation-ppl-to-tensorboard \
+--save-interval 200 \
+--eval-interval 200 \
+--eval-iters 100 \
+--tensorboard-dir "../../cap/data-capability/wd/model_dir/tensorboard" \
+--tensorboard-log-interval 100 \
+--no-async-tensor-model-parallel-allreduce \
+--use-flash-attn 
+
