973330e9c3681604703bf1eb6b5a265d1b9b9b38
Jared Casper <jcasper@nvidia.com>
2023-11-17 19:36:43 -0800

    Update package version.

--- megatron/core/package_info.py
+++ megatron/core/package_info.py
@@ -4,7 +4,7 @@
 MAJOR = 0
 MINOR = 4
 PATCH = 0
-PRE_RELEASE = 'rc0'
+PRE_RELEASE = ''
 
 # Use the following formatting: (major, minor, patch, pre-release)
 VERSION = (MAJOR, MINOR, PATCH, PRE_RELEASE)
@@ -18,7 +18,7 @@ __contact_emails__ = 'nemo-toolkit@nvidia.com'  # use NeMo Email
 __homepage__ = (
     'https://docs.nvidia.com/deeplearning/nemo/user-guide/docs/en/stable/'  # use NeMo homepage
 )
-__repository_url__ = 'https://github.com/NVIDIA/Megatron-LM/megatron/core'
+__repository_url__ = 'https://github.com/NVIDIA/Megatron-LM/tree/core_v0.4.0/megatron/core'
 __download_url__ = 'https://github.com/NVIDIA/Megatron-LM/releases'
 __description__ = (
     'Megatron Core - a library for efficient and scalable training of transformer based models'
