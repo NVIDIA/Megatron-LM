b8c4f1704e19da98eec1a41a0a4be4cb69db6900
M Saiful Bari <skarim@sdaia.gov.sa>
2023-11-22 20:01:38 +0300

    update script

--- examples/pretrain-llama/training/llama_en_reasoning/data_ratio_prep.sh
+++ examples/pretrain-llama/training/llama_en_reasoning/data_ratio_prep.sh
@@ -1,6 +1,5 @@
-
 python examples/pretrain-llama/data-processing/data_ratio_from_file.py \
- --source-prefix-paths "../DUMPED/reasoning-llama2-indexed_data/*.bin" \
+ --prefix-paths-from-json "examples/pretrain-llama/training/llama_en_reasoning/en_reasoning_and_arabic_files.json" \
  --domain-ratio-from-json "examples/pretrain-llama/training/llama_en_reasoning/data_ratio.json" \
  --lang-select-prob-json "examples/pretrain-llama/training/llama_en_reasoning/lang_prob.json" \
  --total-token 500_000_000_000 \
