67caf5e01174e50f4d37004b86414474310af616
M Saiful Bari <sbmaruf@gmail.com>
2023-11-26 16:22:26 +0000

    update batch tokenize

--- examples/pretrain-llama/data-processing/tokenize/remote_az_batch_tokenize.sh
+++ examples/pretrain-llama/data-processing/tokenize/remote_az_batch_tokenize.sh
@@ -11,20 +11,20 @@ NUM_PROC=$6
 LOG_INTERVAL=$7
 SAS_TOKEN=$8
 
-# # echo "azcopy copy \"$AZ_TOK_MODEL\?$SAS_TOKEN\" \".\"" >> out.txt
-# azcopy copy "$AZ_TOK_MODEL?$SAS_TOKEN" "."
+# echo "azcopy copy \"$AZ_TOK_MODEL\?$SAS_TOKEN\" \".\"" >> out.txt
+azcopy copy "$AZ_TOK_MODEL?$SAS_TOKEN" "."
 
-# # echo "azcopy copy \"$AZ_INPUT_FOLDER/$SHARD_NAME?$SAS_TOKEN\" \".\"" >> out.txt
-# azcopy copy "$AZ_INPUT_FOLDER/$SHARD_NAME?$SAS_TOKEN" "."
+# echo "azcopy copy \"$AZ_INPUT_FOLDER/$SHARD_NAME?$SAS_TOKEN\" \".\"" >> out.txt
+azcopy copy "$AZ_INPUT_FOLDER/$SHARD_NAME?$SAS_TOKEN" "."
 
-# python tools/preprocess_data.py \
-# --input $SHARD_NAME \
-# --output-prefix $SHARD_NAME_WITHOUT_EXT \
-# --tokenizer-type $TOK_TYPE \
-# --tokenizer-model $TOK_NAME \
-# --workers $NUM_PROC \
-# --append-eod \
-# --log-interval $LOG_INTERVAL
+python tools/preprocess_data.py \
+--input $SHARD_NAME \
+--output-prefix $SHARD_NAME_WITHOUT_EXT \
+--tokenizer-type $TOK_TYPE \
+--tokenizer-model $TOK_NAME \
+--workers $NUM_PROC \
+--append-eod \
+--log-interval $LOG_INTERVAL
 
 # echo "azcopy copy $SHARD_NAME_WITHOUT_EXT\"_text_document.bin\"  \"$AZ_OUTPUT_FOLDER?$SAS_TOKEN\""  >> out.txt
 azcopy copy $SHARD_NAME_WITHOUT_EXT"_text_document.bin"  "$AZ_OUTPUT_FOLDER?$SAS_TOKEN"
