42bd7b23d614e06e1f2465b37cc08513f1afddb2
M Saiful Bari <sbmaruf@gmail.com>
2023-11-26 17:17:14 +0000

    re-org script with az config

--- examples/pretrain-llama/data-processing/merge_shard/merge_shard.py
+++ examples/pretrain-llama/data-processing/merge_shard/merge_shard.py
@@ -7,21 +7,44 @@ import subprocess
 
 def get_args():
     parser = argparse.ArgumentParser(description="Azure batch job submission for tokenization")
-    parser.add_argument("--az-subscription", type = str, help="Azure subscription id.", required=True)
-    parser.add_argument("--az-resource-group", type = str, help="Azure subscription id.", required=True)
-    parser.add_argument("--az-workspace-name", type = str, help="Azure subscription id.", required=True)
-    parser.add_argument("--az-blob-input-folder", type = str, help="Azure blob folder path.", required=True)
-    parser.add_argument("--az-blob-output-folder-path", type = str, help="Azure blob folder path to output folder for bin and idx", required=True)
-    parser.add_argument("--az-sas-token", type = str, help="Azure blob SAS token", required=True)
+    group = parser.add_argument_group(title='Azure login params.')
+    group.add_argument("--az-subscription", default=None, type = str, help="Azure subscription id.")
+    group.add_argument("--az-resource-group", default=None, type = str, help="Azure subscription id.")
+    group.add_argument("--az-workspace-name", default=None, type = str, help="Azure subscription id.")
+    group.add_argument("--az-sas-token", default=None, type = str, help="Azure blob SAS token")
+    group.add_argument("--az-sample-yaml-job-file", default=None, type = str, help="Path to a sample job file.")
+    group.add_argument("--az-configs", default=None, type = str, help="Path to a sample job file.")
+    
+    parser.add_argument("--input-folder-path", type = str, help="Azure blob folder path.", required=True)
+    parser.add_argument("--output-folder-path", type = str, help="Azure blob folder path to output folder for bin and idx", required=True)
     parser.add_argument("--shard-size", type = int, help="Size of the each merged shard.", required=True)
-    parser.add_argument("--sample-yaml-job-file", type = str, help="Path to a sample job file.", required=True)
     parser.add_argument("--prefix-name", type = str, help="Prefix of the output file name.", required=True)
-    parser.add_argument("--dry-run", action='store_true', help="Simulate run before submitting jobs.")
-    args = parser.parse_args()
+
+    group = parser.add_argument_group(title='Misc. params.')
+    parser.add_argument("--compute-target", type = str, default='azure', choices=['local', 'azure'], help="Conpute targets.")
+    group.add_argument("--dry-run", action='store_true', help="Simulate run before submitting jobs.")
+
+    if args.az_configs is not None:
+        args.az_configs = json.load(open(args.az_configs))
+        if args.az_subscription is not None:
+            print("Overloading config args from  --az-subscription")
+            args.az_configs['az-subscription'] = args.az_subscription
+        if args.az_resource_group is not None:
+            print("Overloading config args from  --az-resource-group")
+            args.az_configs['az-resource-group'] = args.az_resource_group
+        if args.az_workspace_name is not None:
+            print("Overloading config args from  --az-workspace-name")
+            args.az_configs['az-workspace-name'] = args.az_workspace_name
+        if args.az_sas_token is not None:
+            print("Overloading config args from  --az-sas-token")
+            args.az_configs['az-sas-token'] = args.az_sas_token
+        if args.az_sample_yaml_job_file is not None:
+            print("Overloading config args from  --az-sample-yaml-job-file")
+            args.az_configs['az-sample-yaml-job-file'] = args.az_sample_yaml_job_file
     return args
 
 def azcopy_list(path, sas_token):
-    shards = []
+    shard_dict = {}
     path_with_sas_token = f"{path}?{sas_token}"
     cmd = f"azcopy list \"{path_with_sas_token}\" --output-type json --machine-readable"
     out = subprocess.check_output(cmd, shell=True)
@@ -33,19 +56,14 @@ def azcopy_list(path, sas_token):
         if message.strip() == "": continue
         pre_shard_name = message.split(";")[0].replace("INFO: ", "")
         pre_shard_size = int(message.split(";")[1].replace("Content Length: ", ""))
-        shards.append({
-            'name': pre_shard_name,
-            'size_in_byte': pre_shard_size
-        })
-    shards = sorted(shards, key=lambda x: x['name'])
-    return shards
+        shard_dict[pre_shard_name] = pre_shard_size
+    return shard_dict
             
-def group_shards(shards, shard_size_limit):
+def group_shards(shard_dict, shard_size_limit):
     groups, curr_group, curr_group_size = [], [], 0
-    for dt in shards:
-        shard_name = dt['name']
-        assert dt['size_in_byte'] < shard_size_limit
-        curr_group_size += dt['size_in_byte']
+    for shard_name, size_in_byte in  shard_dict.items():
+        assert size_in_byte < shard_size_limit
+        curr_group_size += size_in_byte
         if curr_group_size > shard_size_limit:
             groups.append((curr_group_size,copy.deepcopy(curr_group)))
             curr_group = []
@@ -54,42 +72,71 @@ def group_shards(shards, shard_size_limit):
     if len(curr_group) > 0: groups.append((curr_group_size, curr_group))
     return groups
 
-def write_jobs(args, groups, script_path):
+def azure_submit_jobs(args, groups, script_path):
     output_folder = os.path.dirname(script_path)
     with open(args.sample_yaml_job_file) as fileptr:
         data = yaml.safe_load(fileptr)
-    
+    sas_token = args.az_configs['az-sas-token']
     prefix_command = f"""bash examples/pretrain-llama/data-processing/merge_shard/remote_merge_shard.sh \\
-\"{args.az_blob_input_folder}\" \\
-\"{args.az_blob_output_folder_path}\" \\
-\"{args.az_sas_token}\" \\
+\"{args.input_folder_path}\" \\
+\"{args.output_folder_path}\" \\
+\"{sas_token}\" \\
 \"{args.prefix_name}"""
     for idx, (size, shards) in enumerate(groups):
         command = prefix_command
         command += f"{idx:03}.jsonl\""
         for shard in shards:
             command += f" \\\n\"{shard}\""
-        # print(command)
-        if args.dry_run:
-            # print(f"RUN {size}: {command}")
-            pass
-        else:
+        if not args.dry_run:
             data['command'] = command
             az_yaml_file = os.path.join(output_folder, 'output.yaml')
             with open(az_yaml_file, 'w') as wrt_ptr:
                 yaml.dump(data, wrt_ptr, default_flow_style=False)
-            cmd = f"az ml job create --subscription {args.az_subscription} --resource-group {args.az_resource_group} --workspace-name {args.az_workspace_name} --file {az_yaml_file}"
+            cmd = f"az ml job create --subscription {args.az_configs['az-subscription']} --resource-group {args.az_configs['az-resource-group']} --workspace-name {args.az_configs['az-workspace-name']} --file {args.az_configs['az-sample-yaml-job-file']}"
             subprocess.check_output(cmd, shell=True)
-    for idx, (size, _) in enumerate(groups):
         print(f"[{args.prefix_name}][{idx}] {size/1000000000}GB")
 
+def local_submit_job(args, groups, script_path):
+    output_folder = os.path.dirname(script_path)
+    with open(args.sample_yaml_job_file) as fileptr:
+        data = yaml.safe_load(fileptr)
+    sas_token = args.az_configs['az-sas-token']
+    prefix_command = f"""bash examples/pretrain-llama/data-processing/merge_shard/remote_merge_shard.sh \\
+\"{args.input_folder_path}\" \\
+\"{args.output_folder_path}\" \\
+\"{sas_token}\" \\
+\"{args.prefix_name}"""
+    for idx, (size, shards) in enumerate(groups):
+        command = prefix_command
+        command += f"{idx:03}.jsonl\""
+        for shard in shards:
+            command += f" \\\n\"{shard}\""
+        if not args.dry_run:
+            subprocess.check_output(command, shell=True)
+        print(f"[{args.prefix_name}][{idx}] {size/1000000000}GB")
 
+def submit_jobs(args, groups, script_path):
+    if args.compute_target == "local":
+        raise NotImplementedError()
+    elif args.compute_target == "azure":
+        return azure_submit_jobs(args, groups, script_path)
+    else:
+        raise NotImplementedError()
+
+def list_shard_info(args, shard_folder):
+    if args.compute_target == "local":
+        raise NotImplementedError()
+    elif args.compute_target == "azure":
+        return azcopy_list(shard_folder, args.az_configs['az-sas-token'])
+    else:
+        raise NotImplementedError()
+    
 if __name__ == "__main__":
     script_path = os.path.abspath(__file__)
     args =  get_args()
-    shards = azcopy_list(args.az_blob_input_folder, args.az_sas_token)
-    groups = group_shards(shards, args.shard_size)
-    write_jobs(args, groups, script_path)
+    shard_dict = list_shard_info(args, args.input_folder_path)
+    groups = group_shards(shard_dict, args.shard_size)
+    submit_jobs(args, groups, script_path)
 
 
     
--- examples/pretrain-llama/data-processing/tokenize/az_batch_tokenize.sh
+++ examples/pretrain-llama/data-processing/tokenize/az_batch_tokenize.sh
@@ -1,25 +1,14 @@
-AZ_SUBS="c7209a17-0d9f-41df-8e45-e0172343698d"
-AZ_RESOURCE_GROUP="LLM-Test"
-AZ_WORKSPACE="Provisioning-Test"
+# TOK_MODEL="https://allamllmuksstandard.blob.core.windows.net/vocab-expanded-training-data/allam_data_2-1_splits-llama2-indexed_data/tokenizer/tokenizer.model"
+# TOK_TYPE="Llama2Tokenizer"
+# INPUT_FOLDER_PATH="https://allamllmuksstandard.blob.core.windows.net/vocab-expanded-training-data/processed/merged_shards"
+# BIN_IDX_PATH="https://allamllmuksstandard.blob.core.windows.net/vocab-expanded-training-data/allam_data_2-1_splits-llama2-indexed_data/llama2_bin_idx_trans/"
+# AZJOB_FILE="examples/pretrain-llama/data-processing/tokenize/tokenize.yaml"
+# MATCHING_PREFIX_NAME="ar_translated_"
 
-SAS_TOKEN="sp=racwdli&st=2023-11-26T15:25:43Z&se=2023-11-26T23:25:43Z&spr=https&sv=2022-11-02&sr=c&sig=GeRcxL%2Bvl%2FCyis3P0Gmu8qFJY8jLdu5zyswbDfNPKb4%3D"
-AZJOB_FILE="examples/pretrain-llama/data-processing/merge_shard/template_merge_shard.yaml"
-TOK_MODEL="https://allamllmuksstandard.blob.core.windows.net/vocab-expanded-training-data/allam_data_2-1_splits-llama2-indexed_data/tokenizer/tokenizer.model"
-TOK_TYPE="Llama2Tokenizer"
-INPUT_FOLDER_PATH="https://allamllmuksstandard.blob.core.windows.net/vocab-expanded-training-data/processed/merged_shards"
-BIN_IDX_PATH="https://allamllmuksstandard.blob.core.windows.net/vocab-expanded-training-data/allam_data_2-1_splits-llama2-indexed_data/llama2_bin_idx_trans/"
-AZJOB_FILE="examples/pretrain-llama/data-processing/tokenize/tokenize.yaml"
-MATCHING_PREFIX_NAME="ar_translated_"
-
-python examples/pretrain-llama/data-processing/tokenize/az_batch_tokenize.py \
---az-subscription "$AZ_SUBS" \
---az-resource-group "$AZ_RESOURCE_GROUP" \
---az-workspace-name "$AZ_WORKSPACE" \
---input-folder-path "$INPUT_FOLDER_PATH" \
---bin-idx-folder-path "$BIN_IDX_PATH" \
---tokenizer-model "$TOK_MODEL" \
---tokenizer-type $TOK_TYPE \
---az-sas-token "$SAS_TOKEN" \
---az-sample-yaml-job-file "$AZJOB_FILE" \
---az-configs "examples/pretrain-llama/data-processing/tokenize/azure_login_configs.json" \
---num-proc 16
+# python examples/pretrain-llama/data-processing/tokenize/az_batch_tokenize.py \
+# --input-folder-path "$INPUT_FOLDER_PATH" \
+# --bin-idx-folder-path "$BIN_IDX_PATH" \
+# --tokenizer-model "$TOK_MODEL" \
+# --tokenizer-type $TOK_TYPE \
+# --az-configs "examples/pretrain-llama/data-processing/tokenize/azure_login_configs.json" \
+# --num-proc 16
