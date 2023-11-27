94a0943fd7d3fdc87dd5cc2b3dbf413442ddc793
John Kamalu <jkamalu@nvidia.com>
2023-11-27 13:09:01 -0800

    Allow non core Retro SFT to use core BlendedDataset

--- tools/retro/sft/sft_gpt_dataset.py
+++ tools/retro/sft/sft_gpt_dataset.py
@@ -1,159 +1,91 @@
 # Copyright (c) 2023, NVIDIA CORPORATION. All rights reserved.
 
 """GPT style dataset."""
+from types import SimpleNamespace
 
 from megatron import print_rank_0, get_args
-from megatron.data.blendable_dataset import BlendableDataset
+from megatron.core import mpu
+from megatron.core.datasets.blended_megatron_dataset_builder import BlendedMegatronDatasetBuilder
+from megatron.core.datasets.blended_dataset import BlendedDataset
 from megatron.data.dataset_utils import get_datasets_weights_and_num_samples
 from tools.retro.sft.dataset_conv import FtDataset as SFTDataset
 from tools.retro.sft.dataset_conv import get_processed_dataset
 
 
-def build_train_valid_test_datasets(data_prefix, splits_string,
-                                    train_valid_test_num_samples,
-                                    seq_length, seed, skip_warmup,
-                                    train_data_prefix=None,
-                                    valid_data_prefix=None,
-                                    test_data_prefix=None,
-                                    return_doc_ids=False):
+MEGATRON_CORE_DUMMY_CONFIG = SimpleNamespace(
+    is_built_on_rank = lambda: mpu.get_tensor_model_parallel_rank() == 0,
+    path_to_cache = getattr(get_args(), "data_cache_path")
+)
+
+
+def build_train_valid_test_datasets(data_prefix, seq_length):
     """Build train, valid, and test datasets."""
 
-    if data_prefix:
-        print_rank_0("Single data path provided for train, valid & test")
-
-        # Single dataset.
-        if len(data_prefix) == 1:
-            return _build_train_valid_test_datasets(data_prefix[0],
-                                                    splits_string,
-                                                    train_valid_test_num_samples,
-                                                    seq_length, seed, skip_warmup)
-
-        # Blending dataset.
-        # Parse the values.
-        output = get_datasets_weights_and_num_samples(data_prefix,
-                                                      train_valid_test_num_samples)
-        prefixes, weights, datasets_train_valid_test_num_samples = output
-
-        # Build individual datasets.
-        train_datasets = []
-        valid_datasets = []
-        test_datasets = []
-
-        train_size = 0
-        valid_size = 0
-        test_size = 0
-
-        for i in range(len(prefixes)):
-            train_ds, valid_ds, test_ds = _build_train_valid_test_datasets(
-                prefixes[i], splits_string,
-                datasets_train_valid_test_num_samples[i],
-                seq_length, seed, skip_warmup,
-                return_doc_ids)
-            if train_ds:
-                train_datasets.append(train_ds)
-                train_size += len(train_ds)
-            if valid_ds:
-                valid_datasets.append(valid_ds)
-                valid_size += len(valid_ds)
-            if test_ds:
-                test_datasets.append(test_ds)
-                test_size += len(test_ds)
-
-        # Blend.
-        blending_train_dataset = None
-        if train_datasets:
-            blending_train_dataset = BlendableDataset(train_datasets, weights, train_size)
-        blending_valid_dataset = None
-        if valid_datasets:
-            blending_valid_dataset = BlendableDataset(valid_datasets, weights, valid_size)
-        blending_test_dataset = None
-        if test_datasets:
-            blending_test_dataset = BlendableDataset(test_datasets, weights, test_size)
-
-        return (blending_train_dataset, blending_valid_dataset,
-                blending_test_dataset)
-
-    else:
-        print_rank_0("Separate data paths provided for train, valid & test. Split string will be ignored.")
-
-        train_dataset, valid_dataset, test_dataset = None, None, None
-        # Single dataset.
-        if train_data_prefix is not None:
-            train_dataset = build_dataset("train", train_data_prefix,
-                                          train_valid_test_num_samples[0],
-                                          seq_length, seed, skip_warmup)
-
-        if valid_data_prefix is not None:
-            valid_dataset = build_dataset("valid", valid_data_prefix,
-                                          train_valid_test_num_samples[1],
-                                          seq_length, seed, False)
-
-        if test_data_prefix is not None:
-            test_dataset = build_dataset("test", test_data_prefix,
-                                         train_valid_test_num_samples[2],
-                                         seq_length, seed, False)
-
-        return (train_dataset, valid_dataset, test_dataset)
-
-
-def _build_train_valid_test_datasets(data_prefix, splits_string,
-                                     train_valid_test_num_samples,
-                                     seq_length, seed, skip_warmup,
-                                     return_doc_ids=False):
-    """Build train, valid, and xtest datasets using existing split"""
+    assert data_prefix
 
     args = get_args()
-    # Indexed dataset.
-    indexed_dataset = get_processed_dataset(data_prefix, args.data_folder)
 
-    train_dataset = SFTDataset(data_prefix, indexed_dataset["train"], seq_length)
-    valid_dataset = SFTDataset(data_prefix, indexed_dataset["valid"], seq_length)
-    test_dataset = SFTDataset(data_prefix, indexed_dataset["test"], seq_length)
-    return (train_dataset, valid_dataset, test_dataset)
-
-
-def build_dataset(dataset_name, data_prefix, num_samples,
-                  seq_length, seed, skip_warmup):
-    dataset = None
     if len(data_prefix) == 1:
-        dataset = _build_dataset(dataset_name,
-                        data_prefix[0],
-                        num_samples, seq_length,
-                        seed, skip_warmup)
-    else:
-        # Blending dataset.
-        # Parse the values.
-        output = get_datasets_weights_and_num_samples(data_prefix, num_samples)
-        prefixes, weights, dataset_num_samples = output
+        processed_datasets = get_processed_dataset(data_prefix[0], args.data_folder)
 
-        # Build individual datasets.
-        datasets = []
-        for i in range(len(prefixes)):
-            ds = _build_dataset(dataset_name, prefixes[i],
-                            dataset_num_samples[i],
-                            seq_length, seed, skip_warmup)
-            if ds:
-                datasets.append(ds)
+        train_ds = SFTDataset(prefixes[i], processed_datasets["train"], seq_length)
+        valid_ds = SFTDataset(prefixes[i], processed_datasets["valid"], seq_length)
+        test_ds = SFTDataset(prefixes[i], processed_datasets["test"], seq_length)
 
-        if datasets:
-            dataset = BlendableDataset(datasets, weights)
+        return train_ds, valid_ds, test_ds
 
-    return dataset
+    prefixes, weights, _ = get_datasets_weights_and_num_samples(data_prefix, train_valid_test_num_samples=0)
+    train_datasets, valid_datasets, test_datasets = [], [], []
+    train_size, valid_size, test_size = 0, 0, 0
 
+    for i in range(len(prefixes)):
+        processed_datasets = get_processed_dataset(prefixes[i], args.data_folder)
 
-def _build_dataset(dataset_name, data_prefix,
-                   num_samples, seq_length, seed, skip_warmup):
-    """
-    Build dataset. This method is called when individual
-    train, valid, test datasets are provided
-    """
+        train_ds = SFTDataset(prefixes[i], processed_datasets["train"], seq_length)
+        valid_ds = SFTDataset(prefixes[i], processed_datasets["valid"], seq_length)
+        test_ds = SFTDataset(prefixes[i], processed_datasets["test"], seq_length)
 
-    args = get_args()
-    # Indexed dataset.
-    indexed_dataset = get_processed_dataset(data_prefix, args.data_folder)
+        if train_ds:
+            train_datasets.append(train_ds)
+            train_size += len(train_ds)
+        if valid_ds:
+            valid_datasets.append(valid_ds)
+            valid_size += len(valid_ds)
+        if test_ds:
+            test_datasets.append(test_ds)
+            test_size += len(test_ds)
 
-    dataset = SFTDataset(data_prefix, indexed_dataset[dataset_name], seq_length)
+    # Blend
+    blending_train_dataset = None
+    if train_datasets:
+        blending_train_dataset = BlendedMegatronDatasetBuilder.build_generic_dataset(
+            BlendedDataset,
+            getattr(MEGATRON_CORE_DUMMY_CONFIG, "is_built_on_rank"),
+            train_datasets,
+            weights,
+            train_size,
+            MEGATRON_CORE_DUMMY_CONFIG,
+        )
+    blending_valid_dataset = None
+    if valid_datasets:
+        blending_valid_dataset = BlendedMegatronDatasetBuilder.build_generic_dataset(
+            BlendedDataset,
+            getattr(MEGATRON_CORE_DUMMY_CONFIG, "is_built_on_rank"),
+            valid_datasets,
+            weights,
+            valid_size,
+            MEGATRON_CORE_DUMMY_CONFIG,
+        )
+    blending_test_dataset = None
+    if test_datasets:
+        blending_test_dataset = BlendedMegatronDatasetBuilder.build_generic_dataset(
+            BlendedDataset,
+            getattr(MEGATRON_CORE_DUMMY_CONFIG, "is_built_on_rank"),
+            test_datasets,
+            weights,
+            test_size,
+            MEGATRON_CORE_DUMMY_CONFIG,
+        )
 
-    return dataset
-
-
+    return (blending_train_dataset, blending_valid_dataset,
+            blending_test_dataset)
