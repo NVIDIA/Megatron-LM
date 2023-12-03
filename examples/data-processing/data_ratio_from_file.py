885a58b46ce87ceadcca2cbe1d6540d5813723ca
M Saiful bari <sbamruf@gmail.com>
2023-12-03 02:43:53 +0000

    update script for automated upload.

--- examples/data-processing/data_ratio_from_file.py
+++ examples/data-processing/data_ratio_from_file.py
@@ -20,7 +20,7 @@ def get_args():
                        help='Total token to be sampled.')
     parser.add_argument('--verbose', action='store_true',
                        help='Print additional information')
-    parser.add_argument('--export-for-script', type=str,
+    parser.add_argument('--export-script', type=str,
                        help='Export output to this file for running it in megatron format atgument.')
     parser.add_argument('--prefix-for-file-path', type=str,
                        help='Add additional prefix to the file path.')
@@ -29,9 +29,15 @@ def get_args():
         assert args.prefix_paths_from_json is None
     if args.prefix_paths_from_json is not None:
         assert args.source_prefix_paths is None
-    if args.prefix_for_file_path.endswith("/"):
-        args.prefix_for_file_path = args.prefix_for_file_path[:-1]
+    if args.prefix_for_file_path is not None:
+        if args.prefix_for_file_path.endswith("/"):
+            args.prefix_for_file_path = args.prefix_for_file_path[:-1]
     return args
+
+def normalize(in_json):
+    for k, v in in_json.items():
+        in_json[k] = v/sum(in_json.values())
+    return in_json
 
 if __name__ == "__main__":
     args = get_args()
@@ -40,7 +46,7 @@ if __name__ == "__main__":
     else:
         source_prefix_paths = json.load(open(args.prefix_paths_from_json))
     domain_dict = json.load(open(args.domain_ratio_from_json))
-    lang_prob_dict = json.load(open(args.lang_select_prob_json))
+    lang_prob_dict = normalize(json.load(open(args.lang_select_prob_json)))
     exclude_iterator_list = json.load(open(args.exclude_iterator_json))['exclude_iterator_name']
 
 
@@ -97,11 +103,10 @@ if __name__ == "__main__":
         print(f"\t\t{lang}:{token:_}")
     print("\n")
     lang_token = {k:0 for k, _ in lang_prob_dict.items()}
-    if args.output_for_script is not None:
-        if not args.output_for_script.endswish(".sh"): args.output_for_script = args.output_for_script + ".sh"
-        out_file_ptr = open(f"{args.output_for_script}", "w")
-        out_file_ptr.write("""
-        DATA_PATH=( --data-path """)
+    if args.export_script is not None:
+        if not args.export_script.endswith(".sh"): args.export_script = args.export_script + ".sh"
+        out_file_ptr = open(f"{args.export_script}", "w")
+        out_file_ptr.write("_DATA_PATH=( --data-path ")
     for prob, iterator_name, total_token_to_be_sampled, total_token_exists in iterator_selection_prob:
         lang = iterator_name.split("_")[0]
         lang_token[lang] += total_token_to_be_sampled
@@ -110,9 +115,9 @@ if __name__ == "__main__":
         else:
             __output_format = os.path.basename(iterator_name).replace('=', '\\=')
             print(f"{prob} {args.prefix_for_file_path}/{__output_format}")
-            if args.output_for_script is not None:
-                out_file_ptr.write(f"\n{prob} {args.prefix_for_file_path}/{__output_format}\n")
-    if args.output_for_script is not None:
-        out_file_ptr.write("\n)\nexport DATA_PATH\n")
+            if args.export_script is not None:
+                out_file_ptr.write(f"\n{prob} {args.prefix_for_file_path}/{__output_format}")
+    if args.export_script is not None:
+        out_file_ptr.write("\n)\nexport DATA_PATH=${_DATA_PATH[@]}")
         out_file_ptr.close()
     print(f"\n\nOut of {args.total_token} token, language wise token distribution.\n{json.dumps(lang_token, indent=4)}")
