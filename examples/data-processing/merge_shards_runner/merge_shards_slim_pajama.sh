6b52e6f9ef6a802bbd3de38d81e070740f68ef77
M Saiful bari <sbamruf@gmail.com>
2023-12-04 03:38:16 +0000

    SlimPajama scripts

--- examples/data-processing/merge_shards_runner/merge_shards_slim_pajama.sh
+++ examples/data-processing/merge_shards_runner/merge_shards_slim_pajama.sh
@@ -5,15 +5,25 @@ bash examples/data-processing/merge_shards_runner/slim_pajama_process.sh
 
 azcopy copy "https://allamllmuksstandard.blob.core.windows.net/vocab-expanded-training-data/SlimPajama-627B/export/?sv=2023-01-03&ss=btqf&srt=sco&st=2023-12-02T21%3A07%3A05Z&se=2023-12-03T21%3A07%3A05Z&sp=rwdxftlacup&sig=xaz92d%2B4V7bJO8jcDhm1BbybLMsA4j%2FL6S0XO4XJgoA%3D" ../RAW_DATA_FOLDER/SlimPajama-627B/ --recursive --overwrite=false
 
-mkdir -p "../RAW_DATA_FOLDER/SlimPajama-627B/data_by_domain"
+SLIM_PAJAMA_ROOT="../RAW_DATA_FOLDER/SlimPajama-627B"
+
+mkdir -p "$SLIM_PAJAMA_ROOT/data_by_domain"
 python examples/data-processing/sample_json_data_from_meta.py \
---input-folder-path "../RAW_DATA_FOLDER/SlimPajama-627B/export/" \
---output-folder-path "../RAW_DATA_FOLDER/SlimPajama-627B/data_by_domain" \
+--input-folder-path "$SLIM_PAJAMA_ROOT/export/" \
+--output-folder-path "$SLIM_PAJAMA_ROOT/data_by_domain" \
 --meta-keys 'meta' 'redpajama_set_name'
 
-cd ../RAW_DATA_FOLDER/SlimPajama-627B/data_by_domain
-split --line-bytes=85G --additional-suffix=.jsonl -d -a 4 RedPajamaCommonCrawl.jsonl ../shards/RedPajamaC4_
-split --line-bytes=70G --additional-suffix=.jsonl -d -a 4 RedPajamaGithub.jsonl ../shards/RedPajamaGithub.jsonl
-split --line-bytes=70G --additional-suffix=.jsonl -d -a 4 RedPajamaWikipedia.jsonl ../shards/RedPajamaWikipedia_
-split --line-bytes=70G --additional-suffix=.jsonl -d -a 4 RedPajamaBook.jsonl ../shards/RedPajamaBook_
-split --line-bytes=85G --additional-suffix=.jsonl -d -a 4 RedPajamaCommonCrawl.jsonl ../shards/RedPajamaCommonCrawl_
+cd $SLIM_PAJAMA_ROOT/data_by_domain
+split --line-bytes=85G --additional-suffix=.jsonl -d -a 4 RedPajamaCommonCrawl.jsonl ../merged_shards/RedPajamaC4_
+split --line-bytes=70G --additional-suffix=.jsonl -d -a 4 RedPajamaGithub.jsonl ../merged_shards/RedPajamaGithub_
+split --line-bytes=70G --additional-suffix=.jsonl -d -a 4 RedPajamaWikipedia.jsonl ../merged_shards/RedPajamaWikipedia_
+split --line-bytes=70G --additional-suffix=.jsonl -d -a 4 RedPajamaBook.jsonl ../merged_shards/RedPajamaBook_
+split --line-bytes=85G --additional-suffix=.jsonl -d -a 4 RedPajamaCommonCrawl.jsonl ../merged_shards/RedPajamaCommonCrawl_
+
+cp RedPajamaArXiv.jsonl $SLIM_PAJAMA_ROOT/merged_shards/RedPajamaArXiv_0001.jsonl
+
+cd $SLIM_PAJAMA_ROOT/merged_shards
+ls -l | awk '{print $9}' | xargs -I {} mv {} en_{}
+
+azcopy copy SlimPajama-627B "https://allamllmuksstandard.blob.core.windows.net/llm-data/?sv=2023-01-03&ss=btqf&srt=sco&st=2023-12-03T21%3A43%3A17Z&se=2025-05-30T21%3A43%3A00Z&sp=rwdxftlacup&sig=51chDeU9Xqnk7GrTSA3u2gEfdgCUQIq9SDAJEQCPQxE%3D" --recursive --overwrite=false
+
---
+++ examples/pretrain-allam/training/allam-1b_SlimPajama_mlm/data_ratio_prep.sh
@@ -0,0 +1,9 @@
+
+python examples/data-processing/data_ratio_from_file.py \
+ --prefix-paths-from-json "examples/data-processing/data_signature/the_pile_tokenizer_v5_improved.json" \
+ --domain-ratio-from-json  "examples/pretrain-allam/training/allam-1b_SlimPajama_mlm/data_ratio.json" \
+ --lang-select-prob-json "examples/pretrain-allam/training/allam-1b_SlimPajama_mlm/lang_prob.json" \
+ --exclude-iterator-json "examples/pretrain-allam/allam-1b_SlimPajama_mlm/exclude_iterator.json" \
+ --total-token 300000000000 \
+ --export-script "examples/pretrain-allam/training/allam-1b_SlimPajama_mlm/iter_prob.sh"
+ --prefix-for-file-path '\$BIN_IDX_PATH/'
