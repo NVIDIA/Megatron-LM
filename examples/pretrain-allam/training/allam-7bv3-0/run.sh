a4235a860b54d8237896f51806b9ef140d2dca1b
Haidar Khan <haidark@sdaia.gov.sa>
2023-12-08 18:45:43 +0800

    added initial configs for allam 7bv3.0 on dolma

---
+++ examples/pretrain-allam/training/allam-7bv3-0/run.sh
@@ -0,0 +1,89 @@
+
+TOKENIZER_MODEL=$1
+BIN_IDX_PATH=$2
+DATA_CACHE=$3
+CHECKPOINT_DIR=$4
+TENSORBOARD_LOGS_PATH=$5
+
+# DISTRIBUTED_ARGS=(
+#     --nproc_per_node $GPUS_PER_NODE 
+#     --nnodes $NUM_NODES 
+#     --master_addr $MASTER_ADDR 
+#     --master_port $MASTER_PORT
+# )
+
+GPT_MODEL_ARGS=(
+    --seq-length 4096 
+    --max-position-embeddings 4096 
+    --num-layers 32
+    --hidden-size 4096
+    --ffn-hidden-size 11008
+    --num-attention-heads 32
+    --hidden-dropout 0.0
+    --attention-dropout 0.0
+    --make-vocab-size-divisible-by 128
+    --norm-epsilon 1.0e-05
+    --disable-bias-linear
+    --swiglu
+    --tokenizer-type Llama2Tokenizer
+    --untie-embeddings-and-output-weights
+    --use-rotary-position-embeddings
+    --normalization RMSNorm
+    --no-position-embedding
+    --no-masked-softmax-fusion
+    --no-query-key-layer-scaling
+)
+
+LOGISTICS_ARGS=(
+    --save $CHECKPOINT_DIR 
+    --tokenizer-model $TOKENIZER_MODEL
+    --split 99990,8,2 
+    --log-interval 100
+    --save-interval 5000 
+    --eval-interval 1000 
+    --eval-iters 50
+    --tensorboard-dir $TENSORBOARD_LOGS_PATH 
+    --tensorboard-log-interval 100
+    --data-cache-path $DATA_CACHE
+    --log-validation-ppl-to-tensorboard 
+    --seed 1234
+)
+
+TRAINING_ARGS=(
+    --micro-batch-size 1
+    --global-batch-size 1024
+    --train-iters 119000
+    --lr 3.0e-04 
+    --lr-decay-style cosine 
+    --weight-decay 0.1 
+    --adam-beta1 0.9 
+    --adam-beta2 0.95 
+    --init-method-std 0.01
+    --clip-grad 1.0 
+    --min-lr 3.0e-05
+    --lr-warmup-iters 2000
+    --use-flash-attn
+    --bf16
+)
+
+
+MODEL_PARALLEL_ARGS=(
+	--tensor-model-parallel-size 2
+    --pipeline-model-parallel-size 2
+    --no-async-tensor-model-parallel-allreduce
+)
+
+source examples/pretrain-allam/training/allam-7bv3-0/iterator_prob.sh
+
+# torchrun ${\DISTRIBUTED_ARGS[@]}\ pretrain_gpt.py\ \
+python pretrain_gpt.py \
+    ${GPT_MODEL_ARGS[@]} \
+    ${LOGISTICS_ARGS[@]} \
+    ${TRAINING_ARGS[@]} \
+    ${MODEL_PARALLEL_ARGS[@]} \
+    ${DATA_PATH[@]}
+  
+  
+  
+  
+  
