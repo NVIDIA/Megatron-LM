3d0afe03835a5e3b74a01d63f9e4af3219387aa2
Haidar Khan <haidark@sdaia.gov.sa>
2023-12-09 17:00:29 +0800

    working changes for 7bv3 data

--- examples/pretrain-allam/training/allam-7bv3-0/data_ratio_prep.sh
+++ examples/pretrain-allam/training/allam-7bv3-0/data_ratio_prep.sh
@@ -10,7 +10,7 @@ python examples/data-processing/data_ratio_from_file.py \
  --domain-ratio-from-json  "examples/pretrain-allam/training/allam-7bv3-0/data_ratio.json" \
  --lang-select-prob-json "examples/pretrain-allam/training/allam-7bv3-0/lang_prob.json" \
  --exclude-iterator-json "examples/pretrain-allam/training/allam-7bv3-0/exclude_iterator.json" \
- --total-token 300_000_000_000 \
+ --total-token 500_000_000_000 \
  --export-script examples/pretrain-allam/training/allam-7bv3-0/iterator_prob.sh \
  --prefix-for-file-path "\$BIN_IDX_PATH/" \
  --verbose
