c99088c41a21bf65c16c29528fd13e6f9dd1a050
M Saiful bari <sbamruf@gmail.com>
2023-12-10 18:17:17 +0000

    update training code

---
+++ examples/pretrain-llama/training/llama_en_reasoning_warmup_no_dropout/data_ratio_prep.sh
@@ -0,0 +1,8 @@
+python examples/data-processing/data_ratio_from_file.py \
+ --prefix-paths-from-json "examples/pretrain-llama/training/llama_en_reasoning_warmup_no_dropout/en_reasoning_and_arabic_files.json" \
+ --domain-ratio-from-json "examples/pretrain-llama/training/llama_en_reasoning_warmup_no_dropout/data_ratio.json" \
+ --lang-select-prob-json "examples/pretrain-llama/training/llama_en_reasoning_warmup_no_dropout/lang_prob.json" \
+ --total-token 3_000_000_000 \
+ --exclude-iterator-json "examples/pretrain-llama/training/llama_en_reasoning_warmup_no_dropout/exclude_iterator.json" \
+ --export-script "examples/pretrain-llama/training/llama_en_reasoning_warmup_no_dropout/iter_prob.sh" \
+ --prefix-for-file-path "\$BIN_IDX_PATH/"
