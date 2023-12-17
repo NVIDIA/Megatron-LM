4f21eb6ff9be2108b7114619c203655ea703f4a1
M Saiful bari <sbamruf@gmail.com>
2023-12-17 05:32:41 +0000

    update trans_from_scratch code

--- examples/pretrain-llama/training/llama_en_reasoning_ar_with_trans_from_scratch/azure_run.sh
+++ examples/pretrain-llama/training/llama_en_reasoning_ar_with_trans_from_scratch/azure_run.sh
@@ -3,7 +3,13 @@
 #  --resource-group LLM-Test --workspace-name Provisioning-Test \
 #  --file examples/pretrain-llama/training/llama_en_reasoning_ar_with_trans_from_scratch/llama_en_reasoning_ar_with_trans_from_scratch.yaml
 
-az ml job create \
- --subscription c7209a17-0d9f-41df-8e45-e0172343698d \
- --resource-group LLM-Test --workspace-name Provisioning-Test \
- --file examples/pretrain-llama/training/llama_en_reasoning_ar_with_trans_from_scratch/llama_en_reasoning_ar_with_trans_from_scratch_resume.yaml
+# az ml job create \
+#  --subscription c7209a17-0d9f-41df-8e45-e0172343698d \
+#  --resource-group LLM-Test --workspace-name Provisioning-Test \
+#  --file examples/pretrain-llama/training/llama_en_reasoning_ar_with_trans_from_scratch/llama_en_reasoning_ar_with_trans_from_scratch_resume.yaml
+
+
+#  az ml job create \
+#  --subscription c7209a17-0d9f-41df-8e45-e0172343698d \
+#  --resource-group LLM-Test --workspace-name Provisioning-Test \
+#  --file examples/pretrain-llama/training/llama_en_reasoning_ar_with_trans_from_scratch/llama_en_reasoning_ar_with_trans_from_scratch_resume2.yaml
