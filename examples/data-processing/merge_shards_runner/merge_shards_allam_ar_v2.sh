a0c83ee05e87830b08eae5a68346bcd96f2c290a
M Saiful bari <sbamruf@gmail.com>
2023-12-17 15:21:59 +0000

    update script

--- examples/data-processing/merge_shards_runner/merge_shards_allam_ar_v2.sh
+++ examples/data-processing/merge_shards_runner/merge_shards_allam_ar_v2.sh
@@ -63,20 +63,6 @@ python examples/data-processing/merge_shards.py \
 --use-file-input \
 --compute-target 'azure'
 
-# AZJOB_FILE="examples/data-processing/az_templates/template_merge_shard.yaml"
-# AZJOB_CONFIG="examples/configs/azure_login_configs.json"
-# NUM_TOKEN=40486543872
-
-# python examples/data-processing/merge_shards.py \
-# --input-folder-path "../RAW_DATA_FOLDER/allam-v2-data/ar/books/" \
-# --output-folder-path "../RAW_DATA_FOLDER/allam-v2-data/merged_shards" \
-# --az-sample-yaml-job-file "$AZJOB_FILE" \
-# --prefix-name "ar_books3_" \
-# --shard-size $NUM_TOKEN \
-# --az-configs $AZJOB_CONFIG \
-# --use-file-input \
-# --compute-target 'local'
-
 # python examples/data-processing/merge_shards.py \
 # --input-folder-path "https://allamllmuksstandard.blob.core.windows.net/llm-data/allam-v2-data/ar/translated/en2ar_books_corpus_formatted/" \
 # --output-folder-path "$AZ_OUTPUT_FOLDER" \
