41fc82593da13d33aa9826feba1c64c525fa45c7
M Saiful bari <sbamruf@gmail.com>
2023-12-18 16:12:40 +0000

    update tokenization script

--- examples/pretrain-allam/data-processing/tokenize/az_tokenize_shards_dolma.sh
+++ examples/pretrain-allam/data-processing/tokenize/az_tokenize_shards_dolma.sh
@@ -1,22 +1,3 @@
-# TOK_MODEL="https://allamllmuksstandard.blob.core.windows.net/vocab-expanded-training-data/tokenizer_v5_improved/ar_en.model"
-# TOK_TYPE="sentencepiece"
-# INPUT_FOLDER_PATH="https://allamllmuksstandard.blob.core.windows.net/vocab-expanded-training-data/dolma/merged_shards"
-# BIN_IDX_PATH="https://allamllmuksstandard.blob.core.windows.net/vocab-expanded-training-data/dolma/tok_v5_improved_bin_idx"
-# AZ_LOGIN_CONFIG="examples/configs/azure_login_configs.json"
-# AZJOB_FILE="examples/data-processing/az_templates/template_nemo_tokenize.yaml"
-
-# python examples/data-processing/tokenize_shards.py \
-# --input-folder-path "$INPUT_FOLDER_PATH" \
-# --bin-idx-folder-path "$BIN_IDX_PATH" \
-# --tokenizer-module "nemo" \
-# --tokenizer-model "$TOK_MODEL" \
-# --tokenizer-type $TOK_TYPE \
-# --az-configs  $AZJOB_FILE \
-# --num-proc 16 \
-# --compute-target 'azure' \
-# --az-configs $AZ_LOGIN_CONFIG \
-# --az-sample-yaml-job-file $AZJOB_FILE
-
 TOK_MODEL="https://allamllmuksstandard.blob.core.windows.net/vocab-expanded-training-data/tokenizer_v5_improved/ar_en.model"
 TOK_TYPE="Llama2Tokenizer"
 INPUT_FOLDER_PATH="https://allamllmuksstandard.blob.core.windows.net/vocab-expanded-training-data/dolma/dolma_merged_shards"
@@ -30,7 +11,6 @@ python examples/data-processing/tokenize_shards.py \
 --tokenizer-module "megatron" \
 --tokenizer-model "$TOK_MODEL" \
 --tokenizer-type $TOK_TYPE \
---az-configs  $AZJOB_FILE \
 --num-proc 16 \
 --compute-target 'azure' \
 --az-configs $AZ_LOGIN_CONFIG \
