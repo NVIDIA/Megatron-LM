dfb7ca71cfe4a202102d87669718507c3c46c319
M Saiful bari <sbamruf@gmail.com>
2023-12-20 06:16:43 +0000

    update scripts for the run

--- examples/pretrain-llama/training/llama_ve/llama_ve_en_reasoning_ar_lr_hyp_tune/data_ratio_prep.sh
+++ examples/pretrain-llama/training/llama_ve/llama_ve_en_reasoning_ar_lr_hyp_tune/data_ratio_prep.sh
@@ -1,7 +1,7 @@
 if [ ! -f "examples/pretrain-llama/training/llama_ve/llama_ve_en_reasoning_ar_lr_hyp_tune/data.json" ]; then
     python examples/data-processing/remote_list.py \
     --az-configs "examples/configs/azure_login_configs.json" \
-    --input-folder-path "https://allamllmuksstandard.blob.core.windows.net/llm-data/data_repo/tokenize_by_llama2-ve/meglm_llama2-v2_bin_idx/" \
+    --input-folder-path "https://allamllmuksstandard.blob.core.windows.net/llm-data/data_repo/tokenize_by_llama2-ve/meglm_llama2-ve_bin_idx/" \
     --export-data-signature "examples/pretrain-llama/training/llama_ve/llama_ve_en_reasoning_ar_lr_hyp_tune/data.json"
 fi
 
@@ -9,5 +9,7 @@ python examples/data-processing/data_ratio_from_file.py \
  --prefix-paths-from-json "examples/pretrain-llama/training/llama_ve/llama_ve_en_reasoning_ar_lr_hyp_tune/data.json" \
  --domain-ratio-from-json "examples/pretrain-llama/training/llama_ve/llama_ve_en_reasoning_ar_lr_hyp_tune/data_ratio.json" \
  --lang-select-prob-json "examples/pretrain-llama/training/llama_ve/llama_ve_en_reasoning_ar_lr_hyp_tune/lang_prob.json" \
+ --exclude-iterator-json "examples/pretrain-llama/training/llama_ve/llama_ve_en_reasoning_ar_lr_hyp_tune/exclude_iterator.json" \
  --total-token 10_000_000_000 \
- --exclude-iterator-json "examples/pretrain-llama/training/llama_ve/llama_ve_en_reasoning_ar_lr_hyp_tune/exclude_iterator.json"
+ --export-script examples/pretrain-llama/training/llama_ve/llama_ve_en_reasoning_ar_lr_hyp_tune/iterator_prob.sh \
+ --prefix-for-file-path "\$BIN_IDX_PATH/" 
