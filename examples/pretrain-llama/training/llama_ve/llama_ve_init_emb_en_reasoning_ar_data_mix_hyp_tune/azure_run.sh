65c08c10e9e55073d5e8f07f7e1fa27cd8bf883f
M Saiful Bari <skarim@sdaia.gov.sa>
2023-12-24 18:20:05 +0300

    script linking issue

--- examples/pretrain-llama/training/llama_ve/llama_ve_init_emb_en_reasoning_ar_data_mix_hyp_tune/azure_run.sh
+++ examples/pretrain-llama/training/llama_ve/llama_ve_init_emb_en_reasoning_ar_data_mix_hyp_tune/azure_run.sh
@@ -5,19 +5,19 @@ TOTAL_NUM_TOKENS=20_000_000_000
 for ENG in 5 8 10 12 14 16
 do
    
-    AR=$((20 - $ENG_TOK))
+    AR=$((20 - $ENG))
 
     export ENG_TOK=$ENG
     export AR_TOK=$AR
     echo "Training with ENG_TOK: $ENG_TOK"
     echo "Training with AR_TOK: $AR_TOK"
 
-    echo "{\"en\": $ENG_TOK,\"ar\": $AR_TOK}" > examples/pretrain-llama/training/llama_ve_init_emb_en_reasoning_ar_data_mix_hyp_tune/lang_prob.json
+    echo "{\"en\": $ENG_TOK,\"ar\": $AR_TOK}" > examples/pretrain-llama/training/llama_ve/llama_ve_init_emb_en_reasoning_ar_data_mix_hyp_tune/lang_prob.json
 
     python examples/data-processing/data_ratio_from_file.py \
 --prefix-paths-from-json "examples/pretrain-llama/training/llama_ve/llama_ve_init_emb_en_reasoning_ar_data_mix_hyp_tune/data.json" \
 --domain-ratio-from-json "examples/pretrain-llama/training/llama_ve/llama_ve_init_emb_en_reasoning_ar_data_mix_hyp_tune/data_ratio.json" \
---lang-select-prob-json "examples/pretrain-llama/training/llama_ve_init_emb_en_reasoning_ar_data_mix_hyp_tune/lang_prob.json" \
+--lang-select-prob-json "examples/pretrain-llama/training/llama_ve/llama_ve_init_emb_en_reasoning_ar_data_mix_hyp_tune/lang_prob.json" \
 --total-token $TOTAL_NUM_TOKENS \
 --exclude-iterator-json "examples/pretrain-llama/training/llama_ve/llama_ve_init_emb_en_reasoning_ar_data_mix_hyp_tune/exclude_iterator.json" \
 --prefix-for-file-path "\$BIN_IDX_PATH/" \
