88cc59a5b3c8fbd2aa52d40ce72f88304d36a2f5
M Saiful Bari <skarim@sdaia.gov.sa>
2023-12-25 09:54:18 +0300

    update scripts

--- examples/pretrain-llama/training/llama_ve/llama_ve_init_emb_en_reasoning_ar_data_mix_hyp_tune/azure_run.sh
+++ examples/pretrain-llama/training/llama_ve/llama_ve_init_emb_en_reasoning_ar_data_mix_hyp_tune/azure_run.sh
@@ -2,13 +2,11 @@ set -e
 
 TOTAL_NUM_TOKENS=20_000_000_000
 
-for ENG in 5 8 10 12 14 16
+for ENG_TOK in 2 5 8 11 14 17
 do
    
-    AR=$((20 - $ENG))
+    AR_TOK=$((20 - $ENG_TOK))
 
-    export ENG_TOK=$ENG
-    export AR_TOK=$AR
     echo "Training with ENG_TOK: $ENG_TOK"
     echo "Training with AR_TOK: $AR_TOK"
 
@@ -23,16 +21,19 @@ do
 --prefix-for-file-path "\$BIN_IDX_PATH/" \
 --export-script "examples/pretrain-llama/training/llama_ve/llama_ve_init_emb_en_reasoning_ar_data_mix_hyp_tune/iter_prob.sh"
     
+    sed "s/\${{ENG_TOK}}/$ENG_TOK/g" examples/pretrain-llama/training/llama_ve/llama_ve_init_emb_en_reasoning_ar_data_mix_hyp_tune/llama_ve_init_emb_en_reasoning_ar_data_mix_hyp_tune.yaml > examples/pretrain-llama/training/llama_ve/llama_ve_init_emb_en_reasoning_ar_data_mix_hyp_tune/temp.yaml
+
     sleep 2
 
     az ml job create --subscription c7209a17-0d9f-41df-8e45-e0172343698d \
     --resource-group LLM-Test \
     --workspace-name Provisioning-Test \
-    --file "examples/pretrain-llama/training/llama_ve/llama_ve_init_emb_en_reasoning_ar_data_mix_hyp_tune/llama_ve_init_emb_en_reasoning_ar_data_mix_hyp_tune.yaml"
+    --file "examples/pretrain-llama/training/llama_ve/llama_ve_init_emb_en_reasoning_ar_data_mix_hyp_tune/temp.yaml"
 
     sleep 2
 
     rm examples/pretrain-llama/training/llama_ve/llama_ve_init_emb_en_reasoning_ar_data_mix_hyp_tune/iter_prob.sh
     rm examples/pretrain-llama/training/llama_ve/llama_ve_init_emb_en_reasoning_ar_data_mix_hyp_tune/lang_prob.json
-    break
+    rm examples/pretrain-llama/training/llama_ve/llama_ve_init_emb_en_reasoning_ar_data_mix_hyp_tune/temp.yaml
+
 done
