06807cc1d537f2826a49268a240fdef0a4a43914
root <root@8a2fe6a956b14c1696ab1458ebd2ac5900003U.tbfecwuycnyufbn2mhsokzl1ph.zx.internal.cloudapp.net>
2024-01-10 08:53:28 +0000

    70b checkpoint conversion

--- examples/pretrain-llama/checkpointing/convert_70b_ckpt.sh
+++ examples/pretrain-llama/checkpointing/convert_70b_ckpt.sh
@@ -39,7 +39,7 @@ python tools/checkpoint/util.py \
 --tokenizer-model ../DUMPED/llama2_ar_32K_10M/spm/tokenizer.model
 
 azcopy copy "../DUMPED/Meg-LM-Llama-2-70b-hf-ExpTok-32K_10M_VE_with_llama_avg_tp*" "https://provisioningte0256624006.blob.core.windows.net/sbmaruf/vocab_expansion_assets/megatronlm_models/?sv=2023-01-03&ss=btqf&srt=sco&st=2023-12-25T06%3A25%3A17Z&se=2024-09-26T06%3A25%3A00Z&sp=rwdxftlacup&sig=vlLjVfzHFXr%2BYspTH81Wn88zqKGRli3AUMyWD1Rkoak%3D" --recursive --overwrite=false
-rm -rf "../DUMPED/Meg-LM-Llama-2-70b-hf-ExpTok-32K_10M_VE_with_llama_avg_tp*"
+rm -rf ../DUMPED/Meg-LM-Llama-2-70b-hf-ExpTok-32K_10M_VE_with_llama_avg_tp*
 
 TP=8
 PP=1
@@ -58,7 +58,7 @@ python tools/checkpoint/util.py \
 --tokenizer-model ../DUMPED/llama2_ar_32K_10M/spm/tokenizer.model
 
 azcopy copy "../DUMPED/Meg-LM-Llama-2-70b-hf-ExpTok-32K_10M_VE_with_llama_avg_tp*" "https://provisioningte0256624006.blob.core.windows.net/sbmaruf/vocab_expansion_assets/megatronlm_models/?sv=2023-01-03&ss=btqf&srt=sco&st=2023-12-25T06%3A25%3A17Z&se=2024-09-26T06%3A25%3A00Z&sp=rwdxftlacup&sig=vlLjVfzHFXr%2BYspTH81Wn88zqKGRli3AUMyWD1Rkoak%3D" --recursive --overwrite=false
-rm -rf "../DUMPED/Meg-LM-Llama-2-70b-hf-ExpTok-32K_10M_VE_with_llama_avg_tp*"
+rm -rf ../DUMPED/Meg-LM-Llama-2-70b-hf-ExpTok-32K_10M_VE_with_llama_avg_tp*
 
 TP=1
 PP=8
@@ -77,7 +77,7 @@ python tools/checkpoint/util.py \
 --tokenizer-model ../DUMPED/llama2_ar_32K_10M/spm/tokenizer.model
 
 azcopy copy "../DUMPED/Meg-LM-Llama-2-70b-hf-ExpTok-32K_10M_VE_with_llama_avg_tp*" "https://provisioningte0256624006.blob.core.windows.net/sbmaruf/vocab_expansion_assets/megatronlm_models/?sv=2023-01-03&ss=btqf&srt=sco&st=2023-12-25T06%3A25%3A17Z&se=2024-09-26T06%3A25%3A00Z&sp=rwdxftlacup&sig=vlLjVfzHFXr%2BYspTH81Wn88zqKGRli3AUMyWD1Rkoak%3D" --recursive --overwrite=false
-rm -rf "../DUMPED/Meg-LM-Llama-2-70b-hf-ExpTok-32K_10M_VE_with_llama_avg_tp*"
+rm -rf ../DUMPED/Meg-LM-Llama-2-70b-hf-ExpTok-32K_10M_VE_with_llama_avg_tp*
 
 TP=4
 PP=4
@@ -96,7 +96,7 @@ python tools/checkpoint/util.py \
 --tokenizer-model ../DUMPED/llama2_ar_32K_10M/spm/tokenizer.model
 
 azcopy copy "../DUMPED/Meg-LM-Llama-2-70b-hf-ExpTok-32K_10M_VE_with_llama_avg_tp*" "https://provisioningte0256624006.blob.core.windows.net/sbmaruf/vocab_expansion_assets/megatronlm_models/?sv=2023-01-03&ss=btqf&srt=sco&st=2023-12-25T06%3A25%3A17Z&se=2024-09-26T06%3A25%3A00Z&sp=rwdxftlacup&sig=vlLjVfzHFXr%2BYspTH81Wn88zqKGRli3AUMyWD1Rkoak%3D" --recursive --overwrite=false
-rm -rf "../DUMPED/Meg-LM-Llama-2-70b-hf-ExpTok-32K_10M_VE_with_llama_avg_tp*"
+rm -rf ../DUMPED/Meg-LM-Llama-2-70b-hf-ExpTok-32K_10M_VE_with_llama_avg_tp*
 
 TP=8
 PP=2
@@ -115,7 +115,7 @@ python tools/checkpoint/util.py \
 --tokenizer-model ../DUMPED/llama2_ar_32K_10M/spm/tokenizer.model
 
 azcopy copy "../DUMPED/Meg-LM-Llama-2-70b-hf-ExpTok-32K_10M_VE_with_llama_avg_tp*" "https://provisioningte0256624006.blob.core.windows.net/sbmaruf/vocab_expansion_assets/megatronlm_models/?sv=2023-01-03&ss=btqf&srt=sco&st=2023-12-25T06%3A25%3A17Z&se=2024-09-26T06%3A25%3A00Z&sp=rwdxftlacup&sig=vlLjVfzHFXr%2BYspTH81Wn88zqKGRli3AUMyWD1Rkoak%3D" --recursive --overwrite=false
-rm -rf "../DUMPED/Meg-LM-Llama-2-70b-hf-ExpTok-32K_10M_VE_with_llama_avg_tp*"
+rm -rf ../DUMPED/Meg-LM-Llama-2-70b-hf-ExpTok-32K_10M_VE_with_llama_avg_tp*
 
 TP=2
 PP=8
@@ -134,7 +134,7 @@ python tools/checkpoint/util.py \
 --tokenizer-model ../DUMPED/llama2_ar_32K_10M/spm/tokenizer.model
 
 azcopy copy "../DUMPED/Meg-LM-Llama-2-70b-hf-ExpTok-32K_10M_VE_with_llama_avg_tp*" "https://provisioningte0256624006.blob.core.windows.net/sbmaruf/vocab_expansion_assets/megatronlm_models/?sv=2023-01-03&ss=btqf&srt=sco&st=2023-12-25T06%3A25%3A17Z&se=2024-09-26T06%3A25%3A00Z&sp=rwdxftlacup&sig=vlLjVfzHFXr%2BYspTH81Wn88zqKGRli3AUMyWD1Rkoak%3D" --recursive --overwrite=false
-rm -rf "../DUMPED/Meg-LM-Llama-2-70b-hf-ExpTok-32K_10M_VE_with_llama_avg_tp*"
+rm -rf ../DUMPED/Meg-LM-Llama-2-70b-hf-ExpTok-32K_10M_VE_with_llama_avg_tp*
 
 TP=4
 PP=8
@@ -153,7 +153,7 @@ python tools/checkpoint/util.py \
 --tokenizer-model ../DUMPED/llama2_ar_32K_10M/spm/tokenizer.model
 
 azcopy copy "../DUMPED/Meg-LM-Llama-2-70b-hf-ExpTok-32K_10M_VE_with_llama_avg_tp*" "https://provisioningte0256624006.blob.core.windows.net/sbmaruf/vocab_expansion_assets/megatronlm_models/?sv=2023-01-03&ss=btqf&srt=sco&st=2023-12-25T06%3A25%3A17Z&se=2024-09-26T06%3A25%3A00Z&sp=rwdxftlacup&sig=vlLjVfzHFXr%2BYspTH81Wn88zqKGRli3AUMyWD1Rkoak%3D" --recursive --overwrite=false
-rm -rf "../DUMPED/Meg-LM-Llama-2-70b-hf-ExpTok-32K_10M_VE_with_llama_avg_tp*"
+rm -rf ../DUMPED/Meg-LM-Llama-2-70b-hf-ExpTok-32K_10M_VE_with_llama_avg_tp*
 
 TP=8
 PP=4
@@ -171,7 +171,7 @@ python tools/checkpoint/util.py \
 --tokenizer-model ../DUMPED/llama2_ar_32K_10M/spm/tokenizer.model
 
 azcopy copy "../DUMPED/Meg-LM-Llama-2-70b-hf-ExpTok-32K_10M_VE_with_llama_avg_tp*" "https://provisioningte0256624006.blob.core.windows.net/sbmaruf/vocab_expansion_assets/megatronlm_models/?sv=2023-01-03&ss=btqf&srt=sco&st=2023-12-25T06%3A25%3A17Z&se=2024-09-26T06%3A25%3A00Z&sp=rwdxftlacup&sig=vlLjVfzHFXr%2BYspTH81Wn88zqKGRli3AUMyWD1Rkoak%3D" --recursive --overwrite=false
-rm -rf "../DUMPED/Meg-LM-Llama-2-70b-hf-ExpTok-32K_10M_VE_with_llama_avg_tp*"
+rm -rf ../DUMPED/Meg-LM-Llama-2-70b-hf-ExpTok-32K_10M_VE_with_llama_avg_tp*
 
 TP=8
 PP=8
@@ -189,4 +189,4 @@ python tools/checkpoint/util.py \
 --tokenizer-model ../DUMPED/llama2_ar_32K_10M/spm/tokenizer.model
 
 azcopy copy "../DUMPED/Meg-LM-Llama-2-70b-hf-ExpTok-32K_10M_VE_with_llama_avg_tp*" "https://provisioningte0256624006.blob.core.windows.net/sbmaruf/vocab_expansion_assets/megatronlm_models/?sv=2023-01-03&ss=btqf&srt=sco&st=2023-12-25T06%3A25%3A17Z&se=2024-09-26T06%3A25%3A00Z&sp=rwdxftlacup&sig=vlLjVfzHFXr%2BYspTH81Wn88zqKGRli3AUMyWD1Rkoak%3D" --recursive --overwrite=false
-rm -rf "../DUMPED/Meg-LM-Llama-2-70b-hf-ExpTok-32K_10M_VE_with_llama_avg_tp*"
+rm -rf ../DUMPED/Meg-LM-Llama-2-70b-hf-ExpTok-32K_10M_VE_with_llama_avg_tp*
