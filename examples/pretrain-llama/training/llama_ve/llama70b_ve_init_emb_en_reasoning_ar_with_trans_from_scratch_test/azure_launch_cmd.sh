9638cfc37961cd9f2b0fb50184b6bd299329f5d9
Haidar Khan <haidark@sdaia.gov.sa>
2024-01-23 17:29:37 +0300

    edits to llama2 70b ve configs

--- examples/pretrain-llama/training/llama_ve/llama70b_ve_init_emb_en_reasoning_ar_with_trans_from_scratch_test/azure_launch_cmd.sh
+++ examples/pretrain-llama/training/llama_ve/llama70b_ve_init_emb_en_reasoning_ar_with_trans_from_scratch_test/azure_launch_cmd.sh
@@ -1,12 +1,12 @@
 set -e
 
-TP=8
-PP=8
-sed -e "s/\${{TP}}/$TP/g" -e "s/\${{PP}}/$PP/g" examples/pretrain-llama/training/llama_ve/llama13b_ve_init_emb_en_reasoning_ar_with_trans_from_scratch_test/llama13b_ve_init_emb_en_reasoning_ar_with_trans_from_scratch_test.yaml > examples/pretrain-llama/training/llama_ve/llama13b_ve_init_emb_en_reasoning_ar_with_trans_from_scratch_test/temp.yaml
+TP=$1
+PP=$2
+sed -e "s/\${{TP}}/$TP/g" -e "s/\${{PP}}/$PP/g" examples/pretrain-llama/training/llama_ve/llama70b_ve_init_emb_en_reasoning_ar_with_trans_from_scratch_test/llama70b_ve_init_emb_en_reasoning_ar_with_trans_from_scratch_test.yaml > examples/pretrain-llama/training/llama_ve/llama70b_ve_init_emb_en_reasoning_ar_with_trans_from_scratch_test/temp.yaml
 
 az ml job create --subscription c7209a17-0d9f-41df-8e45-e0172343698d \
  --resource-group LLM-Test \
  --workspace-name Provisioning-Test \
- --file "examples/pretrain-llama/training/llama_ve/llama13b_ve_init_emb_en_reasoning_ar_with_trans_from_scratch_test/temp.yaml"
+ --file "examples/pretrain-llama/training/llama_ve/llama70b_ve_init_emb_en_reasoning_ar_with_trans_from_scratch_test/temp.yaml"
 
- rm examples/pretrain-llama/training/llama_ve/llama13b_ve_init_emb_en_reasoning_ar_with_trans_from_scratch_test/temp.yaml
+ rm examples/pretrain-llama/training/llama_ve/llama70b_ve_init_emb_en_reasoning_ar_with_trans_from_scratch_test/temp.yaml
