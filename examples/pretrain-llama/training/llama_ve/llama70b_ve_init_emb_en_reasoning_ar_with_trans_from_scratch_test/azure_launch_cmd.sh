38bf8dbbde200b5958070b4e013b70abbc8fa42c
M Saiful Bari <sbmaruf@gmail.com>
2024-01-24 00:23:10 +0300

    update data path and auto-launch azure launch.

--- examples/pretrain-llama/training/llama_ve/llama70b_ve_init_emb_en_reasoning_ar_with_trans_from_scratch_test/azure_launch_cmd.sh
+++ examples/pretrain-llama/training/llama_ve/llama70b_ve_init_emb_en_reasoning_ar_with_trans_from_scratch_test/azure_launch_cmd.sh
@@ -1,12 +1,30 @@
 set -e
 
-TP=$1
-PP=$2
-sed -e "s/\${{TP}}/$TP/g" -e "s/\${{PP}}/$PP/g" examples/pretrain-llama/training/llama_ve/llama70b_ve_init_emb_en_reasoning_ar_with_trans_from_scratch_test/llama70b_ve_init_emb_en_reasoning_ar_with_trans_from_scratch_test.yaml > examples/pretrain-llama/training/llama_ve/llama70b_ve_init_emb_en_reasoning_ar_with_trans_from_scratch_test/temp.yaml
+# TP=$1
+# PP=$2
 
-az ml job create --subscription c7209a17-0d9f-41df-8e45-e0172343698d \
- --resource-group LLM-Test \
- --workspace-name Provisioning-Test \
- --file "examples/pretrain-llama/training/llama_ve/llama70b_ve_init_emb_en_reasoning_ar_with_trans_from_scratch_test/temp.yaml"
+pairs=(
+    "1 8"
+    "2 4"
+    "2 8"
+    "4 2"
+    "4 4"
+    "4 8"
+    "8 1"
+    "8 2"
+    "8 4"
+    "8 8"
+)
 
- rm examples/pretrain-llama/training/llama_ve/llama70b_ve_init_emb_en_reasoning_ar_with_trans_from_scratch_test/temp.yaml
+for pair in "${pairs[@]}"; do
+    # Split the pair into TP and PP
+    read TP PP <<< "$pair"
+    sed -e "s/\${{TP}}/$TP/g" -e "s/\${{PP}}/$PP/g" examples/pretrain-llama/training/llama_ve/llama70b_ve_init_emb_en_reasoning_ar_with_trans_from_scratch_test/llama70b_ve_init_emb_en_reasoning_ar_with_trans_from_scratch_test.yaml > examples/pretrain-llama/training/llama_ve/llama70b_ve_init_emb_en_reasoning_ar_with_trans_from_scratch_test/temp.yaml
+
+    az ml job create --subscription c7209a17-0d9f-41df-8e45-e0172343698d \
+    --resource-group LLM-Test \
+    --workspace-name Provisioning-Test \
+    --file "examples/pretrain-llama/training/llama_ve/llama70b_ve_init_emb_en_reasoning_ar_with_trans_from_scratch_test/temp.yaml"
+
+    rm examples/pretrain-llama/training/llama_ve/llama70b_ve_init_emb_en_reasoning_ar_with_trans_from_scratch_test/temp.yaml
+done
