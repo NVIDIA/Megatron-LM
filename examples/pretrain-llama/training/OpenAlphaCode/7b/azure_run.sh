8d97679e0a85eef859660f585f2f2fd44c27556a
M Saiful Bari <sbmaruf@gmail.com>
2024-01-24 15:03:38 +0300

    resume training

--- examples/pretrain-llama/training/OpenAlphaCode/7b/azure_run.sh
+++ examples/pretrain-llama/training/OpenAlphaCode/7b/azure_run.sh
@@ -1,4 +1,9 @@
+# az ml job create --subscription c7209a17-0d9f-41df-8e45-e0172343698d \
+#  --resource-group LLM-Test \
+#  --workspace-name Provisioning-Test \
+#  --file "examples/pretrain-llama/training/OpenAlphaCode/7b/azureml_conf.yaml"
+
 az ml job create --subscription c7209a17-0d9f-41df-8e45-e0172343698d \
  --resource-group LLM-Test \
  --workspace-name Provisioning-Test \
- --file "examples/pretrain-llama/training/OpenAlphaCode/7b/azureml_conf.yaml"
+ --file "examples/pretrain-llama/training/OpenAlphaCode/7b/azureml_conf_resume.yaml"
