88ddc36ec715ee6820bd29fbae3290845622d3a9
Zijie Yan <zijiey@nvidia.com>
2024-01-26 00:03:29 -0800

    Fixing the nightly ci for #1018.

--- megatron/core/pipeline_parallel/schedules.py
+++ megatron/core/pipeline_parallel/schedules.py
@@ -211,8 +211,12 @@ def forward_step(
     # Set the loss scale for the auxiliary loss of the MoE layer.
     # Since we use a trick to do backward on the auxiliary loss, we need to set the scale explicitly.
     if config.num_moe_experts is not None:
-        # Calculate the loss scale based on the grad_scale_func if available, else default to 1.0.
-        loss_scale = config.grad_scale_func(1.0) if config.grad_scale_func is not None else 1.0
+        # Calculate the loss scale based on the grad_scale_func if available, else default to 1.
+        loss_scale = (
+            config.grad_scale_func(torch.tensor(1.0))
+            if config.grad_scale_func is not None
+            else torch.tensor(1.0)
+        )
         # Set the loss scale
         MoEAuxLossAutoScaler.set_loss_scale(loss_scale / num_microbatches)
 
