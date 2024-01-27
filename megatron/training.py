87b03307cb2de0974e6cd31978cf0f97899328d5
Haidar Khan <haidark@sdaia.gov.sa>
2024-01-27 14:59:20 +0300

    add support for ffn_hidden_size in throughput logging

--- megatron/training.py
+++ megatron/training.py
@@ -62,17 +62,19 @@ def num_floating_point_operations(args, batch_size):
     if not args.group_query_attention:
         args.num_query_groups = args.num_attention_heads
     return (
-        60
+        6
         * batch_size
         * args.seq_length
-        * args.num_layers
-        * args.hidden_size
         * args.hidden_size
         * (
-            1
-            + (args.num_query_groups / (5 * args.num_attention_heads))
-            + (args.seq_length / (5 * args.hidden_size))
-            + (args.padded_vocab_size / (10 * args.num_layers * args.hidden_size))
+            args.num_layers
+            * (
+            3 * args.hidden_size
+            + (args.hidden_size // args.num_attention_heads) * args.num_query_groups
+            + 2 * args.seq_length
+            + 2 * args.ffn_hidden_size
+            )
+            + args.padded_vocab_size
         )
     )
 
