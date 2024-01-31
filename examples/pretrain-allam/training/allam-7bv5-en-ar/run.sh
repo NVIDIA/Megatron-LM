6d352c1539f6e84df46c106e6d5ba25d44f3bb59
M Saiful Bari <sbmaruf@gmail.com>
2024-01-31 19:40:54 +0300

    finalize configs

--- examples/pretrain-allam/training/allam-7bv5-en-ar/run.sh
+++ examples/pretrain-allam/training/allam-7bv5-en-ar/run.sh
@@ -1,4 +1,6 @@
-PRETRAINED_MODEL_PATH=$1
+set -e
+
+PRETRAINED_LLAMA_MODEL_PATH=$1
 TOKENIZER_MODEL=$2
 BIN_IDX_PATH=$3
 DATA_CACHE=$4
@@ -15,17 +17,9 @@ TENSORBOARD_LOGS_PATH=$6
 GPT_MODEL_ARGS=(
     --seq-length 4096 
     --max-position-embeddings 4096 
-    --num-layers 32
-    --hidden-size 4096
-    --ffn-hidden-size 11008
-    --num-attention-heads 32
-    --hidden-dropout 0.0
-    --attention-dropout 0.0
-    --make-vocab-size-divisible-by 128
-    --norm-epsilon 1.0e-05
-    --disable-bias-linear
-    --swiglu
     --tokenizer-type Llama2Tokenizer
+    --exit-on-missing-checkpoint
+    --use-checkpoint-args
     --untie-embeddings-and-output-weights
     --use-rotary-position-embeddings
     --normalization RMSNorm
@@ -36,51 +30,53 @@ GPT_MODEL_ARGS=(
 
 LOGISTICS_ARGS=(
     --save $CHECKPOINT_DIR 
-    --load $PRETRAINED_MODEL_PATH               # update when resuming from checkpoint
-    --load-iteration 240000                     # update when resuming from checkpoint
-    --dataloader-type single 
-    --override-dataloader-consumed-samples 0    # update when resuming from checkpoint
+    --load $PRETRAINED_LLAMA_MODEL_PATH 
     --tokenizer-model $TOKENIZER_MODEL
-    --split 1,0,0 
+    --split 100,0,0 
     --log-interval 100
-    --save-interval 5000 
-    --eval-interval 1000 
+    --save-interval 3000 
+    --eval-interval 3000
     --eval-iters 0
     --tensorboard-dir $TENSORBOARD_LOGS_PATH 
     --tensorboard-log-interval 100
     --data-cache-path $DATA_CACHE
     --log-validation-ppl-to-tensorboard 
-    --seed 1234
 )
 
 TRAINING_ARGS=(
-    --micro-batch-size 1
+    --no-initialization
+    --no-load-optim
+    --no-load-rng
+    --micro-batch-size 1 
     --global-batch-size 1024
-    --train-iters 953600 # ~ 4.0e12/(1024*4096)
-    --override-opt_param-scheduler
-    --lr 3.0e-05
-    --lr-decay-style cosine 
+    --train-iters 300_000
     --weight-decay 0.1 
     --adam-beta1 0.9 
     --adam-beta2 0.95 
-    --init-method-std 0.01
+    --init-method-std 0.006 
     --clip-grad 1.0 
-    --min-lr 3.0e-05
-    --lr-warmup-iters 0
+    --lr 3.0e-5
+    --min-lr 3.0e-5
+    --lr-warmup-iters 20
+    --lr-decay-style cosine 
     --use-flash-attn
     --bf16
+    --attention-dropout 0.0
+    --hidden-dropout 0.0
+    --num-workers 8
 )
-
+# --use-mcore-models
 
 MODEL_PARALLEL_ARGS=(
 	--tensor-model-parallel-size 2
-    --pipeline-model-parallel-size 2
+    --pipeline-model-parallel-size 4
+    --sequence-parallel
+    --use-distributed-optimizer
     --no-async-tensor-model-parallel-allreduce
 )
 
-source examples/pretrain-allam/training/allam-7bv3-X/iterator_prob.sh
+source examples/pretrain-allam/training/allam-7bv5-en-ar/iterator_prob.sh
 
-# torchrun ${\DISTRIBUTED_ARGS[@]}\ pretrain_gpt.py\ \
 python pretrain_gpt.py \
     ${GPT_MODEL_ARGS[@]} \
     ${LOGISTICS_ARGS[@]} \
--- examples/pretrain-allam/training/allam-7bv5-en/data_ratio_prep.sh
+++ examples/pretrain-allam/training/allam-7bv5-en/data_ratio_prep.sh
@@ -1,15 +1,15 @@
-if [ ! -f "examples/pretrain-allam/training/allam-7bv3-X/data.json" ]; then
+if [ ! -f "examples/pretrain-allam/training/allam-7bv5-en/data.json" ]; then
     python examples/data-processing/remote_list.py \
     --az-configs "examples/configs/azure_login_configs.json" \
     --input-folder-path "https://allamllmuksstandard.blob.core.windows.net/llm-data/data_repo/tokenize_by_v5_improved/meglm_tok_v5_improved_bin_idx/" \
-    --export-data-signature "examples/pretrain-allam/training/allam-7bv3-X/data.json"
+    --export-data-signature "examples/pretrain-allam/training/allam-7bv5-en/data.json"
 fi
 
 python examples/data-processing/data_ratio_from_file.py \
- --prefix-paths-from-json "examples/pretrain-allam/training/allam-7bv3-X/data.json" \
- --domain-ratio-from-json  "examples/pretrain-allam/training/allam-7bv3-X/data_ratio.json" \
- --lang-select-prob-json "examples/pretrain-allam/training/allam-7bv3-X/lang_prob.json" \
- --exclude-iterator-json "examples/pretrain-allam/training/allam-7bv3-X/exclude_iterator.json" \
- --total-token 4_000_000_000_000 \
- --export-script examples/pretrain-allam/training/allam-7bv3-X/iterator_prob.sh \
+ --prefix-paths-from-json "examples/pretrain-allam/training/allam-7bv5-en/data.json" \
+ --domain-ratio-from-json  "examples/pretrain-allam/training/allam-7bv5-en/data_ratio.json" \
+ --lang-select-prob-json "examples/pretrain-allam/training/allam-7bv5-en/lang_prob.json" \
+ --exclude-iterator-json "examples/pretrain-allam/training/allam-7bv5-en/exclude_iterator.json" \
+ --total-token 400_000_000_000 \
+ --export-script examples/pretrain-allam/training/allam-7bv5-en/iterator_prob.sh \
  --prefix-for-file-path "\$BIN_IDX_PATH/" 
