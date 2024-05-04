b7b98ba28db132f064b4cef3f8e0ba598dc3404b
eharper <eharper@nvidia.com>
2024-05-04 10:01:13 -0600

    update version
    Signed-off-by: eharper <eharper@nvidia.com>

--- megatron/core/package_info.py
+++ megatron/core/package_info.py
@@ -4,7 +4,7 @@
 MAJOR = 0
 MINOR = 7
 PATCH = 0
-PRE_RELEASE = 'rc0'
+PRE_RELEASE = ''
 
 # Use the following formatting: (major, minor, patch, pre-release)
 VERSION = (MAJOR, MINOR, PATCH, PRE_RELEASE)
