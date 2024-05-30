47efd0121383fc49728081051de7323c0a994516
Jared Casper <jcasper@nvidia.com>
2024-05-30 16:20:30 -0700

    Revert "Merge branch 'gg_guard' into 'main'"
    This reverts merge request !1513

--- megatron/core/transformer/moe/grouped_gemm_util.py
+++ megatron/core/transformer/moe/grouped_gemm_util.py
@@ -1,9 +1,5 @@
 # Copyright (c) 2023, NVIDIA CORPORATION. All rights reserved.
 
-from importlib.metadata import version
-
-from pkg_resources import packaging
-
 try:
     import grouped_gemm
 except ImportError:
@@ -17,13 +13,7 @@ def grouped_gemm_is_available():
 def assert_grouped_gemm_is_available():
     assert grouped_gemm_is_available(), (
         "Grouped GEMM is not available. Please run "
-        "`pip install git+https://github.com/fanshiqing/grouped_gemm@v1.1.2`."
-    )
-
-    _gg_version = packaging.version.Version(version("grouped_gemm"))
-    assert _gg_version >= packaging.version.Version("1.1.2"), (
-        "Grouped GEMM should be v1.1.2 or newer. Please run "
-        "`pip install git+https://github.com/fanshiqing/grouped_gemm@v1.1.2`."
+        "`pip install git+https://github.com/fanshiqing/grouped_gemm@v1.0`."
     )
 
 
