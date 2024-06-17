af5941906d262213d7b823b9899cf6be956de2ab
ashors1 <ashors@nvidia.com>
2024-06-17 10:50:02 -0700

    add missing file

---
+++ megatron/core/transformer/torch_layer_norm.py
@@ -0,0 +1,19 @@
+import torch
+from megatron.core.transformer import TransformerConfig
+
+class WrappedTorchLayerNorm(torch.nn.LayerNorm):
+    
+    def __init__(
+        self,
+        config: TransformerConfig, ## unused
+        hidden_size: int,
+        eps: float = 1e-5,
+        persist_layer_norm: bool = True,
+        zero_centered_gamma: bool = False,
+        normalization: str = "LayerNorm",  # included to match TE interface
+    ):
+        ## TODO: make sure computation is identical to fused LN version
+        super().__init__(
+            normalized_shape = hidden_size, ## applied to last len(normalized_shape.size) dimensions
+            eps = eps,
+        )
