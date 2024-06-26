fix numerical differences between apex and non-apex paths

megatron/core/utils.py
@@ -447,20 +447,26 @@ def drain_embedding_wgrad_compute(config, embedding_activation_buffer, grad_outp
     input, all_gathered_input[1], grad_output = None, None, None
 
 def local_multi_tensor_applier(op, noop_flag_buffer, tensor_lists, *args):
-    return op(noop_flag_buffer, tensor_lists, *args)
+    return op(None, noop_flag_buffer, tensor_lists, *args)
 
 ## computes l2 norm for a list of contiguous tensors
-## TODO: check that this gives the same output as amp version!!
-def local_multi_tensor_l2_norm(noop_flag, tensor_lists, per_tensor):
-    l2_sq = [[(torch.norm(tensor)) for tensor in tensor_list] for tensor_list in tensor_lists]
-    l2_reduced = torch.norm(torch.tensor(l2_sq))
+## works as a drop-in replacement for amp_C.multi_tensor_l2norm
+def local_multi_tensor_l2_norm(chunk_size, noop_flag, tensor_lists, per_tensor, *args):
+    l2 = [[(torch.norm(tensor)) for tensor in tensor_list] for tensor_list in tensor_lists]
+    l2_reduced = torch.norm(torch.tensor(l2))
     l2_cuda = torch.tensor([float(l2_reduced)], dtype=torch.float, device='cuda')
     return l2_cuda, None
 
-def local_multi_tensor_scale(noop_flag, tensor_lists, scale):
-    inputs, targets = tensor_lists
-    for i in range(len(targets)):
-        targets[i] = inputs[i] * scale
+## works as a drop-in replacement for amp_C.multi_tensor_scale
+def local_multi_tensor_scale(chunk_size, noop_flag, tensor_lists, scale):
+    inputs, targets = tensor_lists[0], tensor_lists[1]
+    if inputs == targets:
+        for i in range(len(targets)):
+            ## for parity with apex implementation
+            targets[i] *= scale
+    else:
+        for i in range(len(targets)):
+            targets[i] = inputs[i] * scale
 
 class _ValueWithRank:
     """This is an internal class, not for use outside this module