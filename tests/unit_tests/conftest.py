ec27dbb7808e130f22654499a586be17893c8212
Oliver Koenig <okoenig@nvidia.com>
2024-06-26 23:52:40 +0200

    revert: Terminate pytest
    Signed-off-by: Oliver Koenig <okoenig@nvidia.com>

--- tests/unit_tests/conftest.py
+++ tests/unit_tests/conftest.py
@@ -1,8 +1,8 @@
-import os
-import signal
+# import os
+# import signal
 
 
-def pytest_sessionfinish(session, exitstatus):
-    if exitstatus != 0:
-        # Violently terminate process
-        os.kill(os.getpid(), signal.SIGTERM)
+# def pytest_sessionfinish(session, exitstatus):
+#     if exitstatus != 0:
+#         # Violently terminate process
+#         os.kill(os.getpid(), signal.SIGTERM)
