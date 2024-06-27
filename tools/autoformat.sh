70f96be2fd6aed064eae303550ae7aadede358da
Oliver Koenig <okoenig@nvidia.com>
2024-06-27 10:40:06 -0700

    chore: Run autoformat on the changeset

--- tools/autoformat.sh
+++ tools/autoformat.sh
@@ -2,7 +2,12 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+CHANGED_FILES=$(git diff --name-only origin/main | grep '^megatron/core' || true)
+
 # for now we just format core
 
-black ${SCRIPT_DIR}/../megatron/core
-isort ${SCRIPT_DIR}/../megatron/core
+
+if [[ -n "$CHANGED_FILES" ]]; then
+    black $CHANGED_FILES
+    isort $CHANGED_FILES
+fi
