e9872b7ad64db172d57f8c802865a098cd002767
Oliver Koenig <okoenig@nvidia.com>
2024-07-25 02:37:49 -0700

    ADLR/megatron-lm!1805 - chore(fix): Autoformat

--- tools/autoformat.sh
+++ tools/autoformat.sh
@@ -5,14 +5,16 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 CHECK_ONLY=${CHECK_ONLY:-false}
 CHANGED_FILES=$(git diff --name-only --diff-filter=d --merge-base origin/main megatron/core | grep '\.py$' || true)
 ADDITIONAL_ARGS=""
+ADDITIONAL_BLACK_ARGS=""
 
 if [[ $CHECK_ONLY == true ]]; then
-    ADDITIONAL_ARGS="--check "
+    ADDITIONAL_ARGS="--check"
+    ADDITIONAL_BLACK_ARGS="--diff"
 fi
 
 # for now we just format core
 if [[ -n "$CHANGED_FILES" ]]; then
-    black $ADDITIONAL_ARGS --verbose --diff $CHANGED_FILES
+    black $ADDITIONAL_ARGS $ADDITIONAL_BLACK_ARGS --verbose $CHANGED_FILES
     isort $ADDITIONAL_ARGS $CHANGED_FILES
 else
     echo Changeset is empty, all good.
