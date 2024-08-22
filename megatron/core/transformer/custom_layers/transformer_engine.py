27d1c5c728cfa7513750b2cef05dd20c515b7f25
YanJiayi <66017932+1195343015@users.noreply.github.com>
2024-08-22 15:07:22 +0800

    Update _te_version 1.9.0 in get_cpu_offload_context to 1.10.0dev0

--- megatron/core/transformer/custom_layers/transformer_engine.py
+++ megatron/core/transformer/custom_layers/transformer_engine.py
@@ -894,7 +894,7 @@ try:
     def get_cpu_offload_context(
         enabled, num_layers, model_layers, activation_offloading, weight_offloading
     ):
-        if _te_version > packaging.version.Version("1.9.0"):
+        if _te_version >= packaging.version.Version("1.10.0.dev0"):
             context, sync_func = _get_cpu_offload_context(
                 enabled, num_layers, model_layers, activation_offloading, weight_offloading
             )
