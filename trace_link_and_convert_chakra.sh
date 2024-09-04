4fe88f56842f16d80d2fc76944d639093a740306
Jiayi Yan <66017932+1195343015@users.noreply.github.com>
2024-09-04 15:50:12 +0800

    Update trace_link_and_convert_chakra.sh

--- trace_link_and_convert_chakra.sh
+++ trace_link_and_convert_chakra.sh
@@ -1,19 +1,15 @@
-#!/bin/bash 
+#!/bin/bash
 
 set -e -x
 
-model_path="~/examples/distributed/tensor_parallelism/"
 input_type=PyTorch
-
 
 for i in {0..1}
 do
     kineto_file=`ls result/$i`
     # echo $kineto_file
-    chakra_trace_link --pytorch-et-file pytorch_et_${i}.json --kineto-file $(dirname result)/result/$i/$kineto_file --output-file pytorch_et_${i}_plus.json
-    chakra_converter --input_type $input_type --input_filename pytorch_et_${i}_plus.json --output_filename final.${i}.et
+    chakra_trace_link --chakra-host-trace pytorch_et_${i}.json --chakra-device-trace $(dirname result)/result/$i/$kineto_file --output-file pytorch_et_${i}_plus.json
+    chakra_converter $input_type --input pytorch_et_${i}_plus.json --output final.${i}.et
     chakra_jsonizer --input_filename final.${i}.et --output_filename final.${i}.json
 done
 chakra_pg_extractor --input_filename ./final --output_filename comm.json
-
-
