d28e26ed06b728d433dfef1ad602279ca510519a
Tyler Poon <tpoon@nvidia.com>
2024-10-19 09:08:14 -0700

    ADLR/megatron-lm!2227 - qwen2.5 conversion

--- tools/checkpoint/saver_mcore.py
+++ tools/checkpoint/saver_mcore.py
@@ -628,10 +628,11 @@ def save_checkpoint(queue, args):
             else:
                 mlp_l0_weight = chunk_weight(msg.pop("mlp l0 weight"), "column", args.target_tensor_parallel_size, args.target_expert_parallel_size)
 
+            if md.qkv_bias:
+                qkv_bias = chunk_bias(msg.pop("qkv bias"), 'column', args.target_tensor_parallel_size)
             if md.linear_bias:
                 dense_bias = msg.pop("dense bias")
                 mlp_l1_bias = chunk_bias(msg.pop("mlp l1 bias"), 'row', args.target_tensor_parallel_size, args.target_expert_parallel_size)
-                qkv_bias = chunk_bias(msg.pop("qkv bias"), 'column', args.target_tensor_parallel_size)
                 if md.swiglu:
                     mlp_l0_bias_W = chunk_bias(msg.pop("mlp l0 bias W"), 'column', args.target_tensor_parallel_size, args.target_expert_parallel_size)
                     mlp_l0_bias_V = chunk_bias(msg.pop("mlp l0 bias V"), 'column', args.target_tensor_parallel_size, args.target_expert_parallel_size)
@@ -662,9 +663,12 @@ def save_checkpoint(queue, args):
                         "self_attn_norm_bias" : input_norm_bias if md.norm_has_bias else None,
                         "mlp_norm_bias" : post_norm_bias if md.norm_has_bias else None,
                     })
+                    if md.qkv_bias:
+                        params_dict.update({
+                            "self_attn_qkv_bias" : qkv_bias[tp_rank]
+                        })
                     if md.linear_bias:
                         params_dict.update({
-                            "self_attn_qkv_bias" : qkv_bias[tp_rank],
                             "self_attn_proj_bias" : dense_bias
                         })
                         if margs.num_experts:
