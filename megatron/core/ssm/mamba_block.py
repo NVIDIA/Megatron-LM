839dff2a42674e08809854e3b239fba859b620ea
Duncan Riach <duncan@nvidia.com>
2024-10-18 18:38:01 -0700

    ADLR/megatron-lm!2075 - Triton cache fix

--- megatron/core/ssm/mamba_block.py
+++ megatron/core/ssm/mamba_block.py
@@ -1,6 +1,5 @@
 # Copyright (c) 2024, NVIDIA CORPORATION. All rights reserved.
 # Copyright (c) 2024, Tri Dao, Albert Gu.
-# Copyright (c) 2024, NVIDIA CORPORATION. All rights reserved.
 
 # Some of this code was adopted from https://github.com/state-spaces/mamba/
 # This source code is licensed under the Apache license found in the
