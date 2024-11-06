3690420c6528303c98fc5c5c3eea16ba50ba0955
lostkevin <lostkevin666@gmail.com>
2024-11-06 10:08:13 +0800

    overlap GPU step / CPU step with two streams

--- megatron/core/optimizer/cpu_offloading/hybrid_optimizer.py
+++ megatron/core/optimizer/cpu_offloading/hybrid_optimizer.py
@@ -20,6 +20,8 @@ class HybridDeviceOptimizer(torch.optim.Optimizer):
         self.cpu_optimizer = cpu_optimizer_cls(self.cpu_params, **kwargs)
         self.gpu_optimizer = gpu_optimizer_cls(self.gpu_params, **kwargs)
         self.cpu_copy_map_grad: Dict[torch.Tensor, torch.Tensor] = defaultdict(torch.Tensor)
+        self._data_stream = torch.cuda.Stream()
+        self._step_stream = torch.cuda.Stream()
 
         self.register_grad_cpu_copy_hook()
         self.register_param_copy_back_gpu_hook()
@@ -34,8 +36,11 @@ class HybridDeviceOptimizer(torch.optim.Optimizer):
                             dtype=gpu_param.grad.dtype,
                             pin_memory=self.pin_cpu_grads
                         )
-                    self.cpu_copy_map_grad[cpu_copy].data.copy_(gpu_param.grad, non_blocking=True)
-                    cpu_copy.grad = self.cpu_copy_map_grad[cpu_copy]
+                self._data_stream.wait_stream(torch.cuda.default_stream())
+                with torch.cuda.stream(self._data_stream):
+                    for gpu_param, cpu_copy in self.gpu_params_map_cpu_copy.items():
+                        self.cpu_copy_map_grad[cpu_copy].data.copy_(gpu_param.grad, non_blocking=True)
+                        cpu_copy.grad = self.cpu_copy_map_grad[cpu_copy]
             return grad_cpu_copy_hook
 
         self.register_step_pre_hook(grad_cpu_copy_hook_closure())
@@ -43,8 +48,12 @@ class HybridDeviceOptimizer(torch.optim.Optimizer):
     def register_param_copy_back_gpu_hook(self):
         def param_copy_back_gpu_hook_closure():
             def param_copy_back_gpu_hook(optimizer, args, kwargs):
-                for cpu_copy, gpu_param in self.cpu_copys_map_gpu_param.items():
-                    gpu_param.data.copy_(cpu_copy.data)
+                self._data_stream.wait_stream(torch.cuda.default_stream())
+                with torch.cuda.stream(self._data_stream):      
+                    for cpu_copy, gpu_param in self.cpu_copys_map_gpu_param.items():
+                        gpu_param.data.copy_(cpu_copy.data, non_blocking=True)
+                # NOTE: ensure all H2D/D2H Transfer and update operations finish
+                torch.cuda.synchronize()
 
             return param_copy_back_gpu_hook
 
@@ -84,8 +93,11 @@ class HybridDeviceOptimizer(torch.optim.Optimizer):
         return self.cpu_optimizer.param_groups + self.gpu_optimizer.param_groups
 
     def step(self, closure=None):
+        self._step_stream.wait_stream(torch.cuda.default_stream())
+        with torch.cuda.stream(self._step_stream):
+            self.gpu_optimizer.step(closure)
+        self._data_stream.synchronize()
         self.cpu_optimizer.step(closure)
-        self.gpu_optimizer.step(closure)
 
     def _split_parameters_updated_on_the_cpu_and_gpu(self):
         total_params_numel = sum([p.numel() for p in self.params])
@@ -99,7 +111,7 @@ class HybridDeviceOptimizer(torch.optim.Optimizer):
         for param in self.params:
             if offloaded_params_numel < offload_threshold:
                 assert param.is_cuda
-                param_cpu_copy = param.detach().cpu()
+                param_cpu_copy = param.detach().cpu().pin_memory()
                 param_cpu_copy.requires_grad = True
                 gpu_params_map_cpu_copy[param] = param_cpu_copy
                 cpu_copys_map_gpu_param[param_cpu_copy] = param
