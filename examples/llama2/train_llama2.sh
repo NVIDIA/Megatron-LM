a533cceaf077ec49bba7898d501a25d53a1ecd80
Gurpreet Singh Dhami <gurpreet.dhami@amd.com>
2024-11-08 03:13:24 +0530

    update script

--- examples/llama2/train_llama2.sh
+++ examples/llama2/train_llama2.sh
@@ -18,7 +18,6 @@ done
 # Change for multinode config
 export CUDA_DEVICE_MAX_CONNECTIONS=1
 
-TEE_OUTPUT="${TEE_OUTPUT:-1}"
 USE_FLASH_ATTN="${USE_FLASH_ATTN:-1}"
 NO_TRAINING="${NO_TRAINING:-0}" # NO_TRAINING=1: for computing metrics only
 ENABLE_PROFILING="${ENABLE_PROFILING:-0}"
@@ -53,54 +52,26 @@ CONTI_PARAMS="${CONTI_PARAMS:-0}"
 OPTIMIZER="${OPTIMIZER:-sgd}"
 TE_BF16="${TE_BF16:-1}"
 
-EXPERIMENT_DIR="experiment"
-mkdir -p $EXPERIMENT_DIR
+TMP_DIR="tmp"
 
-CHECKPOINT_PATH=$EXPERIMENT_DIR/ckpts
-rm -rf $CHECKPOINT_PATH
-mkdir -p $CHECKPOINT_PATH
-DATA_DIR=$EXPERIMENT_DIR/data
-mkdir -p $DATA_DIR
+CHECKPOINT_PATH=${CHECKPOINT_PATH:-"$TMP/ckpts"}
+mkdir -p ${CHECKPOINT_PATH}
 
-TOKENIZER_MODEL=$EXPERIMENT_DIR/tokenizer.model
+DATA_PATH=${DATA_PATH:-"$TMP_DIR/data/bookcorpus_text_sentence"}
+
+TOKENIZER_MODEL=$TMP/tokenizer.model
 
 # Download the tokenizer model
 if ! [ -f "$TOKENIZER_MODEL" ]; then
 wget -O $TOKENIZER_MODEL https://huggingface.co/NousResearch/Llama-2-7b-chat-hf/resolve/main/tokenizer.model
 fi
 
-if ! [ "$ENABLE_MOCK_DATA" -eq 1 ]; then
-# Prepare the dataset
-echo 'import argparse
-from pathlib import Path
-from datasets import load_dataset
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--out-dir", type=str, required=False, default="tmp/data",
-                       help="Path to output JSON")
-    args = parser.parse_args()
-    out_dir = Path(args.out_dir)
-    out_dir.mkdir(exist_ok=True, parents=True)
-
-    dataset = load_dataset("bookcorpus", split="train", trust_remote_code=True)
-    dataset.to_json(out_dir / "bookcorpus_megatron.json")' > prepare_bookcorpus_megatron_dataset.py
-
-DATA_PATH=${DATA_DIR}/bookcorpus_text_sentence
-
- if ! [ -f "${DATA_DIR}/bookcorpus_text_sentence.idx" ]; then
-   echo "Dataset file does not exist, creating..."
-   python3 prepare_bookcorpus_megatron_dataset.py --out-dir ${DATA_DIR}
-   python3 tools/preprocess_data.py --input ${DATA_DIR}/bookcorpus_megatron.json  --tokenizer-type GPTSentencePieceTokenizer --tokenizer-model ${EXPERIMENT_DIR}/tokenizer.model --output-prefix ${DATA_DIR}/bookcorpus --workers `nproc` --split-sentences
-   python3 tools/preprocess_data.py --input ${DATA_DIR}/bookcorpus_megatron.json  --tokenizer-type GPTSentencePieceTokenizer --tokenizer-model ${EXPERIMENT_DIR}/tokenizer.model --output-prefix ${DATA_DIR}/bookcorpus --workers `nproc` --split-sentences
- else
-   echo "Dataset file already exist."
- fi
-fi
-
 MAX_POSITION_EMBEDDINGS=32768
-
-TRAIN_LOG="${TEMP_DIR}/train_${MODEL_SIZE}B_iter${TOTAL_ITERS}_mbs${MBS}_bs${BS}_tp${TP}_pp${PP}_seq${SEQ_LENGTH}_optim_${OPTIMIZER}_fa_${USE_FLASH_ATTN}_seqpara_${SEQ_PARALLEL}_contiparam_${CONTI_PARAMS}_te_bg16_${TE_BF16}_${LABEL}.log"
+LOG_NAME="${TMP_DIR}/train_${MODEL_SIZE}B_iter${TOTAL_ITERS}_mbs${MBS}_bs${BS}_tp${TP}_pp${PP}_\
+            seq${SEQ_LENGTH}_optim_${OPTIMIZER}_fa_${USE_FLASH_ATTN}_seqpara_${SEQ_PARALLEL}_\
+            contiparam_${CONTI_PARAMS}_te_bg16_${TE_BF16}_${LABEL}.log"
+TRAIN_LOG="${LOG_NAME}.log"
+PROFILING_DIR="profile_${LOG_NAME}"
 
 echo $TRAIN_LOG
 
@@ -143,8 +114,6 @@ fi
 
 GROUP_SIZE=$(( ${NUM_HEADS} / ${NUM_KV_HEADS} ))
 NUM_GROUPS=$(( ${NUM_HEADS} / ${GROUP_SIZE} ))
-
-PROFILING_DIR="${EXPERIMENT_DIR}/perf_${MODEL_SIZE}B_iter${TOTAL_ITERS}_mbs${MBS}_bs${BS}_tp${TP}_pp${PP}_seq${SEQ_LENGTH}_optim_${OPTIMIZER}_fa_${USE_FLASH_ATTN}_seqpara_${SEQ_PARALLEL}_contiparam_${CONTI_PARAMS}"
 
 GPT_ARGS="
     --tensor-model-parallel-size ${TP} \
@@ -276,44 +245,24 @@ echo 'torchrun $DISTRIBUTED_ARGS pretrain_gpt.py \
 "
 fi
 
-if [ "$TEE_OUTPUT" -eq 0 ]; then
-    run_cmd="$run_cmd >& $TRAIN_LOG"
-else
-    run_cmd="$run_cmd |& tee $TRAIN_LOG"
-fi
+run_cmd="$run_cmd |& tee $TRAIN_LOG"
 
 if [ "$NO_TRAINING" -eq 0 ]; then
     eval $run_cmd
 fi
 
-echo 'import argparse
-import numpy as np
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-                        prog="Process Log")
-    parser.add_argument("filename")
-    args = parser.parse_args()
-
-    with open(args.filename) as f:
-        lines = f.readlines()
-    lines = lines[1:-1]
-    lines = [float(a) for a in lines]
-    mean = np.mean(np.array(lines))
-    print(mean)' > mean_log_value.py
-
 
 # echo '============================================================================================================'
 grep -Eo 'throughput per GPU [^|]*' $TRAIN_LOG | sed -E 's/.*throughput per GPU \(TFLOP\/s\/GPU\): ([0-9\.]+).*/\1/' > tmp.txt
 echo "throughput per GPU: $(python mean_log_value.py tmp.txt)" |& tee -a $TRAIN_LOG
-THROUGHPUT=$(python mean_log_value.py tmp.txt)
+THROUGHPUT=$(python parse_logs.py tmp.txt)
 rm tmp.txt
 
 # echo '============================================================================================================'
 grep -Eo 'elapsed time per iteration [^|]*' $TRAIN_LOG | sed -E 's/.*elapsed time per iteration \(ms\): ([0-9\.]+).*/\1/' > tmp.txt
-echo "elapsed time per iteration: $(python mean_log_value.py tmp.txt)" |& tee -a $TRAIN_LOG
+echo "elapsed time per iteration: $(python parse_logs.py  tmp.txt)" |& tee -a $TRAIN_LOG
 
-TIME_PER_ITER=$(python mean_log_value.py tmp.txt 2>/dev/null | awk '{printf "%.6f", $0}')
+TIME_PER_ITER=$(python parse_logs.py  tmp.txt 2>/dev/null | awk '{printf "%.6f", $0}')
 PERFORMANCE=$(awk -v bs="$BS" -v sl="$SEQ_LENGTH" -v tpi="$TIME_PER_ITER" -v ws="$WORLD_SIZE" 'BEGIN {printf "%.6f", bs * sl * 1000/ (tpi * ws)}')
 echo "tokens/GPU/s: $PERFORMANCE" |& tee -a $TRAIN_LOG
 rm tmp.txt
