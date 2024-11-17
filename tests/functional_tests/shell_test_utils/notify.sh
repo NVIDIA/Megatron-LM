9e9d4f53b080fce2ef877f0ce001fb7bb9832231
Oliver Koenig <okoenig@nvidia.com>
2024-11-17 05:54:36 -0800

    ADLR/megatron-lm!2348 - ci: Re-enable llava tests

--- tests/functional_tests/shell_test_utils/notify.sh
+++ tests/functional_tests/shell_test_utils/notify.sh
@@ -1,31 +1,32 @@
 set -euxo pipefail
 
-collect_jobs () {
-  PAGE=1
-  PER_PAGE=100
-  RESULTS="[]"
+collect_jobs() {
+    PAGE=1
+    PER_PAGE=100
+    RESULTS="[]"
 
-  while true; do
-    # Fetch the paginated results
-    RESPONSE=$(curl \
-                  -s \
-                  --globoff \
-                  --header "PRIVATE-TOKEN: $RO_API_TOKEN" \
-                  "https://${GITLAB_ENDPOINT}/api/v4/projects/${CI_PROJECT_ID}/pipelines/${DOWNSTREAM_PIPELINE_ID}/jobs?page=$PAGE&per_page=$PER_PAGE"
-              )
-    # Combine the results
-    RESULTS=$(jq -s '.[0] + .[1]' <<< "$RESULTS $RESPONSE")
+    while true; do
+        # Fetch the paginated results
+        RESPONSE=$(
+            curl \
+                -s \
+                --globoff \
+                --header "PRIVATE-TOKEN: $RO_API_TOKEN" \
+                "https://${GITLAB_ENDPOINT}/api/v4/projects/${CI_PROJECT_ID}/pipelines/${DOWNSTREAM_PIPELINE_ID}/jobs?page=$PAGE&per_page=$PER_PAGE"
+        )
+        # Combine the results
+        RESULTS=$(jq -s '.[0] + .[1]' <<<"$RESULTS $RESPONSE")
 
-    # Check if there are more pages
-    if [[ $(jq 'length' <<< "$RESPONSE") -lt $PER_PAGE ]]; then
-      break
-    fi
+        # Check if there are more pages
+        if [[ $(jq 'length' <<<"$RESPONSE") -lt $PER_PAGE ]]; then
+            break
+        fi
 
-    # Increment the page number
-    PAGE=$((PAGE + 1))
-  done
+        # Increment the page number
+        PAGE=$((PAGE + 1))
+    done
 
-  echo "$RESULTS"
+    echo "$RESULTS"
 }
 
 CI_PIPELINE_ID=${1:-16595865}
@@ -35,12 +36,13 @@ CI_PROJECT_ID=${CI_PROJECT_ID:-19378}
 
 # Fetch Elastic logs
 set +x
-PIPELINE_JSON=$(curl \
-                  --fail \
-                  --silent \
-                  --header "PRIVATE-TOKEN: ${RO_API_TOKEN}" \
-                  "https://${GITLAB_ENDPOINT}/api/v4/projects/${CI_PROJECT_ID}/pipelines/${CI_PIPELINE_ID}/bridges?per_page=100"
-                ) || ret_code=$?
+PIPELINE_JSON=$(
+    curl \
+        --fail \
+        --silent \
+        --header "PRIVATE-TOKEN: ${RO_API_TOKEN}" \
+        "https://${GITLAB_ENDPOINT}/api/v4/projects/${CI_PROJECT_ID}/pipelines/${CI_PIPELINE_ID}/bridges?per_page=100"
+) || ret_code=$?
 set -x
 if [[ ${ret_code:-0} -ne 0 ]]; then
     echo CI_PIPELINE_ID=$CI_PIPELINE_ID does not exist
@@ -48,18 +50,18 @@ if [[ ${ret_code:-0} -ne 0 ]]; then
 fi
 
 # Fetch GitLab logs of JET downstream pipeline
-DOWNSTREAM_PIPELINE_ID=$(jq --arg environment "$ENVIRONMENT" '.[] |select(.name == "functional:run_" + $environment) | .downstream_pipeline.id' <<< "$PIPELINE_JSON")
+DOWNSTREAM_PIPELINE_ID=$(jq --arg environment "$ENVIRONMENT" '.[] |select(.name == "functional:run_" + $environment) | .downstream_pipeline.id' <<<"$PIPELINE_JSON")
 
 PIPELINE_URL=https://${GITLAB_ENDPOINT}/ADLR/megatron-lm/-/pipelines/$CI_PIPELINE_ID
 JOB_URL=https://${GITLAB_ENDPOINT}/ADLR/megatron-lm/-/jobs/
 
 if [[ $DOWNSTREAM_PIPELINE_ID == null ]]; then
     FAILED_JOBS=$(curl \
-                    --fail \
-                    --silent \
-                    --header "PRIVATE-TOKEN: ${RO_API_TOKEN}" \
-                    "https://${GITLAB_ENDPOINT}/api/v4/projects/${CI_PROJECT_ID}/pipelines/${CI_PIPELINE_ID}/jobs?per_page=100" \
-                  | jq --arg JOB_URL "$JOB_URL" '[.[] | select(.status == "failed") | ("<" + $JOB_URL + (.id | tostring) + "|" + .name + ">")] | join("\n• Job: ")' | tr -d '"')
+        --fail \
+        --silent \
+        --header "PRIVATE-TOKEN: ${RO_API_TOKEN}" \
+        "https://${GITLAB_ENDPOINT}/api/v4/projects/${CI_PROJECT_ID}/pipelines/${CI_PIPELINE_ID}/jobs?per_page=100" |
+        jq --arg JOB_URL "$JOB_URL" '[.[] | select(.status == "failed") | ("<" + $JOB_URL + (.id | tostring) + "|" + .name + ">")] | join("\n• Job: ")' | tr -d '"')
     curl \
         -X POST \
         -H "Content-type: application/json" \
@@ -91,40 +93,41 @@ else
     echo $JOBS
     set -x
 
-    FAILED_JOBS=$(echo "$JOBS" \
-                | jq --arg GITLAB_ENDPOINT "$GITLAB_ENDPOINT" '[
+    FAILED_JOBS=$(
+        echo "$JOBS" |
+            jq --arg GITLAB_ENDPOINT "$GITLAB_ENDPOINT" '[
                     .[] 
                     | select(.status != "success")
                     | {
                         name,
                         id,
-                        "url": ("https://" + $GITLAB_ENDPOINT + "/dl/jet/ci/-/jobs/" + (.id | tostring)),
+                        "url": ("https://" + $GITLAB_ENDPOINT + "/adlr/megatron-lm/-/jobs/" + (.id | tostring)),
                     }
                 ]'
-            ) 
+    )
     set -x
 
     for row in $(echo "${FAILED_JOBS}" | jq -r '.[] | @base64'); do
         _jq() {
-        echo ${row} | base64 --decode | jq -r ${1}
+            echo ${row} | base64 --decode | jq -r ${1}
         }
         JOB_ID=$(_jq '.id')
         FULL_LOG=$(curl \
             --location \
             --header "PRIVATE-TOKEN: ${RO_API_TOKEN}" \
             "https://${GITLAB_ENDPOINT}/api/v4/projects/${CI_PROJECT_ID}/jobs/${JOB_ID}/trace")
-        
-        if [[ "$FULL_LOG" == *exception* ]]; then 
+
+        if [[ "$FULL_LOG" == *exception* ]]; then
             LAST_EXCEPTION_POS=$(echo "$FULL_LOG" | grep -o -b 'exception' | tail -1 | cut -d: -f1)
             SHORT_LOG=${FULL_LOG:$LAST_EXCEPTION_POS-500:499}
         else
             SHORT_LOG=${FULL_LOG: -1000}
         fi
 
-        FAILED_JOBS=$(echo "$FAILED_JOBS" \
-                    | jq \
-                        --argjson JOB_ID "$JOB_ID" \
-                        --arg SLURM_FAILURE "$SHORT_LOG" '
+        FAILED_JOBS=$(echo "$FAILED_JOBS" |
+            jq \
+                --argjson JOB_ID "$JOB_ID" \
+                --arg SLURM_FAILURE "$SHORT_LOG" '
                             .[] |= ((select(.id==$JOB_ID) += {
                                 "slurm_failure_reason": $SLURM_FAILURE}))
                     ')
@@ -144,8 +147,9 @@ else
             }
         ]'
     else
-        BLOCKS=$(echo "$FAILED_JOBS" \
-                    | jq --arg DATE "$DATE" --arg CONTEXT "$CONTEXT" --arg URL "$PIPELINE_URL" --arg NUM_FAILED "$NUM_FAILED" --arg NUM_TOTAL "$NUM_TOTAL" '
+        BLOCKS=$(
+            echo "$FAILED_JOBS" |
+                jq --arg DATE "$DATE" --arg CONTEXT "$CONTEXT" --arg URL "$PIPELINE_URL" --arg NUM_FAILED "$NUM_FAILED" --arg NUM_TOTAL "$NUM_TOTAL" '
                         [
                             {                
                                 "type": "section",
