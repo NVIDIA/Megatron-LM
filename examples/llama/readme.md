e8c908017648d8a8e356fe58dbd9b19688d45245
lizamd <161388580+lizamd@users.noreply.github.com>
2024-11-21 19:13:26 -0800

    Submit example/llama with llama2/3 scripts and readme (#19)
    * Updated training script for llama2 and llama3. Added README.
    
    * modification based on review feedback
    
    * removed experiement/ folder
    
    * modified scripts further
    
    * updated readme
    
    * last update
    
    * modifed output format
    
    * add copyright
    
    ---------
    
    Co-authored-by: Gurpreet Singh Dhami <gurpreet.dhami@amd.com>

---
+++ examples/llama/readme.md
@@ -0,0 +1,131 @@
+# Llama2/Llama3 Model Pretraining Instructions
+
+This guide provides the steps for setting up the environment and configuring the script to train Llama2 or Llama3 models.
+
+---
+
+## 1. Environment Setup
+
+1. **Download Docker Image**  
+   Download the Docker image required for training:  
+   `docker pull <image_name>`
+
+2. **Launch Docker Container**  
+   Start the Docker container:  
+   `docker run -it <additional flags> <image_name>`
+
+---
+
+## 2. How to Run
+
+### 2.1 Single Node Training
+To run the training on a single node, go to Megatron-LM folder, use the following command:
+```bash
+TEE_OUTPUT=1 MBS=5 BS=120 TP=8 TE_FP8=0 SEQ_LENGTH=4096 bash examples/llama/train_llama2.sh
+```
+
+
+### 2.2 Multi-node Training
+To run training on multiple nodes, launch the Docker container on each node. Follow these steps:
+
+- **On the Master Node:**
+  ```bash
+  TEE_OUTPUT=1 MBS=4 BS=64 TP=8 TE_FP8=0 SEQ_LENGTH=4096 bash examples/llama/train_llama2.sh
+  ```
+
+- **On the Slave Node(s):**
+  ```bash
+  TEE_OUTPUT=1 MBS=4 BS=64 TP=8 TE_FP8=0 SEQ_LENGTH=4096 bash examples/llama/train_llama2.sh
+  ```
+
+## 3. Configurations in Script (`Megatron/examples/llama`)
+
+### 3.1 Network Interface
+Update the network interface in the script to match your system’s network interface.  
+To find your network interface, run (out of container):  
+```bash
+ip a
+```
+Then, update the following variables in the script:  
+```bash
+export NCCL_SOCKET_IFNAME=ens50f0np0
+export GLOO_SOCKET_IFNAME=ens50f0np0
+```
+
+### 3.2 Dataset
+You can use either mock data or real data for training.
+
+- **Mock Data:**  
+  Replace the data path:
+  ```bash
+  --data-path $DATA_PATH \ with 
+  --mock-data
+  ```
+
+- **Real Data:**  
+  Update the `DATA_PATH` to the location where your dataset is stored:
+  ```bash
+  DATA_DIR="/root/.cache/data"  # Change to where your dataset is stored
+  DATA_PATH=${DATA_DIR}/bookcorpus_text_sentence
+  ```
+
+### 3.3 Tokenizer
+
+- **For Llama2 Training:**  
+  Use the `Llama2Tokenizer`.
+
+- **For Llama3 Training:**  
+  Use the `HuggingFaceTokenizer`. Set the HuggingFace model link in the `TOKENIZER_MODEL` variable:
+  ```bash
+  TOKENIZER_MODEL=meta-llama/Llama-3.1-8B  # For Llama3
+  ```
+
+### 3.4 Multi-node Training
+If you're running multi-node training, update the following environment variables:
+
+- **Master Address:**  
+  Change `localhost` to the master node's hostname:
+  ```bash
+  MASTER_ADDR="${MASTER_ADDR:-localhost}"
+  ```
+
+- **Number of Nodes:**  
+  Set the number of nodes you want to train on (e.g., 2, 4, 8):
+  ```bash
+  NNODES="${NNODES:-1}"
+  ```
+
+- **Node Rank:**  
+  Set the rank of each node (0 for master, 1 for the first slave node, etc.):
+  ```bash
+  NODE_RANK="${NODE_RANK:-0}"
+  ```
+
+---
+
+## 4. Key Variables to Pay Attention To
+
+- **TE_FP8:**  
+  `0` for BP16 (default), `1` for FP8.
+
+- **GEMM_TUNING:**  
+  `1` to enable GEMM tuning, which boosts performance by using the best GEMM kernels.
+
+- **USE_FLASH_ATTN:**  
+  `1` to enable Flash Attention.
+
+- **ENABLE_PROFILING:**  
+  `1` to enable PyTorch profiling for performance analysis.
+
+- **transformer-impl:**  
+  `transformer_engine` to use the Transformer Engine (TE). Set to `local` if you want to disable TE.
+
+- **MODEL_SIZE:**  
+  Set to `7B` or `70B` for Llama2, or `8B` or `70B` for Llama3/3.1.
+
+- **TOTAL_ITERS:**  
+  Set the total number of iterations (default: 10).
+
+--- 
+
+That's it! You've now set up the environment and configured the necessary settings for training Llama2 or Llama3 models.
---
+++ examples/llama/train_llama2.sh
@@ -0,0 +1,307 @@
+#!/bin/bash
+###############################################################################
+# Copyright (c) 2024, Advanced Micro Devices, Inc. All rights reserved.
+#
+# See LICENSE for license information.
+#################################################################################
+# set -x
+
+# set envs 
+export GPU_MAX_HW_QUEUES=2
+export TORCH_NCCL_HIGH_PRIORITY=1
+export NCCL_CHECKS_DISABLE=1
+export NCCL_IB_HCA=rdma0,rdma1,rdma2,rdma3,rdma4,rdma5,rdma6,rdma7 
+export NCCL_IB_GID_INDEX=3
+export NCCL_CROSS_NIC=0
+export NCCL_SOCKET_IFNAME=ens50f0np0 # network interface  
+export GLOO_SOCKET_IFNAME=ens50f0np0 # network interface
+export CUDA_DEVICE_MAX_CONNECTIONS=1
+export NCCL_PROTO=Simple
+export RCCL_MSCCL_ENABLE=0
+export TOKENIZERS_PARALLELISM=false
+export HSA_NO_SCRATCH_RECLAIM=1
+
+
+# parsing input arguments
+for ARGUMENT in "$@"
+do
+   KEY=$(echo $ARGUMENT | cut -f1 -d=)
+
+   KEY_LENGTH=${#KEY}
+   VALUE="${ARGUMENT:$KEY_LENGTH+1}"
+
+   export "$KEY"="$VALUE"
+done
+
+
+TIME_STAMP=$(date +"%Y-%m-%d_%H-%M-%S")
+EXP_NAME="${EXP_NAME:-perf}"
+
+TEE_OUTPUT="${TEE_OUTPUT:-1}"
+USE_FLASH_ATTN="${USE_FLASH_ATTN:-1}"
+NO_TRAINING="${NO_TRAINING:-0}" # NO_TRAINING=1: for computing metrics only
+ENABLE_PROFILING="${ENABLE_PROFILING:-0}" #enable pytorch profiling
+ENABLE_ROPE="${ENABLE_ROPE:-1}"
+DISABLE_ROPE_TE="${DISABLE_ROPE_TE:-0}"
+echo "NO_TRAINING=$NO_TRAINING"
+
+CWD=`pwd`
+GPUS_PER_NODE=`python3 -c "import torch; print(torch.cuda.device_count())"`
+
+# single node config, Change for multinode config
+MASTER_ADDR="${MASTER_ADDR:-localhost}"
+#MASTER_ADDR="${MASTER_ADDR:-tw015}"
+MASTER_PORT="${MASTER_PORT:-6020}"
+NNODES="${NNODES:-1}"
+#NNODES="${NNODES:-2}"
+NODE_RANK="${NODE_RANK:-0}"
+WORLD_SIZE=$(($GPUS_PER_NODE*$NNODES))
+
+MODEL_SIZE="${MODEL_SIZE:-70}"
+TP="${TP:-8}"
+PP="${PP:-1}"
+CP="${CP:-1}"
+MBS="${MBS:-2}"
+BS="${BS:-8}"
+SEQ_LENGTH="${SEQ_LENGTH:-4096}"
+TOTAL_ITERS="${TOTAL_ITERS:-5}"
+SEQ_PARALLEL="${SEQ_PARALLEL:-1}" 
+CONTI_PARAMS="${CONTI_PARAMS:-0}"
+TE_FP8="${TE_FP8:-0}"  # 0: disable FP8, 1: enable FP8
+GEMM_TUNING="${GEMM_TUNING:-1}"
+MCORE="${MCORE:-1}"
+
+EXPERIMENT_DIR="experiment"
+mkdir -p $EXPERIMENT_DIR
+CHECKPOINT_PATH=${CHECKPOINT_PATH:-"$EXPERIMENT_DIR/ckpts"}
+
+
+DATA_DIR="/root/.cache/data"  # change to where the dataset is stored
+
+DATA_PATH=${DATA_PATH:-"$DATA_DIR/bookcorpus_text_sentence"}
+
+TOKENIZER_MODEL=$EXPERIMENT_DIR/tokenizer.model
+# Download the tokenizer model
+if ! [ -f "$TOKENIZER_MODEL" ]; then
+wget -O $TOKENIZER_MODEL https://huggingface.co/NousResearch/Llama-2-7b-chat-hf/resolve/main/tokenizer.model
+fi
+
+MAX_POSITION_EMBEDDINGS=128000
+
+DEFAULT_LOG_DIR="${EXPERIMENT_DIR}/${NNODES}nodes_rank${NODE_RANK}_train_${MODEL_SIZE}B_mbs${MBS}_bs${BS}_tp${TP}_pp${PP}_cp${CP}_iter${TOTAL_ITERS}/TE_FP8_${TE_FP8}/${TIME_STAMP}"
+LOG_DIR="${LOG_DIR:-${DEFAULT_LOG_DIR}}"
+TRAIN_LOG="${LOG_DIR}/output_${EXP_NAME}.log"
+mkdir -p $LOG_DIR
+echo $TRAIN_LOG
+
+# gemm tuning 
+if [ "$GEMM_TUNING" -eq 1 ]; then
+   export TE_HIPBLASLT_TUNING_RUN_COUNT=10
+   export TE_HIPBLASLT_TUNING_ALGO_COUNT=50
+fi
+
+if [ "$SEQ_LENGTH" -le 8192 ]; then
+  ds_works=8
+else
+  ds_works=24
+fi
+
+if [[ $MODEL_SIZE -eq 7 ]]; then #llama2-7B
+        HIDDEN_SIZE=4096 # e.g. llama-13b: 5120
+        FFN_HIDDEN_SIZE=14336 # e.g. llama-13b: 13824
+        NUM_LAYERS=32 # e.g. llama-13b: 40
+        NUM_HEADS=32 # e.g. llama-13b: 40
+        SEQ_LENGTH=$SEQ_LENGTH
+        NUM_KV_HEADS=8 # llama2 70B uses GQA
+elif [[ $MODEL_SIZE -eq 70 ]]; then
+        HIDDEN_SIZE=8192 # e.g. llama-13b: 5120
+        FFN_HIDDEN_SIZE=28672 # e.g. llama-13b: 13824
+        NUM_LAYERS=80 # e.g. llama-13b: 40
+        NUM_HEADS=64 # e.g. llama-13b: 40
+        NUM_KV_HEADS=8 # llama3 70B uses GQA
+        SEQ_LENGTH=$SEQ_LENGTH
+        MAX_POSITION_EMBEDDINGS=$MAX_POSITION_EMBEDDINGS
+else
+        echo "Model size not supported."
+        exit 1
+fi
+
+GROUP_SIZE=$(( ${NUM_HEADS} / ${NUM_KV_HEADS} ))
+NUM_GROUPS=$(( ${NUM_HEADS} / ${GROUP_SIZE} ))
+
+PROFILING_DIR="${LOG_DIR}/trace_${EXP_NAME}"
+
+GPT_ARGS="
+    --tensor-model-parallel-size ${TP} \
+    --pipeline-model-parallel-size ${PP} \
+    --context-parallel-size ${CP} \
+    --num-layers $NUM_LAYERS \
+    --hidden-size $HIDDEN_SIZE \
+    --ffn-hidden-size $FFN_HIDDEN_SIZE \
+    --num-attention-heads $NUM_HEADS \
+    --seq-length $SEQ_LENGTH \
+    --max-position-embeddings $MAX_POSITION_EMBEDDINGS \
+    --untie-embeddings-and-output-weights \
+    --position-embedding-type rope \
+    --no-position-embedding \
+    --disable-bias-linear \
+    --swiglu \
+    --init-method-std 0.02 \
+    --attention-dropout 0.0 \
+    --hidden-dropout 0.0 \
+    --normalization RMSNorm \
+    --micro-batch-size $MBS \
+    --global-batch-size $BS \
+    --train-iters $TOTAL_ITERS \
+    --no-async-tensor-model-parallel-allreduce \
+    --bf16 \
+    --no-masked-softmax-fusion \
+    --disable-bias-linear \
+"
+
+TRAIN_ARGS="--lr 1e-4 \
+        --min-lr 1e-5 \
+        --lr-decay-iters 320000 \
+        --lr-decay-style cosine \
+        --weight-decay 1.0e-1 \
+        --clip-grad 1.0 \
+        --optimizer adam \
+"
+DATA_ARGS="
+    --tokenizer-type Llama2Tokenizer  \
+    --tokenizer-model ${TOKENIZER_MODEL} \
+    --dataloader-type cyclic \
+    --save-interval 200000 \
+    --tensorboard-dir $LOG_DIR \
+    --log-interval 1 \
+    --eval-interval 320000 \
+    --eval-iters 10 \
+    --num-workers $ds_works \
+    --mock-data
+"
+#    --data-path $DATA_PATH \
+OUTPUT_ARGS="
+    --log-interval 1 \
+    --save-interval 5000 \
+    --log-throughput \
+    --no-save-optim \
+    --eval-iters -1   
+"
+#  --save $CHECKPOINT_PATH \
+
+DISTRIBUTED_ARGS="
+    --nproc_per_node $GPUS_PER_NODE \
+    --nnodes $NNODES \
+    --node_rank $NODE_RANK \
+    --master_addr $MASTER_ADDR \
+    --master_port $MASTER_PORT \
+"
+
+CKPT_LOAD_ARGS="--exit-on-missing-checkpoint \
+        --no-load-optim \
+        --use-checkpoint-args \
+        --no-load-rng"
+
+
+EXTRA_ARGS="
+    --group-query-attention \
+    --num-query-groups $NUM_GROUPS \
+    --no-gradient-accumulation-fusion \
+    --distributed-backend nccl \
+    --distributed-timeout-minutes 120 \
+    --use-distributed-optimizer \
+    --overlap-param-gather \
+    --overlap-grad-reduce \
+"
+
+if [ "$ENABLE_PROFILING" -eq 1 ]; then
+EXTRA_ARGS="$EXTRA_ARGS --profile --use-pytorch-profiler --tensorboard-dir $LOG_DIR"
+fi
+
+if [ "$USE_FLASH_ATTN" -eq 1 ]; then
+EXTRA_ARGS="$EXTRA_ARGS --use-flash-attn"
+fi
+
+if [ "$SEQ_PARALLEL" -eq 1 ]; then
+EXTRA_ARGS="$EXTRA_ARGS --sequence-parallel"
+fi
+
+if [ "$CONTI_PARAMS" -eq 1 ]; then
+EXTRA_ARGS="$EXTRA_ARGS --use-contiguous-parameters-in-local-ddp"
+fi
+
+if [ "$MCORE" -eq 1 ]; then
+EXTRA_ARGS="$EXTRA_ARGS --use-mcore-models"
+fi
+
+if [ "$ENABLE_ROPE" -eq 1 ]; then
+EXTRA_ARGS="$EXTRA_ARGS --position-embedding-type rope"
+fi
+
+if [ "$DISABLE_ROPE_TE" -eq 1 ]; then
+EXTRA_ARGS="$EXTRA_ARGS --disable-te-fused-rope"
+fi
+
+if [ "$TE_FP8" -eq 1 ]; then
+EXTRA_ARGS="$EXTRA_ARGS --transformer-impl=transformer_engine \
+    --fp8-margin=0 \
+    --fp8-format=hybrid \
+    --fp8-interval=1 \
+    --fp8-amax-history-len=1024 \
+    --fp8-amax-compute-algo=max \
+    --attention-softmax-in-fp32 \
+"
+fi
+
+run_cmd="
+    torchrun $DISTRIBUTED_ARGS pretrain_gpt.py \
+        $GPT_ARGS \
+        $DATA_ARGS \
+        $OUTPUT_ARGS \
+        $EXTRA_ARGS \
+        $TRAIN_ARGS \
+"
+
+if [ "$TEE_OUTPUT" -eq 0 ]; then 
+    run_cmd="$run_cmd >& $TRAIN_LOG"
+else
+    run_cmd="$run_cmd |& tee $TRAIN_LOG"
+fi
+
+if [ "$NO_TRAINING" -eq 0 ]; then 
+    eval $run_cmd
+fi
+
+
+echo 'import argparse
+import numpy as np
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+                        prog="Process Log")
+    parser.add_argument("filename")
+    args = parser.parse_args()
+
+    with open(args.filename) as f:
+        lines = f.readlines()
+    lines = lines[2:-1]
+    lines = [float(a) for a in lines]
+    mean = np.mean(np.array(lines))
+    print(mean)' > mean_log_value.py
+
+
+# echo '============================================================================================================'
+grep -Eo 'throughput per GPU [^|]*' $TRAIN_LOG | sed -E 's/.*throughput per GPU \(TFLOP\/s\/GPU\): ([0-9\.]+).*/\1/' > tmp.txt
+PERFORMANCE=$(python3 mean_log_value.py tmp.txt)
+echo "throughput per GPU: $PERFORMANCE" |& tee -a $TRAIN_LOG
+rm tmp.txt
+
+# echo '============================================================================================================'
+grep -Eo 'elapsed time per iteration [^|]*' $TRAIN_LOG | sed -E 's/.*elapsed time per iteration \(ms\): ([0-9\.]+).*/\1/' > tmp.txt
+ETPI=$(python3 mean_log_value.py tmp.txt)
+echo "elapsed time per iteration: $ETPI" |& tee -a $TRAIN_LOG
+
+TIME_PER_ITER=$(python3 mean_log_value.py tmp.txt 2>/dev/null | awk '{printf "%.6f", $0}')
+TGS=$(awk -v bs="$BS" -v sl="$SEQ_LENGTH" -v tpi="$TIME_PER_ITER" -v ws="$WORLD_SIZE" 'BEGIN {printf "%.6f", bs * sl * 1000/ (tpi * ws)}')
+echo "tokens/GPU/s: $TGS" |& tee -a $TRAIN_LOG
+rm tmp.txt
