382fa6a8013f9257a8dc8331cc3f810a7094f8cc
Oliver Koenig <okoenig@nvidia.com>
2024-11-30 03:33:14 -0800

    ADLR/megatron-lm!2395 - ci: Use cluster-specific runners

--- tests/functional_tests/python_test_utils/jet/generate_local_jobs.py
+++ tests/functional_tests/python_test_utils/jet/generate_local_jobs.py
@@ -18,7 +18,7 @@ from tests.functional_tests.python_test_utils.jet import common
 def load_script(config_path: str) -> str:
     with open(config_path) as stream:
         try:
-            jetclient.JETWorkloadManifest(**yaml.safe_load(stream)).spec.script
+            return jetclient.JETWorkloadManifest(**yaml.safe_load(stream)).spec.script
         except yaml.YAMLError as exc:
             raise exc
 
