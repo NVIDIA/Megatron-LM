42a76b91e7f599a69cc0b328b0a0eec08e3cf2d3
Matthieu Le <matthieul@nvidia.com>
2025-01-30 21:34:14 -0800

    ADLR/megatron-lm!2610 - Update config for llama 3.1 8b vision projection

--- examples/multimodal/config.py
+++ examples/multimodal/config.py
@@ -186,6 +186,9 @@ def get_vision_projection_config(config, hidden_size):
     elif config.language_model_type == "llama3.1_8b":
         config.ffn_hidden_size = 4096
         config.activation_func = torch.nn.functional.gelu
+        config.layernorm_epsilon = 1e-5
+        config.add_bias_linear = True
+        config.normalization = "LayerNorm"
     elif config.language_model_type == "mistral_7b":
         config.ffn_hidden_size = 14336
         config.activation_func = torch.nn.functional.gelu
