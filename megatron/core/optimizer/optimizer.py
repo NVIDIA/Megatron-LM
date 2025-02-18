4e8d314969016be6bc8fca9557a3cc4b9f1cf772
Maanu Grover <maanug@nvidia.com>
2025-02-17 17:07:54 -0800

    ADLR/megatron-lm!2682 - Guard against 'common_step'=None

--- megatron/core/optimizer/optimizer.py
+++ megatron/core/optimizer/optimizer.py
@@ -298,7 +298,7 @@ class MegatronOptimizer(ABC):
         """
 
     @staticmethod
-    def _extract_common_per_param_step(state_dict) -> Union[int, torch.Tensor]:
+    def _extract_common_per_param_step(state_dict) -> Union[int, torch.Tensor, None]:
         common_step = None
         for param_idx, param_state in state_dict['state'].items():
             param_step = param_state.get('step', None)
@@ -712,7 +712,8 @@ class Float16OptimizerWithFloat16Params(MixedPrecisionOptimizer):
         )
         # save step as a shared step among all parameters. Separate per-parameter
         # steps are not supported
-        state_dict['optimizer']['state']['common_step'] = step
+        if step:
+            state_dict['optimizer']['state']['common_step'] = step
         return state_dict
 
     def load_state_dict(self, state_dict):
@@ -888,7 +889,8 @@ class FP32Optimizer(MegatronOptimizer):
         optim_state_to_sharding_state(state_dict, id_to_sharded_param_map, exclude_keys="step")
         # save step as a shared step among all parameters. Separate per-parameter
         # steps are not supported
-        state_dict['state']['common_step'] = step
+        if step:
+            state_dict['state']['common_step'] = step
         return state_dict
 
 
