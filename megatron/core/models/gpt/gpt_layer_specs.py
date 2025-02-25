588b2118cdc48a2dd217a32e6f64a0c91c775e1a
Oliver Koenig <okoenig@nvidia.com>
2025-02-24 17:59:30 -0800

    ADLR/megatron-lm!2747 - Cherry pick `Use TEColumnParallelLinear instead of TELinear in MLA down proj layers (2710)` into `core_r0.11.0`
    Co-authored-by: Chen Cui <chcui@nvidia.com>

--- megatron/core/models/gpt/gpt_layer_specs.py
+++ megatron/core/models/gpt/gpt_layer_specs.py
@@ -33,7 +33,6 @@ try:
         TEColumnParallelLinear,
         TEDotProductAttention,
         TELayerNormColumnParallelLinear,
-        TELinear,
         TENorm,
         TERowParallelLinear,
     )
@@ -101,13 +100,13 @@ def get_gpt_layer_with_transformer_engine_spec(
                     params={"attn_mask_type": AttnMaskType.causal},
                     submodules=MLASelfAttentionSubmodules(
                         linear_q_proj=TEColumnParallelLinear,
-                        linear_q_down_proj=TELinear,
+                        linear_q_down_proj=TEColumnParallelLinear,
                         linear_q_up_proj=(
                             TELayerNormColumnParallelLinear
                             if qk_layernorm
                             else TEColumnParallelLinear
                         ),
-                        linear_kv_down_proj=TELinear,
+                        linear_kv_down_proj=TEColumnParallelLinear,
                         linear_kv_up_proj=(
                             TELayerNormColumnParallelLinear
                             if qk_layernorm
