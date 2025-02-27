314f8f836a3986511ec03a26495c54431d93ef2d
AleHC <alejandro.hernandezcano@epfl.ch>
2025-02-27 17:56:54 +0100

    added lm_head in converter when tied and also tempdir logic

--- tools/checkpoint/saver_llama_hf.py
+++ tools/checkpoint/saver_llama_hf.py
@@ -4,6 +4,7 @@ import gc
 import json
 from pathlib import Path
 from shutil import rmtree
+from tempfile import TemporaryDirectory
 
 import torch
 import torch.multiprocessing as mp
@@ -118,215 +119,218 @@ def save_checkpoint(queue: mp.Queue, args):
 
     ### init
     save_dir = Path(args.save_dir)
-    tmp_save_dir = save_dir / "tmp"
     save_dir.mkdir(exist_ok=True)
-    tmp_save_dir.mkdir(exist_ok=True)
-    index_dict = {
-        "weight_map": {},
-        "metadata": {"total_size": 0},
-    }
-    tokenizer = None
-    ref_state_dict = None
+    with TemporaryDirectory(prefix=str(save_dir/"tmp")) as tmp_save_dir:
+        index_dict = {
+            "weight_map": {},
+            "metadata": {"total_size": 0},
+        }
+        tokenizer = None
+        ref_state_dict = None
 
-    ### prepare a reference model if needed
-    if args.check_eq_hf:
-        print(f"preparing checks with given HF model {args.check_eq_hf}")
-        ref_model = AutoModelForCausalLM.from_pretrained(args.check_eq_hf)
-        ref_state_dict = ref_model.state_dict()
+        ### prepare a reference model if needed
+        if args.check_eq_hf:
+            print(f"preparing checks with given HF model {args.check_eq_hf}")
+            ref_model = AutoModelForCausalLM.from_pretrained(args.check_eq_hf)
+            ref_state_dict = ref_model.state_dict()
 
-    ### save tokenizer conf files
-    if args.hf_tokenizer:
-        from transformers import AutoTokenizer
+        ### save tokenizer conf files
+        if args.hf_tokenizer:
+            from transformers import AutoTokenizer
 
-        tokenizer = AutoTokenizer.from_pretrained(args.hf_tokenizer)
-        print(f"saving tokenizer to {args.save_dir}")
-        tokenizer.save_pretrained(args.save_dir)
+            tokenizer = AutoTokenizer.from_pretrained(args.hf_tokenizer)
+            print(f"saving tokenizer to {args.save_dir}")
+            tokenizer.save_pretrained(args.save_dir)
 
-    ### save config.json
-    llama_conf = LlamaConfig(
-        vocab_size=md.true_vocab_size if md.true_vocab_size else md.checkpoint_args.padded_vocab_size,
-        hidden_size=md.checkpoint_args.hidden_size,
-        intermediate_size=md.checkpoint_args.ffn_hidden_size,
-        num_hidden_layers=md.checkpoint_args.num_layers,
-        num_attention_heads=md.checkpoint_args.num_attention_heads,
-        num_key_value_heads=md.checkpoint_args.num_query_groups,
-        hidden_act="silu",
-        max_position_embeddings=md.checkpoint_args.max_position_embeddings,
-        rms_norm_eps=md.checkpoint_args.norm_epsilon,
-        tie_word_embeddings=not md.checkpoint_args.untie_embeddings_and_output_weights,
-        rope_theta=md.checkpoint_args.rotary_base,
-        attention_bias=md.checkpoint_args.add_qkv_bias,
-        mlp_bias=md.checkpoint_args.add_bias_linear,
-        torch_dtype=torch_dtype,
-        model_type="llama",
-        architectures=["LlamaForCausalLM"],
-        transformers_version="4.33.1",
-    )
-    if args.hf_tokenizer:
-        llama_conf.eos_token_id = tokenizer.eos_token_id
-        llama_conf.bos_token_id = tokenizer.eos_token_id
-    
-    print(f"saving config.json to {tmp_save_dir}")
-    llama_conf.save_pretrained(tmp_save_dir)
+        ### save config.json
+        llama_conf = LlamaConfig(
+            vocab_size=md.true_vocab_size if md.true_vocab_size else md.checkpoint_args.padded_vocab_size,
+            hidden_size=md.checkpoint_args.hidden_size,
+            intermediate_size=md.checkpoint_args.ffn_hidden_size,
+            num_hidden_layers=md.checkpoint_args.num_layers,
+            num_attention_heads=md.checkpoint_args.num_attention_heads,
+            num_key_value_heads=md.checkpoint_args.num_query_groups,
+            hidden_act="silu",
+            max_position_embeddings=md.checkpoint_args.max_position_embeddings,
+            rms_norm_eps=md.checkpoint_args.norm_epsilon,
+            tie_word_embeddings=not md.checkpoint_args.untie_embeddings_and_output_weights,
+            rope_theta=md.checkpoint_args.rotary_base,
+            attention_bias=md.checkpoint_args.add_qkv_bias,
+            mlp_bias=md.checkpoint_args.add_bias_linear,
+            torch_dtype=torch_dtype,
+            model_type="llama",
+            architectures=["LlamaForCausalLM"],
+            transformers_version="4.33.1",
+        )
+        if args.hf_tokenizer:
+            llama_conf.eos_token_id = tokenizer.eos_token_id
+            llama_conf.bos_token_id = tokenizer.eos_token_id
+        
+        print(f"saving config.json to {tmp_save_dir}")
+        llama_conf.save_pretrained(tmp_save_dir)
 
-    ### save embedding layer
-    # Deal with padding
-    def pad_weight(orig_word_embed, true_vocab_size):
-        if true_vocab_size is not None:
-            # figure out what our padded vocab size is
-            orig_vocab_size = orig_word_embed.shape[0]
-            md.checkpoint_args.padded_vocab_size = _vocab_size_with_padding(true_vocab_size, md.checkpoint_args)
+        ### save embedding layer
+        # Deal with padding
+        def pad_weight(orig_word_embed, true_vocab_size):
+            if true_vocab_size is not None:
+                # figure out what our padded vocab size is
+                orig_vocab_size = orig_word_embed.shape[0]
+                md.checkpoint_args.padded_vocab_size = _vocab_size_with_padding(true_vocab_size, md.checkpoint_args)
 
-            # Cut out extra padding we don't need
-            if orig_vocab_size > md.checkpoint_args.padded_vocab_size:
-                full_word_embed = orig_word_embed[0:md.checkpoint_args.padded_vocab_size,:]
+                # Cut out extra padding we don't need
+                if orig_vocab_size > md.checkpoint_args.padded_vocab_size:
+                    full_word_embed = orig_word_embed[0:md.checkpoint_args.padded_vocab_size,:]
 
-            # Expanding embedding to larger size by replicating final entry
-            elif orig_vocab_size < md.checkpoint_args.padded_vocab_size:
-                padding_size = md.checkpoint_args.padded_vocab_size - orig_vocab_size
+                # Expanding embedding to larger size by replicating final entry
+                elif orig_vocab_size < md.checkpoint_args.padded_vocab_size:
+                    padding_size = md.checkpoint_args.padded_vocab_size - orig_vocab_size
 
-                full_word_embed = torch.cat((
-                    orig_word_embed,
-                    orig_word_embed[-1].unsqueeze(0).expand(padding_size, -1)))
+                    full_word_embed = torch.cat((
+                        orig_word_embed,
+                        orig_word_embed[-1].unsqueeze(0).expand(padding_size, -1)))
 
-            # Same size!
+                # Same size!
+                else:
+                    full_word_embed = orig_word_embed
             else:
+                print("Original vocab size not specified, leaving embedding table as-is. "
+                    "If you've changed the tensor parallel size this could cause problems.")
+                md.checkpoint_args.padded_vocab_size = orig_word_embed.shape[0]
                 full_word_embed = orig_word_embed
-        else:
-            print("Original vocab size not specified, leaving embedding table as-is. "
-                "If you've changed the tensor parallel size this could cause problems.")
-            md.checkpoint_args.padded_vocab_size = orig_word_embed.shape[0]
-            full_word_embed = orig_word_embed
-        return full_word_embed
+            return full_word_embed
 
-    state_dict = {
-        "model.embed_tokens.weight": pad_weight(queue_get("embeddings")["word embeddings"], md.true_vocab_size)
-    }
-    index_dict, ref_state_dict = save_layer(
-        state_dict,
-        index_dict,
-        dir_path=tmp_save_dir,
-        filename="pytorch_model-embedding.bin",
-        check_reference=args.check_eq_hf,
-        ref_state_dict=ref_state_dict,
-    )
-
-    ### save every transformer layer
-    head_size = llama_conf.hidden_size // llama_conf.num_attention_heads
-    heads_per_group = llama_conf.num_attention_heads // llama_conf.num_key_value_heads
-    qkv_total_heads = llama_conf.num_attention_heads + 2 * llama_conf.num_key_value_heads
-    for i_layer in range(llama_conf.num_hidden_layers):
-        message = queue_get(f"transformer layer {i_layer}")
+        # We also keep embeddings as we might need them to set the lm_head weight.
+        embeddings = pad_weight(queue_get("embeddings")["word embeddings"], md.true_vocab_size)
         state_dict = {
-            f"model.layers.{i_layer}.input_layernorm.weight": message[
-                "input norm weight"
-            ],
-            f"model.layers.{i_layer}.post_attention_layernorm.weight": message[
-                "post norm weight"
-            ],
-            f"model.layers.{i_layer}.mlp.gate_proj.weight": message["mlp l0 weight W"],
-            f"model.layers.{i_layer}.mlp.up_proj.weight": message["mlp l0 weight V"],
-            f"model.layers.{i_layer}.self_attn.o_proj.weight": message["dense weight"],
-            f"model.layers.{i_layer}.mlp.down_proj.weight": message["mlp l1 weight"]
+            "model.embed_tokens.weight": embeddings
         }
-        if md.checkpoint_args.add_bias_linear:
-            state_dict |= {
-                f"model.layers.{i_layer}.self_attn.o_proj.bias": message["dense bias"],
-                f"model.layers.{i_layer}.mlp.gate_proj.bias": message["mlp l0 bias W"],
-                f"model.layers.{i_layer}.mlp.up_proj.bias": message["mlp l0 bias V"],
-                f"model.layers.{i_layer}.mlp.down_proj.bias": message["mlp l1 bias"],
-            }
-        q_slice = torch.cat(
-            [
-                torch.arange(
-                    (heads_per_group + 2) * i,
-                    (heads_per_group + 2) * i + heads_per_group,
-                )
-                for i in range(llama_conf.num_key_value_heads)
-            ]
-        )
-        k_slice = torch.arange(heads_per_group, qkv_total_heads, (heads_per_group + 2))
-        v_slice = torch.arange(
-            heads_per_group + 1, qkv_total_heads, (heads_per_group + 2)
-        )
-        qkv_weights = message["qkv weight"]
-        qkv_weights = qkv_weights.reshape(
-            [qkv_total_heads, head_size, llama_conf.hidden_size]
-        )
-        state_dict |= {
-            f"model.layers.{i_layer}.self_attn.q_proj.weight": qkv_weights[
-                q_slice
-            ].reshape(-1, llama_conf.hidden_size),
-            f"model.layers.{i_layer}.self_attn.k_proj.weight": qkv_weights[
-                k_slice
-            ].reshape(-1, llama_conf.hidden_size),
-            f"model.layers.{i_layer}.self_attn.v_proj.weight": qkv_weights[
-                v_slice
-            ].reshape(-1, llama_conf.hidden_size)
-        }
-        if md.checkpoint_args.add_bias_linear:
-            qkv_bias = message["qkv bias"]
-            qkv_bias = qkv_bias.reshape([qkv_total_heads, head_size])
-            state_dict |= {
-                f"model.layers.{i_layer}.self_attn.q_proj.bias": qkv_bias[
-                    q_slice
-                ].reshape(-1, llama_conf.hidden_size),
-                f"model.layers.{i_layer}.self_attn.k_proj.bias": qkv_bias[
-                    k_slice
-                ].reshape(-1, llama_conf.hidden_size),
-                f"model.layers.{i_layer}.self_attn.v_proj.bias": qkv_bias[
-                    v_slice
-                ].reshape(-1, llama_conf.hidden_size)
-            }
         index_dict, ref_state_dict = save_layer(
             state_dict,
             index_dict,
             dir_path=tmp_save_dir,
-            filename=f"pytorch_model-{i_layer + 1}.bin",
+            filename="pytorch_model-embedding.bin",
             check_reference=args.check_eq_hf,
             ref_state_dict=ref_state_dict,
         )
 
-    if not md.checkpoint_args.untie_embeddings_and_output_weights:  # tied embeddings and lm-head
-        state_dict = {
-            "model.norm.weight": queue_get("final norm")["weight"],
+        ### save every transformer layer
+        head_size = llama_conf.hidden_size // llama_conf.num_attention_heads
+        heads_per_group = llama_conf.num_attention_heads // llama_conf.num_key_value_heads
+        qkv_total_heads = llama_conf.num_attention_heads + 2 * llama_conf.num_key_value_heads
+        for i_layer in range(llama_conf.num_hidden_layers):
+            message = queue_get(f"transformer layer {i_layer}")
+            state_dict = {
+                f"model.layers.{i_layer}.input_layernorm.weight": message[
+                    "input norm weight"
+                ],
+                f"model.layers.{i_layer}.post_attention_layernorm.weight": message[
+                    "post norm weight"
+                ],
+                f"model.layers.{i_layer}.mlp.gate_proj.weight": message["mlp l0 weight W"],
+                f"model.layers.{i_layer}.mlp.up_proj.weight": message["mlp l0 weight V"],
+                f"model.layers.{i_layer}.self_attn.o_proj.weight": message["dense weight"],
+                f"model.layers.{i_layer}.mlp.down_proj.weight": message["mlp l1 weight"]
             }
-    else:                                                           # untied embeddings and lm-head
-        state_dict = {
-            "model.norm.weight": queue_get("final norm")["weight"],
-            "lm_head.weight": pad_weight(queue_get("output layer")["weight"], md.true_vocab_size) 
-    }
-    index_dict, ref_state_dict = save_layer(
-        state_dict,
-        index_dict,
-        dir_path=tmp_save_dir,
-        filename="pytorch_model-lm-head.bin",
-        check_reference=args.check_eq_hf,
-        ref_state_dict=ref_state_dict,
-    )
-    # final check
-    assert (
-        not ref_state_dict
-    ), "reference state dict has additional layers not present in model."
+            if md.checkpoint_args.add_bias_linear:
+                state_dict |= {
+                    f"model.layers.{i_layer}.self_attn.o_proj.bias": message["dense bias"],
+                    f"model.layers.{i_layer}.mlp.gate_proj.bias": message["mlp l0 bias W"],
+                    f"model.layers.{i_layer}.mlp.up_proj.bias": message["mlp l0 bias V"],
+                    f"model.layers.{i_layer}.mlp.down_proj.bias": message["mlp l1 bias"],
+                }
+            q_slice = torch.cat(
+                [
+                    torch.arange(
+                        (heads_per_group + 2) * i,
+                        (heads_per_group + 2) * i + heads_per_group,
+                    )
+                    for i in range(llama_conf.num_key_value_heads)
+                ]
+            )
+            k_slice = torch.arange(heads_per_group, qkv_total_heads, (heads_per_group + 2))
+            v_slice = torch.arange(
+                heads_per_group + 1, qkv_total_heads, (heads_per_group + 2)
+            )
+            qkv_weights = message["qkv weight"]
+            qkv_weights = qkv_weights.reshape(
+                [qkv_total_heads, head_size, llama_conf.hidden_size]
+            )
+            state_dict |= {
+                f"model.layers.{i_layer}.self_attn.q_proj.weight": qkv_weights[
+                    q_slice
+                ].reshape(-1, llama_conf.hidden_size),
+                f"model.layers.{i_layer}.self_attn.k_proj.weight": qkv_weights[
+                    k_slice
+                ].reshape(-1, llama_conf.hidden_size),
+                f"model.layers.{i_layer}.self_attn.v_proj.weight": qkv_weights[
+                    v_slice
+                ].reshape(-1, llama_conf.hidden_size)
+            }
+            if md.checkpoint_args.add_bias_linear:
+                qkv_bias = message["qkv bias"]
+                qkv_bias = qkv_bias.reshape([qkv_total_heads, head_size])
+                state_dict |= {
+                    f"model.layers.{i_layer}.self_attn.q_proj.bias": qkv_bias[
+                        q_slice
+                    ].reshape(-1, llama_conf.hidden_size),
+                    f"model.layers.{i_layer}.self_attn.k_proj.bias": qkv_bias[
+                        k_slice
+                    ].reshape(-1, llama_conf.hidden_size),
+                    f"model.layers.{i_layer}.self_attn.v_proj.bias": qkv_bias[
+                        v_slice
+                    ].reshape(-1, llama_conf.hidden_size)
+                }
+            index_dict, ref_state_dict = save_layer(
+                state_dict,
+                index_dict,
+                dir_path=tmp_save_dir,
+                filename=f"pytorch_model-{i_layer + 1}.bin",
+                check_reference=args.check_eq_hf,
+                ref_state_dict=ref_state_dict,
+            )
 
-    ### save index dict
-    index_dict["metadata"]["total_size"] *= {
-        torch.float32: 4,
-        torch.float16: 2,
-        torch.bfloat16: 2,
-    }[torch_dtype]
-    print(f"saving {tmp_save_dir}/pytorch_model.bin.index.json")
-    with open(f"{tmp_save_dir}/pytorch_model.bin.index.json", "w") as f:
-        json.dump(index_dict, f)
+        if not md.checkpoint_args.untie_embeddings_and_output_weights:  # tied embeddings and lm-head
+            state_dict = {
+                "model.norm.weight": queue_get("final norm")["weight"],
+                "lm_head.weight": embeddings
+                }
+        else:                                                           # untied embeddings and lm-head
+            state_dict = {
+                "model.norm.weight": queue_get("final norm")["weight"],
+                "lm_head.weight": pad_weight(queue_get("output layer")["weight"], md.true_vocab_size) 
+        }
+        index_dict, ref_state_dict = save_layer(
+            state_dict,
+            index_dict,
+            dir_path=tmp_save_dir,
+            filename="pytorch_model-lm-head.bin",
+            check_reference=args.check_eq_hf,
+            ref_state_dict=ref_state_dict,
+        )
+        # final check
+        assert (
+            not ref_state_dict
+        ), "reference state dict has additional layers not present in model."
 
-    ### load then save model in HF format
-    # Make space so we can load the model properly now.
-    del state_dict
-    gc.collect()
-    print(f"Loading the converted pytorch checkpoint in a Llama HF model from {tmp_save_dir}")
-    model = LlamaForCausalLM.from_pretrained(
-        str(tmp_save_dir), torch_dtype=torch.bfloat16, low_cpu_mem_usage=False # last arg requires a recent version of accelerate
-    )
+        ### save index dict
+        index_dict["metadata"]["total_size"] *= {
+            torch.float32: 4,
+            torch.float16: 2,
+            torch.bfloat16: 2,
+        }[torch_dtype]
+        print(f"saving {tmp_save_dir}/pytorch_model.bin.index.json")
+        with open(f"{tmp_save_dir}/pytorch_model.bin.index.json", "w") as f:
+            json.dump(index_dict, f)
+
+        ### load then save model in HF format
+        # Make space so we can load the model properly now.
+        del state_dict
+        gc.collect()
+        print(f"Loading the converted pytorch checkpoint in a Llama HF model from {tmp_save_dir}")
+        model = LlamaForCausalLM.from_pretrained(
+            str(tmp_save_dir), torch_dtype=torch.bfloat16, low_cpu_mem_usage=False # last arg requires a recent version of accelerate
+        )
+
     # Avoid saving this as part of the config.
     del model.config._name_or_path
     model.config.torch_dtype = torch.float16
@@ -351,7 +355,5 @@ def save_checkpoint(queue: mp.Queue, args):
     )
     print(f"Saving chat config to {args.save_dir}")
     generation_config.save_pretrained(args.save_dir)
+    queue_get()  # Recv final "exit" message so saver exits gracefully.
 
-    ### cleanup tmp
-    print(f"Deleting {tmp_save_dir}")
-    rmtree(tmp_save_dir)
