75a9043ac5ff0bee847b2c3ec2d32752c879b397
Jemma Stachelek <jemma.stachelek@gmail.com>
2025-03-12 12:14:35 -0600

    set hashlib.md5 security=False, see #1471

--- megatron/core/datasets/blended_dataset.py
+++ megatron/core/datasets/blended_dataset.py
@@ -80,7 +80,8 @@ class BlendedDataset(torch.utils.data.Dataset):
             unique_identifiers, indent=4, default=lambda obj: obj.unique_identifiers
         )
         self.unique_description_hash = hashlib.md5(
-            self.unique_description.encode("utf-8")
+            self.unique_description.encode("utf-8"),
+            usedforsecurity=False
         ).hexdigest()
 
         self.built_anew_on_cache_miss = False
