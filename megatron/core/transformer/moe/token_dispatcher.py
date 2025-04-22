0014f13713bfa38a8bab4ae11edd6917d35165ce
Pingtian Li <pingtianl@nvidia.com>
2025-04-22 06:32:00 -0700

    align deepep dispatch preprocess

--- megatron/core/transformer/moe/token_dispatcher.py
+++ megatron/core/transformer/moe/token_dispatcher.py
@@ -1139,7 +1139,7 @@ class MoEFlexTokenDispatcher(MoETokenDispatcher):
         routing_map, probs = self._initialize_metadata(routing_map, probs)
 
         self._comm_manager.setup_metadata(routing_map, probs)
-        return hidden_states, self._comm_manager.token_probs, None
+        return hidden_states, self._comm_manager.token_probs
 
     def dispatch_all_to_all(
         self,
