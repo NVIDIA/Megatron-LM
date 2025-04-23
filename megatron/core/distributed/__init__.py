1eaed211faa60304a80b50d86e2518617d16fcac
oliver könig <okoenig@nvidia.com>
2025-04-23 22:50:55 +0200

    Revert "ADLR/megatron-lm!2581 - Add support for ZeRO-2 with PyTorch FSDP2"
    This reverts commit 0ad06b525141f6b66992f36e2bd2d3234a3de598.

--- megatron/core/distributed/__init__.py
+++ megatron/core/distributed/__init__.py
@@ -6,4 +6,3 @@ from .distributed_data_parallel import DistributedDataParallel
 from .distributed_data_parallel_config import DistributedDataParallelConfig
 from .finalize_model_grads import finalize_model_grads
 from .torch_fully_sharded_data_parallel import TorchFullyShardedDataParallel
-from .torch_fully_sharded_data_parallel_config import TorchFullyShardedDataParallelConfig
