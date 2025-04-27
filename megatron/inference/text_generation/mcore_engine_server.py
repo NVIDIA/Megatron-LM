a768e50588f6e833741dcd25cfda64a8798f4d8e
Terry Kong <terryk@nvidia.com>
2025-04-27 13:36:20 -0700

    feat: sahil patch
    Signed-off-by: Terry Kong <terryk@nvidia.com>

--- megatron/inference/text_generation/mcore_engine_server.py
+++ megatron/inference/text_generation/mcore_engine_server.py
@@ -10,6 +10,8 @@ from megatron.core.inference.model_inference_wrappers.gpt.gpt_inference_wrapper 
 from megatron.core.inference.sampling_params import SamplingParams
 from megatron.inference.text_generation.communication import broadcast_float_list
 from megatron.inference.text_generation.tokenization import tokenize_prompts
+import torch
+from megatron.training import get_tokenizer
 
 
 class ModelInferenceWrapperServer(GPTInferenceWrapper):
@@ -44,7 +46,7 @@ class ModelInferenceWrapperServer(GPTInferenceWrapper):
 
 
 def run_mcore_engine(
-    engine, prompts=None, temperature=1.0, top_k=0, top_p=0.0, logprobs=True, tokens_to_generate=0
+    engine, prompts=None, prompt_tokens=None, prompt_tokens_tensor=None, prompt_lengths_tensor = None,temperature=1.0, top_k=0, top_p=0.0, logprobs=True, tokens_to_generate=0
 ):
     """Server-compatible version of the MCore Engine, used in
     tools/run_text_generation_server.py."""
@@ -66,9 +68,37 @@ def run_mcore_engine(
         num_tokens_to_generate=tokens_to_generate,
     )
 
-    context_tokens_tensor, context_length_tensor = tokenize_prompts(
-        prompts=prompts, tokens_to_generate=tokens_to_generate, add_BOS=False, data_parallel=False
-    )
+    if prompt_tokens_tensor is not None:
+        context_tokens_tensor = prompt_tokens_tensor
+        context_length_tensor = prompt_lengths_tensor
+    elif prompt_tokens is not None:
+        # Find max length for padding
+        max_length = max(len(tokens) for tokens in prompt_tokens)
+        # Pad all sequences to the max length
+        padded_tokens = []
+        # Get tokenizer once outside the loop to find the padding token
+        tokenizer = get_tokenizer()
+        pad_token = tokenizer.eod
+
+        prompts_length = [len(prompt_tokens_instance) for prompt_tokens_instance in prompt_tokens]
+        # Get the max prompts length.
+        max_prompt_len = max(prompts_length)
+        # Number of tokens in the each sample of the batch.
+        samples_length = max_prompt_len + tokens_to_generate
+        # Now update the list of list to be of the same size: samples_length.
+        for pt, pl in zip(prompt_tokens, prompts_length):
+            padding_size = samples_length - pl
+            pt.extend([pad_token] * padding_size)
+
+        # Now we are in a structured format, we can convert to tensors.
+        context_tokens_tensor = torch.tensor(prompt_tokens, dtype=torch.int64, device='cuda')
+        context_length_tensor = torch.tensor(prompts_length, dtype=torch.int64, device='cuda')
+    else:
+        assert prompts is not None, "Either prompts or prompt_tokens must be provided"
+        context_tokens_tensor, context_length_tensor = tokenize_prompts(
+            prompts=prompts, tokens_to_generate=tokens_to_generate, add_BOS=False, data_parallel=False
+        )
+    # print(context_tokens_tensor)
 
     tokenized_prompts = []
     for p, l in zip(context_tokens_tensor, context_length_tensor):
@@ -100,6 +130,7 @@ def run_mcore_engine(
             response_dict["logprobs"] = response_logprobs
         if sampling_params.return_segments:
             response_dict["segments"] = [x.segments for x in result]
+        response_dict["tokens"] = [(x.prompt_tokens + x.generated_tokens.tolist()) for x in result]
 
         return response_dict
     return None
