0504e973d88a9b20d7f7104e1ddd33ee00af5d1f
Nick Knight <nknight@nvidia.com>
2025-05-03 09:34:35 -0700

    ADLR/megatron-lm!2592 - Fix QK layer scaling for PP > 1

--- megatron/core/transformer/transformer_layer.py
+++ megatron/core/transformer/transformer_layer.py
@@ -125,7 +125,7 @@ class TransformerLayer(MegatronModule, BaseTransformerLayer):
         self.self_attention = build_module(
             submodules.self_attention,
             config=self.config,
-            layer_number=layer_number,
+            layer_number=self.layer_number,
             **attention_optional_kwargs,
         )
 
@@ -144,7 +144,7 @@ class TransformerLayer(MegatronModule, BaseTransformerLayer):
         self.cross_attention = build_module(
             submodules.cross_attention,
             config=self.config,
-            layer_number=layer_number,
+            layer_number=self.layer_number,
             **attention_optional_kwargs,
         )
 
