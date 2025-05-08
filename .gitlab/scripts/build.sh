09c83978b4d66dacceb80ef2e1745dac87110cb6
Oliver Koenig <okoenig@nvidia.com>
2025-05-07 17:14:54 -0700

    ADLR/megatron-lm!3263 - ci: Fixes to the release

--- .gitlab/scripts/build.sh
+++ .gitlab/scripts/build.sh
@@ -13,10 +13,9 @@ if [[ "$CI_COMMIT_BRANCH" == "ci-rebuild-mcore-nemo-image" || "$CI_COMMIT_BRANCH
     ADDITIONAL_PARAMS+=("--pull")
     ADDITIONAL_PARAMS+=("--cache-to type=registry,ref=${IMAGE}-buildcache:main,mode=max")
     ADDITIONAL_PARAMS+=("-t ${IMAGE}:main")
-else
-    ADDITIONAL_PARAMS+=("--cache-to type=registry,ref=${IMAGE}-buildcache:${CI_MERGE_REQUEST_IID:-$CI_COMMIT_REF_SLUG},mode=max")
+elif [[ -n "$CI_MERGE_REQUEST_IID" ]]; then
+    ADDITIONAL_PARAMS+=("--cache-to type=registry,ref=${IMAGE}-buildcache:${CI_MERGE_REQUEST_IID},mode=max")
     ADDITIONAL_PARAMS+=("-t ${IMAGE}:${CI_MERGE_REQUEST_IID}")
-
 fi
 
 if [[ "$CI_COMMIT_BRANCH" == "ci-nightly" ]]; then
