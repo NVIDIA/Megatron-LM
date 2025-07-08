7a8b7452cba5ee18ec38031606fc2eebbaf0da89
oliver könig <okoenig@nvidia.com>
2025-07-08 23:01:22 +0000

    Revert "update "ModelType.encoder_and_decoder" to "ModelType.encoder_or_decoder" since in previous PR we removed `encoder_or_decoder`"
    This reverts commit 9e49aa4446a58cc21c4dc0c5d0806551ad075ca7.

--- examples/mimo/train.py
+++ examples/mimo/train.py
@@ -218,7 +218,7 @@ if __name__ == "__main__":
     pretrain(
         train_valid_test_datasets_provider,
         model_provider,
-        ModelType.encoder_or_decoder,
+        ModelType.encoder_and_decoder,
         forward_step,
         args_defaults={},
         extra_args_provider=add_mimo_args,
