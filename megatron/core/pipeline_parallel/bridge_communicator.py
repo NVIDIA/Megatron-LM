2f408691def924531b079c7c27aa5ef735bdeaf3
ykarnati <ykarnati@nvidia.com>
2025-07-07 21:15:36 -0700

    send fwd recv fwd initial commit

--- megatron/core/pipeline_parallel/bridge_communicator.py
+++ megatron/core/pipeline_parallel/bridge_communicator.py
@@ -54,30 +54,62 @@ class BridgeCommunicator:
         self.dest_grid = dest_grid
         self.current_rank = dist.get_rank()
         self.comm_map: Dict[int, RankCommInfo] = {}
-        
-        # self.build_comm_schedule()
-
+       
         self.activation_gather_ranks, self.activation_gather_pg = self._create_activation_gather_scatter_pg(self.src_grid, is_src=True)
-        # self.activation_scatter_ranks, self.activation_scatter_pg = self._create_activation_gather_scatter_pg(self.dest_grid, is_src=False)
+        self.activation_scatter_ranks, self.activation_scatter_pg = self._create_activation_gather_scatter_pg(self.dest_grid, is_src=False)
+
+        self.src_tp_leaders, self.src_local_leader_rank = self.get_leader_rank(self.src_grid, is_src=True)
+        self.dest_tp_leaders, self.dest_local_leader_rank = self.get_leader_rank(self.dest_grid, is_src=False)
+
+        log_msg = (
+            f"[Rank {self.current_rank}] "
+            f"srcLeader={self.src_local_leader_rank} "
+            f"destLeader={self.dest_local_leader_rank} "
+            f"gatherGrpRanks={self.activation_gather_ranks} "
+            f"scatterGrpRanks={self.activation_scatter_ranks}"
+        )
+
+        print(log_msg, flush=True)
+        dist.barrier()
+
+        self.build_comm_schedule(self.src_tp_leaders, self.dest_tp_leaders)
         
+        if self.current_rank == self.src_local_leader_rank:
+            print(f"comm map  {self.current_rank} is {self.comm_map}")
+        dist.barrier()
 
     def get_leader_rank(self, grid: HyperCommGrid, is_src: bool) -> List[int]:
         """Get the leader rank for a given grid and direction."""
         leader_ranks = []
+        local_leader_rank = None
         # grid.gen_rank_enum(["tp", "cp", "pp"]) # vary tp & cp, freeze dp
         # returns a list of sublists, each sublist is a group of ranks that have different tp & cp & pp, same dp
         per_dp_replica_ranks = grid._gen_rank_enum([x for x in grid.dim_names if x != "dp"])
         if is_src:
             # Add rank from last pp stage
-            leader_ranks.extend(group[-1] for group in per_dp_replica_ranks)
+            ranks = []
+            for group in per_dp_replica_ranks:
+                if self.current_rank in group:
+                    assert local_leader_rank is None, "only one local leader rank is allowed per dp replica"
+                    local_leader_rank = group[-1]
+                ranks.append(group[-1])
+            leader_ranks.extend(ranks)
         else:
             # Add rank from first pp stage
-            leader_ranks.extend(group[0] for group in per_dp_replica_ranks)
-        return leader_ranks
+            ranks = []
+            for group in per_dp_replica_ranks:
+                if self.current_rank in group:
+                    assert local_leader_rank is None, "only one local leader rank is allowed per dp replica"
+                    local_leader_rank = group[0]
+                ranks.append(group[0])
+            leader_ranks.extend(ranks)
+        return leader_ranks, local_leader_rank
 
     def _create_activation_gather_scatter_pg(self, grid: HyperCommGrid, is_src: bool):
-   
-        current_rank  = dist.get_rank()
+
+        if self.current_rank < grid.rank_offset or self.current_rank >= grid.rank_offset + grid.size:
+            return [], None
+        
         pp_group_ranks = dist.get_process_group_ranks(grid.get_pg(['pp']))
 
         activation_comm_ranks = []
@@ -88,42 +120,36 @@ class BridgeCommunicator:
         # on the dest grid, all tp-cp ranks of first pp stage and dp replica that current rank belongs to
         # participtes in the activation scatter
 
-        if is_src and not current_rank == pp_group_ranks[-1]:
+        if is_src and not self.current_rank == pp_group_ranks[-1]:
             # if current rank belongs to src grid. If not belongs to last pp stage, return empty ranks and pg
             return activation_comm_ranks, activation_comm_pg
 
-        if not is_src and not current_rank == pp_group_ranks[0]:
+        if not is_src and not self.current_rank == pp_group_ranks[0]:
             # if current rank belongs to dest grid. If not belongs to first pp stage, return empty ranks and pg
             return activation_comm_ranks, activation_comm_pg  
 
         all_tpcp_group_ranks = grid._gen_rank_enum(['tp', 'cp'])
         for each_group_ranks in all_tpcp_group_ranks:
-            if current_rank in each_group_ranks:
+            if self.current_rank in each_group_ranks:
                 activation_comm_ranks = each_group_ranks
                 break
 
         activation_comm_pg = dist.new_group(ranks=activation_comm_ranks)
         # activation_comm_pg = grid.get_pg(['tp', 'cp'])
 
-        if current_rank == 0:
-            breakpoint()
-        dist.barrier()
-
         return activation_comm_ranks, activation_comm_pg
-
-
-
     
+    def is_current_rank_in_grid(self, grid: HyperCommGrid) -> bool:
+        """Check if the current rank is in the grid."""
+        return grid.rank_offset <= self.current_rank < grid.rank_offset + grid.size
 
-    def build_comm_schedule(self):
+    def build_comm_schedule(self, src_tp_leaders: List[int], dest_tp_leaders: List[int]):
         """Get src/dest tp leaders and populate comm_map for each rank.
         
         This method analyzes the source and destination grids to determine
         which ranks need to send/receive data and builds the communication
         schedule accordingly.
         """
-        src_tp_leaders = self.get_leader_rank(self.src_grid, is_src=True)
-        dest_tp_leaders = self.get_leader_rank(self.dest_grid, is_src=False)
         # Ensure that the number of leaders can be evenly divided
         src_count = len(src_tp_leaders)
         dest_count = len(dest_tp_leaders)
@@ -198,15 +224,7 @@ class BridgeCommunicator:
                             recv_shape=(1,)  # placeholder
                         )]
                     )
-        # Get the local rank (within the DP group) of the leader rank.
-        # dp_group_global_ranks = dist.get_process_group_ranks(self.dp_pg)
-        # for local_rank, global_rank in enumerate(dp_group_global_ranks):
-        #     if global_rank in self.comm_map and self.comm_map[global_rank].role == 'SENDER':
-        #         self.dp_leader_local_rank = local_rank
 
-        if dist.get_rank() == 0:
-            breakpoint()
-        dist.barrier()
 
     def send_forward(self, tensor_to_send: torch.Tensor):
         """Send forward activation tensor.
@@ -214,26 +232,23 @@ class BridgeCommunicator:
         Args:
             tensor_to_send: The tensor to send to the destination grid
         """
-        # Get current rank's communication info
+        if not self.is_current_rank_in_grid(self.src_grid):
+            raise ValueError(f"Rank {self.current_rank} is not in the source grid. Send forward is only allowed on src grid")
+        
         rank_info = self.comm_map.get(self.current_rank)
-        if not rank_info:
-            return
+
+        assert rank_info is not None, f"Rank {self.current_rank} is not in the comm map"
         
-        self._communicate_shapes(tensor_to_send_next=tensor_to_send)
-            
-        # Get DP group information
-        dp_size = dist.get_world_size(self.dp_pg)
-        
+        # self._communicate_shapes(tensor_to_send_next=tensor_to_send)
+
         if rank_info.role == 'SENDER':
-            # Current rank is a sender - gather tensors from all ranks in DP group
-            gathered_tensors = [torch.zeros_like(tensor_to_send) for _ in range(dp_size)]
-            dist.gather(tensor_to_send, gather_list=gathered_tensors, dst=self.dp_leader_local_rank, group=self.dp_pg)          
-            # Determine which grid this rank belongs to
-            current_grid = self.src_grid if self.current_rank in range(self.src_grid.rank_offset, 
-                                                                      self.src_grid.rank_offset + self.src_grid.size) else self.dest_grid
-            
-            # Reconstruct tensor properly handling TP/CP dimensions
-            aggregated_tensor = self._reconstruct_tensor_from_gathered(gathered_tensors, current_grid)
+            # Current rank is a sender - gather tensors from all ranks in activation_gather_group
+            assert self.current_rank == self.src_local_leader_rank, f"Rank {self.current_rank} is not the leader rank"
+            gathered_tensors = [torch.zeros_like(tensor_to_send) for _ in range(len(self.activation_gather_ranks))]
+            dist.gather(tensor_to_send, gather_list=gathered_tensors, dst=self.src_local_leader_rank, group=self.activation_gather_pg)
+
+            # TODO: ykarnati -  naive concat for now - FIXME
+            aggregated_tensor = torch.cat(gathered_tensors, dim=0)
             
             # Send splits to destination ranks
             num_sends = len(rank_info.sends)
@@ -245,10 +260,11 @@ class BridgeCommunicator:
                     end_idx = (i + 1) * split_size if i < num_sends - 1 else batch_size
                     tensor_split = aggregated_tensor[start_idx:end_idx]
                     # Send the tensor split to the destination rank
+                    print(f"rank {self.current_rank} sending tensor to dst rank {send_op.destination_rank} shape {tensor_split.shape} sum {tensor_split.sum()}")
                     dist.send(tensor_split, dst=send_op.destination_rank)
                     
-        elif rank_info.role == 'NOOP':
-            dist.gather(tensor_to_send, gather_list=None, dst=self.dp_leader_local_rank, group=self.dp_pg)
+        elif rank_info.role == 'NOOP' and self.current_rank in self.activation_gather_ranks:
+            dist.gather(tensor_to_send, gather_list=None, dst=self.src_local_leader_rank, group=self.activation_gather_pg)
                 
     def receive_forward(self, tensor_shape: Optional[Tuple[int, ...]] = None, 
                        dtype: Optional[torch.dtype] = None) -> torch.Tensor:
@@ -261,7 +277,50 @@ class BridgeCommunicator:
         Returns:
             torch.Tensor: The received activation tensor
         """
-        pass
+        # receive forward only gets called on the dest grid
+        if not self.is_current_rank_in_grid(self.dest_grid):
+            raise ValueError(f"Rank {self.current_rank} is not in the destination grid. Receive forward is only allowed on dest grid")
+        
+        rank_info = self.comm_map.get(self.current_rank)
+        assert rank_info is not None, f"Rank {self.current_rank} is not in the comm map"
+        
+        if rank_info.role == 'RECEIVER':
+            assert self.current_rank == self.dest_local_leader_rank, f"Rank {self.current_rank} is not the leader rank"
+            # p2p call to receive the tensor
+
+            received_tensors_list = []
+            for each_receive_op in rank_info.receives:
+                tensor_to_recv = torch.empty(tensor_shape, device=torch.cuda.current_device(), dtype=dtype)
+                dist.recv(tensor_to_recv, src=each_receive_op.source_rank)
+                print(f"rank {self.current_rank} received tensor from src rank {each_receive_op.source_rank} shape {tensor_to_recv.shape} sum {tensor_to_recv.sum()}")
+                received_tensors_list.append(tensor_to_recv)
+            
+            # TODO: ykarnati - naive concat for now - FIXME
+            aggregated_tensor = torch.cat(received_tensors_list, dim=0)
+            print(f"rank {self.current_rank} aggregated tensor shape {aggregated_tensor.shape} sum {aggregated_tensor.sum()}")
+            
+            scatter_list = list(torch.chunk(aggregated_tensor, chunks=len(self.activation_scatter_ranks), dim=0))
+            received_tensor = torch.empty_like(scatter_list[0])
+            print('*'*100)
+            print(f"rank {self.current_rank} scatter list shape {[x.shape for x in scatter_list]} doing the scatter in rank group {dist.get_process_group_ranks(self.activation_scatter_pg)}")
+            print('*'*100)
+            dist.scatter(received_tensor, scatter_list=scatter_list, src=self.dest_local_leader_rank, group=self.activation_scatter_pg)
+            print(f"current rank {self.current_rank} received tensor shape {received_tensor.shape}")
+ 
+        
+        elif rank_info.role == 'NOOP' and self.current_rank in self.activation_scatter_ranks:
+            # TODO: ykarnati - naive scatter for now - FIXME
+            # we dont always evenly divide the tensor into chunks. 
+            # This is WRONG - just to test the comms
+            scatter_shape = list(tensor_shape)
+            scatter_shape[0] //= len(self.activation_scatter_ranks)
+            
+            received_tensor = torch.empty(tuple(scatter_shape), device=torch.cuda.current_device(), dtype=dtype)
+            print(f"rank {self.current_rank} doing the scatter with received tensor shape {received_tensor.shape} in rank group {dist.get_process_group_ranks(self.activation_scatter_pg)}")
+            dist.scatter(received_tensor, scatter_list=None, src=self.dest_local_leader_rank, group=self.activation_scatter_pg)
+            print(f"current rank {self.current_rank} received tensor shape {received_tensor.shape}")
+
+     
 
     def send_backward(self, grad_tensor: torch.Tensor, variable_seq_lengths: bool = False):
         """Send backward gradient tensor.
@@ -498,3 +557,56 @@ class BridgeCommunicator:
                 recv_grad_shapes.append(tuple(shape))
         
         return recv_forward_shapes, recv_grad_shapes
+
+
+if __name__ == "__main__":
+    import os
+
+    if not dist.is_initialized():
+        dist.init_process_group(backend="nccl")
+    if torch.cuda.is_available():
+        torch.cuda.set_device(int(os.environ["LOCAL_RANK"]))
+
+    def create_hypercomm_grid(offset=0, tp=1, cp=1, pp=1, dp=1):
+        """Create a HyperCommGrid with tensor parallelism=2, context parallelism=2, and data parallelism=2.
+        
+        Returns:
+            HyperCommGrid: A grid configured with tp=2, cp=2, dp=2 (total size = 8).
+        """
+        # Set up environment for world size 8 if not already set
+        if "WORLD_SIZE" not in os.environ:
+            os.environ["WORLD_SIZE"] = "8"
+
+        grid = HyperCommGrid(
+            shape=[tp, cp, pp, dp],
+            dim_names=["tp", "cp", "pp", "dp"],
+            rank_offset=offset,
+            backend="nccl"
+        )
+        # print(grid)
+        _ = grid.create_pg(["tp"])
+        _ = grid.create_pg(["cp"])
+        _ = grid.create_pg(["pp"])
+        _ = grid.create_pg(["dp"])
+        return grid
+
+
+    grid1 = create_hypercomm_grid(offset=0, tp=2, cp=2, pp=1, dp=1)
+    grid2 = create_hypercomm_grid(offset=4, tp=2, cp=2, pp=1, dp=1)
+    bridge_communicator = BridgeCommunicator(grid1, grid2)
+    assert bridge_communicator.src_grid == grid1
+    assert bridge_communicator.dest_grid == grid2
+    assert bridge_communicator.current_rank == dist.get_rank()
+    # assert bridge_communicator.comm_map is not None
+
+    random_hidden_state = torch.randn(16, 256, 1024).cuda() # (batch_size, seq_len, hidden_size)
+    current_rank = dist.get_rank()
+    if bridge_communicator.is_current_rank_in_grid(bridge_communicator.src_grid):
+        bridge_communicator.send_forward(random_hidden_state)
+    else:
+        bridge_communicator.receive_forward(tensor_shape=(64, 256, 1024), dtype=random_hidden_state.dtype)
+        # recv fwd once we implement
+
+
+    # kill distributed
+    dist.destroy_process_group()
