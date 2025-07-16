fee4941e01b605eead518d10e0cead0ba6a3e385
Hongbin Liu <hongbinl@nvidia.com>
2025-07-15 22:09:12 -0700

    minor fix
    Signed-off-by: Hongbin Liu <hongbinl@nvidia.com>

--- megatron/core/models/gpt/gpt_model.py
+++ megatron/core/models/gpt/gpt_model.py
@@ -28,8 +28,6 @@ from megatron.core.transformer.transformer_block import TransformerBlock
 from megatron.core.transformer.transformer_config import TransformerConfig
 from megatron.core.utils import WrappedTensor, deprecate_inference_params
 
-from .fine_grained_schedule import ModelChunkSchedulePlan
-
 
 class GPTModel(LanguageModule):
     """GPT Transformer language model.
@@ -554,6 +552,8 @@ class GPTModel(LanguageModule):
         Returns:
             ModelChunkSchedulePlan: The model chunk schedule plan.
         """
+        
+        from .fine_grained_schedule import ModelChunkSchedulePlan
 
         return ModelChunkSchedulePlan(
             self,
