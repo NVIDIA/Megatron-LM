5cb6dbb3ed04e6fa11a862d90fe898ac2d3ddfad
oliver könig <okoenig@nvidia.com>
2025-08-12 17:11:42 +0000

    chore: Release 0.12.3
    Signed-off-by: oliver könig <okoenig@nvidia.com>

--- megatron/core/package_info.py
+++ megatron/core/package_info.py
@@ -3,27 +3,21 @@
 
 MAJOR = 0
 MINOR = 12
-PATCH = 3
-PRE_RELEASE = ''
+PATCH = 4
+PRE_RELEASE = ""
 
 # Use the following formatting: (major, minor, patch, pre-release)
 VERSION = (MAJOR, MINOR, PATCH, PRE_RELEASE)
 
-__shortversion__ = '.'.join(map(str, VERSION[:3]))
-__version__ = '.'.join(map(str, VERSION[:3])) + ''.join(VERSION[3:])
+__shortversion__ = ".".join(map(str, VERSION[:3]))
+__version__ = ".".join(map(str, VERSION[:3])) + "".join(VERSION[3:])
 
-__package_name__ = 'megatron_core'
-__contact_names__ = 'NVIDIA'
-__contact_emails__ = 'nemo-toolkit@nvidia.com'  # use NeMo Email
-__homepage__ = (
-    'https://docs.nvidia.com/deeplearning/nemo/user-guide/docs/en/stable/'  # use NeMo homepage
-)
-__repository_url__ = 'https://github.com/NVIDIA/Megatron-LM/megatron/core'
-__download_url__ = 'https://github.com/NVIDIA/Megatron-LM/releases'
-__description__ = (
-    'Megatron Core - a library for efficient and scalable training of transformer based models'
-)
-__license__ = 'BSD-3'
-__keywords__ = (
-    'deep learning, machine learning, gpu, NLP, NLU, language, transformer, nvidia, pytorch, torch'
-)
+__package_name__ = "megatron_core"
+__contact_names__ = "NVIDIA"
+__contact_emails__ = "nemo-toolkit@nvidia.com"  # use NeMo Email
+__homepage__ = "https://docs.nvidia.com/deeplearning/nemo/user-guide/docs/en/stable/"  # use NeMo homepage
+__repository_url__ = "https://github.com/NVIDIA/Megatron-LM/megatron/core"
+__download_url__ = "https://github.com/NVIDIA/Megatron-LM/releases"
+__description__ = "Megatron Core - a library for efficient and scalable training of transformer based models"
+__license__ = "BSD-3"
+__keywords__ = "deep learning, machine learning, gpu, NLP, NLU, language, transformer, nvidia, pytorch, torch"
