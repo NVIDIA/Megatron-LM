e2d65d5a3a01f6bd4d8050840c3550116369cc0a
Lawrence McAfee <lmcafee@nvidia.com>
2025-08-13 06:34:03 -0700

    compared generations/memory across suspend intervals.

--- examples/inference/gpt/gpt_dynamic_inference.py
+++ examples/inference/gpt/gpt_dynamic_inference.py
@@ -67,6 +67,8 @@ torch.serialization.add_safe_globals([megatron.core.rerun_state_machine.RerunDia
 
 # >>>
 from lutil import pax
+
+SUSPEND_RESUME_INTERVAL = 128
 # <<<
 
 
@@ -427,6 +429,9 @@ def main():
             unique_prompt_map[request.prompt_text].append(request_idx)
 
         # Print unique prompts + outputs.
+        # >>>
+        output_hashes = []
+        # <<<
         for unique_idx, (prompt_text, request_idxs) in enumerate(unique_prompt_map.items()):
             # ---- Prompt summary line ----
             prompt_len = len(requests[request_idxs[0]].prompt_tokens)
@@ -483,17 +488,17 @@ def main():
     peak_alloc_gb = stats["allocated_bytes.all.peak"] / 1024**3
     peak_resvd_gb = stats["reserved_bytes.all.peak"] / 1024**3
 
-    p_times = step_times["prefill"]
-    d_times = step_times["decode"]
+        p_times = step_times["prefill"]
+        d_times = step_times["decode"]
 
-    p_total = sum(p_times)
-    d_total = sum(d_times)
+        p_total = sum(p_times)
+        d_total = sum(d_times)
 
-    p_count = len(p_times)
-    d_count = len(d_times)
+        p_count = len(p_times)
+        d_count = len(d_times)
 
-    p_mean = p_total / p_count
-    d_mean = d_total / d_count
+        p_mean = p_total / p_count
+        d_mean = d_total / d_count if d_count != 0 else 0.
 
     # Commented out for now as the step/add/output times are not calculated correctly.
     # print(
