ae2a79c7cac03d19f75cde5182ee318af3b173d6
Siddharth Singh <sidsingh@nvidia.com>
2025-08-14 04:42:17 -0700

    modify warmup comment

--- megatron/core/inference/contexts/dynamic_context.py
+++ megatron/core/inference/contexts/dynamic_context.py
@@ -623,6 +623,7 @@ class DynamicInferenceContext(BaseInferenceContext):
             ), "the enforce_non_decode_mode flag is to be used only during cuda-graph capturing"
             assert self.non_decode_cuda_graphs, "Set non-decode cuda graphs to True"
         
+        # warmup both decode and non-decode engine steps
         if num_warmup_requests is not None:
             self.enforce_non_decode_mode = enforce_non_decode_mode
             assert self.is_decode_only() == (not enforce_non_decode_mode)
