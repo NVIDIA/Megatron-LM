d3df2380b280730faf6184336420734fd56f4c94
Youngeun Kwon <youngeunk@nvidia.com>
2025-08-18 03:28:52 -0700

    ADLR/megatron-lm!3814 - Apex.contrib.nccl_allocator migration

--- megatron/core/distributed/custom_fsdp/param_and_grad_buffer.py
+++ megatron/core/distributed/custom_fsdp/param_and_grad_buffer.py
@@ -32,10 +32,8 @@ try:
 except:
     pass
 
-try:
-    import apex.contrib.nccl_allocator as nccl_allocator
-except ImportError:
-    nccl_allocator = None
+import megatron.core.nccl_allocator as nccl_allocator
+
 NCCL_MEMORY_POOL = None
 
 
@@ -1190,12 +1188,20 @@ class ParamAndGradBuffer:
 
         self._log_parameter_groups()
 
+    def __del__(self):
+        # Free NCCL memory pool if this instance created it
+        global NCCL_MEMORY_POOL
+        if self.ddp_config.nccl_ub and NCCL_MEMORY_POOL is not None:
+            try:
+                del NCCL_MEMORY_POOL
+            except Exception as e:
+                logging.warning(f"Exception during NCCL memory pool destruction: {e}")
+
     def get_mem_alloc_context(self, group=None, additional_group=None):
         """
         Get the memory allocation context for the parameter and gradient buffers.
         """
         if self.ddp_config.nccl_ub:
-            assert nccl_allocator is not None, "NCCL allocator is not available."
             global NCCL_MEMORY_POOL
             if group is None:
                 # data parallel group is a default group for user buffer registration
