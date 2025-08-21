896f56819de29fcacb86f8325494e8e8ef84e9a8
shifangx <shifangx@nvidia.com>
2025-08-21 08:06:34 -0700

    delete BridgeCommunicatorInfo in MultiModulePipelineCommunicator

--- megatron/core/pipeline_parallel/multi_module_communicator.py
+++ megatron/core/pipeline_parallel/multi_module_communicator.py
@@ -14,15 +14,6 @@ Shape = Union[List[int], torch.Size]
 
 
 @dataclass
-class BridgeCommunicatorInfo:
-    """Information about a bridge communicator."""
-
-    bridge_communicator: BridgeCommunicator
-    src_module_name: str
-    dest_module_name: str
-
-
-@dataclass
 class RankModuleInfo:
     """Information about a rank in a module."""
 
@@ -33,8 +24,8 @@ class RankModuleInfo:
     # key is either the src or dst module name connected to the current module
     # one module may have multiple bridge communicators if it has multiple
     # incoming or outgoing connections.
-    bridge_comm_infos_as_src_module: Optional[List[BridgeCommunicatorInfo]]
-    bridge_comm_infos_as_dest_module: Optional[List[BridgeCommunicatorInfo]]
+    bridge_comms_as_src_module: Optional[List[BridgeCommunicator]]
+    bridge_comms_as_dest_module: Optional[List[BridgeCommunicator]]
     # the absolute first stage in the overall model
     # no incoming connections
     is_source_stage: Optional[bool] = True
@@ -82,29 +73,28 @@ class MultiModulePipelineCommunicator:
         self.current_rank = dist.get_rank()
 
         # Build bridge communicators for all modules
-        self.bridge_comm_infos = []
-        self._build_bridge_comm_infos()
+        self.bridge_comms = []
+        self._build_bridge_comms()
 
         self.rank_module_map = {}
         self._build_rank_module_info_map()
 
-    def _build_bridge_comm_infos(self):
-        """Construct and store BridgeCommunicatorInfo objects that describe the outgoing
-        communication relationships for all of the modules. Each info includes the source
-        and destination module names as well as their associated bridge_communicator.
+    def _build_bridge_comms(self):
+        """Construct and store BridgeCommunicator objects that describe the outgoing
+        communication relationships for all of the modules.
         """
         for src_module_name, src_grid in self.module_to_grid_map.items():
             for dest_module_name in self.topology[src_module_name]:
                 dest_grid = self.module_to_grid_map[dest_module_name]
                 bridge_comm = BridgeCommunicator(
-                    src_grid=src_grid, dest_grid=dest_grid, dim_mapping=self.dim_mapping, comm_dtype=self.config.pipeline_dtype
-                )
-                bridge_comm_info = BridgeCommunicatorInfo(
-                    bridge_communicator=bridge_comm,
+                    src_grid=src_grid,
+                    dest_grid=dest_grid,
+                    dim_mapping=self.dim_mapping,
+                    comm_dtype=self.config.pipeline_dtype,
                     src_module_name=src_module_name,
                     dest_module_name=dest_module_name,
                 )
-                self.bridge_comm_infos.append(bridge_comm_info)
+                self.bridge_comms.append(bridge_comm)
 
     def is_current_rank_in_grid(self, grid: HyperCommGrid) -> bool:
         """Check if the current rank is in the grid."""
@@ -124,31 +114,27 @@ class MultiModulePipelineCommunicator:
                 rank_in_pp_group = dist.get_group_rank(pp_group, self.current_rank)
                 pp_stage = rank_in_pp_group % pp_size
 
-                bridge_comm_infos_as_dest_module = []
-                bridge_comm_infos_as_src_module = []
+                bridge_comms_as_dest_module = []
+                bridge_comms_as_src_module = []
                 # If first stage, check if the module has any incoming modules
                 # If so, initialize bridge communicator
                 if pp_stage == 0:
-                    for bridge_comm_info in self.bridge_comm_infos:
-                        if bridge_comm_info.bridge_communicator.is_current_rank_in_grid(
-                            bridge_comm_info.bridge_communicator.dest_grid
-                        ):
-                            bridge_comm_infos_as_dest_module.append(bridge_comm_info)
+                    for bridge_comm in self.bridge_comms:
+                        if bridge_comm.is_current_rank_in_grid(bridge_comm.dest_grid):
+                            bridge_comms_as_dest_module.append(bridge_comm)
                 # If last stage, check if the module has any outgoing modules
                 # If so, initialize bridge communicator
                 if pp_stage == pp_size - 1:
-                    for bridge_comm_info in self.bridge_comm_infos:
-                        if bridge_comm_info.bridge_communicator.is_current_rank_in_grid(
-                            bridge_comm_info.bridge_communicator.src_grid
-                        ):
-                            bridge_comm_infos_as_src_module.append(bridge_comm_info)
+                    for bridge_comm in self.bridge_comms:
+                        if bridge_comm.is_current_rank_in_grid(bridge_comm.src_grid):
+                            bridge_comms_as_src_module.append(bridge_comm)
                 # Build RankModuleInfo for the module
                 rank_module_info = RankModuleInfo(
                     pp_stage=pp_stage,
                     pp_size=pp_size,
                     p2p_communicator=p2p_comm,
-                    bridge_comm_infos_as_dest_module=bridge_comm_infos_as_dest_module,
-                    bridge_comm_infos_as_src_module=bridge_comm_infos_as_src_module,
+                    bridge_comms_as_dest_module=bridge_comms_as_dest_module,
+                    bridge_comms_as_src_module=bridge_comms_as_src_module,
                 )
                 self.rank_module_map[module_name] = rank_module_info
 
@@ -167,10 +153,8 @@ class MultiModulePipelineCommunicator:
             if rank_module_info.pp_stage == 0:
                 # If first stage, and has incoming modules, receive forward activation
                 # from incoming modules.
-                for bridge_comm_info in rank_module_info.bridge_comm_infos_as_dest_module:
-                    input_dict[bridge_comm_info.src_module_name] = (
-                        bridge_comm_info.bridge_communicator.recv_forward()
-                    )
+                for bridge_comm in rank_module_info.bridge_comms_as_dest_module:
+                    input_dict[bridge_comm.src_module_name] = bridge_comm.recv_forward()
             else:
                 # If not first stage, receive forward activation tensor from P2P communicator.
                 input_dict[module_name] = rank_module_info.p2p_communicator.recv_forward(
@@ -188,8 +172,8 @@ class MultiModulePipelineCommunicator:
             if rank_module_info.pp_stage == rank_module_info.pp_size - 1:
                 # If last stage, and has outgoing modules, send forward activation
                 # by using bridge communicator.
-                for bridge_comm_info in rank_module_info.bridge_comm_infos_as_src_module:
-                    bridge_comm_info.bridge_communicator.send_forward(output_dict[module_name])
+                for bridge_comm in rank_module_info.bridge_comms_as_src_module:
+                    bridge_comm.send_forward(output_dict[module_name])
             else:
                 # If not last stage, send forward activation by using P2P communicator.
                 rank_module_info.p2p_communicator.send_forward(
@@ -213,11 +197,9 @@ class MultiModulePipelineCommunicator:
             if rank_module_info.pp_stage == rank_module_info.pp_size - 1:
                 # If last stage, and has outgoing modules, send forward activation and
                 # receive backward gradient by using bridge communicator.
-                for bridge_comm_info in rank_module_info.bridge_comm_infos_as_src_module:
-                    grad_dict[bridge_comm_info.src_module_name] = (
-                        bridge_comm_info.bridge_communicator.send_forward_recv_backward(
-                            output_dict[module_name]
-                        )
+                for bridge_comm in rank_module_info.bridge_comms_as_src_module:
+                    grad_dict[bridge_comm.src_module_name] = bridge_comm.send_forward_recv_backward(
+                        output_dict[module_name]
                     )
             else:
                 # If not last stage, send forward activation and receive backward gradient
@@ -244,12 +226,12 @@ class MultiModulePipelineCommunicator:
         input_dict = {}
         for module_name, rank_module_info in self.rank_module_map.items():
             if rank_module_info.pp_stage == 0:
-                for bridge_comm_info in rank_module_info.bridge_comm_infos_as_dest_module:
+                for bridge_comm in rank_module_info.bridge_comms_as_dest_module:
                     # If first stage, and has incoming modules, send backward gradient and
                     # receive forward activation by using bridge communicator.
-                    input_dict[bridge_comm_info.src_module_name] = (
-                        bridge_comm_info.bridge_communicator.send_backward_recv_forward(
-                            grad_dict[bridge_comm_info.src_module_name]
+                    input_dict[bridge_comm.src_module_name] = (
+                        bridge_comm.send_backward_recv_forward(
+                            grad_dict[bridge_comm.src_module_name]
                         )
                     )
             else:
@@ -276,10 +258,8 @@ class MultiModulePipelineCommunicator:
             if rank_module_info.pp_stage == rank_module_info.pp_size - 1:
                 # If last stage, and has incoming modules, receive backward gradient
                 # by using bridge communicator.
-                for bridge_comm_info in rank_module_info.bridge_comm_infos_as_src_module:
-                    grad_dict[bridge_comm_info.src_module_name] = (
-                        bridge_comm_info.bridge_communicator.recv_backward()
-                    )
+                for bridge_comm in rank_module_info.bridge_comms_as_src_module:
+                    grad_dict[bridge_comm.src_module_name] = bridge_comm.recv_backward()
             else:
                 # If not last stage, receive backward gradient by using P2P communicator.
                 grad_dict[module_name] = rank_module_info.p2p_communicator.recv_backward(
@@ -297,10 +277,8 @@ class MultiModulePipelineCommunicator:
             if rank_module_info.pp_stage == 0:
                 # If first stage, and has incoming modules, send backward activation
                 # by using bridge communicator.
-                for bridge_comm_info in rank_module_info.bridge_comm_infos_as_dest_module:
-                    bridge_comm_info.bridge_communicator.send_backward(
-                        grad_dict[bridge_comm_info.src_module_name]
-                    )
+                for bridge_comm in rank_module_info.bridge_comms_as_dest_module:
+                    bridge_comm.send_backward(grad_dict[bridge_comm.src_module_name])
             else:
                 # If not first stage, send backward activation by using P2P communicator.
                 rank_module_info.p2p_communicator.send_backward(
