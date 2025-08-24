c7fd91ac688d8d71c51b0fc75b0cc49492acff15
oliver könig <okoenig@nvidia.com>
2025-08-24 19:12:18 +0000

    ci(hotfix): Increase n_nondeterminism_attemps
    Signed-off-by: oliver könig <okoenig@nvidia.com>

--- tests/test_utils/python_scripts/launch_jet_workload.py
+++ tests/test_utils/python_scripts/launch_jet_workload.py
@@ -374,7 +374,7 @@ def main(
     n_attempts = 0
     n_nondeterminism_attemps = 0
     n_iteration = 0
-    while True and n_attempts < 3 and n_nondeterminism_attemps < 2:
+    while True and n_attempts < 3 and n_nondeterminism_attemps < 4:
         pipeline = launch_and_wait_for_completion(
             test_case=test_case,
             environment=environment,
@@ -479,7 +479,7 @@ def main(
 
             if is_flaky_failure(concat_allranks_logs):
                 logger.error("Detected flaky failure, attempt restart.")
-                n_attempts += 1
+                n_nondeterminism_attemps += 1
                 continue
 
             if "FAILED tests/functional_tests/python_test_utils" in concat_mainrank_log:
