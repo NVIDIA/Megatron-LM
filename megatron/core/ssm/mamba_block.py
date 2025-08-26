cae79ec8c65a552a06a8b62b809146adbdefd961
Keshav Santhanam <ksanthanam@nvidia.com>
2025-08-26 00:40:27 -0700

    fix

--- megatron/core/ssm/mamba_block.py
+++ megatron/core/ssm/mamba_block.py
@@ -316,7 +316,7 @@ class MambaStack(MegatronModule):
         outer_fp8_context = get_fp8_context(self.config) if use_outer_fp8_context else nullcontext()
 
         with outer_fp8_context:
-            for layer in enumerate(self.layers):
+            for layer in self.layers:
                 inner_fp8_context = (
                     get_fp8_context(self.config, layer.layer_number - 1)
                     if use_inner_fp8_context
