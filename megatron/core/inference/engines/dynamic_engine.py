19db79c64f89123c90cf2708593d981552aecb3c
oliver könig <okoenig@nvidia.com>
2025-08-30 20:45:25 +0000

    Revert "ADLR/megatron-lm!3688 - Non-decode CUDA graphs for the dynamic inference backend."
    This reverts commit 256c8554d8d71b5e6e715345c48a620e17b8b296.

--- megatron/core/inference/engines/dynamic_engine.py
+++ megatron/core/inference/engines/dynamic_engine.py
@@ -20,7 +20,6 @@ from megatron.core.inference.contexts.dynamic_context import (
     MaxSequenceLengthOverflowError,
     RequestOverflowError,
     TokenOverflowError,
-    WarmupEngineMode,
 )
 from megatron.core.inference.data_parallel_inference_coordinator import (
     DataParallelInferenceCoordinator,
@@ -124,58 +123,40 @@ class DynamicInferenceEngine(AbstractEngine):
 
             print(
                 "> dynamic_engine.py: building cuda graphs for %d batch size(s): %s."
-                % (len(context.cuda_graph_token_counts), context.cuda_graph_token_counts)
+                % (len(context.cuda_graph_request_counts), context.cuda_graph_request_counts)
             )
-            for warmup_engine_mode in [WarmupEngineMode.DECODE, WarmupEngineMode.NON_DECODE]:
-                # Iterate cuda graph dims.
-                if (
-                    warmup_engine_mode == WarmupEngineMode.NON_DECODE
-                    and not context.non_decode_cuda_graphs
-                ):
-                    continue
-                tbar = enumerate(context.cuda_graph_token_counts)
+
+            # Iterate cuda graph dims.
+            tbar = enumerate(context.cuda_graph_request_counts)
+            if HAVE_TQDM:
+                tbar = tqdm(tbar, total=len(context.cuda_graph_request_counts))
+            for tbar_idx, cuda_graph_request_count in tbar:
+
+                # Initialize attention state.
+                context.initialize_attention_state(num_warmup_requests=cuda_graph_request_count)
+                assert (
+                    cuda_graph_request_count == context.padded_active_token_count
+                ), f"{cuda_graph_request_count} vs. {context.padded_active_token_count}."
+                assert context.is_decode_only(), "Decode-only required for cuda graph capture."
+
+                # Progress.
+                tbar_str = f"cuda graph warmup, d {cuda_graph_request_count}"
                 if HAVE_TQDM:
-                    tbar = tqdm(tbar, total=len(context.cuda_graph_token_counts))
-                for tbar_idx, cuda_graph_token_count in tbar:
-                    if (
-                        cuda_graph_token_count == 1
-                        and warmup_engine_mode == WarmupEngineMode.NON_DECODE
-                    ):
-                        # This case is not supported`` as we require atleast two
-                        # tokens for a non-decode engine step.
-                        continue
-                    # Initialize attention state.
-                    context.initialize_attention_state(
-                        num_warmup_tokens=cuda_graph_token_count,
-                        warmup_engine_mode=warmup_engine_mode,
+                    tbar.set_description(tbar_str)
+                else:
+                    print(f"{tbar_idx}/{len(context.cuda_graph_request_counts)}. {tbar_str}")
+
+                # Get flat tokens, position ids.
+                input_ids, position_ids = context.current_input_and_position_ids(
+                    num_warmup_tokens=cuda_graph_request_count
+                )
+
+                # Forward pass -> logits.
+                with torch.inference_mode():
+                    logits = controller.inference_wrapped_model.run_one_forward_step(
+                        {"tokens": input_ids, "position_ids": position_ids, "attention_mask": None}
                     )
-                    assert (
-                        cuda_graph_token_count == context.padded_active_token_count
-                    ), f"{cuda_graph_token_count} vs. {context.padded_active_token_count}."
-
-                    # Progress.
-                    mode_str = warmup_engine_mode.name.lower()
-                    tbar_str = f"cuda graph warmup - {mode_str}, d {cuda_graph_token_count}"
-                    if HAVE_TQDM:
-                        tbar.set_description(tbar_str)
-                    else:
-                        print(f"{tbar_idx}/{len(context.cuda_graph_token_counts)}. {tbar_str}")
-
-                    # Get flat tokens, position ids.
-                    input_ids, position_ids = context.current_input_and_position_ids(
-                        num_warmup_tokens=cuda_graph_token_count
-                    )
-
-                    # Forward pass -> logits.
-                    with torch.inference_mode():
-                        controller.inference_wrapped_model.run_one_forward_step(
-                            {
-                                "tokens": input_ids,
-                                "position_ids": position_ids,
-                                "attention_mask": None,
-                            }
-                        )
-                        context.reset()  # todo: @lmcafee, remove if unnecessary.
+                    context.reset()  # todo: @lmcafee, remove if unnecessary.
 
             # Create cuda graphs.
             with torch.inference_mode():
@@ -489,7 +470,6 @@ class DynamicInferenceEngine(AbstractEngine):
         prev_is_decode_only = self.context.is_decode_only()
         prev_total_request_count = self.context.total_request_count
         prev_paused_request_count = self.context.paused_request_count
-        prev_active_token_count = self.context.active_token_count
 
         range_push("Prefill" if not prev_is_decode_only else "Decode")
 
@@ -527,7 +507,6 @@ class DynamicInferenceEngine(AbstractEngine):
         if verbose:
             context = self.context
             mem = torch.cuda.memory_stats()
-            step_type = "decode" if is_decode_only else "non-decode"
             output_str = (
                 "* step %d | %s ... time: %.3f%s ... "
                 "reqs: %d [ gtd %d, active %d, paused %d, finished %d ] ... "
@@ -537,16 +516,20 @@ class DynamicInferenceEngine(AbstractEngine):
                     datetime.now().strftime("%H:%M:%S"),
                     step_time,
                     (
-                        " [%s + cuda graph %s]"
-                        % (
-                            step_type,
-                            (
+                        (
+                            " [decode + cuda graph %s]"
+                            % (
                                 "DIM %d:%d"
-                                % (context.padded_active_token_count, prev_active_token_count)
-                                if self.context.using_cuda_graph_this_step()
+                                % (
+                                    context.padded_active_request_count,
+                                    prev_total_request_count - prev_paused_request_count,
+                                )
+                                if self.enable_cuda_graph
                                 else "OFF"
-                            ),
+                            )
                         )
+                        if prev_is_decode_only
+                        else "[prefill]"
                     ),
                     prev_total_request_count,
                     context.gtd_request_count,
--- megatron/core/transformer/cuda_graphs.py
+++ megatron/core/transformer/cuda_graphs.py
@@ -586,17 +586,8 @@ class _CudaGraphRunner(torch.nn.Module):
 
         # Initialize inference context.
         if inference_context and inference_context.is_dynamic_batching():
-            # to prevent circular imports
-            from megatron.core.inference.contexts.dynamic_context import WarmupEngineMode
-
-            num_warmup_tokens = kwargs["hidden_states"].size(0)
-            is_decode_only = kwargs["dynamic_inference_decode_only"]
-            inference_context.initialize_attention_state(
-                num_warmup_tokens=num_warmup_tokens,
-                warmup_engine_mode=(
-                    WarmupEngineMode.DECODE if is_decode_only else WarmupEngineMode.NON_DECODE
-                ),
-            )
+            num_warmup_requests = kwargs["hidden_states"].size(0)
+            inference_context.initialize_attention_state(num_warmup_requests=num_warmup_requests)
 
         context = (
             torch.cuda.graph(cuda_graph=graph, pool=pool) if graph is not None else nullcontext()
