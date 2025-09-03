34f63cf77ccc585a5abc15cf7f537eaaa3f10072
qun chen <chenqun@zhejianglab.com>
2025-09-03 13:47:36 +0800

    add support for packing-with-padding

--- pretrain_gpt.py
+++ pretrain_gpt.py
@@ -189,6 +189,7 @@ def core_gpt_dataset_config_from_args(args):
         create_attention_mask=args.create_attention_mask_in_dataloader,
         object_storage_cache_path=args.object_storage_cache_path,
         mid_level_dataset_surplus=args.mid_level_dataset_surplus,
+        use_truncateless_packing=args.use_truncateless_packing,
     )
 
 
