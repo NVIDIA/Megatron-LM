55a528e907399731598d957a08f000e72e3880c9
Teodor-Dumitru Ene <tene@nvidia.com>
2025-09-09 12:02:19 -0500

    Fix pipeline

--- megatron/core/inference/unified_memory.py
+++ megatron/core/inference/unified_memory.py
@@ -8,13 +8,24 @@ import torch
 from torch.cuda.memory import CUDAPluggableAllocator
 from torch.utils.cpp_extension import CUDA_HOME, load_inline
 
+from megatron.core.utils import is_torch_min_version
+
+try:
+    if is_torch_min_version("2.8.0"):
+        from torch.cuda.memory import MemPool
+    else:
+        from torch.cuda import MemPool
+    has_mem_pool = True
+except ImportError:
+    has_mem_pool = False
+
 src = r"""
 #include <cuda_runtime_api.h>
 #include <cstddef>
 
 #define EXPORT extern "C"
 
-EXPORT void* managed_malloc(ssize_t size, int device, void* stream) {
+EXPORT void* managed_malloc(size_t size, int device, void* stream) {
   (void)stream;
   int cur = -1;
   cudaGetDevice(&cur);
@@ -31,31 +42,31 @@ EXPORT void* managed_malloc(ssize_t size, int device, void* stream) {
   return ptr;
 }
 
-EXPORT void managed_free(void* ptr, ssize_t size, int device, void* stream) {
+EXPORT void managed_free(void* ptr, size_t size, int device, void* stream) {
   (void)size; (void)device; (void)stream;
   if (ptr) cudaFree(ptr);
 }
 """
 
-extra_ldflags = ["-lcudart"]
-if CUDA_HOME:
-    cuda_lib = os.path.join(CUDA_HOME, "lib64")
-    if os.path.isdir(cuda_lib):
-        extra_ldflags = [f"-L{cuda_lib}", "-lcudart"]
+unified_memory_mempool = None
 
-try:
-    mod = load_inline(
-        name="managed_alloc_runtime",
-        cpp_sources=[src],
-        functions=[]
-        with_cuda=True,
-        extra_ldflags=extra_ldflags,
-        verbose=False,
-    )
-
-    so_path = Path(mod.__file__).as_posix()
-    alloc = CUDAPluggableAllocator(so_path, "managed_malloc", "managed_free").allocator()
-    unified_memory_mempool = torch.cuda.MemPool(allocator=alloc)
-except RuntimeError:
-    warnings.warn("Unified memory mempool is not available.")
-    unified_memory_mempool = None
+if has_mem_pool:
+    extra_ldflags = ["-lcudart"]
+    if CUDA_HOME:
+        cuda_lib = os.path.join(CUDA_HOME, "lib64")
+        if os.path.isdir(cuda_lib):
+            extra_ldflags = [f"-L{cuda_lib}", "-lcudart"]
+    try:
+        mod = load_inline(
+            name="managed_alloc_runtime",
+            cpp_sources=[src],
+            functions=[],
+            with_cuda=True,
+            extra_ldflags=extra_ldflags,
+            verbose=False,
+        )
+        so_path = Path(mod.__file__).as_posix()
+        alloc = CUDAPluggableAllocator(so_path, "managed_malloc", "managed_free").allocator()
+        unified_memory_mempool = MemPool(allocator=alloc)
+    except (RuntimeError, ImportError):
+        warnings.warn("Unified memory mempool is not available.")
