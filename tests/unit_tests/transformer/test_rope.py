f8cf909fa8d78b6156623cfe607f531e197802a1
Vasudevan Rengasamy <vrengasamy@nvidia.com>
2025-09-15 12:21:40 -0700

    Skip test if Fused QKV RoPE not available

--- tests/unit_tests/transformer/test_rope.py
+++ tests/unit_tests/transformer/test_rope.py
@@ -120,6 +120,7 @@ class TestQKVRotaryEmbedding:
         assert self.rope_gpu_init.inv_freq.device.type == 'cuda'
 
     @pytest.mark.skipif(not torch.cuda.is_available(), reason="CUDA not available")
+    @pytest.mark.skipif(not HAVE_FUSED_QKV_ROPE, reason="Fused QKV RoPE not available.")
     def test_gpu_forward(self):
         pos_embed = self.rope_gpu_init(self.seq_len)
         assert pos_embed.shape[0] == self.seq_len
