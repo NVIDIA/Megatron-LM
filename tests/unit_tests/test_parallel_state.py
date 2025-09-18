ff5600b6b79396237c08305ac8b32dbdb5c49cea
Parth Mannan <pmannan@nvidia.com>
2025-09-17 17:12:46 -0700

    Fix arg name in test

--- tests/unit_tests/test_parallel_state.py
+++ tests/unit_tests/test_parallel_state.py
@@ -515,7 +515,7 @@ def test_hybrid_dp_cp_groups(world_size, tp_size, cp_size, dp_size):
     Utils.initialize_model_parallel(
         tensor_model_parallel_size=tp_size,
         context_parallel_size=cp_size,
-        hybrid_data_context_parallel=True,
+        hybrid_context_parallel=True,
     )
 
     dp_cp_size = parallel_state.get_data_parallel_world_size(with_context_parallel=True)
