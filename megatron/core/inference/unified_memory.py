671855616c3a3c79928477551d3e3b68ee9b9b55
Lawrence McAfee <lmcafee@nvidia.com>
2025-09-22 02:45:12 -0700

    clean up.

--- megatron/core/inference/unified_memory.py
+++ megatron/core/inference/unified_memory.py
@@ -18,51 +18,6 @@ try:
 except ImportError:
     _has_mem_pool = False
 
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-# _mempool_c_src = r"""
-# #include <cuda_runtime_api.h>
-# #include <cstddef>
-
-# #define EXPORT extern "C"
-
-# EXPORT void* managed_malloc(size_t size, int device, void* stream) {
-#   // >>>
-#   printf("******************************* inside managed_malloc.\n");
-#   // <<<
-#   (void)stream;
-#   int cur = -1;
-#   cudaGetDevice(&cur);
-#   if (device != cur && device >= 0) cudaSetDevice(device);
-
-#   // cudaMallocManaged allows for more memory to be allocated than the device memory size.
-#   // The cudaMemAttachGlobal flag makes the memory accessible from both host and device.
-#   void* ptr = nullptr;
-#   cudaError_t err = cudaMallocManaged(&ptr, (size_t)size, cudaMemAttachGlobal);
-#   if (err != cudaSuccess) return nullptr;
-
-#   if (device >= 0) {
-#     // cudaMemAdviseSetPreferredLocation sets the preferred location for the memory.
-#     // This is a hint that tries to prevent data from being migrated away from the device.
-#     cudaMemAdvise(ptr, (size_t)size, cudaMemAdviseSetPreferredLocation, device);
-#     // cudaMemAdviseSetAccessedBy ensures the memory always lives in the device's page table.
-#     // Even if the memory has to be migrated away from the device, it still does not page fault.
-#     // The CUDA docs claim that cudaMemAdviseSetPreferredLocation completely overrides this flag,
-#     // but there is no harm in adding this flag as well for future-proofing.
-#     cudaMemAdvise(ptr, (size_t)size, cudaMemAdviseSetAccessedBy, device);
-#   }
-#   return ptr;
-# }
-
-# EXPORT void managed_free(void* ptr, size_t size, int device, void* stream) {
-#   // >>>
-#   printf("******************************* inside managed_free.\n");
-#   // <<<
-#   // Memory allocated with cudaMallocManaged should be released with cudaFree.
-#   (void)size; (void)device; (void)stream;
-#   if (ptr) cudaFree(ptr);
-# }
-# """
-# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 _mempool_c_src = r"""
 #include <cuda_runtime_api.h>
 #include <cstddef>
@@ -70,22 +25,42 @@ _mempool_c_src = r"""
 #define EXPORT extern "C"
 
 EXPORT void* managed_malloc(size_t size, int device, void* stream) {
+  // >>>
   printf("******************************* inside managed_malloc.\n");
+  // <<<
+  (void)stream;
+  int cur = -1;
+  cudaGetDevice(&cur);
+  if (device != cur && device >= 0) cudaSetDevice(device);
+
+  // cudaMallocManaged allows for more memory to be allocated than the device memory size.
+  // The cudaMemAttachGlobal flag makes the memory accessible from both host and device.
   void* ptr = nullptr;
-  cudaError_t err = cudaMalloc(&ptr, size);
-  if (err != cudaSuccess) {
-    printf("******************************* ERROR.\n");
-    return nullptr;
+  cudaError_t err = cudaMallocManaged(&ptr, (size_t)size, cudaMemAttachGlobal);
+  if (err != cudaSuccess) return nullptr;
+
+  if (device >= 0) {
+    // cudaMemAdviseSetPreferredLocation sets the preferred location for the memory.
+    // This is a hint that tries to prevent data from being migrated away from the device.
+    cudaMemAdvise(ptr, (size_t)size, cudaMemAdviseSetPreferredLocation, device);
+    // cudaMemAdviseSetAccessedBy ensures the memory always lives in the device's page table.
+    // Even if the memory has to be migrated away from the device, it still does not page fault.
+    // The CUDA docs claim that cudaMemAdviseSetPreferredLocation completely overrides this flag,
+    // but there is no harm in adding this flag as well for future-proofing.
+    cudaMemAdvise(ptr, (size_t)size, cudaMemAdviseSetAccessedBy, device);
   }
   return ptr;
 }
 
 EXPORT void managed_free(void* ptr, size_t size, int device, void* stream) {
+  // >>>
   printf("******************************* inside managed_free.\n");
+  // <<<
+  // Memory allocated with cudaMallocManaged should be released with cudaFree.
+  (void)size; (void)device; (void)stream;
   if (ptr) cudaFree(ptr);
 }
 """
-# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 # Avoid linting errors.
 has_unified_memory = False
@@ -99,20 +74,15 @@ if _has_mem_pool:
         if os.path.isdir(_cuda_lib):
             _extra_ldflags = [f"-L{_cuda_lib}", "-lcudart"]
     try:
-        # >>>
-        # _mod = load_inline(
-        #     name="managed_alloc_runtime",
-        #     cpp_sources=[_mempool_c_src],
-        #     functions=[],
-        #     with_cuda=True,
-        #     extra_ldflags=_extra_ldflags,
-        #     verbose=False,
-        # )
-        # _so_path = Path(_mod.__file__).as_posix()
-        # +++
-        _so_path = "/lustre/fsw/portfolios/adlr/users/lmcafee/inference/megatrons/unified-memory/scripts_ignore_me/c/lib.so"
-        # <<<
-        # >>>
+        _mod = load_inline(
+            name="managed_alloc_runtime",
+            cpp_sources=[_mempool_c_src],
+            functions=[],
+            with_cuda=True,
+            extra_ldflags=_extra_ldflags,
+            verbose=False,
+        )
+        _so_path = Path(_mod.__file__).as_posix()
         _alloc = CUDAPluggableAllocator(_so_path, "managed_malloc", "managed_free").allocator()
         has_unified_memory = True
     except (RuntimeError, ImportError, OSError):
