27f40be1939c2a7580caa928d7ec45b2f56e6863
oliver könig <okoenig@nvidia.com>
2025-09-22 14:10:37 +0000

    nemo-run
    Signed-off-by: oliver könig <okoenig@nvidia.com>

--- tests/test_utils/python_scripts/generate_local_jobs.py
+++ tests/test_utils/python_scripts/generate_local_jobs.py
@@ -9,25 +9,30 @@ import pathlib
 from typing import Optional
 
 import click
-import jetclient
 import yaml
-
 from tests.test_utils.python_scripts import common
 
 
 def load_script(config_path: str) -> str:
     with open(config_path) as stream:
         try:
-            return jetclient.JETWorkloadManifest(**yaml.safe_load(stream)).spec.script
+            return yaml.safe_load(stream)["spec"]["script"]
         except yaml.YAMLError as exc:
             raise exc
 
 
 @click.command()
-@click.option("--model", required=False, type=str, help="Filters all tests by matching model")
-@click.option("--scope", required=False, type=str, help="Filters all tests by matching scope")
 @click.option(
-    "--test-case", required=False, type=str, help="Returns a single test-case with matching name."
+    "--model", required=False, type=str, help="Filters all tests by matching model"
+)
+@click.option(
+    "--scope", required=False, type=str, help="Filters all tests by matching scope"
+)
+@click.option(
+    "--test-case",
+    required=False,
+    type=str,
+    help="Returns a single test-case with matching name.",
 )
 @click.option(
     "--environment",
@@ -70,12 +75,12 @@ def main(
     record_checkpoints: bool = False,
 ):
     workloads = common.load_workloads(
-        container_image='none',
+        container_image="none",
         scope=scope,
         model=model,
         test_case=test_case,
         environment=environment,
-        container_tag='none',
+        container_tag="none",
     )
 
     for workload in workloads:
@@ -85,23 +90,25 @@ def main(
         magic_values["assets_dir"] = "$OUTPUT_PATH"
         magic_values["artifacts_dir"] = "$OUTPUT_PATH"
         magic_values["environment"] = environment
-        magic_values["test_case"] = workload.spec.test_case
-        magic_values["name"] = workload.spec.name.format(**magic_values)
+        magic_values["test_case"] = workload.spec["test_case"]
+        magic_values["name"] = workload.spec["name"].format(**magic_values)
 
         file_path = (
             pathlib.Path.cwd()
             / "test_cases"
-            / workload.spec.model
-            / f"{workload.spec.test_case}.sh"
+            / workload.spec["model"]
+            / f"{workload.spec["test_case"]}.sh"
         )
         file_path.parent.mkdir(parents=True, exist_ok=True)
         with open(file_path, "w", encoding="utf-8") as fh:
-            fh.write(f"export ENABLE_LIGHTWEIGHT_MODE={str(enable_lightweight_mode).lower()}\n")
+            fh.write(
+                f"export ENABLE_LIGHTWEIGHT_MODE={str(enable_lightweight_mode).lower()}\n"
+            )
             fh.write(f"export RECORD_CHECKPOINTS={str(record_checkpoints).lower()}\n")
             fh.write(
                 f'export OUTPUT_PATH={output_path}/runs/$(python3 -c "import uuid; print(uuid.uuid4())")\n'
             )
-            fh.write(workload.spec.script.format(**magic_values))
+            fh.write(workload.spec["script"].format(**magic_values))
 
 
 if __name__ == "__main__":
