4d2053719b9e76e50627117881851433f5ee38a8
Keshav Santhanam <ksanthanam@nvidia.com>
2025-09-29 08:19:18 -0700

    Undo generated text change

--- megatron/core/inference/text_generation_controllers/text_generation_controller.py
+++ megatron/core/inference/text_generation_controllers/text_generation_controller.py
@@ -1053,9 +1053,7 @@ class TextGenerationController:
             request.text = text  # Inference server returns prompts & generations together
             if sampling_params.return_segments:
                 request.segments = segments[0]
-            request.generated_text = self._detokenize(
-                request.generated_tokens.tolist()
-            )  # text[len(request.prompt) :]
+            request.generated_text = text[len(request.prompt) :]
         return active_requests
 
     def prep_inference_input(
