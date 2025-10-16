935cfd6c4b817816e3bcde3e6d70543e78b5796b
Lawrence McAfee <lmcafee@nvidia.com>
2025-10-16 05:50:03 -0700

    more debugging code.

--- megatron/core/inference/engines/dynamic_engine.py
+++ megatron/core/inference/engines/dynamic_engine.py
@@ -1158,6 +1158,9 @@ class DynamicInferenceEngine(AbstractEngine):
         self, sampling_params: SamplingParams, *, verbose: Optional[bool] = False
     ):
         """Continually steps the engine asynchronously."""
+        # >>>
+        n_steps = 0
+        # <<<
         try:
             while True:
                 self.schedule_requests()
@@ -1187,7 +1190,7 @@ class DynamicInferenceEngine(AbstractEngine):
 
                 # >>>
                 else:
-                    print("+++++++++++++++ resume.")
+                    print("+++++++++++++++ resume ... step %d." % n_steps)
                     self.resume()
                 # <<<
 
@@ -1201,6 +1204,9 @@ class DynamicInferenceEngine(AbstractEngine):
                 engine_output = await self.async_step(
                     sampling_params=sampling_params, verbose=verbose
                 )
+                # >>>
+                n_steps += 1
+                # <<<
 
                 is_tp0_and_pp0 = (
                     parallel_state.get_tensor_model_parallel_rank() == 0
