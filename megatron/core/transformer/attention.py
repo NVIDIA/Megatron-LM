bd697a92d05e3bf4ea0e5b8a4321b7eba09254ef
Ye Yu <yeyu@nvidia.com>
2025-10-17 10:24:30 -0700

    remove import TE as it is already imported

--- megatron/core/transformer/attention.py
+++ megatron/core/transformer/attention.py
@@ -4,7 +4,6 @@ from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from typing import NoReturn, Optional, Tuple, Union
 from packaging.version import parse
-import transformer_engine
 
 import torch
 from torch import Tensor
