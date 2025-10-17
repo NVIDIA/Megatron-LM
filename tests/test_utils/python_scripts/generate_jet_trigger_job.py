807cd6694e633d32b81b266168ff29bff705edaf
oliver könig <okoenig@nvidia.com>
2025-10-17 08:45:09 +0000

    ci: Retry on unknown_failure
    Signed-off-by: oliver könig <okoenig@nvidia.com>

--- tests/test_utils/python_scripts/generate_jet_trigger_job.py
+++ tests/test_utils/python_scripts/generate_jet_trigger_job.py
@@ -119,7 +119,14 @@ def main(
                 "needs": [{"pipeline": '$PARENT_PIPELINE_ID', "job": dependent_job}],
                 "script": ["sleep 1"],
                 "artifacts": {"paths": ["results/"], "when": "always"},
-                "retry": {"max": 2, "when": ["stuck_or_timeout_failure", "runner_system_failure"]},
+                "retry": {
+                    "max": 2,
+                    "when": [
+                        "unknown_failure",
+                        "stuck_or_timeout_failure",
+                        "runner_system_failure",
+                    ],
+                },
             },
         }
 
@@ -201,7 +208,14 @@ def main(
                 "script": [" ".join(script)],
                 "artifacts": {"paths": ["results/"], "when": "always"},
                 "allow_failure": test_case["spec"]["model"] == "gpt-nemo",
-                "retry": {"max": 2, "when": ["stuck_or_timeout_failure", "runner_system_failure"]},
+                "retry": {
+                    "max": 2,
+                    "when": [
+                        "unknown_failure",
+                        "stuck_or_timeout_failure",
+                        "runner_system_failure",
+                    ],
+                },
             }
 
     with open(output_path, 'w') as outfile:
