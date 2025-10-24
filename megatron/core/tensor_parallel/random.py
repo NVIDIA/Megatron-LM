ada9057174dfca0e08534ebe4e51724de5d77526
lhb8125 <lhb8125@gmail.com>
2025-10-23 20:24:46 -0700

    update years in copyright
    Signed-off-by: lhb8125 <lhb8125@gmail.com>

--- megatron/core/tensor_parallel/random.py
+++ megatron/core/tensor_parallel/random.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2022, NVIDIA CORPORATION. All rights reserved.
+# Copyright (c) 2022-2025, NVIDIA CORPORATION. All rights reserved.
 
 # Parts of the code here are adapted from PyTorch
 # repo: https://github.com/pytorch/pytorch
