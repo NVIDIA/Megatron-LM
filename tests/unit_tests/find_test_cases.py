33ddb971a4b41ae494bea4e92ce2ee3f5e3eefd4
oliver könig <okoenig@nvidia.com>
2025-10-24 01:06:59 +0000

    fix
    Signed-off-by: oliver könig <okoenig@nvidia.com>

--- tests/unit_tests/find_test_cases.py
+++ tests/unit_tests/find_test_cases.py
@@ -1,4 +1,4 @@
-# Copyright (c) {}-{}, NVIDIA CORPORATION.
+# Copyright (c) 2019-2025, NVIDIA CORPORATION.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
