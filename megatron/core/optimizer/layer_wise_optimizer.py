e0fdf45f42031622207cbe88d041c6bf2d38d4d4
Deyu Fu <deyuf@nvidia.com>
2025-10-27 19:57:39 +0800

    speed and memory improvements for layerwise distopt: - switch to faster all gather - sort param size and assign to rank in a ping-pong loop for balanced memory usage
    Signed-off-by: Deyu Fu <deyuf@nvidia.com>

--- megatron/core/optimizer/layer_wise_optimizer.py
+++ megatron/core/optimizer/layer_wise_optimizer.py
@@ -1,8 +1,9 @@
-# Copyright (c) 2025, NVIDIA CORPORATION. All rights reserved.
+# Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 
 from typing import Callable, List, Optional
 
 import torch
+from torch._utils import _flatten_dense_tensors, _unflatten_dense_tensors
 
 from megatron.core.dist_checkpointing import ShardedTensor
 from megatron.core.dist_checkpointing.dict_utils import nested_values
@@ -18,20 +19,17 @@ from .optimizer_config import OptimizerConfig
 class LayerWiseDistributedOptimizer(ChainedOptimizer):
     """Layer-wise distributed optimizer for Megatron-core models.
 
-    This is a experimental distributed optimizer wrapper that distributes weight to DP ranks
-    by full layer. Implemented as ChainedOptimizer to support different weights use different
-    optimizers (e.g. muon+adam). When using, keep all megatron distributed optimizer related
-    options OFF.
+    Experimental distributed optimizer wrapper that distributes weight to DP ranks by layer.
+    Implemented as ChainedOptimizer to support multiple optimizers (e.g. muon + adamW)
+    When using, keep all megatron distributed-optimizer related options OFF.
 
     How LayerWiseDistributedOptimizer work:
     1. weights are splited into lists and each rank only keep its shard in its optimizer
-    2. Megatron DDP handle allreduce grad for all params, note that each rank have full model
-    and grad.
+    2. Megatron DDP handle allreduce grad, note that each rank have full model and grad
     3. optimizer is already modified so only param belong to this DP rank is updated
-    3. grad_norm and zero counting will reduce metrics globally in step function
-    4. Do regular update with chained optimizers, optimizer is already modified so partial update
-    happens.
-    5. allgather updated params to every rank(currently through broadcast loop)
+    4. grad_norm and zero counting will reduce metrics globally in step function
+    5. Do regular update with chained optimizers, modified optimizer only update shard
+    6. allgather updated params to every rank(currently through broadcast loop)
     """
 
     def __init__(
@@ -73,20 +71,17 @@ class LayerWiseDistributedOptimizer(ChainedOptimizer):
         super().__init__(optimizers)
 
         # TODO(kunlun, deyuf): potential future perf optimization
-        # since allreduce is unchanged and handled by megatron DDP, they're already in contiguous
-        # gbuf, so instead of shard param by layer randomly, we can still shard by buf range but
-        # keep some "extras" to keep boundary weight not sharded. This way each rank do some
-        # duplicated work but we can call single allgather later and all current distopt
-        # optimization can be applied.
+        # since allreduce is unchanged and handled by megatron DDP, they're already in
+        # contiguous gbuf. So instead of shard param by layer randomly, we can shard by
+        # buf range but keep some "extras" to keep boundary weight not sharded.
+        # This way each rank do some duplicated work but allgather_v is no longer needed
+        # All current distopt optimization can also be potentially applied
 
     def shard_params(self, optimizers):
         """Shard all params into lists by rank."""
-        # We'll optimize sharding later if there is perf issue. should be ok since linear are
-        # grouped already.
-        # Key is to create separate sharding for dp/expt parallel, saved in dp_cp_params_list,
-        # expt_dp_params_list.
-        # Example of 4 dp rank and 10 non-expert parameters p0-p9, then dp_cp_params_list will
-        # look like: [[p0, p4, p8], [p1, p5, p9], [p2, p6], [p3, p7]]
+        # list of parameter are sorted by numel and assigned to ranks in ping-pong style
+        # example of 4 ranks and 10 parameters p0-p9 after sorting, then dp_cp_params_list will be
+        # [[p0, p7, p8], [p1, p6, p9], [p2, p5], [p3, p4]]
 
         # simplify when dp_cp group size is 1
         if get_pg_size(self.pg_collection.dp_cp) == 1:
@@ -97,52 +92,80 @@ class LayerWiseDistributedOptimizer(ChainedOptimizer):
         dp_cp_idx, expt_dp_idx = 0, 0
         dp_cp_size = get_pg_size(self.pg_collection.dp_cp)
         expt_dp_size = get_pg_size(self.pg_collection.expt_dp)
+        # create ping-pong style loop so memory is more balanced
+        dp_cp_loop = list(range(dp_cp_size)) + list(range(dp_cp_size))[::-1]
+        expt_dp_loop = list(range(expt_dp_size)) + list(range(expt_dp_size))[::-1]
         self.dp_cp_params_list = [[] for _ in range(dp_cp_size)]
         self.expt_dp_params_list = [[] for _ in range(expt_dp_size)]
-        # get all param groups, this is called before init so cannot rely on
-        # Chained optimizer method
+        # get all param groups
         param_groups = []
         for optimizer in optimizers:
             param_groups += optimizer.param_groups
-        for group in param_groups:
-            params_this_rank = []
-            if group.get("is_expert_parallel", False):
-                for p in group["params"]:
-                    if expt_dp_idx == get_pg_rank(self.pg_collection.expt_dp):
-                        params_this_rank.append(p)
-                    self.expt_dp_params_list[expt_dp_idx].append(p)
-                    expt_dp_idx = (expt_dp_idx + 1) % expt_dp_size
+
+        # sort param in all groups by param numel and assign to each rank evenly
+        param_list = []
+        for group_index, group in enumerate(param_groups):
+            for p in group["params"]:
+                param_list.append((p, group_index))
+        param_list.sort(key=lambda x: x[0].numel())
+        param_groups_this_rank = [[] for g in param_groups]
+
+        # assign params to rank in ping-pong style loop
+        for p, group_index in param_list:
+            if param_groups[group_index].get("is_expert_parallel", False):
+                if expt_dp_loop[expt_dp_idx] == get_pg_rank(self.pg_collection.expt_dp):
+                    param_groups_this_rank[group_index].append(p)
+                self.expt_dp_params_list[expt_dp_loop[expt_dp_idx]].append(p)
+                expt_dp_idx = (expt_dp_idx + 1) % len(expt_dp_loop)
             else:
-                for p in group["params"]:
-                    if dp_cp_idx == get_pg_rank(self.pg_collection.dp_cp):
-                        params_this_rank.append(p)
-                    self.dp_cp_params_list[dp_cp_idx].append(p)
-                    dp_cp_idx = (dp_cp_idx + 1) % dp_cp_size
-            # now we modify the group to only handle local params
-            group["params"] = params_this_rank
+                if dp_cp_loop[dp_cp_idx] == get_pg_rank(self.pg_collection.dp_cp):
+                    param_groups_this_rank[group_index].append(p)
+                self.dp_cp_params_list[dp_cp_loop[dp_cp_idx]].append(p)
+                dp_cp_idx = (dp_cp_idx + 1) % len(dp_cp_loop)
+
+        # now we modify the group to only handle local params
+        for groups, params in zip(param_groups, param_groups_this_rank):
+            groups["params"] = params
 
         # simplify when expt_dp group size is 1 or expert parallel is off
         if expt_dp_size == 1 or len(self.expt_dp_params_list[0]) == 0:
             self.expt_dp_params_list = None
 
     @torch.no_grad()
-    def broadcast_params(self):
-        """All rank broadcast updated local params(allgatherv)."""
-        # Broadcast linear layer weights to all other ranks.
-        # This may not be slower than PyTorch allgatherv which calls broadcast internally.
-        # TODO(skyw): Profile and implement more efficient version.
-        if self.dp_cp_params_list is None:
-            return
-        for i, params in enumerate(self.dp_cp_params_list):
-            src_global_rank = torch.distributed.get_global_rank(self.pg_collection.dp_cp, i)
-            for p in params:
-                torch.distributed.broadcast(p, src_global_rank, self.pg_collection.dp_cp)
-        if self.expt_dp_params_list is None:
-            return
-        for i, params in enumerate(self.expt_dp_params_list):
-            src_global_rank = torch.distributed.get_global_rank(self.pg_collection.expt_dp, i)
-            for p in params:
-                torch.distributed.broadcast(p, src_global_rank, self.pg_collection.expt_dp)
+    def allgather_params(self) -> None:
+        """All-gather updated params from all ranks."""
+
+        # helper function to flatten local params, allgather, unflatten and copy to model params
+        def _allgather_helper(params_list, group):
+            # flatten this rank's params and create empty tensor output list
+            device = params_list[0][0].device
+            dtype = params_list[0][0].dtype
+            rank = get_pg_rank(group)
+            # for rank without params create empty tensor and participate in allgather
+            src = (
+                _flatten_dense_tensors(params_list[rank])
+                if len(params_list[rank]) > 0
+                else torch.empty(0, device=device, dtype=dtype)
+            )
+            output_list = [
+                torch.empty(sum([p.numel() for p in params]), device=device, dtype=dtype)
+                for params in params_list
+            ]
+            # single all_gather_v to collect all updated params
+            torch.distributed.all_gather(output_list, src, group=group)
+            # unflatten and copy gathered params for each rank i
+            for idx, (flat_params, params) in enumerate(zip(output_list, params_list)):
+                # skip local params and empty tensors
+                if len(params) == 0 or idx == rank:
+                    continue
+                updated_params = _unflatten_dense_tensors(flat_params, params)
+                for updated_p, model_p in zip(updated_params, params):
+                    model_p.data.copy_(updated_p)
+
+        if self.dp_cp_params_list:
+            _allgather_helper(self.dp_cp_params_list, self.pg_collection.dp_cp)
+        if self.expt_dp_params_list:
+            _allgather_helper(self.expt_dp_params_list, self.pg_collection.expt_dp)
 
     @torch.no_grad()
     def get_grad_norm(self):
@@ -170,7 +193,7 @@ class LayerWiseDistributedOptimizer(ChainedOptimizer):
         update_successful, grad_norm, num_zeros_in_grad = super().step()
 
         # All gather updated params.
-        self.broadcast_params()
+        self.allgather_params()
 
         return update_successful, grad_norm, num_zeros_in_grad
 
