fa384d200e4571d0f60ce954eef7d029a0d9cbb6
Xin Yao <xiny@nvidia.com>
2025-10-27 16:56:51 +0800

    [Dev] JIT for MoE router and preprocess (#1918)
    Signed-off-by: Xin Yao <xiny@nvidia.com>

--- megatron/core/fusions/fused_pad_routing_map.py
+++ megatron/core/fusions/fused_pad_routing_map.py
@@ -1,9 +1,11 @@
-# Copyright (c) 2025, NVIDIA CORPORATION. All rights reserved.
+# Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+
 from unittest.mock import MagicMock
 
 import torch
 from packaging import version
 
+from megatron.core.jit import jit_fuser
 from megatron.core.utils import experimental_fn, null_decorator
 
 try:
@@ -69,6 +71,7 @@ def _pad_routing_map_kernel(
 
 
 @experimental_fn(introduced_with_version="0.13.0")
+@jit_fuser
 def fused_pad_routing_map(routing_map: torch.Tensor, pad_multiple: int) -> torch.Tensor:
     """Fused version of pad_routing_map.
     Args:
