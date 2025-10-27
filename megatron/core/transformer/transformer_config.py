0b188a5753681c3b2115a69e3421e2c367932d75
oliver könig <okoenig@nvidia.com>
2025-10-27 11:49:51 +0000

    whitespace
    Signed-off-by: oliver könig <okoenig@nvidia.com>

--- megatron/core/transformer/transformer_config.py
+++ megatron/core/transformer/transformer_config.py
@@ -1,4 +1,5 @@
 # Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+
 import warnings
 from dataclasses import dataclass
 from typing import Callable, List, Literal, Optional, Tuple, Union
