2271c1320519c15a35ac83243b49820b561bb82f
Robin Zhang <robinz@nvidia.com>
2025-10-27 19:34:24 -0700

    update copyright
    Signed-off-by: Robin Zhang <robinz@nvidia.com>

--- megatron/core/models/common/language_module/language_module.py
+++ megatron/core/models/common/language_module/language_module.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2024, NVIDIA CORPORATION. All rights reserved.
+# Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 import logging
 import os
 from typing import Optional, Tuple
--- megatron/core/ssm/mamba_block.py
+++ megatron/core/ssm/mamba_block.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2024, NVIDIA CORPORATION. All rights reserved.
+# Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # Copyright (c) 2024, Tri Dao, Albert Gu.
 
 # Some of this code was adopted from https://github.com/state-spaces/mamba/
