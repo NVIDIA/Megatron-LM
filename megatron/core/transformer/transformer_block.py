44df020a79be3595d7a39b535894cdb3a14230cd
buptzyb <robinz@nvidia.com>
2025-10-31 19:36:04 +0800

    [DEV] feat(MoE): Refactor cuda_graph_scope (#1917)
    Signed-off-by: Robin Zhang <robinz@nvidia.com>

--- megatron/core/transformer/transformer_block.py
+++ megatron/core/transformer/transformer_block.py
@@ -522,7 +522,7 @@ class TransformerBlock(GraphableMegatronModule, MegatronModule):
                 kwargs.get('inference_context') is not None
                 or kwargs.get('inference_params') is not None
             )
-            and self.config.cuda_graph_scope == 'full_iteration'
+            and 'full_iteration' in self.config.cuda_graph_scope
         ):
             if kwargs['inference_context'].is_static_batching():
                 using_cuda_graph = kwargs['inference_context'].is_decode_only()
