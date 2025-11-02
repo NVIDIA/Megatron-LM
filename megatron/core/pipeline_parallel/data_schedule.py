fix nan bugs, need to fix flops calculation.
Signed-off-by: tailaim <tailaim@nvidia.com>

megatron/core/pipeline_parallel/data_schedule.py
@@ -1,9 +1,10 @@
 # Copyright (c) 2025, NVIDIA CORPORATION.  All rights reserved.
 
+import enum
 from collections import deque
 from functools import lru_cache
 from math import ceil, log2
-from typing import Callable, List, Optional, Tuple
+from typing import Callable, Dict, List, Optional, Tuple, Type, Union
 
 import numpy as np
 import torch
@@ -13,8 +14,18 @@ from megatron.core.process_groups_config import ProcessGroupCollection
 from megatron.core.rerun_state_machine import RerunDataIterator
 
 
-def wrap_hybrid_cp_dataloader(
-    data_iterator, config, pg_collection: Optional[ProcessGroupCollection] = None
+class PackingScheduler(enum.Enum):
+    """Enum for supported sequence packing algorithms."""
+
+    HYBRID_CP = "hybrid_cp"
+    NAIVE_SEQUENCE_PACKING = "naive_sequence_packing"
+
+
+def wrap_dataloader(
+    data_iterator,
+    config,
+    scheduler_type: Union[PackingScheduler, str],
+    pg_collection: Optional[ProcessGroupCollection] = None,
 ):
     """
     A wrapper function that wraps around an existing data_iterator
@@ -25,6 +36,13 @@ def wrap_hybrid_cp_dataloader(
         config: The config object containing the max_seqlen_per_dp_cp_rank
         dp_cp_group: Data parallel context parallel group.
     """
+
+    scheduler_map = {"hybrid_cp": BalancedHybridCPscheduler, "naive": NaiveSequencePackingScheduler}
+
+    scheduler_map: Dict[PackingScheduler, Type[BaseScheduler]] = {
+        PackingScheduler.HYBRID_CP: BalancedHybridCPscheduler,
+        PackingScheduler.NAIVE_SEQUENCE_PACKING: NaiveSequencePackingScheduler,
+    }
 
     def _get_global_seqlens(subsample_seqlens: torch.Tensor, dp_group) -> List[int]:
         """
@@ -151,16 +169,17 @@ def wrap_hybrid_cp_dataloader(
         ]
         # send_counts = [len(combined_sample_id_groups[d]) for d in range(total_hdp_gpus)]
 
+        send_num_split = [0] * total_hdp_gpus
         send_lens_split = [0] * total_hdp_gpus
         for dest_rank in range(total_hdp_gpus):
             if dest_rank in dp_ranks:
-                send_lens_split[dest_rank] = sum(
-                    [
-                        global_id_seqlens[gid][1]
-                        for gid in combined_sample_id_groups[dest_rank]
-                        if gid in global_ids_this_rank
-                    ]
-                )
+                send_seq_lens = [
+                    global_id_seqlens[gid][1]
+                    for gid in combined_sample_id_groups[dest_rank]
+                    if gid in global_ids_this_rank
+                ]
+                send_num_split[dest_rank] = len(send_seq_lens)
+                send_lens_split[dest_rank] = sum(send_seq_lens)
             else:
                 # We only need to share local data with DP ranks that have different data.
                 send_lens_split[dest_rank] = 0
@@ -197,20 +216,30 @@ def wrap_hybrid_cp_dataloader(
         def _unpack_sample_by_key(key: str, recv_tensor: torch.Tensor):
             cursor = 0
             for i, gid in enumerate(recv_ids_sorted):
-                sample_len = global_id_seqlens[gid][1]
+                sample_len = 1 if key in ["original_seq_len"] else global_id_seqlens[gid][1]
                 recv_samples[i][key] = recv_tensor[cursor : cursor + sample_len]
                 cursor += sample_len
 
         for key in data_keys:
-            send_tensor = _pack_sample_by_key(key)
-            recv_tensor = torch.empty(
-                sum(recv_lens_split), device=torch.cuda.current_device(), dtype=send_tensor.dtype
+            output_split_sizes, input_split_sizes = (
+                (recv_counts, send_num_split)
+                if key in ["original_seq_len"]
+                else (recv_lens_split, send_lens_split)
             )
+            send_tensor = _pack_sample_by_key(key)
+            recv_tensor_size = sum(output_split_sizes)
+            recv_tensor = torch.empty(
+                recv_tensor_size, device=torch.cuda.current_device(), dtype=send_tensor.dtype
+            )
+            # debugmtl
+            # print(f"ready to all to all for key:{key}, output_split_sizes:{output_split_sizes},
+            # input_split_sizes:{input_split_sizes}, recv_tensor_size:
+            # {tensor_size},send_tensor_size:{send_tensor.size(0)}")
             torch.distributed.all_to_all_single(
                 output=recv_tensor,
                 input=send_tensor,
-                output_split_sizes=recv_lens_split,
-                input_split_sizes=send_lens_split,
+                output_split_sizes=output_split_sizes,
+                input_split_sizes=input_split_sizes,
                 group=dp_cp_group,
             )
             _unpack_sample_by_key(key, recv_tensor)
@@ -245,29 +274,24 @@ def wrap_hybrid_cp_dataloader(
                 group=parallel_state.get_tensor_model_parallel_group(),
             )
 
-    def _broadcast_num_samples_this_group(num_samples_this_group):
-        dev = torch.cuda.current_device()
-        # TODO(tailaim) do we need this barrier?
-        torch.distributed.barrier()
+    # Convert string to enum if needed
+    if isinstance(scheduler_type, str):
+        try:
+            scheduler_type = PackingScheduler[scheduler_type.upper()]
+        except KeyError:
+            available_scheduler = ", ".join([scheduler.name for scheduler in PackingScheduler])
+            raise ValueError(
+                f"Unknown packing scheduler: {scheduler_type}. "
+                f"Available schedulers: {available_scheduler}"
+            )
 
-        n = 0 if num_samples_this_group is None else int(num_samples_this_group.numel())
-        n = torch.tensor([n], dtype=torch.int64, device=dev)
-
-        _broadcast(n)
-        n = int(n.item())
-
-        assert n > 0, "there should be at least 1 sub samples in the group"
-        num_samples_this_group_broadcast = (
-            torch.empty(n, dtype=torch.int32, device=dev)
-            if num_samples_this_group is None
-            else num_samples_this_group
+    if scheduler_type not in scheduler_map:
+        available_scheduler = ", ".join([scheduler.name for scheduler in PackingScheduler])
+        raise ValueError(
+            f"Unknown scheduler: {scheduler}. " f"Available schedulers: {available_scheduler}"
         )
-        _broadcast(num_samples_this_group_broadcast)
-        return num_samples_this_group_broadcast
 
-    cp_balancing_scheduler = BalancedHybridCPScheduler(
-        max_seq_len_per_rank=config.max_seqlen_per_dp_cp_rank
-    )
+    scheduler = scheduler_map[scheduler_type](config)
     if pg_collection is None:
         dp_cp_group = parallel_state.get_data_parallel_group(with_context_parallel=True)
         dp_group = parallel_state.get_data_parallel_group()
@@ -305,9 +329,7 @@ def wrap_hybrid_cp_dataloader(
             subsample_seqlens.shape[0], offsets, seqlens_gathered, dp_group
         )
 
-        groups, sample_id_groups = cp_balancing_scheduler.get_groups_and_subsamples(
-            global_id_seqlens, config
-        )
+        groups, sample_id_groups = scheduler.get_groups_and_subsamples(global_id_seqlens, config)
 
         batch = _unpack_batch(batch)
         samples_this_rank_with_id = _reroute_samples_to_hdp_ranks(
@@ -353,10 +375,10 @@ def wrap_hybrid_cp_dataloader(
 
             # TODO(tailaim): do we need attention_mask for sequence packing?
             new_sample = {}
-            new_sample["tokens"] = tokens
-            new_sample["labels"] = labels
-            new_sample["loss_mask"] = loss_mask
-            new_sample["position_ids"] = position_ids
+            new_sample["tokens"] = tokens.unsqueeze(0)
+            new_sample["labels"] = labels.unsqueeze(0)
+            new_sample["loss_mask"] = loss_mask.unsqueeze(0)
+            new_sample["position_ids"] = position_ids.unsqueeze(0)
             new_sample["local_cp_size"] = torch.tensor(
                 partner_cp_size, dtype=torch.int32, device=dev
             )
@@ -367,9 +389,11 @@ def wrap_hybrid_cp_dataloader(
             )
             cu_seqlens_padded = np.empty(len(samples) + 1, dtype=np.int32)
             cu_seqlens_padded[0] = 0
-            np.cumsum(lengths_padding, out=cu_seqlens_padded[1:])
-            cu_seqlens_padded = torch.from_numpy(cu_seqlens_padded).to(
-                device=dev, non_blocking=True
+            cu_seqlens_padded[1:] = np.cumsum(lengths_padding, out=cu_seqlens_padded[1:])
+            cu_seqlens_padded = (
+                torch.from_numpy(cu_seqlens_padded)
+                .to(device=dev, non_blocking=True, dtype=torch.int32)
+                .reshape(-1)
             )
             new_sample["cu_seqlens_padded"] = cu_seqlens_padded
 
@@ -379,27 +403,95 @@ def wrap_hybrid_cp_dataloader(
             new_sample["max_seqlen"] = max_seqlen
 
             # create cu_seqlens without padding
-            lengths = torch.stack([s["original_seq_len"] for s in samples], dim=0)
+            lengths = torch.stack([s["original_seq_len"] for s in samples], dim=0).reshape(-1)
             cu_seqlens = torch.empty(lengths.numel() + 1, device=dev, dtype=torch.int32)
             cu_seqlens[0] = 0
-            cu_seqlens[1:] = torch.cumsum(lengths, dim=0)
+            cu_seqlens[1:] = torch.cumsum(lengths, dim=0).reshape(-1)
             new_sample["cu_seqlens"] = cu_seqlens
 
             new_samples.append(new_sample)
-
+        # debugmtl
+        # print(f"new_samples type: {type(new_samples)}, new_sample type: {type(new_samples[0])}")
         new_data_iterator = RerunDataIterator(iter(new_samples))
+
+        # debugmtl
+        # data = next(new_data_iterator)
+        # print(f"data type: {type(data)}")
+        # print(data)
 
         return new_data_iterator, num_micro_batches
 
 
-class BalancedHybridCPScheduler:
+class BaseScheduler:
+    """
+    Base class for sequence packing schedulers.
+    """
+
+    def __init__(self, config):
+        pass
+
+
+class NaiveSequencePackingScheduler(BaseScheduler):
+    """
+    This scheduler simply packs sequences in their original order
+    until reaching the max sequence length.
+    It does not reorder sequences nor perform any load balancing.
+    """
+
+    def __init__(self, config):
+        super().__init__(config)
+        self.max_seq_len_all_ranks = config.max_seqlen_per_dp_cp_rank * config.context_parallel_size
+        self.dp_size = parallel_state.get_data_parallel_world_size()
+
+    def get_groups_and_subsamples(self, sample_id_seqlens, config):
+        """
+        This scheduler simply packs sequences in their original order
+        until reaching the max sequence length.
+        It does not reorder sequences nor perform any load balancing.
+        """
+        groups = []
+        sample_id_groups = []
+        sum_seqlen = 0
+        single_microbatch = []
+
+        for i in range(len(sample_id_seqlens)):
+            if sum_seqlen + sample_id_seqlens[i] <= self.max_seq_len_all_ranks:
+                single_microbatch.append(i)
+                sum_seqlen += sample_id_seqlens[i][1]
+            else:
+                groups.append(single_microbatch)
+                sample_id_groups.append(single_microbatch)
+                single_microbatch = [i]
+                sum_seqlen = sample_id_seqlens[i][1]
+
+        # we want the number of microbatches to be multiple of dp_size
+        # so we move few samples from previous microbatch
+        # to the end of the microbatches if needed
+        num_microbatches_before = len(sample_id_groups)
+        if num_microbatches_before % self.dp_size != 0:
+            remainder = num_microbatches_before % self.dp_size
+            num_to_move = self.dp_size - remainder
+            i = num_microbatches_before - 1
+            while num_to_move > 0:
+                assert i > 0, "Not enough samples to move"
+                if len(sample_id_groups[i]) > 1:
+                    seq_id = sample_id_groups[i].pop()
+                    sample_id_groups[i].append(seq_id)
+                    num_to_move -= 1
+                else:
+                    i -= 1
+        return groups, sample_id_groups
+
+
+class BalancedHybridCPscheduler(BaseScheduler):
     """
     This class provides the functionality to form groups of sub-samples
     such that all DPxCP ranks have a roughly balanced workload in the group.
     """
 
-    def __init__(self, max_seq_len_per_rank: int):
-        self.max_seq_len_per_rank = max_seq_len_per_rank
+    def __init__(self, config):
+        super().__init__(config)
+        self.max_seq_len_per_rank = config.max_seqlen_per_dp_cp_rank
         self.num_subsamples = 0
         self.num_subsamples_processed = 0
         self.free_resources = []
@@ -614,6 +706,8 @@ class BalancedHybridCPScheduler:
                 else:
                     chosen_members = group_members[best_gid]
             else:
+                if best_gid is None:
+                    break
                 chosen_members = group_members[best_gid]
 
             # ---- Step 2 – if we decided to create a fresh group ----------------
@@ -731,7 +825,6 @@ class BalancedHybridCPScheduler:
                 else:
                     break
 
-        # debugmtl make sure total_seq_len after packing smaller than max_seq_len
         # trim_overload()
 
         # Track samples in this group before redistribution to empty GPUs

megatron/core/pipeline_parallel/schedules.py
@@ -12,7 +12,7 @@ from megatron.core.enums import ModelType
 from megatron.core.pipeline_parallel.fine_grained_activation_offload import (
     fine_grained_offloading_reset,
 )
-from megatron.core.pipeline_parallel.data_schedule import wrap_hybrid_cp_dataloader
+from megatron.core.pipeline_parallel.data_schedule import PackingScheduler, wrap_dataloader
 from megatron.core.pipeline_parallel.p2p_communication import P2PCommunicator
 from megatron.core.pipeline_parallel.utils import (
     is_pp_first_stage,
@@ -215,6 +215,17 @@ def forward_step_calc_loss(
     """Calculate the loss and number of tokens for forward_step()"""
 
     from megatron.core.transformer.multi_token_prediction import MTPLossAutoScaler
+
+    # TODO(tailaim): this is wrong, must fix it
+    num_tokens_this_microbatch = output_tensor.numel()
+    if len(forward_data_store) == 0:
+        # the first element is the total number of tokens within a single GA
+        forward_data_store.append(num_tokens_this_microbatch)
+        # the second element is the squared sum of the sequences within a single GA
+        forward_data_store.append(num_tokens_this_microbatch * num_tokens_this_microbatch)
+    else:
+        forward_data_store[0] += num_tokens_this_microbatch
+        forward_data_store[1] += num_tokens_this_microbatch * num_tokens_this_microbatch
 
     model_vp_stage = getattr(model, "vp_stage", None)
     if vp_stage is not None and model_vp_stage is not None:
@@ -498,6 +509,33 @@ def check_first_val_step(first_val_step, forward_only, cond):
         return cond
 
 
+def wrap_iterator_helper(
+    config,
+    data_iterator: Union[Iterator, List[Iterator]],
+    num_microbatches: int,
+    pg_collection: Optional[ProcessGroupCollection] = None,
+):
+    """Warp data iterator for sequence packing if needed."""
+    if config.sft_sequence_packing:
+        if config.hybrid_context_parallel:
+            data_iterator, num_microbatches = wrap_dataloader(
+                data_iterator, config, PackingScheduler.HYBRID_CP, pg_collection=None
+            )
+        else:
+            if config.balanced_sequence_packing:
+                # enable balanced sequence packing scheduler
+                pass
+            else:
+                # naive sequence packing scheduler
+                data_iterator, num_microbatches = wrap_dataloader(
+                    data_iterator,
+                    config,
+                    PackingScheduler.NAIVE_SEQUENCE_PACKING,
+                    pg_collection=None,
+                )
+    return data_iterator, num_microbatches
+
+
 def forward_backward_no_pipelining(
     *,
     forward_step_func,
@@ -579,29 +617,9 @@ def forward_backward_no_pipelining(
     input_tensor, output_tensor_grad = None, None
     total_num_tokens = torch.zeros([], dtype=torch.int, device="cuda")
 
-    # here we need to do something to data_iterator
-    # 这里我们需要返回一个new_data_iterator和num_microbatches
-
-    if config.sft_sequence_packing:
-        if config.hybrid_context_parallel:
-            data_iterator, num_microbatches = wrap_hybrid_cp_dataloader(
-                data_iterator, config, pg_collection=None
-            )
-        else:
-            if config.balanced_sequence_packing:
-                pass
-            else:
-                pass
-    else:
-        # if we want to enable hybrid-cp without sequence packing,
-        # then we need to pad sequence to multiple sizes otherwise
-        # all the sequence same length and hybrid-cp is meaningless.
-        # However, it's meaningless, why not use sequence packing directly?
-        if config.hybrid_context_parallel:
-            pass
-        else:
-            # 这就是最正常的bshd
-            pass
+    data_iterator, num_microbatches = wrap_iterator_helper(
+        config, data_iterator, num_microbatches, pg_collection
+    )
 
     if config.overlap_moe_expert_parallel_comm and not forward_only:
         forward_data_store, total_num_tokens = combined_1f1b_schedule_for_no_pipelining(
@@ -620,24 +638,6 @@ def forward_backward_no_pipelining(
             total_num_tokens,
             partial(check_first_val_step, first_val_step, forward_only),
         )
-    # elif config.hybrid_context_parallel:
-    #     forward_data_store, total_num_tokens = hybrid_context_parallel_forward_backward(
-    #         forward_step_func,
-    #         data_iterator,
-    #         model,
-    #         num_microbatches,
-    #         input_tensor,
-    #         output_tensor_grad,
-    #         forward_data_store,
-    #         config,
-    #         collect_non_loss_data,
-    #         first_val_step,
-    #         forward_only,
-    #         no_sync_func,
-    #         total_num_tokens,
-    #         check_first_val_step,
-    #         model_type,
-    #     )
     else:
         with no_sync_func():
             for i in range(num_microbatches - 1):
@@ -678,6 +678,9 @@ def forward_backward_no_pipelining(
         )
 
         total_num_tokens += num_tokens
+
+        # debugmtl
+        # print(f"num_microbatches: {num_microbatches}, total_num_tokens: {total_num_tokens}")
 
         if not forward_only:
             backward_step(input_tensor, output_tensor, output_tensor_grad, model_type, config)
@@ -952,6 +955,10 @@ def forward_backward_pipelining_with_interleaving(
 
     if config.overlap_p2p_comm and config.batch_p2p_comm:
         raise ValueError("Can not use both overlap_p2p_comm and batch_p2p_comm")
+
+    data_iterator, num_microbatches = wrap_iterator_helper(
+        config, data_iterator, num_microbatches, pg_collection
+    )
 
     # Needed only when gradients are finalized in M-Core
     if config.finalize_model_grads_func is not None and not forward_only:
@@ -2085,6 +2092,10 @@ def forward_backward_pipelining_without_interleaving(
             "Invalid combination of p2p_communicator, pg_collection "
             "provide none or provide all the process groups"
         )
+
+    data_iterator, num_microbatches = wrap_iterator_helper(
+        config, data_iterator, num_microbatches, pg_collection
+    )
 
     # Needed only when gradients are finalized in M-Core
     if config.finalize_model_grads_func is not None and not forward_only:

megatron/core/utils.py
@@ -56,6 +56,12 @@ try:
 except ImportError:
     HAVE_NVTX = False
 
+# Register the TE CUDA kernels
+import transformer_engine  # pylint: disable=unused-import
+
+# Alias the PyTorch wrapper so we can call tex.* APIs
+import transformer_engine_torch as tex
+
 logger = logging.getLogger(__name__)
 
 
@@ -1860,7 +1866,7 @@ def get_thd_batch_on_this_cp_rank(
     batch: Dict[str, Any],
     cu_seqlens: torch.Tensor,
     cu_seqlens_padded: torch.Tensor,
-    max_seqlen: torch.Tensor,
+    max_seqlen: Optional[int] = None,
     cp_size: Optional[int] = None,
     cp_rank: Optional[int] = None,
     local_cp_size: Optional[int] = None,
@@ -1872,19 +1878,24 @@ def get_thd_batch_on_this_cp_rank(
     """
     if local_cp_size:
         # enable hybrid context parallel
+        cp_size = local_cp_size
         if cp_group is None:
             # TODO: debugmtl modify this comments
             # Get the local cp group required for as defined by the HybridCPDataLoaderWrapper
-            if local_cp_size > 1:
+            if cp_size > 1:
                 cp_group = parallel_state.get_hybrid_data_context_parallel_groups(
-                    group_size=local_cp_size
+                    group_size=cp_size
                 )
+                cp_rank = torch.distributed.get_rank(group=cp_group)
+                assert cp_group.size() == cp_size
         else:
             # TODO: debugmtl modify this comments
             # If cp group is provided, it must match the local cp size
             # as defined by the HybridCPDataLoaderWrapper
             assert cp_group.size() == local_cp_size
     else:
+        cp_size = parallel_state.get_context_parallel_world_size()
+        cp_rank = parallel_state.get_context_parallel_rank()
         cp_group = None
 
     packed_seq_params = PackedSeqParams(
@@ -1893,13 +1904,12 @@ def get_thd_batch_on_this_cp_rank(
         cu_seqlens_kv=cu_seqlens,
         cu_seqlens_q_padded=cu_seqlens_padded,
         cu_seqlens_kv_padded=cu_seqlens_padded,
-        max_seqlen_q=int(max_seqlen[0].item()),
-        max_seqlen_kv=int(max_seqlen[0].item()),
+        max_seqlen_q=max_seqlen,
+        max_seqlen_kv=max_seqlen,
+        local_cp_size=cp_size,
         cp_group=cp_group,
     )
 
-    cp_size = get_context_parallel_world_size() if cp_size is None else cp_size
-    cp_rank = get_context_parallel_rank() if cp_rank is None else cp_rank
     if cp_size > 1:  # slice batch along sequence dimension for context parallelism
         assert tex is not None and is_te_min_version("1.10.0"), (
             "Please update Transformer Engine to >= 1.10 to use "
@@ -1909,7 +1919,7 @@ def get_thd_batch_on_this_cp_rank(
             cu_seqlens_padded, batch['tokens'].size(1), cp_size, cp_rank
         )
         for key, data in batch.items():
-            if key in {'attention_mask', 'cu_seqlens', 'cu_seqlens_padded', 'max_seqlen'}:
+            if key in {'attention_mask'}:
                 continue
             batch[key] = data.index_select(1, index)
 

megatron/training/datasets/sft_dataset.py
@@ -227,7 +227,7 @@ class MockSFTLowLevelDataset:
         length = self.sequence_lengths[idx % self.size]
         # the length of sample is 'length', but only length-1 elements are generated here, 
         # because an eod token will be appended at the end later in SFTDataset
-        sample = np.arange(1, length, dtype=np.int64)
+        sample = np.arange(2, length + 1 , dtype=np.int64)
         return sample
 class MockSFTDataset(SFTDataset):
     """The mock dataset used during SFT"""
@@ -262,8 +262,11 @@ class MockSFTDataset(SFTDataset):
         force_eod_length = int(tokenizer.force_eod)
 
         if len(tokens) > max_seq_len - force_eod_length:
+            # cut the right side
             tokens = tokens[: max_seq_len - force_eod_length]
             target = target[: max_seq_len - force_eod_length]
+            # tokens = tokens[(-max_seq_len + force_eod_length):]
+            # target = target[(-max_seq_len + force_eod_length):]
 
         # padding
         num_tokens = len(tokens) + force_eod_length

megatron/training/training.py
@@ -98,8 +98,6 @@ from megatron.core.parallel_state import (
 )
 
 from megatron.core.pipeline_parallel import get_forward_backward_func
-# debugmtl 
-# from megatron.core.pipeline_parallel.hybrid_cp_schedule import HybridCPDataLoaderWrapper
 from megatron.core.num_microbatches_calculator import (
     destroy_num_microbatches_calculator,
     get_current_global_batch_size,
@@ -158,7 +156,7 @@ def print_datetime(string):
     print_rank_0(f'[{string}] datetime: {time_str} ')
 
 
-def num_floating_point_operations(args, batch_size):
+def num_floating_point_operations(args, num_total_tokens_this_GB, sequence_square_sum_this_GB):
     def calculate_layer_counts():
         """Calculate the number of attention, Mamba, and MLP layers."""
         if args.hybrid_override_pattern:
@@ -173,27 +171,25 @@ def num_floating_point_operations(args, batch_size):
             num_mamba_layers = args.num_layers - num_attn_layers - num_mlp_layers
             return num_attn_layers, num_mamba_layers, num_mlp_layers
 
-    def mlp_layer_flops(batch_size, seq_len, hidden_size, expansion=4.0, swiglu=False):
+    def mlp_layer_flops(num_total_tokens_this_GB, hidden_size, expansion=4.0, swiglu=False):
         """Calculate FLOPs for an MLP layer."""
         scale_factor = 3.0 / 2.0 if swiglu else 1.0
-        return 4 * expansion * scale_factor * batch_size * seq_len * hidden_size**2
+        return 4 * expansion * scale_factor * num_total_tokens_this_GB * hidden_size**2
 
     def attn_layer_flops(
-        batch_size, seq_len, hidden_size, num_heads, gqa=True, gqa_groups=8, kv_channels=None
+        num_total_tokens_this_GB, sequence_square_sum_this_GB, hidden_size, num_heads, gqa=True, gqa_groups=8, kv_channels=None
     ):
         """Calculate FLOPs for an attention layer."""
         p = (kv_channels * num_heads / hidden_size) if kv_channels else 1
         g = gqa_groups if gqa else num_heads
         return (
             4
-            * batch_size
-            * seq_len
             * hidden_size
             * p
-            * (hidden_size + (hidden_size * (g / num_heads)) + (seq_len / 2))
+            * (hidden_size * num_total_tokens_this_GB + (hidden_size * (g / num_heads)) * num_total_tokens_this_GB + (sequence_square_sum_this_GB / 2))
         )
 
-    def mamba_layer_flops(batch_size, seq_len, hidden_size, state_dim=16,
+    def mamba_layer_flops(num_total_tokens_this_GB, hidden_size, state_dim=16,
                           head_dim=64, num_groups=1, num_heads=128):
         """Calculate FLOPs for a Mamba layer."""
         # Note (rwaleffe): flops estimate for scan should be updated based on new SSD kernels,
@@ -206,16 +202,15 @@ def num_floating_point_operations(args, batch_size):
         return (
             (
                 2
-                * batch_size
-                * seq_len
+                * num_total_tokens_this_GB
                 * hidden_size
                 * (2 * d_in + 2 * num_groups * state_dim + nheads)
             )  # in_proj
-            + (7 * batch_size * seq_len * d_in * state_dim)  # scan
-            + (2 * batch_size * seq_len * d_in * hidden_size)  # out_proj
+            + (7 * num_total_tokens_this_GB * d_in * state_dim)  # scan
+            + (2 * num_total_tokens_this_GB * d_in * hidden_size)  # out_proj
         )
 
-    def hybrid_flops(batch_size, seq_len, hidden_size,
+    def hybrid_flops(num_total_tokens_this_GB, sequence_square_sum_this_GB, hidden_size,
                      num_attn_layers, num_mamba_layers, num_mlp_layers,
                      mamba_state_dim=128, mamba_head_dim=64,
                      mamba_num_groups=8, mamba_num_heads=128,
@@ -225,20 +220,23 @@ def num_floating_point_operations(args, batch_size):
                      vocab_size=256000):
         """Calculate total FLOPs for the hybrid model."""
         flops_fwd = (
-                num_attn_layers * attn_layer_flops(batch_size, seq_len, hidden_size,
+                num_attn_layers * attn_layer_flops(num_total_tokens_this_GB, sequence_square_sum_this_GB, hidden_size,
                                                    num_attn_heads, gqa, gqa_groups, kv_channels) +
-                num_mlp_layers * mlp_layer_flops(batch_size, seq_len, hidden_size,
+                num_mlp_layers * mlp_layer_flops(num_total_tokens_this_GB, hidden_size,
                                                  mlp_expansion, swiglu) +
-                num_mamba_layers * mamba_layer_flops(batch_size, seq_len, hidden_size,
+                num_mamba_layers * mamba_layer_flops(num_total_tokens_this_GB, hidden_size,
                                                      mamba_state_dim, mamba_head_dim,
                                                      mamba_num_groups, mamba_num_heads) +
-                (2 * batch_size * seq_len * hidden_size * vocab_size)  # logits computation
+                (2 * num_total_tokens_this_GB * hidden_size * vocab_size)  # logits computation
         )
         return flops_fwd * 3
 
     def transformer_flops():
         """Calculate FLOPs for a standard Transformer model."""
         # TODO(helenn/dnarayanan): Refactor this to reuse the helper methods.
+        # Attention projection size.
+        query_projection_size = args.kv_channels * args.num_attention_heads
+        query_projection_to_hidden_size_ratio = query_projection_size / args.hidden_size
         # Group Query Attention.
         if not args.group_query_attention:
             args.num_query_groups = args.num_attention_heads
@@ -305,13 +303,18 @@ def num_floating_point_operations(args, batch_size):
             assert not args.group_query_attention
             '''
             Basic arithmetic
-            let B is batch size, s is seq_len, h is embedding dim,
-            for one self_attnetion block (prenorm is not included)
-            qkv projection:  6Bsh^2
-            attn:            2Bs^2h
-            attn over value: 2Bs^2h
-            oproj:           2Bsh^2
+            
+            Let h be the embedding dim.
+            We use two statistics to unify BSHD and THD cases:
+                num_total_tokens_this_GB: total number of tokens in this global batch
+                sequence_square_sum_this_GB: sum of squared sequence lengths in this global batch
 
+            For one self-attention block (prenorm not included):
+                qkv projection:      6 * num_total_tokens_this_GB * h^2
+                attn:    2 * sequence_square_sum_this_GB * h
+                attn over value:   2 * sequence_square_sum_this_GB * h
+                oproj:   2 * num_total_tokens_this_GB * h^2
+            
             references
             https://arxiv.org/abs/2305.10403
             https://arxiv.org/abs/2205.05198
@@ -332,7 +335,7 @@ def num_floating_point_operations(args, batch_size):
             standard_self_attn_term = (
                 3
                 * 2  # fwd(1) + bwd(2) *FMA
-                * (
+                * ( num_total_tokens_this_GB * (
                     ## q lora + rope + q norm
                     q_term
                     ## kv lora + rope + kv norm
@@ -344,12 +347,12 @@ def num_floating_point_operations(args, batch_size):
                     )
                     + args.hidden_size * args.qk_pos_emb_head_dim
                     ## o proj
-                    + (args.num_attention_heads * args.v_head_dim) * args.hidden_size
+                    + (args.num_attention_heads * args.v_head_dim) * args.hidden_size)
                     ## core attn
-                    + args.seq_length
+                    + sequence_square_sum_this_GB
                     * (args.num_attention_heads * (args.qk_head_dim + args.qk_pos_emb_head_dim))
-                    / 2  # causal mask (only half of the mask is non-zero)
-                    + args.seq_length * args.num_attention_heads * args.v_head_dim / 2
+                    / 2
+                    + sequence_square_sum_this_GB * args.num_attention_heads * args.v_head_dim / 2
                 )
             )
 
@@ -361,17 +364,17 @@ def num_floating_point_operations(args, batch_size):
             standard_self_attn_term = (
                 3
                 * 2  # fwd(1) + bwd(2) *FMA
-                * (
+                * ( num_total_tokens_this_GB *(
                     ## qkv proj
                     args.hidden_size
-                    * (query_projection_size + key_projection_size + value_projection_size)
+                    * (query_projection_size + key_projection_size + value_projection_size))
                     ## core attention
                     + query_projection_size
-                    * args.seq_length
+                    * sequence_square_sum_this_GB
                     / 2  # causal mask (only half of the mask is non-zero)
                     * 2  # QK^T and (QK^T)V
                     ## out proj
-                    + query_projection_size
+                    + num_total_tokens_this_GB * query_projection_size
                     * args.hidden_size
                 )
             )
@@ -441,8 +444,7 @@ def num_floating_point_operations(args, batch_size):
         )
 
         total_floating_point_operations = (
-            batch_size
-            * args.seq_length
+            num_total_tokens_this_GB
             * (
                 # MLP
                 expansion_factor
@@ -459,8 +461,6 @@ def num_floating_point_operations(args, batch_size):
                     + (shared_expert_ffn_hidden_size * gated_linear_multiplier)
                     * (num_moe_layers / num_layers)
                 )
-                # Self Attention
-                + self_attn_term
                 # MTP norms and proj
                 + 3
                 * 2
@@ -474,6 +474,10 @@ def num_floating_point_operations(args, batch_size):
                 # Logit.
                 + 3 * 2 * args.hidden_size * args.padded_vocab_size * (mtp_num_layers + 1)
             )
+            +                
+            # Self Attention
+            standard_self_attn_term
+            
         )
         return total_floating_point_operations
 
@@ -484,8 +488,8 @@ def num_floating_point_operations(args, batch_size):
 
         # Compute hybrid model FLOPs.
         return hybrid_flops(
-            batch_size=batch_size,
-            seq_len=args.seq_length,
+            num_total_tokens_this_GB=num_total_tokens_this_GB, 
+            sequence_square_sum_this_GB=sequence_square_sum_this_GB,
             hidden_size=args.hidden_size,
             num_attn_layers=num_attn_layers,
             num_mamba_layers=num_mamba_layers,
@@ -1339,7 +1343,7 @@ def setup_model_and_optimizer(
 
 
 def dummy_train_step(data_iterator):
-    # TODO: debugmtl this need to be modified
+    # TODO(tailaim): this need to be modified
     """Single dummy training step."""
     num_microbatches = get_num_microbatches()
     rerun_state_machine = get_rerun_state_machine()
@@ -1390,9 +1394,22 @@ def train_step(forward_step_func, data_iterator, model, optimizer, opt_param_sch
             forward_only=False,
             adjust_tensor_shapes_fn=adjust_tensor_shapes_fn,
         )
+    
+    num_total_tokens_this_GA = losses_reduced.pop(0)
+    sequence_square_sum_this_GA = losses_reduced.pop(0)
+    if args.sft_sequence_packing:
+        pack = torch.tensor([num_total_tokens_this_GA, sequence_square_sum_this_GA],
+                        device='cuda', dtype=torch.float32)
+        torch.distributed.all_reduce(pack, group=mpu.get_data_parallel_group())
+        num_total_tokens_this_GB = pack[0].item() * mpu.get_context_parallel_world_size()
+        sequence_square_sum_this_GB = pack[1].item() * mpu.get_context_parallel_world_size()
+    else:
+        num_total_tokens_this_GB = num_total_tokens_this_GA * mpu.get_data_parallel_world_size(with_context_parallel=True)
+        sequence_square_sum_this_GB = sequence_square_sum_this_GA * mpu.get_data_parallel_world_size(with_context_parallel=True)
+
     should_checkpoint, should_exit, exit_code = rerun_state_machine.should_checkpoint_and_exit()
     if should_exit:
-        return {}, True, should_checkpoint, should_exit, exit_code, None, None
+        return {}, True, should_checkpoint, should_exit, exit_code, None, None, num_total_tokens_this_GB, sequence_square_sum_this_GB
 
     # Empty unused memory.
     if args.empty_unused_memory_level >= 1:
@@ -1464,8 +1481,10 @@ def train_step(forward_step_func, data_iterator, model, optimizer, opt_param_sch
             exit_code,
             grad_norm,
             num_zeros_in_grad,
+            num_total_tokens_this_GB,
+            sequence_square_sum_this_GB,
         )
-    return {}, skipped_iter, should_checkpoint, should_exit, exit_code, grad_norm, num_zeros_in_grad
+    return {}, skipped_iter, should_checkpoint, should_exit, exit_code, grad_norm, num_zeros_in_grad, num_total_tokens_this_GB, sequence_square_sum_this_GB
 
 
 def training_log(
@@ -1480,6 +1499,8 @@ def training_log(
     grad_norm,
     params_norm,
     num_zeros_in_grad,
+    num_total_tokens_this_GB,
+    sequence_square_sum_this_GB,
 ):
     """Log training information such as losses, timing, ...."""
     args = get_args()
@@ -1677,7 +1698,7 @@ def training_log(
         elapsed_time = timers('interval-time').elapsed(barrier=True)
         elapsed_time_per_iteration = elapsed_time / total_iterations
 
-        throughput = num_floating_point_operations(args, batch_size) / (
+        throughput = num_floating_point_operations(args,num_total_tokens_this_GB, sequence_square_sum_this_GB) / (
             elapsed_time_per_iteration * 10**12 * args.world_size
         )
 
@@ -2118,11 +2139,6 @@ def train(
     energy_monitor = get_energy_monitor()
     one_logger = get_one_logger()
 
-    # debugmtl 
-    # we don't need this after my changes
-    # if args.hybrid_context_parallel:
-    #     train_data_iterator = iter(HybridCPDataLoaderWrapper(train_data_iterator, config))
-
     if args.run_workload_inspector_server:
         try:
             from workload_inspector.utils.webserver import run_server
@@ -2404,6 +2420,8 @@ def train(
             exit_code,
             grad_norm,
             num_zeros_in_grad,
+            num_total_tokens_this_GB, 
+            sequence_square_sum_this_GB,
         ) = train_step(
             forward_step_func, train_data_iterator, model, optimizer, opt_param_scheduler, config, forward_backward_func
         )
@@ -2474,7 +2492,7 @@ def train(
         else:
             assert num_skipped_samples_in_batch == 0
         args.skipped_train_samples += num_skipped_samples_in_batch
-        num_floating_point_operations_in_batch = num_floating_point_operations(args, batch_size)
+        num_floating_point_operations_in_batch = num_floating_point_operations(args, num_total_tokens_this_GB, sequence_square_sum_this_GB)
         num_floating_point_operations_so_far += num_floating_point_operations_in_batch
         num_floating_point_operations_since_last_log_event += num_floating_point_operations_in_batch
 
@@ -2508,6 +2526,8 @@ def train(
             grad_norm,
             params_norm,
             num_zeros_in_grad,
+            num_total_tokens_this_GB, 
+            sequence_square_sum_this_GB
         )
 
         # Evaluation.
@@ -2673,6 +2693,8 @@ def evaluate(
                 decoder_seq_length=args.decoder_seq_length,
                 forward_only=True,
             )
+            # need to drop first two elements which are total_num_tokens and total_sequence_square_sum
+            loss_dicts = loss_dicts[2:]
             ft_integration.on_eval_step_end()
             config.timers = get_timers()
 
@@ -2693,24 +2715,18 @@ def evaluate(
                         if args.sft:
                             # normalize over micro batch instead of global
                             val = torch.vstack(val)
-                            val = val[:, 0] / val[:, 1]
-                            val = val.mean()
-                            torch.distributed.all_reduce(
-                                val,
-                                group=mpu.get_data_parallel_group(with_context_parallel=True)
-                            )
-                            val /= torch.distributed.get_world_size(
-                                group=mpu.get_data_parallel_group(with_context_parallel=True)
-                            )
-                            total_loss_dict[key][0] += val
-                            total_loss_dict[key][1] += 1
-                        else :
-                            val = torch.vstack(val).sum(dim=0)
-                            torch.distributed.all_reduce(
-                                val,
-                                group=mpu.get_data_parallel_group(with_context_parallel=True)
-                            )
-                            total_loss_dict[key] += val
+                            local_sum = val[:, 0].sum()
+                            local_tokens = val[:, 1].sum()
+                            pack = torch.stack([local_sum, local_tokens])   
+                            torch.distributed.all_reduce(pack, group=mpu.get_data_parallel_group(with_context_parallel=True))
+                            global_sum, global_tokens = pack[0], pack[1]
+                            if global_tokens.item() == 0:
+                                raise RuntimeError("[LOSS] global valid token count is 0 (all masked across DP/CP)")
+                            else:
+                                total_loss_dict[key][0] += global_sum / global_tokens
+                                total_loss_dict[key][1] += 1
+                             
+
                     elif val[0].numel() == 1:
                         val = torch.cat(val).sum()
                         total_loss_dict[key][0] += val