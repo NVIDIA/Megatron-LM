ca4d1bf46be26889ca35651e91e4d9c609b13af0
dimapihtar <dpihtar@gmail.com>
2025-11-05 04:34:53 -0800

    add tp_group to state_dict
    Signed-off-by: dimapihtar <dpihtar@gmail.com>

--- megatron/core/extensions/transformer_engine.py
+++ megatron/core/extensions/transformer_engine.py
@@ -809,11 +809,11 @@ class TERowParallelLinear(TELinear):
                 setattr(self.bias, "allreduce", True)
                 setattr(self.bias, "sequence_parallel", config.sequence_parallel)
 
-    def sharded_state_dict(self, prefix="", sharded_offsets=(), metadata=None):
+    def sharded_state_dict(self, prefix="", sharded_offsets=(), metadata=None, tp_group=None):
         """Sharding along axis 1, bias not sharded"""
         state_dict = self.state_dict(prefix="", keep_vars=True)
         return make_sharded_tensors_for_checkpoint(
-            state_dict, prefix, {"weight": 1}, sharded_offsets
+            state_dict, prefix, {"weight": 1}, sharded_offsets, tp_group=tp_group, dp_cp_group=metadata["dp_cp_group"]
         )
 
     def __repr__(self):
