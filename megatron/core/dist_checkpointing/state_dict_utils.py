2ee6f66208ea1680e89048be126fd1fdcab99203
dimapihtar <dpihtar@gmail.com>
2025-11-10 07:49:15 -0800

    fix style
    Signed-off-by: dimapihtar <dpihtar@gmail.com>

--- megatron/core/dist_checkpointing/state_dict_utils.py
+++ megatron/core/dist_checkpointing/state_dict_utils.py
@@ -101,10 +101,7 @@ def filter_out_empty_flatten_tensor(sharded_state_dict: Union[dict, list]):
     # `TorchShardedTensor._init_from_local_shards_and_global_metadata` to fail.
     # This situation may occur in custom Fully Sharded Data Parallel (FSDP) cases.
     sharded_state_dict, _ = extract_matching_values(
-        sharded_state_dict,
-        lambda v: not (
-            isinstance(v, ShardedTensor)
-        ),
+        sharded_state_dict, lambda v: not (isinstance(v, ShardedTensor))
     )
 
     return sharded_state_dict
