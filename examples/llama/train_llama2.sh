98edb377f1f5307f34e6e4b5776f7e34b60b1101
Wen Chen <Wen.Chen@amd.com>
2025-11-10 19:53:53 +0000

    Use e4m3 for both fwd and bwd for mxfp8.

--- examples/llama/train_llama2.sh
+++ examples/llama/train_llama2.sh
@@ -310,11 +310,13 @@ if [ "$MCORE" -eq 1 ]; then
 fi
 
 if [ "$TE_FP8" -eq 1 ]; then
-EXTRA_ARGS="$EXTRA_ARGS --transformer-impl=transformer_engine \
-    --fp8-format=hybrid \
+    EXTRA_ARGS="$EXTRA_ARGS --transformer-impl=transformer_engine \
 "
+
     if [ "$TE_FP8_RECIPE" == "delayed" ]; then
-        EXTRA_ARGS="$EXTRA_ARGS --fp8-margin=0 \
+        EXTRA_ARGS="$EXTRA_ARGS --fp8-recipe=delayed \
+            --fp8-format=hybrid \
+            --fp8-margin=0 \
             --fp8-interval=1 \
             --fp8-amax-history-len=1024 \
             --fp8-amax-compute-algo=max \
@@ -322,6 +324,7 @@ EXTRA_ARGS="$EXTRA_ARGS --transformer-impl=transformer_engine \
         "
     elif [ "$TE_FP8_RECIPE" == "mxfp8" ]; then
         EXTRA_ARGS="$EXTRA_ARGS --fp8-recipe=mxfp8 \
+            --fp8-format=e4m3 \
             --keep_fp8_weight_transpose_cache \
         "
         # Currently we have to keep fp8 weight tranpose cache due to an issue
@@ -329,6 +332,7 @@ EXTRA_ARGS="$EXTRA_ARGS --transformer-impl=transformer_engine \
         export NVTE_ROCM_ENABLE_MXFP8=1
     elif [ "$TE_FP8_RECIPE" == "tensorwise" ]; then
         EXTRA_ARGS="$EXTRA_ARGS --fp8-recipe=tensorwise \
+            --fp8-format=hybrid \
         "
     else
         echo "$TE_FP8_RECIPE is not supported"
