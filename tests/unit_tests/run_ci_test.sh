ab451105414469fb304e501d9c637d06ba20b263
Dennis Liu <denliu@nvidia.com>
2025-11-11 00:06:29 -0800

    Revert "Move NCCL env set to conftest."
    This reverts commit aa0faf01a667b882dec1daa3bb5305dc4a8026ba.

--- tests/unit_tests/run_ci_test.sh
+++ tests/unit_tests/run_ci_test.sh
@@ -136,6 +136,9 @@ DISTRIBUTED_ARGS=(
     --redirects "3"
 )
 
+# Reduce memory usage by NCCL
+export NCCL_MAX_NCHANNELS=1
+export NCCL_NVLS_ENABLE=0
 export ONE_LOGGER_JOB_CATEGORY=test
 
 for i in $(seq $UNIT_TEST_REPEAT); do
