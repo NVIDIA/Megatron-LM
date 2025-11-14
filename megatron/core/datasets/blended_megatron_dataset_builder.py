319390b2fcb97baf6d822966df6283f23c753ab0
lit <lit@nvidia.com>
2025-11-13 18:20:33 -0800

    remove abundant argument in BlendedMegatronDatasetBuilder
    Signed-off-by: lit <lit@nvidia.com>

--- megatron/core/datasets/blended_megatron_dataset_builder.py
+++ megatron/core/datasets/blended_megatron_dataset_builder.py
@@ -47,13 +47,11 @@ class BlendedMegatronDatasetBuilder(object):
         sizes: List[int],
         is_built_on_rank: Callable,
         config: BlendedMegatronDatasetConfig,
-        vp_stage: Optional[int] = None,
     ):
         self.cls = cls
         self.sizes = sizes
         self.is_built_on_rank = is_built_on_rank
         self.config = config
-        self.vp_stage = vp_stage
 
         log_single_rank(
             logger,
