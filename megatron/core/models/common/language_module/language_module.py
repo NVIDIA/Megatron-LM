63a958ff898b25fd21bb98f62be4a8b337efc740
Robin Zhang <robinz@nvidia.com>
2025-11-20 16:53:28 -0800

    remove None check in language_module
    Signed-off-by: Robin Zhang <robinz@nvidia.com>

--- megatron/core/models/common/language_module/language_module.py
+++ megatron/core/models/common/language_module/language_module.py
@@ -136,7 +136,6 @@ class LanguageModule(MegatronModule):
                     # Use is_cg_capturable=True for full iteration CUDA graphs to avoid torch.equal checks
                     is_cg_capturable = (
                         hasattr(self.config, 'cuda_graph_scope')
-                        and self.config.cuda_graph_scope
                         and CudaGraphScope.full_iteration in self.config.cuda_graph_scope
                     )
                     if is_cg_capturable and not is_te_min_version("2.7.0"):
