f9b3ad2e20614f906c860fd2740ff23c572f4703
dimapihtar <dpihtar@gmail.com>
2025-11-21 07:41:04 -0800

    fix code style
    Signed-off-by: dimapihtar <dpihtar@gmail.com>

--- tests/unit_tests/test_optimizer.py
+++ tests/unit_tests/test_optimizer.py
@@ -303,7 +303,9 @@ def test_optim_sharded_state_dict(use_distributed_optimizer: bool, precision: st
     model_sharded_state_dict = model.sharded_state_dict()
     metadata = {'distrib_optim_sharding_type': 'fully_reshardable'}
     if precision == 'bf16' or use_distributed_optimizer:
-        sharded_state_dict = optim.sharded_state_dict(model_sharded_state_dict, metadata=metadata, is_loading=True)
+        sharded_state_dict = optim.sharded_state_dict(
+            model_sharded_state_dict, metadata=metadata, is_loading=True
+        )
     else:
         sharded_state_dict = optim.sharded_state_dict(model_sharded_state_dict)
 
