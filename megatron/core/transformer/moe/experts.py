3431dfb66f553adca168859c3d204af3a09cdeb6
yiakwy-xpu-ml-framework-team <961186938@qq.com>
2025-11-25 05:07:57 +0800

    clean file

--- megatron/core/transformer/moe/experts.py
+++ megatron/core/transformer/moe/experts.py
@@ -824,7 +824,6 @@ class TEGroupedMLP(MegatronModule):
         if bias_parallel is None:
             return intermediate_parallel
         shape = intermediate_parallel.shape
-
         return (
             torch.cat(
                 [
