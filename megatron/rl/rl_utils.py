Fix the entropy sign. (#2374)
Co-authored-by: Jon Barker <19699370+jon-barker@users.noreply.github.com>
Co-authored-by: Teodor-Dumitru Ene <34819528+tdene@users.noreply.github.com>

megatron/rl/rl_utils.py
@@ -879,6 +879,8 @@ def maybe_log_training_metrics(
                     'advantages_hist': wandb_writer.plot.histogram(
                         advantages, 'advantages', 'Advantages'
                     ),
+                    'nonzero_groups_ratio': np.count_nonzero(group_stats.advantages)
+                            / len(group_stats.advantages),
                     'min_piold_to_inf_prob': group_stats.min_piold_to_inf_prob,
                     'max_piold_to_inf_prob': group_stats.max_piold_to_inf_prob,
                     'mean_piold_to_inf_prob': group_stats.mean_piold_to_inf_prob,
@@ -2206,7 +2208,7 @@ def calculate_grpo_loss(
 
     ref_diff = ref_logprobs - current_logprobs
     kl_term = ref_diff.exp() - ref_diff - 1
-    entropy_term = current_logprobs.exp() * current_logprobs
+    entropy_term = -current_logprobs.exp() * current_logprobs
 
     is_weights = torch.tensor(1.0, dtype=old_logprobs.dtype).to(old_logprobs.device)
     if inference_logprobs is not None:
@@ -2220,7 +2222,7 @@ def calculate_grpo_loss(
     loss = (
         -is_weights * torch.min(ratios * advantages, clamped_ratios * advantages)
         + kl_beta * kl_term
-        + entropy_weight * entropy_term
+        - entropy_weight * entropy_term
     )
 
     return loss, kl_term, ratios, entropy_term, truncated_from_above, truncated_from_below

tests/unit_tests/test_rl_utils.py
@@ -210,7 +210,7 @@ def test_grpo_loss_calculation_all_pi_eq():
     torch.testing.assert_close(loss, torch.zeros_like(loss))
     torch.testing.assert_close(kl_term, torch.zeros_like(kl_term))
     torch.testing.assert_close(ratios, torch.ones_like(ratios))
-    torch.testing.assert_close(entropy_term, torch.ones_like(ratios) * torch.e)
+    torch.testing.assert_close(entropy_term, -torch.ones_like(ratios) * torch.e)
 
 
 def test_grpo_loss_calculation_2x_ratios():
@@ -255,7 +255,7 @@ def test_entropy_calculation():
         entropy_weight=1.0,
     )
     torch.testing.assert_close(loss, torch.ones_like(ratios) * torch.e)
-    torch.testing.assert_close(entropy_term, torch.ones_like(ratios) * torch.e)
+    torch.testing.assert_close(entropy_term, -torch.ones_like(ratios) * torch.e)
 
 
 def test_grpo_loss_truncation():