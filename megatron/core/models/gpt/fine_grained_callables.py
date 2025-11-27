e0e6da12f2fbde9b58a52af4ffffbf02bf2177ad
Hongbin Liu <hongbinl@nvidia.com>
2025-11-26 20:34:02 -0800

    minor fix
    Signed-off-by: Hongbin Liu <hongbinl@nvidia.com>

--- megatron/core/models/gpt/fine_grained_callables.py
+++ megatron/core/models/gpt/fine_grained_callables.py
@@ -285,7 +285,6 @@ class TransformerLayerNode(ScheduleNode):
         with torch.cuda.nvtx.range(f"{self.name} wgrad"):
             for module in self.bwd_dw_callables:
                 module.backward_dw()
-
         self.bwd_dw_callables = None
 
     def _release_state(self):
