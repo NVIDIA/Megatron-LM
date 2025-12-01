f59afcc24308f66e5f7a797f021c1c3af73c4763
Hongbin Liu <hongbinl@nvidia.com>
2025-11-30 21:30:56 -0800

    format
    Signed-off-by: Hongbin Liu <hongbinl@nvidia.com>

--- megatron/core/extensions/transformer_engine.py
+++ megatron/core/extensions/transformer_engine.py
@@ -2193,3 +2193,8 @@ except ImportError:
         from transformer_engine.pytorch import cpu_offload
     except ImportError:
         cpu_offload = None
+try:
+    # pylint: disable=unused-import
+    from transformer_engine.pytorch.float8_tensor import Float8Tensor
+except ImportError:
+    Float8Tensor = None
