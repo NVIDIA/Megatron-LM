1c8d66c733fb4a5354630e1d6847aaf9126a9600
dimapihtar <dpihtar@gmail.com>
2025-12-01 11:27:57 -0800

    revert copyright changes
    Signed-off-by: dimapihtar <dpihtar@gmail.com>

--- megatron/core/dist_checkpointing/strategies/two_stage.py
+++ megatron/core/dist_checkpointing/strategies/two_stage.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# Copyright (c) 2022-2023, NVIDIA CORPORATION.  All rights reserved.
 
 """ 2-stage checkpoint loading. """
 import time
