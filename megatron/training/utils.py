7348e73c420e225af15c6ab52fa831f0f42bdc13
Kunlun Li <94586211+kunlunl@users.noreply.github.com>
2025-12-01 20:40:14 +0800

    Fix TP hanging issue

--- megatron/training/utils.py
+++ megatron/training/utils.py
@@ -635,7 +635,7 @@ def get_batch_on_this_tp_rank(data_iterator):
             1,
             dtype=torch.int32,
             device=torch.cuda.current_device(),
-        ) if not args.hybrid_context_parallel else None
+        ) if args.hybrid_context_parallel else None
         local_cp_size = torch.empty(
             1,
             dtype=torch.int32,
