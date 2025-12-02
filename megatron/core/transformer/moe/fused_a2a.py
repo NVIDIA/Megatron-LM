73def487b4b909eeb2487de95be53126e60b858b
Robin Zhang <robinz@nvidia.com>
2025-12-01 17:33:04 -0800

    Replay #2353

--- megatron/core/transformer/moe/fused_a2a.py
+++ megatron/core/transformer/moe/fused_a2a.py
@@ -320,6 +320,14 @@ def init_hybrid_ep_buffer(
     )
 
 
+def reset_hybrid_ep_buffer():
+    '''
+    Reset the HybridEP buffer
+    '''
+    global _hybrid_ep_buffer
+    _hybrid_ep_buffer = None
+
+
 class HybridEPDispatch(torch.autograd.Function):
     '''
     Fused dispatch operation for permute + dispatch a2a + permute using the HybridEP backend
