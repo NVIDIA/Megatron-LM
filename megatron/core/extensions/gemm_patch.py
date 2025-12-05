79da4369517d583de4f348998cec3cb587893ed0
Keith Wyss <kwyss@nvidia.com>
2025-12-04 23:46:14 -0800

    Move second iteration outside of loop structure.
    Signed-off-by: Keith Wyss <kwyss@nvidia.com>

--- megatron/core/extensions/gemm_patch.py
+++ megatron/core/extensions/gemm_patch.py
@@ -55,6 +55,7 @@ def new_general_gemm(A, B, workspace, out=None, accumulate=False, bias=None, **k
     and compares results for consistency.
     """
     cksum = [0, 0]
+    # Run twice for comparison.
     for run_idx in range(2):
         is_first_iter = run_idx == 0
         need_output_copy = out is not None and (bias is out or accumulate)
@@ -72,31 +73,29 @@ def new_general_gemm(A, B, workspace, out=None, accumulate=False, bias=None, **k
             A, B, workspace, out=out_arg, accumulate=accumulate, bias=bias_arg, **kwargs
         )
         out_res = res[0]
-        is_second_iter = run_idx == 1
         cksum[run_idx] = out_res.sum(dtype=torch.float32).item()
         if is_first_iter:
             rerun_res = out_res
             continue
-        if is_second_iter:
-            same_cksum = (
-                math.fabs(cksum[1] - cksum[0]) / (math.fabs(cksum[1]) + EPSILON) <= CKSUM_TOLERANCE
-            )
-            if not same_cksum:
-                logger.log(
-                    logging.ERROR,
-                    f"RANK {get_rank_info()}: DIFFERENT CHECKSUM ON 2ND RUN for dtype "
-                    f"{get_dtype(A)}. {cksum[1]} != {cksum[0]}, "
-                    f"shapes A:{list(A.size())},B:{list(B.size())},out:{list(out_res.shape)}",
-                )
-            elif not torch.equal(rerun_res, out_res):
-                logger.log(
-                    logging.ERROR,
-                    f"RANK {get_rank_info()}: DIFFERENT BITWISE on 2ND RUN for dtype "
-                    f"{get_dtype(A)}. "
-                    f"shapes A:{list(A.size())},B:{list(B.size())},out:{list(out_res.shape)}",
-                )
-            del rerun_res
-        return res
+
+    # Check the consistency of the first and second run and return.
+    same_cksum = math.fabs(cksum[1] - cksum[0]) / (math.fabs(cksum[1]) + EPSILON) <= CKSUM_TOLERANCE
+    if not same_cksum:
+        logger.log(
+            logging.ERROR,
+            f"RANK {get_rank_info()}: DIFFERENT CHECKSUM ON 2ND RUN for dtype "
+            f"{get_dtype(A)}. {cksum[1]} != {cksum[0]}, "
+            f"shapes A:{list(A.size())},B:{list(B.size())},out:{list(out_res.shape)}",
+        )
+    elif not torch.equal(rerun_res, out_res):
+        logger.log(
+            logging.ERROR,
+            f"RANK {get_rank_info()}: DIFFERENT BITWISE on 2ND RUN for dtype "
+            f"{get_dtype(A)}. "
+            f"shapes A:{list(A.size())},B:{list(B.size())},out:{list(out_res.shape)}",
+        )
+    del rerun_res
+    return res
 
 
 def new_general_grouped_gemm(
@@ -109,9 +108,10 @@ def new_general_grouped_gemm(
     saved_output = None
     out_first_iter = None
     bias_first_iter = None
+    # Run twice for comparison
     for run_idx in range(2):
-        is_second_iter = run_idx == 1
-        if not is_second_iter:
+        is_first_iter = run_idx == 0
+        if is_first_iter:
             out_first_iter = (
                 [t.clone().detach() for t in out]
                 if out is not None and (accumulate or out is bias)
@@ -121,29 +121,30 @@ def new_general_grouped_gemm(
         res = orig_general_grouped_gemm(
             A,
             B,
-            out_first_iter if not is_second_iter else out,
+            out_first_iter if is_first_iter else out,
             out_dtype,
             workspaces,
             grad=grad if is_second_iter else False,
             accumulate=accumulate,
-            bias=bias_first_iter if not is_second_iter else bias,
+            bias=bias_first_iter if is_first_iter else bias,
             **kwargs,
         )
         output = res[0]
-        if not is_second_iter:
+        if is_first_iter:
             saved_output = [t.clone().detach() for t in output]
             continue
-        if is_second_iter:
-            for tidx in range(len(output)):
-                if not torch.equal(output[tidx], saved_output[tidx]):
-                    logging.log(
-                        logging.ERROR,
-                        f"RANK {get_rank_info()}: DIFFERENT GROUPED_GEMM RESULT ON 2ND RUN FOR OP "
-                        f"GroupedGEMM group {tidx} = {get_dtype(A[tidx])}({list(A[tidx].size())}) "
-                        f"x {get_dtype(B[tidx])}({list(B[tidx].size())}) -> "
-                        f"{get_dtype(output[tidx])}({list(output[tidx].size())})",
-                    )
-        return res
+
+    # Check comparison and return
+    for tidx in range(len(output)):
+        if not torch.equal(output[tidx], saved_output[tidx]):
+            logging.log(
+                logging.ERROR,
+                f"RANK {get_rank_info()}: DIFFERENT GROUPED_GEMM RESULT ON 2ND RUN FOR OP "
+                f"GroupedGEMM group {tidx} = {get_dtype(A[tidx])}({list(A[tidx].size())}) "
+                f"x {get_dtype(B[tidx])}({list(B[tidx].size())}) -> "
+                f"{get_dtype(output[tidx])}({list(output[tidx].size())})",
+            )
+    return res
 
 
 def patch_te_gemms() -> None:
@@ -161,15 +162,21 @@ def patch_te_gemms() -> None:
         if 'general_gemm' in dir(sys.modules[module]):
             if sys.modules[module].general_gemm is orig_general_gemm:
                 log_single_rank(logger, logging.INFO, f"PATCHING general_gemm IN MODULE: {module}")
-                sys.modules[module].general_gemm = new_general_gemm
+                sys.modules[module].general_gemm = new_general_gemm  # type: ignore[attr-defined]
             else:
-                log_single_rank(logger, logging.WARN, f"SKIP PATCHING general_gemm IN MODULE: {module}")
+                log_single_rank(
+                    logger, logging.WARN, f"SKIP PATCHING general_gemm IN MODULE: {module}"
+                )
         if 'general_grouped_gemm' in dir(sys.modules[module]):
             if sys.modules[module].general_grouped_gemm is orig_general_grouped_gemm:
                 log_single_rank(
                     logger, logging.INFO, f"PATCHING general_grouped_gemm IN MODULE: {module}"
                 )
-                sys.modules[module].general_grouped_gemm = new_general_grouped_gemm
+                sys.modules[module].general_grouped_gemm = (  # type: ignore[attr-defined]
+                    new_general_grouped_gemm
+                )
             else:
-                log_single_rank(logger, logging.WARN, f"SKIP PATCHING general_gemm IN MODULE: {module}")
+                log_single_rank(
+                    logger, logging.WARN, f"SKIP PATCHING general_gemm IN MODULE: {module}"
+                )
     _GLOBAL_IS_PATCHED_ = True
