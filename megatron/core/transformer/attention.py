15055311bce588f0f8c10d5ce271c319e8ab6784
Keshav Santhanam <ksanthanam@nvidia.com>
2025-12-07 04:06:58 +0000

    Fix FA3 import (#2577)
    Signed-off-by: Keshav Santhanam <ksanthanam@nvidia.com>

--- megatron/core/transformer/attention.py
+++ megatron/core/transformer/attention.py
@@ -61,7 +61,7 @@ except ImportError as e:
 
 if not HAVE_FA3:
     try:
-        from flash_attn_3.flash_attn_interface import _flash_attn_forward
+        from flashattn_hopper.flash_attn_interface import _flash_attn_forward
         from flashattn_hopper.flash_attn_interface import (
             flash_attn_with_kvcache as flash_attn3_with_kvcache,
         )
