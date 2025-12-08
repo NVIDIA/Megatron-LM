e38c2a9e6c4deb6ff21dae97424181df5862d038
Lawrence McAfee <lmcafee@nvidia.com>
2025-12-07 19:52:22 -0800

    added docstring.

--- megatron/core/models/gpt/gpt_model.py
+++ megatron/core/models/gpt/gpt_model.py
@@ -258,7 +258,12 @@ class GPTModel(LanguageModule):
                 quant_config = get_quant_config_or_none(name, self.config.quant_recipe)
                 module.finish_init(quant_config)
 
-    def get_max_sequence_length(self):
+    def get_max_sequence_length(self) -> int:
+        """Get max sequence length.
+
+        Returns:
+            (int) Max sequence length.
+        """
         return self.max_sequence_length
 
     def set_input_tensor(self, input_tensor: Tensor) -> None:
