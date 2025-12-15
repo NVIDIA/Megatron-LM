0685facb808b37c8ac6e7686394010ed38fa7dad
Siddharth Singh <sidsingh@nvidia.com>
2025-12-15 07:07:00 -0800

    restore arg for backwards compatibility

--- megatron/core/transformer/transformer_config.py
+++ megatron/core/transformer/transformer_config.py
@@ -726,6 +726,9 @@ class TransformerConfig(ModelParallelConfig):
     symmetric_ar_type: Optional[str] = None
     """Type of symmetric all reduce to use"""
 
+    use_inference_optimized_layers: bool = False
+    """If True, use inference optimized transformer layers during inference."""
+
     inference_fuse_tp_communication: bool = False
     """ If true, uses a fused reduce-scatter-residual-norm-allgather kernel during inference. """
 
