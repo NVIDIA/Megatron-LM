6b3b905884baed75c7affe2b907c4dde36378cf6
Dennis Liu <denliu@nvidia.com>
2026-01-06 05:09:21 -0800

    Formatting

--- tests/functional_tests/python_test_utils/test_pretraining_regular_pipeline.py
+++ tests/functional_tests/python_test_utils/test_pretraining_regular_pipeline.py
@@ -20,8 +20,14 @@ CHECK_THRESHOLDS = {
     "generated_tokens": [common.DeterministicTest(), common.ApproximateTest(atol=0, rtol=0.05)],
     "logprobs": [common.DeterministicTest(), common.ApproximateTest(atol=0, rtol=0.05)],
     "load_balancing_loss": [common.DeterministicTest(), common.ApproximateTest(atol=0, rtol=0.05)],
-    "seq_load_balancing_loss": [common.DeterministicTest(), common.ApproximateTest(atol=0, rtol=0.05)],
-    "global_load_balancing_loss": [common.DeterministicTest(), common.ApproximateTest(atol=0, rtol=0.05)],
+    "seq_load_balancing_loss": [
+        common.DeterministicTest(),
+        common.ApproximateTest(atol=0, rtol=0.05),
+    ],
+    "global_load_balancing_loss": [
+        common.DeterministicTest(),
+        common.ApproximateTest(atol=0, rtol=0.05),
+    ],
 }
 
 
