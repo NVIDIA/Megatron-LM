1b4f4a01aeda4e2bfb2ec9214f6598862973b298
jianbinc <shjwudp@gmail.com>
2026-01-12 23:53:48 +0800

    Add LinearCrossEntropyModule to enable backward compatibility of Linear-Cross-Entropy Fusion with existing checkpoints

--- megatron/core/models/gpt/gpt_model.py
+++ megatron/core/models/gpt/gpt_model.py
@@ -25,6 +25,7 @@ from megatron.core.process_groups_config import ProcessGroupCollection
 from megatron.core.quantization.utils import get_quant_config_or_none
 from megatron.core.tensor_parallel import gather_from_sequence_parallel_region
 from megatron.core.transformer.enums import CudaGraphScope, ModelType
+from megatron.core.transformer.linear_cross_entropy import LinearCrossEntropyModule
 from megatron.core.transformer.multi_token_prediction import (
     MTPLossAutoScaler,
     MTPLossLoggingHelper,
@@ -234,7 +235,7 @@ class GPTModel(LanguageModule):
                 self.embedding_activation_buffer = None
                 self.grad_output_buffer = None
 
-            self.output_layer = tensor_parallel.ColumnParallelLinear(
+            self.output_layer = LinearCrossEntropyModule(
                 config.hidden_size,
                 self.vocab_size,
                 config=config,
@@ -248,17 +249,6 @@ class GPTModel(LanguageModule):
                 grad_output_buffer=self.grad_output_buffer,
                 tp_group=self.pg_collection.tp,
             )
-
-            # The Linear-Cross-Entropy fusion bypasses the output layer during the forward pass.
-            # The output_layer submodule shares its weights with the gpt_model to prevent
-            # issues arising from pre-forward hooks attached to the output layer.
-            if (
-                self.config.cross_entropy_loss_fusion
-                and self.config.cross_entropy_fusion_impl == 'linear'
-            ):
-                self.shared_output_layer_weight = self.output_layer.weight
-            else:
-                self.shared_output_layer_weight = None
 
         if self.pre_process or self.post_process or self.mtp_process:
             self.setup_embeddings_and_output_layer()
@@ -599,20 +589,11 @@ class GPTModel(LanguageModule):
                 )
 
                 # Compute mtp loss without storing logits to save memory.
-                mtp_loss = self.compute_output_layer_and_language_model_loss(
-                    hidden_states_list[mtp_layer_number + 1],
-                    labels=mtp_labels,
-                    weight=(
-                        output_weight
-                        if output_weight is not None
-                        else self.shared_output_layer_weight
-                    ),
-                    sequence_parallel_enabled=self.output_layer.sequence_parallel,
-                    column_parallel_linear=self.output_layer,
-                    col_linear_kwargs={
-                        'weight': output_weight,
-                        'runtime_gather_output': runtime_gather_output,
-                    },
+                mtp_loss = self.output_layer(
+                    output_cross_entropy_loss=True,
+                    input_=hidden_states_list[mtp_layer_number + 1],
+                    weight=output_weight,
+                    runtime_gather_output=runtime_gather_output,
                 )
 
                 mtp_loss = loss_mask * mtp_loss
@@ -691,16 +672,12 @@ class GPTModel(LanguageModule):
             # [s b h] => [b s h]
             return logits.transpose(0, 1).contiguous()
 
-        loss = self.compute_output_layer_and_language_model_loss(
-            hidden_states,
+        loss = self.output_layer(
+            output_cross_entropy_loss=True,
+            input_=hidden_states,
             labels=labels,
-            weight=output_weight if output_weight is not None else self.shared_output_layer_weight,
-            sequence_parallel_enabled=self.output_layer.sequence_parallel,
-            column_parallel_linear=self.output_layer,
-            col_linear_kwargs={
-                'weight': output_weight,
-                'runtime_gather_output': runtime_gather_output,
-            },
+            weight=output_weight,
+            runtime_gather_output=runtime_gather_output,
         )
 
         return loss
