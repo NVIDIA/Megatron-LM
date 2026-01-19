e96dd28ca3afb7e7b8fd39e789d4a0f616621ced
dimapihtar <dpihtar@gmail.com>
2026-01-19 05:31:13 -0800

    fix rl unit test
    Signed-off-by: dimapihtar <dpihtar@gmail.com>

--- tests/unit_tests/test_rl_utils.py
+++ tests/unit_tests/test_rl_utils.py
@@ -61,6 +61,7 @@ class MockTokenizer:
         self.eod = 43
         self.vocab_size = VOCAB
         self.bos = None
+        self.library = None
 
     def detokenize(self, tokens):
         return [str(tok) for tok in tokens]
