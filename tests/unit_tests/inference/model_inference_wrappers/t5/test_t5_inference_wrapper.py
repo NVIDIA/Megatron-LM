cff3c1efeb095a7621f851d90e2351ad1a30670e
Keshav Santhanam <ksanthanam@nvidia.com>
2026-01-21 13:20:31 -0800

    Fix copyright
    Signed-off-by: Keshav Santhanam <ksanthanam@nvidia.com>

--- tests/unit_tests/inference/model_inference_wrappers/t5/test_t5_inference_wrapper.py
+++ tests/unit_tests/inference/model_inference_wrappers/t5/test_t5_inference_wrapper.py
@@ -1,3 +1,5 @@
+# Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+
 from argparse import Namespace
 from copy import deepcopy
 from unittest import mock
--- tests/unit_tests/inference/text_generation_controllers/test_encoder_decoder_text_generation_controller.py
+++ tests/unit_tests/inference/text_generation_controllers/test_encoder_decoder_text_generation_controller.py
@@ -1,3 +1,5 @@
+# Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+
 import random
 import string
 import time
