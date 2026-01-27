964302c097c18e2ceb52a182acb4888352d6edd3
Keshav Santhanam <ksanthanam@nvidia.com>
2026-01-26 16:18:52 -0800

    Remove --inference-max-batch-size
    Signed-off-by: Keshav Santhanam <ksanthanam@nvidia.com>

--- examples/rl/model_configs/llama3p1_8b_instruct.sh
+++ examples/rl/model_configs/llama3p1_8b_instruct.sh
@@ -77,7 +77,7 @@ MODEL_OPTIONS="\
   --ckpt-format torch_dist \
   --seq-length $MAX_SEQ_LENGTH \
   --inference-max-seq-length $MAX_SEQ_LENGTH \
-  --inference-max-batch-size $MAX_INFERENCE_BS \
+  --inference-max-requests $MAX_INFERENCE_BS \
   --pretrained-checkpoint $CHECKPOINT \
   --add-qkv-bias \
   --normalization RMSNorm \
