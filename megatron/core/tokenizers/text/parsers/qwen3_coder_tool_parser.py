441e49a9edd229b7fc78256830d2bf9aa1da799e
Robert Kirby <rkirby@cw-dfw-cs-001-vscode-01.cm.cluster>
2026-02-04 14:23:44 -0600

    More copyright

--- megatron/core/tokenizers/text/parsers/qwen3_coder_tool_parser.py
+++ megatron/core/tokenizers/text/parsers/qwen3_coder_tool_parser.py
@@ -1,3 +1,4 @@
+# Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 
