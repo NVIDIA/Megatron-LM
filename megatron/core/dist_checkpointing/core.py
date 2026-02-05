53ea213ee5bb2471a19e49983a334398418406fe
dimapihtar <dpihtar@gmail.com>
2026-02-05 07:15:09 -0800

    fix check_is_distributed_checkpoint
    Signed-off-by: dimapihtar <dpihtar@gmail.com>

--- megatron/core/dist_checkpointing/core.py
+++ megatron/core/dist_checkpointing/core.py
@@ -2,7 +2,9 @@
 
 """ Module for managing distributed checkpoints metadata. """
 
-from pathlib import Path
+import os
+
+from megatron.core.msc_utils import MultiStorageClientFeature
 
 
 class CheckpointingException(Exception):
@@ -11,16 +13,21 @@ class CheckpointingException(Exception):
     pass
 
 
-def check_is_distributed_checkpoint(checkpoint_dir):
-    """Checks if the checkpoint directory contains distributed checkpoint files.
+def check_is_distributed_checkpoint(checkpoint_dir: str) -> bool:
+    """Checks if the checkpoint directory contains .metadata file.
 
     Args:
         checkpoint_dir: Path to the checkpoint directory to check
 
     Returns:
-        bool: True if the directory contains .distcp files indicating it is a distributed checkpoint
+        bool: True if .metadata file exists, indicating a distributed checkpoint.
     """
-
-    checkpoint_path = Path(checkpoint_dir)
-    distcp_files = list(checkpoint_path.glob('*.distcp'))
-    return len(distcp_files) > 0
+    metadata_file = os.path.join(checkpoint_dir, '.metadata')
+    common_file = os.path.join(checkpoint_dir, 'common.pt')
+    if checkpoint_dir:
+        if MultiStorageClientFeature.is_enabled():
+            msc = MultiStorageClientFeature.import_package()
+            return msc.os.path.exists(metadata_file) and msc.os.path.exists(common_file)
+        else:
+            return os.path.exists(metadata_file) and os.path.exists(common_file)
+    return False
