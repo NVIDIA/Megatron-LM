7a023861bed1568e66933cae6df1dcb2892b864a
oliver könig <okoenig@nvidia.com>
2026-02-06 13:11:26 +0000

    use print_rank_0
    Signed-off-by: oliver könig <okoenig@nvidia.com>

--- megatron/training/training.py
+++ megatron/training/training.py
@@ -2959,7 +2959,7 @@ def train(
         for param_group in optimizer.param_groups:
             if len(param_group['params']) == 0:
                 continue
-            print(f"DEFAULT CONFIG VALUE: {param_group['default_config']}")
+            print_rank_0(f"DEFAULT CONFIG VALUE: {param_group['default_config']}")
             if param_group['default_config']:
                 learning_rate = param_group['lr']
 
