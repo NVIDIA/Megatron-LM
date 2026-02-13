89aac400815f3675e9057c93b5c6532e79255b43
dimapihtar <dpihtar@gmail.com>
2026-02-13 04:23:23 -0800

    remove unused import
    Signed-off-by: dimapihtar <dpihtar@gmail.com>

--- megatron/core/pipeline_parallel/schedules.py
+++ megatron/core/pipeline_parallel/schedules.py
@@ -8,7 +8,6 @@ import torch
 from torch.autograd.variable import Variable
 
 from megatron.core import parallel_state
-from megatron.core.enums import ModelType
 from megatron.core.pipeline_parallel.fine_grained_activation_offload import (
     FineGrainedActivationOffloadingInterface as off_interface,
 )
