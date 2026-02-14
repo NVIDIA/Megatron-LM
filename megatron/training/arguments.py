fc75657faf82c464398bc48b6b47259c84064908
tailaim <tailaim@nvidia.com>
2026-02-14 05:25:44 -0800

    minor fixes
    Signed-off-by: tailaim <tailaim@nvidia.com>

--- megatron/training/arguments.py
+++ megatron/training/arguments.py
@@ -1065,17 +1065,6 @@ def validate_args(args, defaults={}):
         assert args.context_parallel_size * args.max_seqlen_per_dp_cp_rank >= args.seq_length, \
             f'Packed sequence buffer size ({args.context_parallel_size * args.max_seqlen_per_dp_cp_rank}) ' \
             f'must be >= single sequence max length ({args.seq_length})'
-        if args.mock_data and args.sft_mock_dataset_config_json is None:
-            args.sft_mock_dataset_config_json = json.dumps(
-                {
-                    "mode": "distribution",
-                    "type": "lognormal",
-                    "min_seq_len": args.seq_length // 2,
-                    "max_seq_len": args.seq_length,
-                    "mean_seq_len": args.seq_length // 4 * 3,
-                    "lognormal_sigma": 1.1,
-                }
-            )
 
     # disable async_tensor_model_parallel_allreduce when
     # model parallel memory optimization is enabled
@@ -1831,9 +1820,6 @@ def _add_network_size_args(parser):
         "persist_layer_norm",
         "bias_dropout_fusion",
         "apply_rope_fusion",
-        "max_seqlen_per_dp_cp_rank",
-        "hybrid_context_parallel",
-        "sequence_packing_scheduler",
     ]
     transformer_factory = ArgumentGroupFactory(TransformerConfig, exclude=exclude)
     transformer_group = transformer_factory.build_group(parser, "transformer configuration")
@@ -2535,7 +2521,7 @@ def _add_distributed_args(parser):
                        'all layers will share the same communication type. Users can also '
                        'specify separated types for each layer like '
                        '--cp-comm-type p2p p2p a2a a2a a2a+p2p a2a+p2p')
-    group.add_argument('--sequence-packing-scheduler', type=str, default='default_sequence_packing', choices=['default_sequence_packing'])
+    group.add_argument('--sequence-packing-scheduler', type=str, default=None, choices=['dp_balanced'])
     group.add_argument('--fake-process-group', action='store_true', default=False,
                        help='If set, initialize with fake distributed process group and all distributed communication operations will be skipped. \
                        This is quite useful for profiling memory usage of distributed training with just one GPU. \
