3a0a527cc10ec45c650f6d391b3151c1312a71af
Siddharth Singh <sidsingh@nvidia.com>
2026-02-20 09:16:04 -0800

    minor

--- megatron/core/transformer/cuda_graphs.py
+++ megatron/core/transformer/cuda_graphs.py
@@ -1586,11 +1586,11 @@ class CudaGraphManager(torch.nn.Module):
                     runner.fwd_graph_recorded = True
                     runner.cudagraph_created = True
                     runner = runner.eval()
+                    
                     # Record this to the global execution record
                     _CudagraphGlobalRecord.cudagraph_inference_record.append(
                         (runner, "fwd", args, kwargs)
                     )
-                   
 
                 # Now replay the graph
                 out = runner.replay_graph_capture(self.is_first_microbatch, args, kwargs)
