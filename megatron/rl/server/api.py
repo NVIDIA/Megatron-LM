51cb504e6e949ce40f88f2961cde1955ce7750b1
Teodor-Dumitru Ene <teodord.ene@gmail.com>
2026-02-20 06:29:43 -0600

    Address reviewer comments

--- megatron/rl/server/api.py
+++ megatron/rl/server/api.py
@@ -21,9 +21,6 @@ class Server(TypeLookupable):
     def resume(self):
         pass
 
-    def increment_staleness(self):
-        pass
-
     async def kill(self):
         raise NotImplementedError
 
