36548acf01a4d250e15ed48da778defc496bc767
qiyuw <qiyuw@nvidia.com>
2026-02-25 19:52:46 +0000

    address comments
    Signed-off-by: qiyuw <qiyuw@nvidia.com>

--- megatron/core/distributed/distributed_data_parallel.py
+++ megatron/core/distributed/distributed_data_parallel.py
@@ -7,7 +7,7 @@ from typing import Optional
 import torch
 
 from ..config_logger import has_config_logger_enabled, log_config_to_disk
-from ..fp8_utils import is_float8tensor, is_mxfp8tensor, post_all_gather_processing
+from ..fp8_utils import is_float8tensor, post_all_gather_processing
 from ..process_groups_config import ProcessGroupCollection
 from ..transformer.cuda_graphs import is_graph_capturing
 from ..transformer.transformer_config import TransformerConfig
@@ -488,26 +488,26 @@ class DistributedDataParallel(_BaseDataParallel):
                 # The paramaters are cast from bf16 to MXFP8 during copy.
                 # In the case of "overlap_param_gather=True", the param copy is done
                 # in "finish_param_sync" stage after zeroing the shared gardient buffers.
-                is_bf16_weight_group = False
                 if self.ddp_config.reuse_grad_buf_for_mxfp8_param_ag:
                     for bucket in bucket_group.buckets:
+                        is_bf16_weight_bucket = False
                         for param in bucket.params:
                             # Skip copying since bf16 weights in the mxfp8 model
                             # are already mapped to param.data.
-                            if not is_mxfp8tensor(param) and not is_float8tensor(param):
-                                is_bf16_weight_group = True
+                            if not is_float8tensor(param):
+                                is_bf16_weight_bucket = True
                                 break
                             param_start, param_end = bucket.param_to_index[param]
                             param_slice = bucket.param_data.view(-1)[param_start:param_end]
                             param.data.copy_(param_slice.view(param.data.shape))
+                        if is_bf16_weight_bucket:
+                            continue
                         # All-gathered params are not needed after being copied to param.data.
                         # Zero out the param buffer (shared with grad buffer) for gradient
                         # accumulation. We cannot zero out the entire grad buffer because one grad
                         # buffer may correspond to multiple param buffers. If we zero out the entire
                         # grad buffer, it would clear the data of those param buffers that have not
                         # yet completed AG.
-                        if is_bf16_weight_group:
-                            break
                         bucket.param_data.zero_()
                 else:
                     fp8_params = []
--- megatron/core/distributed/param_and_grad_buffer.py
+++ megatron/core/distributed/param_and_grad_buffer.py
@@ -342,26 +342,26 @@ class _ParamAndGradBucketGroup:
 
             # For the mxfp8_param with "reuse_grad_buf_for_mxfp8_param_ag=True",
             # we need to copy the param_data from the shared_param/grad_buffer to param.data
-            # after the param all-gather.
-            is_bf16_weight_group = False
+            # after the param all-gather.           
             if self.ddp_config.reuse_grad_buf_for_mxfp8_param_ag:
                 for bucket in self.buckets:
+                    is_bf16_weight_bucket = False
                     for param in bucket.params:
                         # Skip copying since bf16 weights in the mxfp8 model
                         # are already mapped to param.data.
-                        if not is_mxfp8tensor(param) and not is_float8tensor(param):
-                            is_bf16_weight_group = True
+                        if not is_float8tensor(param):
+                            is_bf16_weight_bucket = True
                             break
                         param_start, param_end = bucket.param_to_index[param]
                         param_slice = bucket.param_data.view(-1)[param_start:param_end]
                         param.data.copy_(param_slice.view(param.data.shape))
+                    if is_bf16_weight_bucket:
+                        continue
                     # All-gathered params are not needed after being copied to param.data.
                     # Zero out the param buffer (shared with grad buffer) for gradient accumulation.
                     # We cannot zero out the entire grad buffer because one grad buffer may
                     # correspond to multiple param buffers. If we zero out the entire grad buffer,
                     # it would clear the data of those param buffers that have not yet completed AG.
-                    if is_bf16_weight_group:
-                        break
                     bucket.param_data.zero_()
             else:
                 fp8_params = []
@@ -830,20 +830,7 @@ class _ParamAndGradBuffer:
             param_start_index, param_end_index, bucket_id = self.param_index_map[param]
             # For MXFP8 param:
             # we only need to map bf16 weights (layernorm, embedding, etc) to the buffer.
-            if self.ddp_config.reuse_grad_buf_for_mxfp8_param_ag:
-                if not is_mxfp8tensor(param) and not is_float8tensor(param):
-                    if self.param_data is not None:
-                        new_param_data = self._get(
-                            param.data.shape, param_start_index, buffer_type=BufferType.PARAM
-                        )
-                        old_param_data = param.data
-                        param.data = new_param_data
-                        assert old_param_data._base is None
-                        # Copy tensor values (from initialization or checkpoint).
-                        param.data.detach().copy_(old_param_data)
-                        del old_param_data
-            else:
-                # Assign param.data to appropriate segment of self.param_data.
+            if not self.ddp_config.reuse_grad_buf_for_mxfp8_param_ag or not is_mxfp8tensor(param):
                 if self.param_data is not None:
                     new_param_data = self._get(
                         param.data.shape, param_start_index, buffer_type=BufferType.PARAM
