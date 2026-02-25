f0058b18429366f67f5f174cf90bc1d0d8558576
oliver könig <okoenig@nvidia.com>
2026-02-25 23:17:07 +0100

    ci: Test docs build (#3583)
    Signed-off-by: oliver könig <okoenig@nvidia.com>

--- docs/discussions/megatron-fsdp-user-guide/megatron-fsdp-user-guide.md
+++ docs/discussions/megatron-fsdp-user-guide/megatron-fsdp-user-guide.md
@@ -1,3 +1,16 @@
+---
+orphan: true
+---
+
+<!---
+   Copyright (c) 2022-2026, NVIDIA CORPORATION. All rights reserved.
+   NVIDIA CORPORATION and its licensors retain all intellectual property
+   and proprietary rights in and to this software, related documentation
+   and any modifications thereto. Any use, reproduction, disclosure or
+   distribution of this software and related documentation without an express
+   license agreement from NVIDIA CORPORATION is strictly prohibited.
+-->
+
 # Megatron-FSDP User Guide
 
 ## Table of Contents
@@ -30,6 +43,7 @@ We also recommend adding the following configurations to further improve perform
 ```bash
 unset CUDA_DEVICE_MAX_CONNECTIONS
 ```
+
 ```bash
 --calculate-per-token-loss
 --init-model-with-meta-device
--- megatron/core/utils.py
+++ megatron/core/utils.py
@@ -309,28 +309,6 @@ def experimental_cls(introduced_with_version: str):
     return validator
 
 
-def get_torch_version():
-    """Get pytorch version from __version__; if not available use pip's. Use caching."""
-
-    if not HAVE_PACKAGING:
-        raise ImportError(
-            "packaging is not installed. Please install it with `pip install packaging`."
-        )
-
-    def get_torch_version_str():
-        import torch
-
-        if hasattr(torch, "__version__"):
-            return str(torch.__version__)
-        else:
-            return version("torch")
-
-    global _torch_version
-    if _torch_version is None:
-        _torch_version = PkgVersion(get_torch_version_str())
-    return _torch_version
-
-
 def get_te_version():
     """Get TE version from __version__; if not available use pip's. Use caching."""
     if not HAVE_PACKAGING:
