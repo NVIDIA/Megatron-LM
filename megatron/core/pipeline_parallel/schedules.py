bb509eeb03e4ed0fe67d2f2c80d786ceb01b7ae6
jinliangl <jinliangl@nvidia.com>
2026-02-26 23:10:39 +0800

    add lint
    Signed-off-by: jinliangl <jinliangl@nvidia.com>

--- megatron/core/pipeline_parallel/schedules.py
+++ megatron/core/pipeline_parallel/schedules.py
@@ -260,12 +260,12 @@ def forward_step_calc_loss(
             if len(outputs) == 3:
                 output_tensor, num_tokens, loss_reduced = outputs
                 if not config.calculate_per_token_loss:
+                    # When num_tokens is different across cp group, we need to allreduce it across cp group to get the correct average.
+                    torch.distributed.allreduce(num_tokens, group=parallel_state.get_context_parallel_group())
                     # Protect against division by zero when all tokens are masked
                     #   in a microbatch.
-                    torch.distributed.allreduce(num_tokens, group=parallel_state.get_context_parallel_group())
-                    # after allreduce across cp group, num_tokens includes whole sentences
-                    output_tensor /= num_tokens
-                    output_tensor *= mpu.get_context_parallel_size()
+                    output_tensor /= torch.clamp(num_tokens, min=1)
+                    output_tensor *= parallel_state.get_context_parallel_size()
                     output_tensor /= num_microbatches
             else:
                 # preserve legacy loss averaging behavior (ie, over the number of microbatches)
