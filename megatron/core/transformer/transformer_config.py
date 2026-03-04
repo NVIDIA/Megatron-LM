8a889c0db43bfd0bc4d5c89368bfdb93627d2bf7
Hongbin Liu <hongbinl@nvidia.com>
2026-03-04 05:28:48 -0800

    minor fix
    Signed-off-by: Hongbin Liu <hongbinl@nvidia.com>

--- megatron/core/transformer/transformer_config.py
+++ megatron/core/transformer/transformer_config.py
@@ -1425,7 +1425,7 @@ class TransformerConfig(ModelParallelConfig):
                 )
             if self.delay_offload_until_cuda_graph:
                 assert (
-                    self.external_cuda_graph or self.enable_cuda_graph
+                    self.transformer_impl == "transformer_engine"
                 ), "delay_offload_until_cuda_graph must be used with cuda graph."
             assert (
                 self.min_offloaded_tensor_size >= 0
@@ -1440,6 +1440,7 @@ class TransformerConfig(ModelParallelConfig):
                 assert (
                     self.cuda_graph_impl == "transformer_engine"
                 ), "cuda_graph_impl must be transformer_engine when enabling offloading."
+            if self.cuda_graph_impl == "transformer_engine":
                 assert (
                     self.cuda_graph_scope is not None
                 ), "cuda_graph_scope must be set when enabling offloading."
