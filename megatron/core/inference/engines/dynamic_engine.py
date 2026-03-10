48e954238661a1d3c5828c9682eb0bcd1443c4ed
Vitaly Kurin <vitalyk@nvidia.com>
2026-03-10 05:04:16 -0700

    Add request_id to logs.

--- megatron/core/inference/engines/dynamic_engine.py
+++ megatron/core/inference/engines/dynamic_engine.py
@@ -790,14 +790,14 @@ class DynamicInferenceEngine(AbstractEngine):
             > self.context.max_sequence_length
         ) or (request.sampling_params.num_tokens_to_generate < 0):
             logging.error(
-                f"Invalid number of tokens to generate. Prompt len: {len(request.prompt_tokens)}, tokens to generate: {request.sampling_params.num_tokens_to_generate}, max seq len: {self.context.max_sequence_length}."
+                f"{request_id=} Invalid number of tokens to generate. Prompt len: {len(request.prompt_tokens)}, tokens to generate: {request.sampling_params.num_tokens_to_generate}, max seq len: {self.context.max_sequence_length}."
             )
             request.status = Status.FAILED
             request.add_event_error_nontransient(MaxSequenceLengthOverflowError(request_id))
 
         if len(request.prompt_tokens) > self.context.max_tokens and not self.enable_chunked_prefill:
             logging.error(
-                f"Prompt is longer than context.max_tokens. Prompt tokens: {len(request.prompt_tokens)}, context.max_tokens: {self.context.max_tokens}, chunked_prefill: {self.enable_chunked_prefill}"
+                f"{request_id=} Prompt is longer than context.max_tokens. Prompt tokens: {len(request.prompt_tokens)}, context.max_tokens: {self.context.max_tokens}, chunked_prefill: {self.enable_chunked_prefill}"
             )
             request.status = Status.FAILED
             request.add_event_error_nontransient(TokenOverflowError(request_id))
