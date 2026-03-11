b8c814923b8ca06b88cf7a2aa026a90b54487ec6
Helen Ngo <helenn@nvidia.com>
2026-03-11 11:02:29 -0700

    fix

--- megatron/core/transformer/moe/moe_layer.py
+++ megatron/core/transformer/moe/moe_layer.py
@@ -414,12 +414,6 @@ class MoELayer(BaseMoELayer):
         dispatched_input, tokens_per_expert, permuted_probs = (
             self.token_dispatcher.dispatch_postprocess(hidden_states, probs)
         )
-        # If this expert has zero tokens after the dispatch A2A, it still needs to participate in
-        # the call to `combine_preprocess` and `combine`, but we can skip calling the expert module
-        # and avoid mismatched shapes on the cudagraphed forward() when there are zero tokens.
-        if dispatched_input.numel() == 0:
-            output = torch.tensor([], device=dispatched_input.device)
-            return output, None
         if (
             hasattr(self, "_inference_token_dispatcher")
             and self.is_inference_cuda_graphed_iteration
