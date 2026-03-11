66f8f83bfd100cfe1e7b3c0a82d7b516cf881255
Jennifer Chen <jennifchen@nvidia.com>
2026-03-11 09:23:46 -0700

    fix config

--- examples/post_training/modelopt/conf/nvidia/NVIDIA-Nemotron-3-Super-120B-A12B-BF16.sh
+++ examples/post_training/modelopt/conf/nvidia/NVIDIA-Nemotron-3-Super-120B-A12B-BF16.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ -z ${HF_MODEL_CKPT} ]; then
-    HF_MODEL_CKPT=nvidia/nemotron-super-sft-020426
-    TOKENIZER_MODEL=nvidia/nemotron-super-sft-020426
+    HF_MODEL_CKPT=nvidia/NVIDIA-Nemotron-3-Super-120B-A12B-BF16
+    TOKENIZER_MODEL=nvidia/NVIDIA-Nemotron-3-Super-120B-A12B-BF16
 else
     TOKENIZER_MODEL=${HF_MODEL_CKPT}
 fi
@@ -10,24 +10,25 @@ fi
 
 
 MODEL_ARGS=" \
+    --trust-remote-code \
     --save-interval 100000 \
     --micro-batch-size 1 \
-    --moe-token-dispatcher-type allgather \
     --enable-experimental \
-    --moe-permute-fusion \
     --use-fused-weighted-squared-relu \
     --cross-entropy-loss-fusion \
     --cross-entropy-fusion-impl native \
+    --num-experts 512 \
     --moe-router-score-function sigmoid \
     --moe-grouped-gemm \
-    --num-experts 512 \
-    --moe-router-topk 22 \
     --moe-aux-loss-coeff 1e-4 \
+    --moe-router-topk 22 \
+    --moe-permute-fusion \
     --moe-router-topk-scaling-factor 5.0 \
     --moe-router-enable-expert-bias \
     --moe-router-dtype fp32 \
     --moe-router-load-balancing-type seq_aux_loss \
     --moe-shared-expert-intermediate-size 5376 \
+    --moe-token-dispatcher-type allgather \
     --moe-latent-size 1024 \
     \
     --attention-backend flash \
@@ -54,18 +55,9 @@ MODEL_ARGS=" \
     --attention-dropout 0.0 \
     --hidden-dropout 0.0 \
     \
-     --tokenizer-type HuggingFaceTokenizer \
+    --tokenizer-type HuggingFaceTokenizer \
     --bf16 \
     --seq-length 8192 \
     --max-position-embeddings 8192 \
     --export-model-type MambaModel \
     "
-
-#--mtp-use-repeated-layer
-#--mtp-loss-scaling-factor 0.3 \
-
-# --mtp-hybrid-override-pattern *E \
-# --mtp-spec megatron.core.models.mamba.mamba_layer_specs mamba_stack_spec \
-
-
-# --hybrid-override-pattern MEMEMEM*EMEMEMEM*EMEMEMEM*EMEMEMEMEM*EMEMEMEMEM*EMEMEMEMEM*EMEMEMEMEM*EMEMEMEM*EMEMEMEME/*E \
