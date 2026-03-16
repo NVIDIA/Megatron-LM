ab7f8e47c8d7e6301c72a772725041e78a9488ba
Pingtian Li <pingtianl@nvidia.com>
2026-03-15 20:50:26 -0700

    fix ut

--- tests/unit_tests/models/test_mamba_moe_model.py
+++ tests/unit_tests/models/test_mamba_moe_model.py
@@ -75,6 +75,7 @@ GOLDEN_CONFIG: Dict[str, Any] = {
     "deallocate_pipeline_outputs": True,
     "defer_embedding_wgrad_compute": False,
     "delay_wgrad_compute": False,
+    "delay_wgrad_compute_for_te_grouped_gemm": False,
     "deterministic_mode": False,
     "disable_bf16_reduced_precision_matmul": False,
     "disable_parameter_transpose_cache": False,
