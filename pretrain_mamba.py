946620fcf6ee494a00f4c8949ec22d9f94a8d892
Antoni-Joan Solergibert <asolergibert@nvidia.com>
2026-03-31 19:48:28 +0200

    typing

--- pretrain_mamba.py
+++ pretrain_mamba.py
@@ -16,7 +16,7 @@ if rank != 0:
     warnings.filterwarnings("ignore", category=FutureWarning)
 
 from functools import partial
-from typing import List, Optional, Tuple
+from typing import Any, List, Optional, Tuple
 
 import torch
 
@@ -273,7 +273,7 @@ def is_dataset_built_on_rank(vp_stage=None, is_packed_sequence=False):
         return is_first_or_last_pipeline_stage(vp_stage)
 
 
-def core_gpt_dataset_config_from_args(args):
+def core_gpt_dataset_config_from_args(args: Any) -> GPTDatasetConfig:
     tokenizer = build_tokenizer(args)
 
     # Sometimes --data-path is too long, instead we parse it from a file.
@@ -353,7 +353,7 @@ if __name__ == "__main__":
     set_startup_timestamps(program_start=_PROGRAM_START_TIME, main_entry=_MAIN_ENTRY_TIME)
 
     # Temporary for transition to core datasets
-    train_valid_test_datasets_provider.is_distributed = True
+    setattr(train_valid_test_datasets_provider, "is_distributed", True)
 
     # Optionally enable inprocess restart on pretrain
     pretrain, store = inprocess_restart.maybe_wrap_for_inprocess_restart(pretrain)
