1781578dd1eb6dfd94614f1ddc026eac8784a0d3
Maanu Grover <maanug@nvidia.com>
2026-04-03 15:46:05 -0700

    build config in pretrain
    Signed-off-by: Maanu Grover <maanug@nvidia.com>

--- megatron/training/training.py
+++ megatron/training/training.py
@@ -278,6 +278,7 @@ def destroy_global_state():
     destroy_model_parallel()
     destroy_rerun_state_machine()
 
+from megatron.training.argument_utils import pretrain_container_from_args
 
 def print_datetime(string, override_timestamp=None):
     """Note that this call will sync across all ranks. Use override_timestamp if provided;
@@ -910,6 +911,7 @@ def pretrain(
 
     args = get_args()
     timers = get_timers()
+    config = pretrain_container_from_args(args)
 
     if args.fine_grained_activation_offloading:
         from megatron.core.pipeline_parallel.utils import (
