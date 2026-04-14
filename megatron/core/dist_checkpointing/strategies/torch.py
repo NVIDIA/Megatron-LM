8400f817a35551c0b8b0a34e9e48b39311635520
CarlosGomes98 <cgomes@nvidia.com>
2026-04-14 11:19:32 +0100

    Antoni patches

--- megatron/core/dist_checkpointing/strategies/torch.py
+++ megatron/core/dist_checkpointing/strategies/torch.py
@@ -951,6 +951,7 @@ class TorchDistLoadShardedStrategy(LoadShardedStrategy):
                 shapes_validation_sharded_tensors=flexible_shape_sharded_tensors,
                 allow_shape_mismatch_sharded_tensors=allow_shape_mismatch_sharded_tensors,
             ),
+            no_dist=True,
         )
 
         self.cached_global_metadata = (
