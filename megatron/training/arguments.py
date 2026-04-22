384e6183fe4daec2b142409ed098c9c453576101
Youngeun Kwon <youngeunk@nvidia.com>
2026-04-22 10:54:04 -0700

    Fix: Auto enable manual registration and enhance the docummentation (#3295)
    Signed-off-by: Youngeun Kwon <youngeunk@nvidia.com>

--- megatron/training/arguments.py
+++ megatron/training/arguments.py
@@ -1042,7 +1042,13 @@ def validate_args(args, defaults={}):
 
         assert args.ckpt_format == "fsdp_dtensor", \
             "Megatron-FSDP requires the `fsdp_dtensor` checkpointing format."
-        
+    
+    if args.nccl_ub and args.use_megatron_fsdp:
+        # In Megatron-LM, required implementation for manual registration is already provided.
+        # So we enable the manual registration by default when nccl-ub and use_megatron_fsdp is set.
+        args.fsdp_manual_registration = True
+        warn_rank_0('FSDP manual registration is enabled by default when nccl-ub is enabled')
+
     if args.fsdp_manual_registration:
         assert args.use_megatron_fsdp, "FSDP manual registration is only supported with Megatron FSDP."
         assert args.nccl_ub, "FSDP manual registration is only supported with --nccl-ub argument."      
