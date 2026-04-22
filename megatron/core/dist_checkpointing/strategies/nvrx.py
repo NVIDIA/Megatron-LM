805473a8e31e2ba6ba4a5809ba221c7d5a5b169b
Seonmyeong Bak <sbak@nvidia.com>
2026-04-22 10:55:42 -0700

    Update the year in copyright for `nvrx.py`

--- megatron/core/dist_checkpointing/strategies/nvrx.py
+++ megatron/core/dist_checkpointing/strategies/nvrx.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 
 """Helpers for interacting with the experimental nvidia-resiliency-ext API."""
 
