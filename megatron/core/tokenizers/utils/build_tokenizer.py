415d5e5536c94384f2f7933063e0b940ab8da28f
dimapihtar <dpykhtar@nvidia.com>
2026-04-24 10:49:15 -0700

    fix code style
    Signed-off-by: dimapihtar <dpykhtar@nvidia.com>

--- megatron/core/tokenizers/utils/build_tokenizer.py
+++ megatron/core/tokenizers/utils/build_tokenizer.py
@@ -20,9 +20,7 @@ def build_tokenizer(args, **kwargs):
     if args.tokenizer_type in MEGATRON_TOKENIZERS:
         tokenizer_library = 'megatron'
         tokenizer_path = args.tokenizer_type
-        kwargs['additional_special_tokens'] = (
-            args.special_tokens if args.special_tokens else []
-        )
+        kwargs['additional_special_tokens'] = args.special_tokens if args.special_tokens else []
         if tokenizer_path == 'BertWordPieceCase':
             special_tokens = {}
             special_tokens['additional_special_tokens'] = [f'<extra_id_{i}>' for i in range(100)]
@@ -51,9 +49,7 @@ def build_tokenizer(args, **kwargs):
         tokenizer_path = args.tokenizer_model
         kwargs['vocab_file'] = args.vocab_file
         kwargs['merges_file'] = args.merge_file
-        kwargs['additional_special_tokens'] = (
-            args.special_tokens if args.special_tokens else []
-        )
+        kwargs['additional_special_tokens'] = args.special_tokens if args.special_tokens else []
         kwargs['use_fast'] = not args.tokenizer_hf_no_use_fast
         kwargs['trust_remote_code'] = args.trust_remote_code
         kwargs['include_special_tokens'] = not args.tokenizer_hf_no_include_special_tokens
