6316a3c74165671cfe970252042c1edb88485e6d
dimapihtar <dpykhtar@nvidia.com>
2026-04-29 16:48:20 -0700

    add nvrx version check
    Signed-off-by: dimapihtar <dpykhtar@nvidia.com>

--- megatron/core/dist_checkpointing/strategies/nvrx.py
+++ megatron/core/dist_checkpointing/strategies/nvrx.py
@@ -5,6 +5,15 @@
 from importlib import import_module
 from typing import Any, Callable, Dict
 
+try:
+    from packaging.version import Version as PkgVersion
+
+    HAVE_PACKAGING = True
+except ImportError:
+    HAVE_PACKAGING = False
+
+NVRX_MIN_VERSION = "0.6.0.dev33+b2bb3d7"
+
 
 def has_nvrx_async_support() -> bool:
     """Checks whether the NVRx async checkpointing symbols Megatron uses are importable."""
@@ -32,6 +41,10 @@ def has_nvrx_async_support() -> bool:
         getattr(state_dict_saver, "save_state_dict_async_finalize", None),
         getattr(state_dict_saver, "save_state_dict_async_plan", None),
     )
+    assert is_nvrx_min_version (
+        f"Minimum required nvidia-resiliency-ext package version is {NVRX_MIN_VERSION}."
+    )
+
     return all(symbol is not None for symbol in required_symbols) and hasattr(
         filesystem_async, "_results_queue"
     )
@@ -53,3 +66,22 @@ def make_nvrx_async_request(
         async_fn_kwargs=async_fn_kwargs or {},
         preload_fn=preload_fn,
     )
+
+
+def is_nvrx_min_version(version: str = NVRX_MIN_VERSION) -> bool:
+    """Check if minimum version of `NVRx` is installed."""
+    if not HAVE_PACKAGING:
+        raise ImportError(
+            "packaging is not installed. Please install it with `pip install packaging`."
+        )
+
+    try:
+        import nvidia_resiliency_ext as nvrx
+
+        HAVE_NVRX = True
+    except (ImportError, ModuleNotFoundError):
+        HAVE_NVRX = False
+
+    nvrx_version = str(nvrx.__version__) if HAVE_NVRX else "0.0.0"
+
+    return PkgVersion(nvrx_version) >= PkgVersion(version)
