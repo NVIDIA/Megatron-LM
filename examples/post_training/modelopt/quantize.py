a1a96c226ee7eda89e8a28449b849971f81368fd
Asha Anoosheh <aanoosheh@nvidia.com>
2026-04-29 19:31:47 +0200

    Remove load_modelopt_checkpoint()
    Signed-off-by: Asha Anoosheh <aanoosheh@nvidia.com>

--- examples/post_training/modelopt/quantize.py
+++ examples/post_training/modelopt/quantize.py
@@ -54,12 +54,11 @@ from utils import get_hf_tokenizer
 from megatron.core.parallel_state import get_context_parallel_group
 from megatron.core.utils import get_batch_on_this_cp_rank, unwrap_model
 from megatron.post_training.arguments import add_modelopt_args
-from megatron.post_training.checkpointing import load_modelopt_checkpoint
 from megatron.post_training.model_builder import modelopt_gpt_hybrid_builder
 from megatron.post_training.utils import print_distributed_quant_summary, report_current_memory_info
 from megatron.training import get_args, get_model, initialize_megatron
 from megatron.training.arguments import parse_and_validate_args
-from megatron.training.checkpointing import save_checkpoint
+from megatron.training.checkpointing import load_checkpoint, save_checkpoint
 from megatron.training.utils import print_rank_0
 from model_provider import model_provider
 
@@ -333,7 +332,7 @@ if __name__ == "__main__":
     report_current_memory_info()
 
     if args.load is not None:
-        load_modelopt_checkpoint(model, strict=not args.untie_embeddings_and_output_weights)
+        load_checkpoint(model, None, None, strict=not args.untie_embeddings_and_output_weights)
         print_rank_0("Done loading checkpoint")
 
     if args.pretrained_model_path is not None:
--- megatron/post_training/checkpointing.py
+++ megatron/post_training/checkpointing.py
@@ -8,12 +8,9 @@ import modelopt.torch.opt as mto
 import torch.nn as nn
 from modelopt.torch.opt.plugins import restore_sharded_modelopt_state
 
-from megatron.core import dist_checkpointing
-from megatron.core.utils import get_torch_version, is_torch_min_version, unwrap_model
 from megatron.training import get_args
-from megatron.training.checkpointing import _load_base_checkpoint, load_checkpoint
+from megatron.training.checkpointing import _load_base_checkpoint
 from megatron.training.utils import print_rank_0
-from .utils import print_distributed_quant_summary
 
 logger = logging.getLogger(__name__)
 
@@ -125,73 +122,3 @@ def load_modelopt_state(model: nn.Module, load_dir: Optional[str] = None) -> Non
             return
         restore_sharded_modelopt_state([model], sharded_load_dir)
 
-
-def load_modelopt_checkpoint(
-    model,
-    optimizer=None,
-    opt_param_scheduler=None,
-    strict: bool = True,
-    additional_sharded_prefix: str = "",
-    load_arg: str = "load",
-) -> None:
-    """Load a sharded (untar .nemo or megatron --use-dist-ckpt) or unsharded checkpoint.
-
-    Essentially, the function is detecting whether the checkpoint is a .nemo sharded checkpoint.
-    If so, we load the sharded state_dict with additional_sharded_prefix `model.`.
-    This additional prefix is tha artifact of the lightning module wrapper. Once the sharded
-    state_dict is loaded, we use a state_dict pre_hook to pop this additional prefix (`model.`)
-    from all state_dict keys.
-
-    If this is not a .nemo sharded checkpoint, then this function will simply call
-    load_checkpoint. See megatron.checkpointing.load_checkpoint for explanation.
-
-    Args:
-        additional_sharded_prefix: append additional prefix to align the sharded checkpoint keys.
-            When loading an .nemo sharded checkpoint, this is usually `model.`. Otherwise, this is
-            typically an empty string.
-    """
-
-    def _remove_prefix_state_dict_pre_hook(
-        state_dict, prefix, local_metadata, strict, missing_keys, unexpected_keys, error_msgs
-    ):
-        """Pytorch state_dict pre_hook to remove prefix of the state_dict keys."""
-        if additional_sharded_prefix is None:
-            return
-        key_rewrite_list = []
-        for key, _ in state_dict.items():
-            if key.startswith(additional_sharded_prefix):
-                key_rewrite_list.append(key)
-        for old_key in key_rewrite_list:
-            new_key = old_key[len(additional_sharded_prefix) :]
-            state_dict[new_key] = state_dict.pop(old_key)
-
-    args = get_args()
-    load_dir = getattr(args, load_arg)
-    sharded_load_dir, additional_sharded_prefix = get_sharded_load_dir(load_dir)
-
-    unwrapped_model = unwrap_model(model)
-
-    if args.ckpt_format == "torch":
-        state_dict, checkpoint_name, release, ckpt_type = _load_base_checkpoint(
-            load_dir, args, rank0=False
-        )
-        model_state_dict = state_dict["model"]
-        unwrapped_model[0].load_state_dict(model_state_dict, strict=False)
-        print_distributed_quant_summary(unwrapped_model[0])
-    elif sharded_load_dir is not None and optimizer is None and opt_param_scheduler is None:
-        sharded_state_dict_metadata = dist_checkpointing.load_content_metadata(sharded_load_dir)
-        sharded_state_dict = unwrapped_model[0].sharded_state_dict(
-            prefix=additional_sharded_prefix, metadata=sharded_state_dict_metadata
-        )
-
-        if additional_sharded_prefix:
-            unwrapped_model[0]._register_load_state_dict_pre_hook(
-                _remove_prefix_state_dict_pre_hook
-            )
-        model_state_dict = dist_checkpointing.load(
-            sharded_state_dict, sharded_load_dir, strict=args.dist_ckpt_strictness
-        )
-        unwrapped_model[0].load_state_dict(model_state_dict, strict=False)
-        print_distributed_quant_summary(unwrapped_model[0])
-    else:
-        _ = load_checkpoint(model, optimizer, opt_param_scheduler, strict=strict, load_arg=load_arg)
