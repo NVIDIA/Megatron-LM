abeb5295b4d2eeb260d60f878404071df469671b
dimapihtar <dpykhtar@nvidia.com>
2026-04-29 16:50:23 -0700

    fix typo
    Signed-off-by: dimapihtar <dpykhtar@nvidia.com>

--- megatron/core/dist_checkpointing/strategies/nvrx.py
+++ megatron/core/dist_checkpointing/strategies/nvrx.py
@@ -41,7 +41,7 @@ def has_nvrx_async_support() -> bool:
         getattr(state_dict_saver, "save_state_dict_async_finalize", None),
         getattr(state_dict_saver, "save_state_dict_async_plan", None),
     )
-    assert is_nvrx_min_version (
+    assert is_nvrx_min_version() (
         f"Minimum required nvidia-resiliency-ext package version is {NVRX_MIN_VERSION}."
     )
 
