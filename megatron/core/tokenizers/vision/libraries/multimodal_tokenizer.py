ee5ea00aa8fe987056f2ca17d39bd60cfb377538
dimapihtar <dpykhtar@nvidia.com>
2026-05-04 12:16:26 -0700

    fix tokenizers
    Signed-off-by: dimapihtar <dpykhtar@nvidia.com>

--- megatron/core/tokenizers/vision/libraries/multimodal_tokenizer.py
+++ megatron/core/tokenizers/vision/libraries/multimodal_tokenizer.py
@@ -256,9 +256,8 @@ class MegatronMultimodalTokenizer:
             tokenize=True,
             add_generation_prompt=add_generation_prompt,
             return_assistant_token_mask=False,
-            return_tensors="np",
             chat_template=self._prompt_config.custom_chat_template,
-        )[0]
+        )
 
         if not return_target:
             return tokens
