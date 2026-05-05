0da0924a640e3ae68d273c18cb05873d35241ea6
dimapihtar <dpykhtar@nvidia.com>
2026-05-05 06:47:50 -0700

    set return_dict=False
    Signed-off-by: dimapihtar <dpykhtar@nvidia.com>

--- megatron/core/tokenizers/vision/libraries/multimodal_tokenizer.py
+++ megatron/core/tokenizers/vision/libraries/multimodal_tokenizer.py
@@ -251,15 +251,14 @@ class MegatronMultimodalTokenizer:
         # Apply possible image tag.
         conversation = self._apply_image_tag(conversation)
 
-        tokens = np.array(
-            self.tokenizer.apply_chat_template(
-                conversation,
-                tokenize=True,
-                add_generation_prompt=add_generation_prompt,
-                return_assistant_token_mask=False,
-                chat_template=self._prompt_config.custom_chat_template,
-            )
-        )
+        tokens = self.tokenizer.apply_chat_template(
+            conversation,
+            tokenize=True,
+            add_generation_prompt=add_generation_prompt,
+            return_assistant_token_mask=False,
+            return_dict=False,
+            chat_template=self._prompt_config.custom_chat_template,
+        )[0]
 
         if not return_target:
             return tokens
