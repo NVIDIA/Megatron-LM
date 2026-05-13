5a5196a602b4b722229398864fd00d37e72c8fb1
Jingyue Wu <wujingyue@gmail.com>
2026-05-13 00:45:30 -0700

    [38;5;129;01mTreat[39;00m[38;5;250m [39mMambaLayer[38;5;250m [39m[38;5;129;01mas[39;00m[38;5;250m [39man[38;5;250m [39mFSDP[38;5;250m [39munit[38;5;250m [39m[38;5;129;01mmodule[39;00m
    Co[38;5;241m-[39mAuthored[38;5;241m-[39m[38;5;129;01mBy[39;00m:[38;5;250m [39mClaude[38;5;250m [39mOpus[38;5;250m [39m[38;5;241m4.7[39m[38;5;250m [39m([38;5;241m1[39mM[38;5;250m [39mcontext)[38;5;250m [39m[38;5;241m<[39mnoreply[38;5;136m@anthropic[39m.com[38;5;241m>[39m

--- megatron/core/distributed/fsdp/mcore_fsdp_adapter.py
+++ megatron/core/distributed/fsdp/mcore_fsdp_adapter.py
@@ -40,6 +40,7 @@ from megatron.core.config_logger import has_config_logger_enabled, log_config_to
 from megatron.core.distributed.data_parallel_base import _BaseDataParallel
 from megatron.core.distributed.distributed_data_parallel_config import DistributedDataParallelConfig
 from megatron.core.process_groups_config import ProcessGroupCollection
+from megatron.core.ssm.mamba_layer import MambaLayer
 from megatron.core.transformer.transformer_config import TransformerConfig
 from megatron.core.transformer.transformer_layer import TransformerLayer
 from megatron.core.utils import is_te_min_version, log_single_rank
@@ -151,7 +152,7 @@ class FullyShardedDataParallel(_BaseDataParallel):
             self.fsdp_unit_modules = fsdp_unit_modules
         else:
             if self.ddp_config.data_parallel_sharding_strategy == "optim_grads_params":
-                self.fsdp_unit_modules = [TransformerLayer]
+                self.fsdp_unit_modules = [TransformerLayer, MambaLayer]
             else:
                 self.fsdp_unit_modules = []
 
