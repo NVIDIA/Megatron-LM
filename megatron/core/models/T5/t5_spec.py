0a094cd2d9ebb27826b67c46f9f5cf8d5456e4ce
Nick Schank <nick@reflection.ai>
2026-05-14 14:19:17 -0400

    Use Protocols to type-check linear_proj submodules of Attention (#3434)
    Co-authored-by: gautham-kollu <gkollu@nvidia.com>
    Co-authored-by: Yashaswi Karnati <144376261+yashaswikarnati@users.noreply.github.com>
    Co-authored-by: Philip Petrakian <ppetrakian@nvidia.com>
    Signed-off-by: Dmytro Pykhtar <dpykhtar@nvidia.com>

--- megatron/core/models/T5/t5_spec.py
+++ megatron/core/models/T5/t5_spec.py
@@ -65,7 +65,7 @@ def encoder_model_with_transformer_engine_default_spec() -> ModuleSpec:
                 submodules=SelfAttentionSubmodules(
                     linear_qkv=not_none(TELayerNormColumnParallelLinear),
                     core_attention=not_none(TEDotProductAttention),
-                    linear_proj=TERowParallelLinear,
+                    linear_proj=not_none(TERowParallelLinear),
                     q_layernorm=IdentityOp,
                     k_layernorm=IdentityOp,
                 ),
@@ -95,7 +95,7 @@ def decoder_model_with_transformer_engine_default_spec() -> ModuleSpec:
                 submodules=SelfAttentionSubmodules(
                     linear_qkv=not_none(TELayerNormColumnParallelLinear),
                     core_attention=not_none(TEDotProductAttention),
-                    linear_proj=TERowParallelLinear,
+                    linear_proj=not_none(TERowParallelLinear),
                     q_layernorm=IdentityOp,
                     k_layernorm=IdentityOp,
                 ),
@@ -109,7 +109,7 @@ def decoder_model_with_transformer_engine_default_spec() -> ModuleSpec:
                     linear_q=not_none(TEColumnParallelLinear),
                     linear_kv=not_none(TEColumnParallelLinear),
                     core_attention=not_none(TEDotProductAttention),
-                    linear_proj=TERowParallelLinear,
+                    linear_proj=not_none(TERowParallelLinear),
                 ),
             ),
             cross_attn_bda=get_bias_dropout_add,
