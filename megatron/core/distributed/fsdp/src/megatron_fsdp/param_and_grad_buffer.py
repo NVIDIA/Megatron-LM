d0334c5c9c7a5e511aa2d03643488275b01b7c67
Cory Ye <cye@nvidia.com>
2026-05-18 12:13:26 -0700

    Also persist asymmetrical units for the MXFP8 transpose weight buffer as well!
    Signed-off-by: Cory Ye <cye@nvidia.com>

--- megatron/core/distributed/fsdp/src/megatron_fsdp/param_and_grad_buffer.py
+++ megatron/core/distributed/fsdp/src/megatron_fsdp/param_and_grad_buffer.py
@@ -2134,6 +2134,7 @@ class ParamAndGradBuffer:
                 name="fsdp_fp8_transpose_params",
                 fsdp_param_groups=self.parameter_groups,
                 size=UB_BUFFER_NUM,
+                fallback_to_persistent_buffer=self.ddp_config.fsdp_db_use_persist_buf_on_alloc_fail,
             )
             self.main_grad_alloc = FixedPoolAllocator(
                 name="fsdp_grads",
