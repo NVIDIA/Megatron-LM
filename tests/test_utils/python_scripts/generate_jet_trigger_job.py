eb50654c261d3aecaf31c3b9bba0d32a57e7bbea
Ajay Balasa <abalasa@nvidia.com>
2026-05-20 15:03:02 -0700

    Add allow_failure flag to gpt and moe recipes that are failing in nightlies

--- tests/test_utils/python_scripts/generate_jet_trigger_job.py
+++ tests/test_utils/python_scripts/generate_jet_trigger_job.py
@@ -225,7 +225,8 @@ def main(
                 "needs": needs,
                 "script": [" ".join(script)],
                 "artifacts": {"paths": ["results/"], "when": "always"},
-                "allow_failure": test_case["spec"]["model"] == "gpt-nemo",
+                "allow_failure": bool(test_case["spec"].get("allow_failure", False))
+                or test_case["spec"]["model"] == "gpt-nemo",
                 "retry": {
                     "max": 2,
                     "when": [
