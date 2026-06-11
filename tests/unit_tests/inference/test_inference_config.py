bfa66d9fc2d35c691e6bbd884d7693c7b6933327
Lawrence McAfee <lmcafee@nvidia.com>
2026-06-11 13:20:55 -0700

    Add async scheduling config and CLI flag
    Signed-off-by: Lawrence McAfee <lmcafee@nvidia.com>

--- tests/unit_tests/inference/test_inference_config.py
+++ tests/unit_tests/inference/test_inference_config.py
@@ -1,9 +1,11 @@
 # Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 
+import argparse
 import dataclasses
 
 from megatron.core.inference.config import InferenceConfig
 from megatron.core.transformer.transformer_config import TransformerConfig
+from megatron.training.arguments import _add_inference_args
 
 
 class TestInferenceConfig:
@@ -15,3 +17,19 @@ class TestInferenceConfig:
         dynamic_inference_config_fields = set(dataclasses.fields(InferenceConfig))
         transformer_config_fields = set(dataclasses.fields(TransformerConfig))
         assert len(dynamic_inference_config_fields.intersection(transformer_config_fields)) == 0
+
+    def test_async_scheduling_defaults_to_disabled(self):
+        """Async scheduling is opt-in."""
+        config = InferenceConfig()
+
+        assert not config.enable_async_scheduling
+
+    def test_async_scheduling_cli_flag(self):
+        """The dynamic batching async scheduling CLI flag is parsed as an opt-in bool."""
+        parser = _add_inference_args(argparse.ArgumentParser())
+
+        default_args = parser.parse_args([])
+        enabled_args = parser.parse_args(["--inference-dynamic-batching-async-scheduling"])
+
+        assert not default_args.inference_dynamic_batching_async_scheduling
+        assert enabled_args.inference_dynamic_batching_async_scheduling
