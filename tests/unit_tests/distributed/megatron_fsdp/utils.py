ghadiaravi13/NT3-Ultra-Megatron-LM

fix model setup in unit tests
Signed-off-by: Maanu Grover <maanug@nvidia.com>

tests/unit_tests/distributed/megatron_fsdp/utils.py
@@ -8,6 +8,7 @@ from torch.utils.data import DataLoader, Dataset
 from torch.utils.data.distributed import DistributedSampler
 
 from gpt_builders import gpt_builder
+from megatron.core.process_groups_config import ProcessGroupCollection
 from megatron.core.distributed import finalize_model_grads
 from megatron.core.enums import ModelType
 from megatron.core.num_microbatches_calculator import destroy_num_microbatches_calculator
@@ -19,6 +20,8 @@ from megatron.training.global_vars import destroy_global_vars, set_global_variab
 from megatron.training.training import setup_model_and_optimizer
 from megatron.training.utils import is_first_or_last_pipeline_stage
 from model_provider import model_provider
+
+from tests.unit_tests.test_utilities import Utils
 
 
 def pretrain_forward_backward(
@@ -90,9 +93,13 @@ def make_moe_args_model_and_optimizer(ut_filename, **overrides):
     destroy_num_microbatches_calculator()
     set_global_variables(args, build_tokenizer=False)
 
+    cfg_container = Utils.pretrain_config_from_global_args(args, "gpt")
+    pg_collection = ProcessGroupCollection.use_mpu_process_groups()
     model, optimizer, _ = setup_model_and_optimizer(
         model_provider_func=partial(model_provider, gpt_builder),
         model_type=ModelType.encoder_or_decoder,
+        cfg_container=cfg_container,
+        pg_collection=pg_collection,
     )
     return model, optimizer
 

NVIDIA/Megatron-LM

tests/unit_tests/distributed/megatron_fsdp/utils.py
@@ -8,6 +8,7 @@ from torch.utils.data import DataLoader, Dataset
 from torch.utils.data.distributed import DistributedSampler
 
 from hybrid_builders import hybrid_builder
+from megatron.core.process_groups_config import ProcessGroupCollection
 from megatron.core.distributed import finalize_model_grads
 from megatron.core.enums import ModelType
 from megatron.core.num_microbatches_calculator import destroy_num_microbatches_calculator
@@ -19,6 +20,8 @@ from megatron.training.global_vars import destroy_global_vars, set_global_variab
 from megatron.training.training import setup_model_and_optimizer
 from megatron.training.utils import is_first_or_last_pipeline_stage
 from model_provider import model_provider
+
+from tests.unit_tests.test_utilities import Utils
 
 
 def pretrain_forward_backward(
@@ -93,9 +96,13 @@ def make_moe_args_model_and_optimizer(ut_filename, **overrides):
     destroy_num_microbatches_calculator()
     set_global_variables(args, build_tokenizer=False)
 
+    cfg_container = Utils.pretrain_config_from_global_args(args, "hybrid")
+    pg_collection = ProcessGroupCollection.use_mpu_process_groups()
     model, optimizer, _ = setup_model_and_optimizer(
         model_provider_func=partial(model_provider, hybrid_builder),
         model_type=ModelType.encoder_or_decoder,
+        cfg_container=cfg_container,
+        pg_collection=pg_collection,
     )
     return model, optimizer
 