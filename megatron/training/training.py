1ecd38e312b7e39f355ff87815472e7fae6fabeb
Evgenii Zheltonozhskii <zheltonozhskiy@gmail.com>
2026-07-02 02:09:11 +0300

    Fix more imports
    Signed-off-by: Evgenii Zheltonozhskii <zheltonozhskiy@gmail.com>

--- megatron/training/training.py
+++ megatron/training/training.py
@@ -60,7 +60,7 @@ from megatron.core.inference.unified_memory import create_unified_mempool
 from megatron.core.models.gpt.experimental_attention_variant_module_specs import (
     is_linear_attention_variant,
 )
-from megatron.core.msc_utils import MultiStorageClientFeature, open_file
+from megatron.core.msc_utils import maybe_msc
 from megatron.core.num_microbatches_calculator import (
     destroy_num_microbatches_calculator,
     get_current_global_batch_size,
