9bff48fbdd013b388de9072128aacd73d74b8971
qiyuw <qiyuw@nvidia.com>
2026-07-16 23:54:04 +0000

    clean verision
    Signed-off-by: qiyuw <qiyuw@nvidia.com>

--- megatron/core/distributed/param_and_grad_buffer.py
+++ megatron/core/distributed/param_and_grad_buffer.py
@@ -279,10 +279,10 @@ class _ParamAndGradBucketGroup:
     def _post_param_sync(self):
         """Run post-processing after param all-gather completes."""
         if self.ddp_config.reuse_grad_buf_for_mxfp8_param_ag:
-            def _prepare_mxfp8_param_for_gemm(param_data):
-                if not is_mxfp8tensor(param_data):
+            def _prepare_mxfp8_param_for_gemm(param):
+                if not is_mxfp8tensor(param):
                     return
-                quantizer = param_data._get_quantizer()
+                quantizer = param._get_quantizer()
                 quantizer.set_usage(rowwise=True, columnwise=True)
                 quantizer.optimize_for_gemm = True
 
@@ -296,9 +296,9 @@ class _ParamAndGradBucketGroup:
                         break
                     param_start, param_end = bucket.param_to_index[param]
                     param_slice = bucket.param_data.view(-1)[param_start:param_end]
-                    param_data = param.data
-                    _prepare_mxfp8_param_for_gemm(param_data)
-                    param_data.copy_(param_slice.view(param_data.shape))
+                    _prepare_mxfp8_param_for_gemm(param)
+                    with torch.no_grad():
+                        param.copy_(param_slice.view(param.shape))
                 if is_bf16_weight_bucket:
                     continue
                 # All-gathered params are not needed after being copied to param.data.
