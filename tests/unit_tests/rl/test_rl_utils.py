52646692b5fdaa59e5112a70d5545bc6fb1fa43d
Teodor-Dumitru Ene <teodord.ene@gmail.com>
2026-07-25 08:36:11 -0500

    Pass CI
    Signed-off-by: Teodor-Dumitru Ene <teodord.ene@gmail.com>

--- tests/unit_tests/rl/test_rl_utils.py
+++ tests/unit_tests/rl/test_rl_utils.py
@@ -572,13 +572,13 @@ class TestRLUtils:
         # so the combined generation mask / logprobs are well-formed.
         mt1 = make_token_rollout(
             [[1, 2, 3], [1, 2, 3, 4]],
-            [[0.1, 0.2], [0.3]],
+            [[-0.1, -0.2], [-0.3]],
             [[False, True, True], [False, False, False, True]],
             problem_id="1",
         )
         mt2 = make_token_rollout(
             [[1, 2], [1, 2, 3], [1, 2, 3, 4]],
-            [[0.1], [0.2], [0.3]],
+            [[-0.1], [-0.2], [-0.3]],
             [[False, True], [False, False, True], [False, False, False, True]],
             reward=0.0,
             problem_id="3",
@@ -636,7 +636,7 @@ class TestRLUtils:
         def single(problem_id, reward):
             return make_token_rollout(
                 [[1, 2, 3, tokenizer.eod]],
-                [[0.1, 0.2, 0.3]],
+                [[-0.1, -0.2, -0.3]],
                 [[False, True, True, True]],
                 reward=reward,
                 problem_id=problem_id,
@@ -658,7 +658,7 @@ class TestRLUtils:
             )
             non_prefix = make_token_rollout(
                 [[1, 2, 3, tokenizer.eod], [7, 8, 9, tokenizer.eod]],
-                [[0.1, 0.2, 0.3], [0.4, 0.5, 0.6]],
+                [[-0.1, -0.2, -0.3], [-0.4, -0.5, -0.6]],
                 [[False, True, True, True], [False, True, True, True]],
                 reward=0.0,
                 problem_id="x",
