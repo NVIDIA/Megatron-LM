32587dfdb2d4ccc1c55d85cd3cbf7a0bab8da30e
Jingyue Wu <jingyuew@nvidia.com>
2026-08-01 10:44:36 -0700

    Add meta-parameter support (#5369)
    Signed-off-by: Jingyue Wu <wujingyue@gmail.com>
    Signed-off-by: Jingyue Wu <jingyuew@nvidia.com>
    Signed-off-by: Dmytro Pykhtar <dpykhtar@nvidia.com>

--- megatron/core/distributed/fsdp/src/megatron_fsdp/experimental/parameter_group.py
+++ megatron/core/distributed/fsdp/src/megatron_fsdp/experimental/parameter_group.py
@@ -157,8 +157,19 @@ class FsdpParameterGroup:
         unsharded_parameters: list[nn.Parameter] = []
         main_grad_dtype = self.main_grad.dtype if self.main_grad is not None else None
         for index, parameter in enumerate(parameters.values()):
-            parameter.data = self._unsharded_model_weight.get_local_tensor(index)
-            parameter.grad = None
+            unsharded_tensor = self._unsharded_model_weight.get_local_tensor(index)
+            if parameter.is_meta:
+                # A meta Parameter cannot set .data to a real tensor because their
+                # TensorImpl types are incompatible, so swap in a materialized Parameter.
+                # This may be problematic if attributes from the original Parameter need
+                # to be copied to the unsharded Parameter.
+                materialized_parameter = nn.Parameter(
+                    unsharded_tensor, requires_grad=parameter.requires_grad
+                )
+                torch.utils.swap_tensors(parameter, materialized_parameter)
+            else:
+                parameter.data = unsharded_tensor
+                parameter.grad = None
             setattr(parameter, _CONTAINING_PARAMETER_GROUP_ATTR, self)
             unsharded_parameters.append(parameter)
 
