c41112eff56055627324b88b0e10cdd8314a82e1
Maanu Grover <maanug@nvidia.com>
2026-08-07 12:46:24 -0700

    configurable rank selection on interval memory profiling
    Signed-off-by: Maanu Grover <maanug@nvidia.com>

--- megatron/training/training.py
+++ megatron/training/training.py
@@ -41,6 +41,7 @@ logging.basicConfig(handlers=[CustomHandler()], level=logging.INFO)
 _LEGACY_TRAIN_START_TIME = time.time()  # NOTE(asolergi-nv): Legacy timestamp
 
 # First-party.
+from megatron.core._rank_utils import safe_get_rank
 from megatron.core import mpu, nccl_allocator, tensor_parallel
 from megatron.core.datasets.data_schedule import HybridCPDataLoaderWrapper
 from megatron.core.distributed import DistributedDataParallel as DDP
@@ -2850,7 +2851,8 @@ def training_log(
 
     # Dump memory snapshot and print metrics to stdout.
     if iteration % args.log_interval == 0 or is_first_iteration:
-        if args.record_memory_history and (is_last_rank() or torch.distributed.get_backend() == 'fake'):
+        should_prof_rank = (args.profile_ranks == [] or safe_get_rank() in args.profile_ranks)  # [] is all ranks
+        if args.record_memory_history and (should_prof_rank or torch.distributed.get_backend() == 'fake'):
             snapshot = torch.cuda.memory._snapshot()
             from pickle import dump
 
