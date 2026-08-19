b9d0e7f4b4c3ac7339c620f41d33871a23e253af
Dmytro Pykhtar <dpykhtar@nvidia.com>
2026-08-19 07:09:07 -0700

    fix code style
    Signed-off-by: Dmytro Pykhtar <dpykhtar@nvidia.com>

--- megatron/core/dist_checkpointing/utils.py
+++ megatron/core/dist_checkpointing/utils.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2022-2023, NVIDIA CORPORATION.  All rights reserved.
 
 """Helpers for manipulating sharded tensors and sharded state dicts."""
+
 import logging
 from contextlib import contextmanager
 from time import time
