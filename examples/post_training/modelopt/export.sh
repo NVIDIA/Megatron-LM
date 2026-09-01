e1c7d420ff6618614c1858c1bc6f1b6368da685c
Jennifer Chen <jennifchen@nvidia.com>
2026-09-01 09:19:04 -0700

    Keep ModelOpt export flags in MLM_EXTRA_ARGS
    Signed-off-by: Jennifer Chen <jennifchen@nvidia.com>

--- examples/post_training/modelopt/export.sh
+++ examples/post_training/modelopt/export.sh
@@ -26,8 +26,6 @@ if [ "${TP}" != "1" ]; then
     printf "${MLM_WARNING} Variable ${PURPLE}TP${WHITE} is forced to be 1 during export!!\n"
 fi
 
-shift
-
 ${LAUNCH_SCRIPT} ${SCRIPT_DIR}/export.py \
     ${MODEL_ARGS} \
     --tensor-model-parallel-size ${TP} \
@@ -36,4 +34,4 @@ ${LAUNCH_SCRIPT} ${SCRIPT_DIR}/export.py \
     --load ${MLM_MODEL_CKPT} \
     --pretrained-model-name ${HF_MODEL_CKPT} \
     --export-dir ${EXPORT_DIR} \
-    ${MLM_DEFAULT_ARGS} ${MLM_EXTRA_ARGS} "$@"
+    ${MLM_DEFAULT_ARGS} ${MLM_EXTRA_ARGS}
