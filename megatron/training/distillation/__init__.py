1240fcf46a316be386002cb2ad2f9584aac55afc
Asha Anoosheh <aanoosheh@nvidia.com>
2026-09-01 17:17:04 +0200

    Stop main process if logits saver in async worker crashes
    Signed-off-by: Asha Anoosheh <aanoosheh@nvidia.com>

--- megatron/training/distillation/__init__.py
+++ megatron/training/distillation/__init__.py
@@ -1,11 +1,16 @@
 # Copyright (c) 2026, NVIDIA CORPORATION. All rights reserved.
 
 from megatron.training.distillation.cached_logits_loss import LossFuncCallable, StudentLogitsCapture
-from megatron.training.distillation.logits_saver import LogitsSaverHooks, get_logits_saver
+from megatron.training.distillation.logits_saver import (
+    LogitsSaverHooks,
+    check_logits_saver_failure,
+    get_logits_saver,
+)
 
 __all__ = [
     "LossFuncCallable",
     "LogitsSaverHooks",
     "StudentLogitsCapture",
+    "check_logits_saver_failure",
     "get_logits_saver",
 ]
