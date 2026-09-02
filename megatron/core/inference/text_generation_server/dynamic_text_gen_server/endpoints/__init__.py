025e509e30784f5309d917f42a87e755f49e67a1
Keshav Santhanam <ksanthanam@nvidia.com>
2026-09-02 15:43:43 -0700

    Linting
    Signed-off-by: Keshav Santhanam <ksanthanam@nvidia.com>

--- megatron/core/inference/text_generation_server/dynamic_text_gen_server/endpoints/__init__.py
+++ megatron/core/inference/text_generation_server/dynamic_text_gen_server/endpoints/__init__.py
@@ -5,8 +5,8 @@ try:
     from .chat_completions import bp as ChatCompletions
     from .completions import bp as Completions
     from .health import bp as Health
-    from .raw_completions import bp as RawCompletions
     from .profile import bp as Profile
+    from .raw_completions import bp as RawCompletions
 
     __all__ = [Completions, ChatCompletions, Health, Profile, RawCompletions]
 except ImportError:
