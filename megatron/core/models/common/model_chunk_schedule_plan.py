33c712044427e33318f1efe0043c08c9e6a17b09
Jack Chang <jianbinc@nvidia.com>
2026-09-07 13:26:35 +0800

    remove duplicate setup_combined_1f1b_hooks register at model_chunk_schedule_plan.py
    Signed-off-by: Jack Chang <jianbinc@nvidia.com>

--- megatron/core/models/common/model_chunk_schedule_plan.py
+++ megatron/core/models/common/model_chunk_schedule_plan.py
@@ -6,10 +6,7 @@ import torch
 from torch import Tensor
 
 from megatron.core.distributed.fsdp.src.megatron_fsdp.experimental.module import FsdpModule
-from megatron.core.models.common.fine_grained_mfsdp_scheduler import (
-    reshard_fsdp_module,
-    setup_combined_1f1b_hooks,
-)
+from megatron.core.models.common.fine_grained_mfsdp_scheduler import reshard_fsdp_module
 from megatron.core.pipeline_parallel.utils import (
     AbstractSchedulePlan,
     NoopScheduleNode,
@@ -431,7 +428,6 @@ class TransformerModelChunkSchedulePlan(AbstractSchedulePlan):
         # setup FSDP hooks
         has_fsdp_module = any(isinstance(submodule, FsdpModule) for submodule in model.modules())
         if has_fsdp_module:
-            setup_combined_1f1b_hooks(model)
             for layer_plan in self._transformer_layers:
                 # Forward resharding follows the schedule. Backward resharding and
                 # reduction are triggered by each FsdpModule's gradient countdown.
