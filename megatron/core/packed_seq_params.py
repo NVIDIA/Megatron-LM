57ffd6d4d071cba9e3794a4fc85c26c752254faf
Hollow Man <hollowman@opensuse.org>
2026-09-17 01:47:52 +0300

    KDA
    Signed-off-by: Hollow Man <hollowman@opensuse.org>

--- megatron/core/packed_seq_params.py
+++ megatron/core/packed_seq_params.py
@@ -31,6 +31,7 @@ class PackedSeqParams:
     tokens_per_sample: int = None
     pad_between_seqs: bool = None
     cp_scatter_cache: object = None
+    cp_partition_mode: Literal["zigzag", "contiguous"] = "zigzag"
 
     def __post_init__(self):
         """Pre-compute seq_idx for Mamba mixer CUDA graph compatibility.
