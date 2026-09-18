e22d5b955dcb6f43c138625437e4af0865d28983
Tyler Poon <tpoon@nvidia.com>
2026-09-18 23:26:52 +0000

    Add multimodal tokenizer conversation formats (#7248)
    Signed-off-by: Tyler Poon <tpoon@nvidia.com>

--- megatron/core/tokenizers/utils/build_tokenizer.py
+++ megatron/core/tokenizers/utils/build_tokenizer.py
@@ -89,6 +89,7 @@ def build_tokenizer(args, **kwargs):
         )
         kwargs['image_tag_type'] = args.image_tag_type
         kwargs['force_system_message'] = args.force_system_message
+        kwargs['keep_history_thinking'] = getattr(args, 'tokenizer_keep_history_thinking', False)
         kwargs['use_gigatoken'] = args.use_gigatoken
     elif args.tokenizer_type == 'SFTTokenizer':
         tokenizer_library = 'sft'
