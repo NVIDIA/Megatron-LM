5ad481988df56dda5a71a9198b2280d714e96d85
Hollow Man <hollowman@opensuse.org>
2026-09-21 10:46:35 +0300

    Remove cp_partition_mode
    Signed-off-by: Hollow Man <hollowman@opensuse.org>

--- megatron/core/packed_seq_params.py
+++ megatron/core/packed_seq_params.py
@@ -1,6 +1,5 @@
 # Copyright (c) 2024, NVIDIA CORPORATION. All rights reserved.
 from dataclasses import dataclass
-from typing import Literal
 
 import torch
 import torch.distributed as dist
@@ -33,7 +32,6 @@ class PackedSeqParams:
     tokens_per_sample: int = None
     pad_between_seqs: bool = None
     cp_scatter_cache: object = None
-    cp_partition_mode: Literal["zigzag", "contiguous"] = "zigzag"
 
     def __post_init__(self):
         """Pre-compute seq_idx for Mamba mixer CUDA graph compatibility.
